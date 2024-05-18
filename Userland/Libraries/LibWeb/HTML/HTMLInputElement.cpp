150de2890e9e6266c7ef72826f6019ea8ed9f6d1
Tim Ledbetter <timledbetter@gmail.com>
2024-05-18 05:45:13 +0100

    LibWeb: Fire input event on user interaction with input element buttons
    An input event is now fired when the step up or step down button of an
    input element of type number is clicked.
    
    This ensures that any associated <output> element is updated when these
    buttons are clicked.

--- Userland/Libraries/LibWeb/HTML/HTMLInputElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLInputElement.cpp
@@ -840,6 +840,7 @@ void HTMLInputElement::create_text_input_shadow_tree()
         auto up_callback_function = JS::NativeFunction::create(
             realm(), [this](JS::VM&) {
                 MUST(step_up());
+                user_interaction_did_change_input_value();
                 return JS::js_undefined();
             },
             0, "", &realm());
@@ -858,6 +859,7 @@ void HTMLInputElement::create_text_input_shadow_tree()
         auto down_callback_function = JS::NativeFunction::create(
             realm(), [this](JS::VM&) {
                 MUST(step_down());
+                user_interaction_did_change_input_value();
                 return JS::js_undefined();
             },
             0, "", &realm());
