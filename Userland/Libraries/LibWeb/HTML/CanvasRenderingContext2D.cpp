f42c18bc4c4a5d49fccbfab996bf04d77476b366
Andreas Kling <andreas@ladybird.org>
2024-06-05 10:43:00 +0200

    LibGfx: Make Painter::target() return a Bitmap&
    Painter always has a target bitmap, so let's return a reference.

--- Userland/Libraries/LibWeb/HTML/CanvasRenderingContext2D.cpp
+++ Userland/Libraries/LibWeb/HTML/CanvasRenderingContext2D.cpp
@@ -387,7 +387,7 @@ void CanvasRenderingContext2D::reset_to_default_state()
 
     // 1. Clear canvas's bitmap to transparent black.
     if (painter)
-        painter->clear_rect(painter->target()->rect(), Color::Transparent);
+        painter->clear_rect(painter->target().rect(), Color::Transparent);
 
     // 2. Empty the list of subpaths in context's current default path.
     path().clear();
@@ -399,7 +399,7 @@ void CanvasRenderingContext2D::reset_to_default_state()
     reset_drawing_state();
 
     if (painter)
-        did_draw(painter->target()->rect().to_type<float>());
+        did_draw(painter->target().rect().to_type<float>());
 }
 
 // https://html.spec.whatwg.org/multipage/canvas.html#dom-context-2d-measuretext
