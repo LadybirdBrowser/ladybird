9bfed9a9ae4cc39243573c5f39a85630a72c5297
Andreas Kling <andreas@ladybird.org>
2024-06-18 21:47:37 +0200

    Ladybird/Qt: Use "Ladybird" as the QSettings organization

--- Ladybird/Qt/Settings.cpp
+++ Ladybird/Qt/Settings.cpp
@@ -14,7 +14,7 @@ namespace Ladybird {
 Settings::Settings()
     : m_search_engine(WebView::default_search_engine())
 {
-    m_qsettings = make<QSettings>("SerenityOS", "Ladybird", this);
+    m_qsettings = make<QSettings>("Ladybird", "Ladybird", this);
 
     auto default_search_engine = WebView::default_search_engine();
     auto default_search_engine_name = qstring_from_ak_string(default_search_engine.name);
