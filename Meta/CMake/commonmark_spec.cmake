37dcb165ebf74dc00fec03d54de6517f0b8f5b25
circl <circl.lastname@gmail.com>
2024-06-23 17:20:29 +0200

    Meta: Remove CommonMark spec test downloader
    We no longer have a Markdown implementation to test

--- Meta/CMake/commonmark_spec.cmake
+++
@@ -1,9 +0,0 @@
-option(ENABLE_COMMONMARK_SPEC_DOWNLOAD "Enable download of commonmark test suite at build time" ON)
-
-set(MARKDOWN_TEST_PATH ${SERENITY_CACHE_DIR}/commonmark.spec.json)
-set(MARKDOWN_TEST_URL https://spec.commonmark.org/0.30/spec.json)
-
-if(ENABLE_COMMONMARK_SPEC_DOWNLOAD)
-    download_file(${MARKDOWN_TEST_URL} ${MARKDOWN_TEST_PATH})
-    install(FILES ${MARKDOWN_TEST_PATH} DESTINATION home/anon/Tests)
-endif()
