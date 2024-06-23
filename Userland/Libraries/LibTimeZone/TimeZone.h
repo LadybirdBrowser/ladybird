b06385c0702717d7d4ac624011ee52c130cc39c0
Timothy Flynn <trflynn89@pm.me>
2024-06-23 16:54:10 -0400

    LibTimeZone: Remove entirely unused time zone methods
    Evaluating third-party alternatives to LibTimeZone will be simpler with
    unused functionality removed.

--- Userland/Libraries/LibTimeZone/TimeZone.h
+++ Userland/Libraries/LibTimeZone/TimeZone.h
@@ -1,13 +1,11 @@
 /*
- * Copyright (c) 2022, Tim Flynn <trflynn89@serenityos.org>
+ * Copyright (c) 2022-2024, Tim Flynn <trflynn89@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-#include <AK/Array.h>
-#include <AK/ByteString.h>
 #include <AK/Error.h>
 #include <AK/Format.h>
 #include <AK/Optional.h>
@@ -39,46 +37,15 @@ struct Offset {
     InDST in_dst { InDST::No };
 };
 
-struct NamedOffset : public Offset {
-    ByteString name;
-};
-
-struct Coordinate {
-    constexpr float decimal_coordinate() const
-    {
-        return static_cast<float>(degrees) + (static_cast<float>(minutes) / 60.0f) + (static_cast<float>(seconds) / 3'600.0f);
-    }
-
-    i16 degrees { 0 };
-    u8 minutes { 0 };
-    u8 seconds { 0 };
-};
-
-struct Location {
-    Coordinate latitude;
-    Coordinate longitude;
-};
-
-StringView system_time_zone();
 StringView current_time_zone();
-ErrorOr<void> change_time_zone(StringView time_zone);
 ReadonlySpan<TimeZoneIdentifier> all_time_zones();
 
 Optional<TimeZone> time_zone_from_string(StringView time_zone);
 StringView time_zone_to_string(TimeZone time_zone);
 Optional<StringView> canonicalize_time_zone(StringView time_zone);
 
-Optional<DaylightSavingsRule> daylight_savings_rule_from_string(StringView daylight_savings_rule);
-StringView daylight_savings_rule_to_string(DaylightSavingsRule daylight_savings_rule);
-
 Optional<Offset> get_time_zone_offset(TimeZone time_zone, AK::UnixDateTime time);
 Optional<Offset> get_time_zone_offset(StringView time_zone, AK::UnixDateTime time);
-
-Optional<Array<NamedOffset, 2>> get_named_time_zone_offsets(TimeZone time_zone, AK::UnixDateTime time);
-Optional<Array<NamedOffset, 2>> get_named_time_zone_offsets(StringView time_zone, AK::UnixDateTime time);
-
-Optional<Location> get_time_zone_location(TimeZone time_zone);
-Optional<Location> get_time_zone_location(StringView time_zone);
 
 Optional<Region> region_from_string(StringView region);
 StringView region_to_string(Region region);
