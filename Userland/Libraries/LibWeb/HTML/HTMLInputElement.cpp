5e5417fe74382da9fbfbf97f8dd5941081a1e6a0
Andreas Kling <andreas@ladybird.org>
2024-06-25 11:28:58 +0200

    LibWeb: Rename Element::shadow_root_internal() to shadow_root()
    And let the old shadow_root(), which was only supposed to be used by
    bindings, be called shadow_root_for_bindings() instead.
    
    This makes it much easier to read DOM code, and we don't have to worry
    about when to use shadow_root_internal() or why.

--- Userland/Libraries/LibWeb/HTML/HTMLInputElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLInputElement.cpp
@@ -732,7 +732,7 @@ Optional<String> HTMLInputElement::placeholder_value() const
 
 void HTMLInputElement::create_shadow_tree_if_needed()
 {
-    if (shadow_root_internal())
+    if (shadow_root())
         return;
 
     switch (type_state()) {
