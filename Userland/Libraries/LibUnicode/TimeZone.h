1b2d47e6bbda997e2cf4a9e2685cd1c409cd8c2f
Timothy Flynn <trflynn89@pm.me>
2024-06-25 11:33:26 -0400

    LibJS+LibUnicode: Port retrieving available regional time zones to ICU

--- Userland/Libraries/LibUnicode/TimeZone.h
+++ Userland/Libraries/LibUnicode/TimeZone.h
@@ -14,6 +14,7 @@ namespace Unicode {
 
 String current_time_zone();
 Vector<String> const& available_time_zones();
+Vector<String> available_time_zones_in_region(StringView region);
 Optional<String> resolve_primary_time_zone(StringView time_zone);
 
 }
