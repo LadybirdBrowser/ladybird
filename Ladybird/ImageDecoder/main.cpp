e7ece774a2227f938b7eec8767b11bc78d9e2135
Andrew Kaster <akaster@serenityos.org>
2024-06-26 12:54:57 -0600

    ImageDecoder: Register with mach server on macOS
    This allows viewing the resource usage in the task manager widget

--- Ladybird/ImageDecoder/main.cpp
+++ Ladybird/ImageDecoder/main.cpp
@@ -12,11 +12,25 @@
 #include <LibIPC/SingleServer.h>
 #include <LibMain/Main.h>
 
-ErrorOr<int> serenity_main(Main::Arguments)
+#if defined(AK_OS_MACOS)
+#    include <LibCore/Platform/ProcessStatisticsMach.h>
+#endif
+
+ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
     AK::set_rich_debug_enabled(true);
 
+    Core::ArgsParser args_parser;
+    StringView mach_server_name;
+    args_parser.add_option(mach_server_name, "Mach server name", "mach-server-name", 0, "mach_server_name");
+    args_parser.parse(arguments);
+
     Core::EventLoop event_loop;
+
+#if defined(AK_OS_MACOS)
+    if (!mach_server_name.is_empty())
+        Core::Platform::register_with_mach_server(mach_server_name);
+#endif
 
     auto client = TRY(IPC::take_over_accepted_client_from_system_server<ImageDecoder::ConnectionFromClient>());
 
