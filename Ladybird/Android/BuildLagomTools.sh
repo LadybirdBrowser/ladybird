045bc3ffd63a764fa40bd1576b26828eb7547afc
Andrew Kaster <andrewdkaster@gmail.com>
2024-07-11 08:57:36 -0600

    Android: Pass new Lagom Tool install options to BuildLagomTools.sh

--- Ladybird/Android/BuildLagomTools.sh
+++ Ladybird/Android/BuildLagomTools.sh
@@ -35,6 +35,8 @@ cmake -S "${LADYBIRD_SOURCE_DIR}/Meta/Lagom" -B "$BUILD_DIR/lagom-tools" \
     -DCMAKE_C_COMPILER="$CC" \
     -DCMAKE_CXX_COMPILER="$CXX" \
     -DSERENITY_CACHE_DIR="$CACHE_DIR" \
+    -DLAGOM_TOOLS_ONLY=ON \
+    -DINSTALL_LAGOM_TOOLS=ON \
     -DCMAKE_TOOLCHAIN_FILE="$LADYBIRD_SOURCE_DIR/Toolchain/Tarballs/vcpkg/scripts/buildsystems/vcpkg.cmake" \
     -DVCPKG_INSTALL_OPTIONS="--no-print-usage" \
     -DVCPKG_OVERLAY_TRIPLETS="$LADYBIRD_SOURCE_DIR/Meta/CMake/vcpkg/release-triplets" \
