a443f508079971489af1f81265857417f617963e
circl <circl.lastname@gmail.com>
2024-07-15 18:00:14 +0200

    Base: Move config files out of home/anon and into default-config

--- Ladybird/Android/src/main/cpp/WebContentService.cpp
+++ Ladybird/Android/src/main/cpp/WebContentService.cpp
@@ -106,9 +106,7 @@ ErrorOr<NonnullRefPtr<Client>> bind_service(void (*bind_method)(int))
 
 static ErrorOr<void> load_content_filters()
 {
-    auto file_or_error = Core::File::open(ByteString::formatted("{}/home/anon/.config/BrowserContentFilters.txt", s_serenity_resource_root), Core::File::OpenMode::Read);
-    if (file_or_error.is_error())
-        file_or_error = Core::File::open(ByteString::formatted("{}/res/ladybird/BrowserContentFilters.txt", s_serenity_resource_root), Core::File::OpenMode::Read);
+    auto file_or_error = Core::File::open(ByteString::formatted("{}/res/ladybird/default-config/BrowserContentFilters.txt", s_serenity_resource_root), Core::File::OpenMode::Read);
     if (file_or_error.is_error())
         return file_or_error.release_error();
 
@@ -135,9 +133,7 @@ static ErrorOr<void> load_content_filters()
 
 static ErrorOr<void> load_autoplay_allowlist()
 {
-    auto file_or_error = Core::File::open(TRY(String::formatted("{}/home/anon/.config/BrowserAutoplayAllowlist.txt", s_serenity_resource_root)), Core::File::OpenMode::Read);
-    if (file_or_error.is_error())
-        file_or_error = Core::File::open(TRY(String::formatted("{}/res/ladybird/BrowserAutoplayAllowlist.txt", s_serenity_resource_root)), Core::File::OpenMode::Read);
+    auto file_or_error = Core::File::open(TRY(String::formatted("{}/res/ladybird/default-config/BrowserAutoplayAllowlist.txt", s_serenity_resource_root)), Core::File::OpenMode::Read);
     if (file_or_error.is_error())
         return file_or_error.release_error();
 
