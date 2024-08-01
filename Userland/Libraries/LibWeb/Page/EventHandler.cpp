0c36e6156013dcb05b48544df236db07d7672da1
Timothy Flynn <trflynn89@pm.me>
2024-08-01 13:43:06 -0400

    LibWeb: Remove debug spam when clicking a link

--- Userland/Libraries/LibWeb/Page/EventHandler.cpp
+++ Userland/Libraries/LibWeb/Page/EventHandler.cpp
@@ -312,7 +312,7 @@ bool EventHandler::handle_mouseup(CSSPixelPoint viewport_position, CSSPixelPoint
                     JS::NonnullGCPtr<DOM::Document> document = *m_navigable->active_document();
                     auto href = link->href();
                     auto url = document->parse_url(href);
-                    dbgln("Web::EventHandler: Clicking on a link to {}", url);
+
                     if (button == UIEvents::MouseButton::Middle) {
                         m_navigable->page().client().page_did_middle_click_link(url, link->target().to_byte_string(), modifiers);
                     } else if (button == UIEvents::MouseButton::Secondary) {
