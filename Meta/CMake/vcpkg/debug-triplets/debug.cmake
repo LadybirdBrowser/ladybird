1ad19e0d4bd8dd3e571680566b19307835a1c986
Timothy Flynn <trflynn89@pm.me>
2024-08-03 10:40:00 -0400

    Meta: Use dynamic vcpkg linkage for release builds
    By using static linkage, we are opening ourselves up to ODR violations
    that result in runtime crashes (rather than build-time link errors). For
    example, we've seen this happen while trying to use Skia context in both
    LibGfx and LibWeb.

--- Meta/CMake/vcpkg/debug-triplets/debug.cmake
+++ Meta/CMake/vcpkg/debug-triplets/debug.cmake
@@ -1,3 +1,3 @@
 # Ideally, we would set VCPKG_BUILD_TYPE="debug", but that is currently not supported as a standalone build type.
 # See: https://github.com/microsoft/vcpkg/issues/38224
-set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
--- Meta/CMake/vcpkg/sanitizer-triplets/sanitizer.cmake
+++ Meta/CMake/vcpkg/sanitizer-triplets/sanitizer.cmake
@@ -1,5 +1,5 @@
 set(VCPKG_BUILD_TYPE release)
-set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 set(VCPKG_C_FLAGS "")
 set(VCPKG_CXX_FLAGS "-frtti")
