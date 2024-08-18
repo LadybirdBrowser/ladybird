1dde9c8fba4720fae0b91f8aa45fd13c09e38398
Andrew Kaster <andrew@ladybird.org>
2024-08-17 20:13:54 -0600

    AK+LibGfx+LibWebView: Add wrapper header around swift/bridging
    When using a configuration without a swift compiler, we need to no-op
    the swift annotations. Other, cleverer solutions beyond the has include
    all fell flat in the face of the clang modules implementation used by
    swift to parse-once use-everywhere each module.

---
+++ AK/Swift.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024, Andrew Kaster <andrew@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#if __has_include(<swift/bridging>)
+#    include <swift/bridging>
+#else
+#    define SWIFT_SELF_CONTAINED
+#    define SWIFT_RETURNS_INDEPENDENT_VALUE
+#    define SWIFT_SHARED_REFERENCE(retain, release)
+#    define SWIFT_IMMORTAL_REFERENCE
+#    define SWIFT_UNSAFE_REFERENCE
+#    define SWIFT_NAME(name)
+#    define SWIFT_CONFORMS_TO_PROTOCOL(protocol)
+#    define SWIFT_COMPUTED_PROPERTY
+#    define SWIFT_MUTATING
+#endif
