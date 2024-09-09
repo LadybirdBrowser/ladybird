2671820aceba2ab4a474753ad3a0e5993a87bfee
Sam Atkins <sam@ladybird.org>
2024-09-09 17:16:25 +0100

    LibWeb: Move our "default sizing algorithm" function into a public place
    Specifically, this is used by the updated algorithm for parsing a sizes
    attribute, so we need access to it there.
    
    No behaviour changes.

--- Userland/Libraries/LibWeb/Painting/BackgroundPainting.cpp
+++ Userland/Libraries/LibWeb/Painting/BackgroundPainting.cpp
@@ -6,6 +6,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibWeb/CSS/Sizing.h>
 #include <LibWeb/Layout/Node.h>
 #include <LibWeb/Layout/Viewport.h>
 #include <LibWeb/Painting/BackgroundPainting.h>
@@ -13,48 +14,6 @@
 #include <LibWeb/Painting/PaintableBox.h>
 
 namespace Web::Painting {
-
-// https://drafts.csswg.org/css-images/#default-sizing
-static CSSPixelSize run_default_sizing_algorithm(
-    Optional<CSSPixels> specified_width, Optional<CSSPixels> specified_height,
-    Optional<CSSPixels> natural_width, Optional<CSSPixels> natural_height,
-    Optional<CSSPixelFraction> natural_aspect_ratio,
-    CSSPixelSize default_size)
-{
-    // If the specified size is a definite width and height, the concrete object size is given that width and height.
-    if (specified_width.has_value() && specified_height.has_value())
-        return CSSPixelSize { specified_width.value(), specified_height.value() };
-    // If the specified size is only a width or height (but not both) then the concrete object size is given that specified width or height.
-    // The other dimension is calculated as follows:
-    if (specified_width.has_value() || specified_height.has_value()) {
-        // 1. If the object has a natural aspect ratio,
-        // the missing dimension of the concrete object size is calculated using that aspect ratio and the present dimension.
-        if (natural_aspect_ratio.has_value() && !natural_aspect_ratio->might_be_saturated()) {
-            if (specified_width.has_value())
-                return CSSPixelSize { specified_width.value(), (CSSPixels(1) / natural_aspect_ratio.value()) * specified_width.value() };
-            if (specified_height.has_value())
-                return CSSPixelSize { specified_height.value() * natural_aspect_ratio.value(), specified_height.value() };
-        }
-        // 2. Otherwise, if the missing dimension is present in the object’s natural dimensions,
-        // the missing dimension is taken from the object’s natural dimensions.
-        if (specified_height.has_value() && natural_width.has_value())
-            return CSSPixelSize { natural_width.value(), specified_height.value() };
-        if (specified_width.has_value() && natural_height.has_value())
-            return CSSPixelSize { specified_width.value(), natural_height.value() };
-        // 3. Otherwise, the missing dimension of the concrete object size is taken from the default object size.
-        if (specified_height.has_value())
-            return CSSPixelSize { default_size.width(), specified_height.value() };
-        if (specified_width.has_value())
-            return CSSPixelSize { specified_width.value(), default_size.height() };
-        VERIFY_NOT_REACHED();
-    }
-    // If the specified size has no constraints:
-    // 1. If the object has a natural height or width, its size is resolved as if its natural dimensions were given as the specified size.
-    if (natural_width.has_value() || natural_height.has_value())
-        return run_default_sizing_algorithm(natural_width, natural_height, natural_width, natural_height, natural_aspect_ratio, default_size);
-    // FIXME: 2. Otherwise, its size is resolved as a contain constraint against the default object size.
-    return default_size;
-}
 
 static RefPtr<DisplayList> compute_text_clip_paths(PaintContext& context, Paintable const& paintable, CSSPixelPoint containing_block_location)
 {
@@ -373,7 +332,7 @@ ResolvedBackground resolve_background_layers(Vector<CSS::BackgroundLayerData> co
             if (!layer.size_y.is_auto())
                 specified_height = layer.size_y.to_px(layout_node, background_positioning_area.height());
         }
-        auto concrete_image_size = run_default_sizing_algorithm(
+        auto concrete_image_size = CSS::run_default_sizing_algorithm(
             specified_width, specified_height,
             image.natural_width(), image.natural_height(), image.natural_aspect_ratio(),
             background_positioning_area.size());
