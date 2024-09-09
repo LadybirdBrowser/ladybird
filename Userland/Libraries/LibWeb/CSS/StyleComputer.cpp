LadybirdBrowser/ladybird

commit a9d5a99568b41c83d724a9d8f81c1fdfe10a830e

LibGfx+LibWeb: Replace remaining OpenType implementation with Skia
This change should move us forward toward emoji support, as we are no
longer limited by our own OpenType implementation, which was failing
to parse the TrueType Collection format used to store emoji fonts
(at least on macOS).

Userland/Libraries/LibWeb/CSS/StyleComputer.cpp
@@ -19,7 +19,6 @@
 #include <LibGfx/Font/Font.h>
 #include <LibGfx/Font/FontDatabase.h>
 #include <LibGfx/Font/FontStyleMapping.h>
-#include <LibGfx/Font/OpenType/Typeface.h>
 #include <LibGfx/Font/ScaledFont.h>
 #include <LibGfx/Font/Typeface.h>
 #include <LibGfx/Font/WOFF/Loader.h>
@@ -172,7 +171,7 @@ ErrorOr<NonnullRefPtr<Gfx::Typeface>> FontLoader::try_load_font()
     }
     if (mime_type.has_value()) {
         if (mime_type->essence() == "font/ttf"sv || mime_type->essence() == "application/x-font-ttf"sv) {
-            if (auto result = OpenType::Typeface::try_load_from_externally_owned_memory(resource()->encoded_data()); !result.is_error()) {
+            if (auto result = Gfx::Typeface::try_load_from_externally_owned_memory(resource()->encoded_data()); !result.is_error()) {
                 return result;
             }
         }
@@ -188,7 +187,7 @@ ErrorOr<NonnullRefPtr<Gfx::Typeface>> FontLoader::try_load_font()
         }
     }
 
-    auto ttf = OpenType::Typeface::try_load_from_externally_owned_memory(resource()->encoded_data());
+    auto ttf = Gfx::Typeface::try_load_from_externally_owned_memory(resource()->encoded_data());
     if (!ttf.is_error())
         return ttf.release_value();
     return Error::from_string_literal("Automatic format detection failed");

AK/LsanSuppressions.h
@@ -1,0 +1,22 @@
+/*
+ * Copyright (c) 2024, Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Platform.h>
+
+#ifdef HAS_ADDRESS_SANITIZER
+extern "C" {
+char const* __lsan_default_suppressions();
+char const* __lsan_default_suppressions()
+{
+    // Both Skia and Chromium suppress false positive FontConfig leaks
+    // https://github.com/google/skia/blob/main/tools/LsanSuppressions.cpp#L20
+    // https://chromium.googlesource.com/chromium/src/build/+/master/sanitizers/lsan_suppressions.cc#25
+    return "leak:FcPatternObjectInsertElt";
+}
+}
+#endif

Meta/Lagom/CMakeLists.txt
@@ -523,7 +523,6 @@ if (BUILD_TESTING)
         list(APPEND TEST_DIRECTORIES
             LibGfx
             LibMedia
-            LibTTF
             LibWeb
             LibWebView
         )

Meta/Lagom/Fuzzers/FuzzTTF.cpp
@@ -1,16 +1,0 @@
-/*
- * Copyright (c) 2021, the SerenityOS developers.
- *
- * SPDX-License-Identifier: BSD-2-Clause
- */
-
-#include <LibGfx/Font/OpenType/Typeface.h>
-#include <stddef.h>
-#include <stdint.h>
-
-extern "C" int LLVMFuzzerTestOneInput(u8 const* data, size_t size)
-{
-    AK::set_debug_enabled(false);
-    (void)OpenType::Typeface::try_load_from_externally_owned_memory({ data, size });
-    return 0;
-}

Meta/Lagom/Fuzzers/fuzzers.cmake
@@ -36,7 +36,6 @@ set(FUZZER_TARGETS
     Tar
     TextDecoder
     TIFFLoader
-    TTF
     TinyVGLoader
     URL
     WasmParser

Tests/LibTTF/CMakeLists.txt
@@ -1,7 +1,0 @@
-set(TEST_SOURCES
-    TestCmap.cpp
-)
-
-foreach(source IN LISTS TEST_SOURCES)
-    serenity_test("${source}" LibTTF LIBS LibGfx)
-endforeach()

Tests/LibTTF/TestCmap.cpp
@@ -1,88 +1,0 @@
-/*
- * Copyright (c) 2022, Nico Weber <thakis@chromium.org>
- *
- * SPDX-License-Identifier: BSD-2-Clause
- */
-
-#include <LibGfx/Font/OpenType/Cmap.h>
-#include <LibTest/TestCase.h>
-
-TEST_CASE(test_cmap_format_4)
-{
-    // clang-format off
-    // Big endian.
-    Array<u8, 52> const cmap_table =
-    {
-        // https://docs.microsoft.com/en-us/typography/opentype/spec/cmap#cmap-header
-        0, 0,  // uint16 version
-        0, 1,  // uint16 numTables
-
-        // https://docs.microsoft.com/en-us/typography/opentype/spec/cmap#encoding-records-and-encodings
-        0, 0,  // uint16 platformID, 0 means "Unicode"
-        0, 3,  // uint16 encodingID, 3 means "BMP only" for platformID==0.
-        0, 0, 0, 12,  // Offset32 to encoding subtable.
-
-        // https://docs.microsoft.com/en-us/typography/opentype/spec/cmap#format-4-segment-mapping-to-delta-values
-        0, 4,  // uint16 format = 4
-        0, 42,  // uint16 length in bytes
-        0, 0,  // uint16 language, must be 0
-        0, 6,  // segCount * 2
-        0, 4,  // searchRange
-        0, 1,  // entrySelector
-        0, 2,  // rangeShift
-
-        // endCode array, last entry must be 0xffff.
-        0, 128,
-        1, 0,
-        0xff, 0xff,
-
-        0, 0,  // uint16 reservedPad
-
-        // startCode array
-        0, 16,
-        1, 0,
-        0xff, 0xff,
-
-        // delta array
-        0, 0,
-        0, 10,
-        0, 0,
-
-        // glyphID array
-        0, 0,
-        0, 0,
-        0, 0,
-    };
-    // clang-format on
-    auto cmap = OpenType::Cmap::from_slice(cmap_table.span()).value();
-    cmap.set_active_index(0);
-
-    // Format 4 can't handle code points > 0xffff.
-
-    // First range is 16..128.
-    EXPECT_EQ(cmap.glyph_id_for_code_point(15), 0u);
-    EXPECT_EQ(cmap.glyph_id_for_code_point(16), 16u);
-    EXPECT_EQ(cmap.glyph_id_for_code_point(128), 128u);
-    EXPECT_EQ(cmap.glyph_id_for_code_point(129), 0u);
-
-    // Second range is 256..256, with delta 10.
-    EXPECT_EQ(cmap.glyph_id_for_code_point(255), 0u);
-    EXPECT_EQ(cmap.glyph_id_for_code_point(256), 266u);
-    EXPECT_EQ(cmap.glyph_id_for_code_point(257), 0u);
-
-    // Third range is 0xffff..0xffff.
-    // From https://docs.microsoft.com/en-us/typography/opentype/spec/cmap#format-4-segment-mapping-to-delta-values:
-    // "the final start code and endCode values must be 0xFFFF. This segment need not contain any valid mappings.
-    // (It can just map the single character code 0xFFFF to missingGlyph). However, the segment must be present."
-    // FIXME: Make OpenType::Cmap::from_slice() reject inputs where this isn't true.
-    EXPECT_EQ(cmap.glyph_id_for_code_point(0xfeff), 0u);
-    EXPECT_EQ(cmap.glyph_id_for_code_point(0xffff), 0xffffu);
-    EXPECT_EQ(cmap.glyph_id_for_code_point(0x1'0000), 0u);
-
-    // Set the number of subtables to a value, where the record offset for the last subtable is greater than the
-    // total table size. We should not crash if a Cmap table is truncated in this way.
-    auto malformed_cmap_table = cmap_table;
-    malformed_cmap_table[3] = 13;
-    auto cmap_with_invalid_subtable_offset = OpenType::Cmap::from_slice(malformed_cmap_table.span()).value();
-    EXPECT(!cmap_with_invalid_subtable_offset.subtable(12).has_value());
-}

commit a185d1a168372aae78462ca3e7e6722d858dedc1

LibWeb: Do not try to load a font if format detection has failed
Skia is more permissive when it comes to font loading, compared to our
own OpenType implementation, which it has superseded, parsing an invalid
TTF does not result in an error but rather produces a font that is
incorrectly displayed. This change updates the FontLoader to address
this behavior and to stop attempting to parse a font as a last resort
when format detection has failed.

Fixes regression on x.com when text is not displayed introduced in
a9d5a99568b41c83d724a9d8f81c1fdfe10a830e

Userland/Libraries/LibWeb/CSS/StyleComputer.cpp
@@ -190,9 +190,6 @@ ErrorOr<NonnullRefPtr<Gfx::Typeface>> FontLoader::try_load_font()
         }
     }
 
-    auto ttf = Gfx::Typeface::try_load_from_externally_owned_memory(resource()->encoded_data());
-    if (!ttf.is_error())
-        return ttf.release_value();
     return Error::from_string_literal("Automatic format detection failed");
 }
 