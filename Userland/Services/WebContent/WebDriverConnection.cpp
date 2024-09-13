9b61b36acc00d6e8a894fb6d1838f66f9602e4d1
Timothy Flynn <trflynn89@pm.me>
2024-09-13 19:41:31 -0400

    LibWeb+WebContent: Implement the WebDriver JSON clone steps for elements
    This is needed by WPT to serialize NodeList instances.

--- Userland/Services/WebContent/WebDriverConnection.cpp
+++ Userland/Services/WebContent/WebDriverConnection.cpp
@@ -1783,6 +1783,9 @@ Messages::WebDriverClient::ExecuteScriptResponse WebDriverConnection::execute_sc
         case Web::WebDriver::ExecuteScriptResultType::BrowsingContextDiscarded:
             response = Web::WebDriver::Error::from_code(Web::WebDriver::ErrorCode::StaleElementReference, "Browsing context has been discarded", move(result.value));
             break;
+        case Web::WebDriver::ExecuteScriptResultType::StaleElement:
+            response = Web::WebDriver::Error::from_code(Web::WebDriver::ErrorCode::StaleElementReference, "Referenced element has become stale", move(result.value));
+            break;
         }
 
         async_script_executed(move(response));
@@ -1830,6 +1833,9 @@ Messages::WebDriverClient::ExecuteAsyncScriptResponse WebDriverConnection::execu
             break;
         case Web::WebDriver::ExecuteScriptResultType::BrowsingContextDiscarded:
             response = Web::WebDriver::Error::from_code(Web::WebDriver::ErrorCode::StaleElementReference, "Browsing context has been discarded", move(result.value));
+            break;
+        case Web::WebDriver::ExecuteScriptResultType::StaleElement:
+            response = Web::WebDriver::Error::from_code(Web::WebDriver::ErrorCode::StaleElementReference, "Referenced element has become stale", move(result.value));
             break;
         }
 
