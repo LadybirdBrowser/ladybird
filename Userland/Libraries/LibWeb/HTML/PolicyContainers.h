1ca7034808c5a7f44c65c50e30dd7d5d3ccb60af
Noah Bright <noah.bright.1@gmail.com>
2024-09-15 17:05:41 -0400

    LibWeb: Stub in CSP List
    Working backwards from
    https://html.spec.whatwg.org/multipage/semantics.html#update-a-style-block
    
    One of the steps in the above algorithm is
    https://w3c.github.io/webappsec-csp/#should-block-inline
    
    In implementing Should element’s inline type behavior be blocked
    by Content Security Policy?, add class declarations for
    Directives, Policy's, and CSP Lists

--- Userland/Libraries/LibWeb/HTML/PolicyContainers.h
+++ Userland/Libraries/LibWeb/HTML/PolicyContainers.h
@@ -7,7 +7,9 @@
 #pragma once
 
 #include <LibIPC/Forward.h>
+#include <LibJS/Runtime/Object.h>
 #include <LibWeb/HTML/EmbedderPolicy.h>
+#include <LibWeb/HTML/Policy.h>
 #include <LibWeb/ReferrerPolicy/ReferrerPolicy.h>
 
 namespace Web::HTML {
@@ -16,7 +18,8 @@ namespace Web::HTML {
 // A policy container is a struct containing policies that apply to a Document, a WorkerGlobalScope, or a WorkletGlobalScope. It has the following items:
 struct PolicyContainer {
     // https://html.spec.whatwg.org/multipage/origin.html#policy-container-csp-list
-    // FIXME: A CSP list, which is a CSP list. It is initially empty.
+    // A CSP list, which is a CSP list. It is initially empty.
+    CSPList csp_list {};
 
     // https://html.spec.whatwg.org/multipage/origin.html#policy-container-embedder-policy
     // An embedder policy, which is an embedder policy. It is initially a new embedder policy.
@@ -26,6 +29,9 @@ struct PolicyContainer {
     // A referrer policy, which is a referrer policy. It is initially the default referrer policy.
     ReferrerPolicy::ReferrerPolicy referrer_policy { ReferrerPolicy::DEFAULT_REFERRER_POLICY };
 };
+
+// https://w3c.github.io/webappsec-csp/#get-csp-of-object
+Optional<CSPList> retrieve_the_csp_list_of_an_object(JS::Object const&);
 
 }
 
