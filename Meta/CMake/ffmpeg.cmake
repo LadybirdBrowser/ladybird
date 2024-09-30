af33ad44b0573a8584ee2e8d63ae275deff69efe
Olekoop <mlglol360xd@gmail.com>
2024-09-30 16:14:30 +0200

    CMake: Make sure that we don't use ffmpeg on Android
    We cannot use ffmpeg on Android due to legal reasons. Also pkgconfig
    will try to use host's libraries if it cannot find them for Android.

--- Meta/CMake/ffmpeg.cmake
+++ Meta/CMake/ffmpeg.cmake
@@ -4,7 +4,7 @@ find_package(PkgConfig REQUIRED)
 pkg_check_modules(AVCODEC IMPORTED_TARGET libavcodec)
 pkg_check_modules(AVFORMAT IMPORTED_TARGET libavformat)
 
-if (AVCODEC_FOUND AND AVFORMAT_FOUND)
+if (AVCODEC_FOUND AND AVFORMAT_FOUND AND NOT ANDROID)
     set(HAS_FFMPEG ON CACHE BOOL "" FORCE)
     add_compile_definitions(USE_FFMPEG=1)
 endif()
