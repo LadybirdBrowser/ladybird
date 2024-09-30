3a935aa07641645797e44683882de72be3f6fd83
Sam Atkins <sam@ladybird.org>
2024-09-30 10:36:11 +0100

    LibCore: Clarify StandardPaths::data_directory() name
    Let's make it clear that this is for user data.

--- Userland/Libraries/LibCore/StandardPaths.h
+++ Userland/Libraries/LibCore/StandardPaths.h
@@ -23,7 +23,7 @@ public:
     static ByteString videos_directory();
     static ByteString tempfile_directory();
     static ByteString config_directory();
-    static ByteString data_directory();
+    static ByteString user_data_directory();
     static ErrorOr<ByteString> runtime_directory();
     static ErrorOr<Vector<String>> font_directories();
 };
