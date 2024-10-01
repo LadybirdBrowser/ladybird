6bf01ea0156d7cc2b146e69fea009e3993a9caae
Sam Atkins <sam@ladybird.org>
2024-10-01 11:06:48 +0100

    Base: Remove old LibWeb demo pages
    These used to serve as tests before we had proper testing infrastructure
    set up. Now, they just sit forgotten about, gathering dust. Let's remove
    them.

--- Base/res/html/misc/welcome.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log(
-        "Hello from DOMContentLoaded! There should be a message before this from an inline script"
-    );
-    document.getElementById("ua").innerHTML = navigator.userAgent;
-});
