LadybirdBrowser/ladybird

commit 6eca60504e9c6c687dc03ffc448d75ccc2f9baac

headless-browser: Only rebaseline tests that would have failed
We have a bit of forgiveness around allowing tests to pass with varying
trailing newlines. Only write a rebaselined test to disk if it would not
have passed under those conditions.

Userland/Utilities/headless-browser.cpp
@@ -339,26 +339,32 @@ static void run_dump_test(HeadlessWebContentView& view, Test& test, URL::URL con
             return TestResult::Pass;
         }
 
-        auto expectation_file_or_error = Core::File::open(test.expectation_path, Application::the().rebaseline ? Core::File::OpenMode::Write : Core::File::OpenMode::Read);
-        if (expectation_file_or_error.is_error()) {
-            warnln("Failed opening '{}': {}", test.expectation_path, expectation_file_or_error.error());
-            return expectation_file_or_error.release_error();
-        }
+        auto open_expectation_file = [&](auto mode) {
+            auto expectation_file_or_error = Core::File::open(test.expectation_path, mode);
+            if (expectation_file_or_error.is_error())
+                warnln("Failed opening '{}': {}", test.expectation_path, expectation_file_or_error.error());
 
-        auto expectation_file = expectation_file_or_error.release_value();
+            return expectation_file_or_error;
+        };
+
+        ByteBuffer expectation;
+        {
+            auto expectation_file = TRY(open_expectation_file(Core::File::OpenMode::Read));
+            expectation = TRY(expectation_file->read_until_eof());
+
+            auto result_trimmed = StringView { test.text }.trim("\n"sv, TrimMode::Right);
+            auto expectation_trimmed = StringView { expectation }.trim("\n"sv, TrimMode::Right);
+
+            if (result_trimmed == expectation_trimmed)
+                return TestResult::Pass;
+        }
 
         if (Application::the().rebaseline) {
+            auto expectation_file = TRY(open_expectation_file(Core::File::OpenMode::Write));
             TRY(expectation_file->write_until_depleted(test.text));
+
             return TestResult::Pass;
         }
-
-        auto expectation = TRY(expectation_file->read_until_eof());
-
-        auto result_trimmed = StringView { test.text }.trim("\n"sv, TrimMode::Right);
-        auto expectation_trimmed = StringView { expectation }.trim("\n"sv, TrimMode::Right);
-
-        if (result_trimmed == expectation_trimmed)
-            return TestResult::Pass;
 
         auto const color_output = isatty(STDOUT_FILENO) ? Diff::ColorOutput::Yes : Diff::ColorOutput::No;
 

commit 41558f8bcf7a50439c76918f9ddac457aa7bd0d2

headless-browser: Restore ability to rebaseline new tests
This regressed in commit 6eca60504e9c6c687dc03ffc448d75ccc2f9baac.

If we are rebaslining tests, do not fail if we are unable to open the
expectation file for reading.

Userland/Utilities/headless-browser.cpp
@@ -348,15 +348,17 @@ static void run_dump_test(HeadlessWebContentView& view, Test& test, URL::URL con
         };
 
         ByteBuffer expectation;
-        {
-            auto expectation_file = TRY(open_expectation_file(Core::File::OpenMode::Read));
-            expectation = TRY(expectation_file->read_until_eof());
+
+        if (auto expectation_file = open_expectation_file(Core::File::OpenMode::Read); !expectation_file.is_error()) {
+            expectation = TRY(expectation_file.value()->read_until_eof());
 
             auto result_trimmed = StringView { test.text }.trim("\n"sv, TrimMode::Right);
             auto expectation_trimmed = StringView { expectation }.trim("\n"sv, TrimMode::Right);
 
             if (result_trimmed == expectation_trimmed)
                 return TestResult::Pass;
+        } else if (!Application::the().rebaseline) {
+            return expectation_file.release_error();
         }
 
         if (Application::the().rebaseline) {