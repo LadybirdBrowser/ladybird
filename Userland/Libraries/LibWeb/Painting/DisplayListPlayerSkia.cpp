4f0ed6fa156af97dbe635866c14da6ad199e1ada
Lucas CHOLLET <lucas.chollet@free.fr>
2024-10-12 19:16:16 -0400

    LibGfx+LibWeb: Do some color management on images with an ICC profile
    This patch introduces the `Gfx::ColorSpace` class, this is basically a
    serializable wrapper for skia's SkColorSpace. Creation of the instances
    of this class (and thus ICC profiles parsing) is performed in the
    ImageDecoder process. Then the object is serialized and sent through
    IPC, to finally be handed to skia for rendering.
    
    However, to make sure that we're not making all LibGfx's users dependent
    on Skia as well, we need to ensure the `Gfx::ColorSpace` object has no
    dependency on objects from Skia. To that end, the only member of the
    `ColorSpace` class is the opaque `ColorSpaceImpl` struct. Though, there
    is on issue with that design, the code in `DisplayListPlayer.cpp` needs
    access to the underlying `sk_sp<SkColorSpace>`. It is provided by a
    template function, that is only specialized for this type.
    
    Doing this work allows us to pass the following WPT tests:
    - https://wpt.live/css/css-color/untagged-images-001.html
    - https://wpt.live/css/css-color/untagged-images-003.html
    - https://wpt.live/css/css-color/untagged-images-004.html
    - https://wpt.live/css/css-color/untagged-images-001.html
    
    Other test cases can also be found here:
    - https://github.com/svgeesus/PNG-ICC-tests
    
    Note that SkColorSpace support quite a limited amount of color spaces,
    so color profiles like the ones in [1] or the v4 profiles in [2] are not
    supported yet. In fact, SkColorSpace only accepts skcms_ICCProfile with
    a linear conversion to XYZ D50.
    
    [1] https://www.color.org/browsertest.xalter
    [2] https://www.color.org/version4html.xalter

--- Userland/Libraries/LibWeb/Painting/DisplayListPlayerSkia.cpp
+++ Userland/Libraries/LibWeb/Painting/DisplayListPlayerSkia.cpp
@@ -24,6 +24,7 @@
 #include <gpu/ganesh/SkSurfaceGanesh.h>
 #include <pathops/SkPathOps.h>
 
+#include <LibGfx/ColorSpace.h>
 #include <LibGfx/Font/ScaledFont.h>
 #include <LibGfx/PathSkia.h>
 #include <LibWeb/CSS/ComputedValues.h>
@@ -278,11 +279,11 @@ static SkColorType to_skia_color_type(Gfx::BitmapFormat format)
     }
 }
 
-static SkBitmap to_skia_bitmap(Gfx::Bitmap const& bitmap)
+static SkBitmap to_skia_bitmap(Gfx::Bitmap const& bitmap, Gfx::ColorSpace maybe_profile = {})
 {
     SkColorType color_type = to_skia_color_type(bitmap.format());
     SkAlphaType alpha_type = bitmap.alpha_type() == Gfx::AlphaType::Premultiplied ? kPremul_SkAlphaType : kUnpremul_SkAlphaType;
-    SkImageInfo image_info = SkImageInfo::Make(bitmap.width(), bitmap.height(), color_type, alpha_type);
+    SkImageInfo image_info = SkImageInfo::Make(bitmap.width(), bitmap.height(), color_type, alpha_type, maybe_profile.color_space<sk_sp<SkColorSpace>>());
     SkBitmap sk_bitmap;
     sk_bitmap.setInfo(image_info);
 
@@ -379,7 +380,7 @@ void DisplayListPlayerSkia::draw_scaled_immutable_bitmap(DrawScaledImmutableBitm
 {
     auto src_rect = to_skia_rect(command.src_rect);
     auto dst_rect = to_skia_rect(command.dst_rect);
-    auto bitmap = to_skia_bitmap(command.bitmap->bitmap());
+    auto bitmap = to_skia_bitmap(command.bitmap->bitmap(), command.bitmap->color_space());
     auto image = SkImages::RasterFromBitmap(bitmap);
     auto& canvas = surface().canvas();
     SkPaint paint;
@@ -388,7 +389,7 @@ void DisplayListPlayerSkia::draw_scaled_immutable_bitmap(DrawScaledImmutableBitm
 
 void DisplayListPlayerSkia::draw_repeated_immutable_bitmap(DrawRepeatedImmutableBitmap const& command)
 {
-    auto bitmap = to_skia_bitmap(command.bitmap->bitmap());
+    auto bitmap = to_skia_bitmap(command.bitmap->bitmap(), command.bitmap->color_space());
     auto image = SkImages::RasterFromBitmap(bitmap);
 
     SkMatrix matrix;
