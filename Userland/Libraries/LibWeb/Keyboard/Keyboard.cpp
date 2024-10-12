0583e5371e732ef805141f09f361abee7e0541e7
Saksham Goyal <sakgoy2001@gmail.com>
2024-10-12 17:32:38 -0400

    following spec

--- Userland/Libraries/LibWeb/Keyboard/Keyboard.cpp
+++ Userland/Libraries/LibWeb/Keyboard/Keyboard.cpp
@@ -3,13 +3,17 @@
 #include <LibWeb/Bindings/HostDefined.h>
 #include <LibWeb/Bindings/KeyboardPrototype.h>
 #include <LibWeb/FileAPI/Blob.h>
+#include <LibWeb/HTML/BrowsingContext.h>
 #include <LibWeb/HTML/Scripting/Environments.h>
 #include <LibWeb/HTML/Scripting/TemporaryExecutionContext.h>
+#include <LibWeb/HTML/TraversableNavigable.h>
 #include <LibWeb/HTML/Window.h>
 #include <LibWeb/Keyboard/Keyboard.h>
 #include <LibWeb/MimeSniff/MimeType.h>
 #include <LibWeb/Page/Page.h>
 #include <LibWeb/Platform/EventLoopPlugin.h>
+#include <LibWeb/UIEvents/KeyCode.h>
+#include <LibWeb/WebIDL/DOMException.h>
 #include <LibWeb/WebIDL/Promise.h>
 
 namespace Web::Keyboard {
@@ -26,13 +30,56 @@ void Keyboard::initialize(JS::Realm& realm)
     WEB_SET_PROTOTYPE_FOR_INTERFACE(Keyboard);
 }
 // https://wicg.github.io/keyboard-lock/#keyboard-lock
-static WebIDL::ExceptionOr<JS::Promise> lock(AK::Vector<AK::String> key_codes)
+WebIDL::ExceptionOr<JS::Promise> Keyboard::lock(const AK::Vector<AK::String>& key_codes = {})
 {
-    // auto& realm = HTML::relevant_realm(*this);
-    // auto promise = WebIDL::create_promise(realm);
-    // return JS::throw_completion(WebIDL::InvalidStateError::create(realm, "not currently executing in the currently active top-level browsing context"_fly_string));
+    // TODO FIXME: This entire section is not correct. I just did the basic logic but this needs to be parallel
 
-    // return JS::NonnullGCPtr { verify_cast<JS::Promise>(*promise->promise()) };
+    // 1.1. Let realm be this's relevant realm.
+    auto& realm = HTML::relevant_realm(*this);
+
+    // 1.2. Let p be a new promise in realm.
+    auto promise = WebIDL::create_promise(realm);
+
+    // 2. If not currently executing in the currently active top-level browsing context, then
+    // if (!HTML::BrowsingContext::top_level_browsing_context()) {
+    //     // 2.1
+    //     return JS::throw_completion(WebIDL::InvalidStateError::create(realm, "not currently executing in the currently active top-level browsing context"_fly_string));
+    // }
+
+    // 3. Run the following steps in parallel:
+    // 3.1
+    m_reserved_key_codes = {};
+    // 3.2
+    if (!key_codes.is_empty()) {
+        // 3.2.1
+        for (auto const& key : key_codes) {
+            auto const code = UIEvents::key_code_from_string(key);
+            // 3.2.1.1
+            if (code == UIEvents::KeyCode::Key_Invalid) {
+                // 3.2.1.1.1
+                m_enable_keyboard_lock = false;
+                // 3.2.1.1.2
+                return JS::throw_completion(WebIDL::InvalidAccessError::create(realm, "Invalid Key Code"_fly_string));
+            }
+            // 3.2.1.2
+            m_reserved_key_codes.append(code);
+        }
+    }
+    // 3.3
+    if (m_enable_keyboard_lock) {
+        // 3.3.1
+        // TODO FIXME
+        // 3.3.2
+        m_enable_keyboard_lock = true;
+    }
+    // 3.4
+    // TODO FIXME
+
+    // 3.5
+    WebIDL::resolve_promise(realm, promise, JS::js_undefined());
+
+    // 4. Return p.
+    return JS::GCPtr { verify_cast<JS::Promise>(*promise->promise()) };
 }
 
 }
--- Userland/Libraries/LibWeb/Keyboard/Keyboard.h
+++ Userland/Libraries/LibWeb/Keyboard/Keyboard.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "AK/Vector.h"
+#include "LibWeb/HTML/DOMStringList.h"
+#include "LibWeb/UIEvents/KeyCode.h"
 #include <AK/String.h>
 #include <LibJS/Forward.h>
 #include <LibJS/Heap/GCPtr.h>
 #include <LibWeb/DOM/EventTarget.h>
 #include <LibWeb/Forward.h>
 #include <LibWeb/WebIDL/ExceptionOr.h>
-
 
 namespace Web::Keyboard {
 
@@ -19,12 +21,16 @@ public:
 
     // JS::NonnullGCPtr<JS::Promise>
     // https://wicg.github.io/keyboard-lock/#keyboard-lock
-    static WebIDL::ExceptionOr<JS::Promise> lock(AK::Vector<AK::String> key_codes);
+    WebIDL::ExceptionOr<JS::Promise> lock(const AK::Vector<AK::String>& key_codes);
 
-    void unlock() {}
+    void unlock() { }
 
 private:
     Keyboard(JS::Realm& realm);
+
+    AK::Vector<UIEvents::KeyCode> m_reserved_key_codes;
+
+    bool m_enable_keyboard_lock = false;
 
     virtual void initialize(JS::Realm& realm) override;
 };
