5559a3dc6e55254f792b5be573aecd468fce7e08
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2024-10-13 20:40:43 +0200

    LibWeb: Remove BrowsingContext pointer from Paintable
    It's no longer needed.

--- Userland/Libraries/LibWeb/Painting/Paintable.cpp
+++ Userland/Libraries/LibWeb/Painting/Paintable.cpp
@@ -14,7 +14,6 @@ namespace Web::Painting {
 
 Paintable::Paintable(Layout::Node const& layout_node)
     : m_layout_node(layout_node)
-    , m_browsing_context(const_cast<HTML::BrowsingContext&>(layout_node.browsing_context()))
 {
     auto& computed_values = layout_node.computed_values();
     if (layout_node.is_grid_item() && computed_values.z_index().has_value()) {
@@ -42,7 +41,6 @@ void Paintable::visit_edges(Cell::Visitor& visitor)
     TreeNode::visit_edges(visitor);
     visitor.visit(m_dom_node);
     visitor.visit(m_layout_node);
-    visitor.visit(m_browsing_context);
     if (m_containing_block.has_value())
         visitor.visit(m_containing_block.value());
 }
@@ -66,16 +64,6 @@ JS::GCPtr<DOM::Node> Paintable::dom_node()
 JS::GCPtr<DOM::Node const> Paintable::dom_node() const
 {
     return m_dom_node;
-}
-
-HTML::BrowsingContext const& Paintable::browsing_context() const
-{
-    return m_browsing_context;
-}
-
-HTML::BrowsingContext& Paintable::browsing_context()
-{
-    return m_browsing_context;
 }
 
 JS::GCPtr<HTML::Navigable> Paintable::navigable() const
