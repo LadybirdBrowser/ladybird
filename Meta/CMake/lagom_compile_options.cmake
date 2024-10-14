SerenityOS/serenity

CMake: Add definitions to start building with windows clang and gcc
clang.exe and gcc.exe both work after these changes and can be used
like so in a VS 2022 Developer Powershell:
```
python3 ./Meta/serenity.py run lagom js
```

Both compilers still can't compile LibCore, but these cmake changes
make that the fault of the code, rather than the compiler options.

Co-authored-by: Andrew Kaster <akaster@serenityos.org>

Meta/CMake/lagom_compile_options.cmake
@@ -19,8 +19,10 @@ function(add_linker_flag_if_supported flag)
     endif()
 endfunction()
 
-add_linker_flag_if_supported(LINKER:--gdb-index)
+if (NOT WIN32)
+    add_linker_flag_if_supported(LINKER:--gdb-index)
 
-if (NOT ENABLE_FUZZERS)
-    add_linker_flag_if_supported(LINKER:-Bsymbolic-non-weak-functions)
+    if (NOT ENABLE_FUZZERS)
+        add_linker_flag_if_supported(LINKER:-Bsymbolic-non-weak-functions)
+    endif()
 endif()

LadybirdBrowser/ladybird

Meta/CMake/lagom_compile_options.cmake
@@ -46,10 +46,12 @@ function(add_cxx_linker_flag_if_supported flag)
     endif()
 endfunction()
 
-add_cxx_linker_flag_if_supported(LINKER:--gdb-index)
+if (NOT WIN32)
+    add_cxx_linker_flag_if_supported(LINKER:--gdb-index)
 
-if (NOT ENABLE_FUZZERS)
-    add_cxx_linker_flag_if_supported(LINKER:-Bsymbolic-non-weak-functions)
+    if (NOT ENABLE_FUZZERS)
+        add_cxx_linker_flag_if_supported(LINKER:-Bsymbolic-non-weak-functions)
+    endif()
 endif()
 
 if (ENABLE_LAGOM_COVERAGE_COLLECTION)