3606b6a4756ff22835a11a06cf83aa1d2db10f29
Kostya Farber <kostya.farber@gmail.com>
2024-10-15 18:21:49 +0100

    LibWeb: Start implementation of rendering tabs according to tab-size

--- Userland/Libraries/LibGfx/TextLayout.cpp
+++ Userland/Libraries/LibGfx/TextLayout.cpp
@@ -32,6 +32,9 @@ RefPtr<GlyphRun> shape_text(FloatPoint baseline_start, Utf8View string, Gfx::Fon
     Vector<Gfx::DrawGlyph> glyph_run;
     FloatPoint point = baseline_start;
     for (size_t i = 0; i < glyph_count; ++i) {
+        if (input_glyph_info[i].codepoint == '\t')
+            continue;
+
         auto position = point
             - FloatPoint { 0, font.pixel_metrics().ascent }
             + FloatPoint { positions[i].x_offset, positions[i].y_offset } / text_shaping_resolution;
