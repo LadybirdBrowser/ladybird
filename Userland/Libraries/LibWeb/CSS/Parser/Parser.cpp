85356094b50878e78d40d79c2a9e96ff9aac5a6b
Ankush Chatterjee <ac.ankush15@gmail.com>
2024-10-23 22:15:19 +0530

    LibWeb/CSS: Add math expression support for transform-origin

--- Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
+++ Userland/Libraries/LibWeb/CSS/Parser/Parser.cpp
@@ -6929,6 +6929,9 @@ RefPtr<CSSStyleValue> Parser::parse_transform_origin_value(TokenStream<Component
                 return OptionalNone {};
             }
         }
+        if (value->is_math()) {
+            return AxisOffset { Axis::None, value->as_math() };
+        }
         return OptionalNone {};
     };
 
