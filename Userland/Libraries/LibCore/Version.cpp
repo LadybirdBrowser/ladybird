7cd69def086ced1f6b1a6af08be329f266eab2b4
Tim Ledbetter <tim.ledbetter@ladybird.org>
2024-10-24 11:17:32 +0100

    Meta+LibCore: Include git hash in version string

--- Userland/Libraries/LibCore/Version.cpp
+++ Userland/Libraries/LibCore/Version.cpp
@@ -1,17 +1,23 @@
 /*
  * Copyright (c) 2021, Mahmoud Mandour <ma.mandourr@gmail.com>
+ * Copyright (c) 2024, Tim Ledbetter <tim.ledbetter@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include <AK/String.h>
+#include <LibCore/GitHash.h>
 #include <LibCore/Version.h>
 
 namespace Core::Version {
 
 ErrorOr<String> read_long_version_string()
 {
+#if defined(GIT_HASH)
+    return MUST(String::formatted("Version 1.0-{}", GIT_HASH));
+#else
     return "Version 1.0"_string;
+#endif
 }
 
 }
