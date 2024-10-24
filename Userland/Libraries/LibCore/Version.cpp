fb6a8224faadde827dc7b7649017dd686263baba
Tim Ledbetter <tim.ledbetter@ladybird.org>
2024-10-24 11:14:20 +0100

    LibCore: Remove SerenityOS specific version number code

--- Userland/Libraries/LibCore/Version.cpp
+++ Userland/Libraries/LibCore/Version.cpp
@@ -7,27 +7,11 @@
 #include <AK/String.h>
 #include <LibCore/Version.h>
 
-#ifdef AK_OS_SERENITY
-#    include <sys/utsname.h>
-#endif
-
 namespace Core::Version {
 
 ErrorOr<String> read_long_version_string()
 {
-#ifdef AK_OS_SERENITY
-    struct utsname uts;
-    int rc = uname(&uts);
-    if ((rc) < 0) {
-        return Error::from_syscall("uname"sv, rc);
-    }
-    auto const* version = uts.release;
-    auto const* git_hash = uts.version;
-
-    return String::formatted("Version {} revision {}", version, git_hash);
-#else
     return "Version 1.0"_string;
-#endif
 }
 
 }
