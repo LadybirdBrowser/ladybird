4622249d75c0a9c02149a59805cda66c95b59b72
Shannon Booth <shannon@serenityos.org>
2024-10-26 21:35:21 +1300

    LibWeb: Introduce the 'ShadowRealmGlobalScope' interface
    This object represents the global object for a shadow realm. The IDL
    generator will need to be adjusted to the '[Global]' extended attribute
    and no '[Exposed]' field (the change in the test is not correct, as I
    understand it), but this should be enough to get us started on
    shadow realms.

---
+++ Userland/Libraries/LibWeb/HTML/ShadowRealmGlobalScope.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024, Shannon Booth <shannon@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/Bindings/ShadowRealmGlobalScopePrototype.h>
+#include <LibWeb/HTML/ShadowRealmGlobalScope.h>
+
+namespace Web::HTML {
+
+JS_DEFINE_ALLOCATOR(ShadowRealmGlobalScope);
+
+ShadowRealmGlobalScope::ShadowRealmGlobalScope(JS::Realm& realm)
+    : DOM::EventTarget(realm)
+{
+}
+
+ShadowRealmGlobalScope::~ShadowRealmGlobalScope() = default;
+
+JS::NonnullGCPtr<ShadowRealmGlobalScope> ShadowRealmGlobalScope::create(JS::Realm& realm)
+{
+    return realm.heap().allocate<ShadowRealmGlobalScope>(realm, realm);
+}
+
+void ShadowRealmGlobalScope::initialize(JS::Realm&)
+{
+    // FIXME: This does not work as we do not have any intrinsics in the [HostDefined] of a shadow realm. Figure out how this _should_ work.
+    // Base::initialize(realm);
+    // WEB_SET_PROTOTYPE_FOR_INTERFACE(ShadowRealmGlobalScope);
+}
+
+void ShadowRealmGlobalScope::visit_edges(Cell::Visitor& visitor)
+{
+    Base::visit_edges(visitor);
+}
+
+}
