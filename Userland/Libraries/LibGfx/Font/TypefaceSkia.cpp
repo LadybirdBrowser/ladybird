6e86ad65e90f3d3e8ab849b0e217574aff75bc8d
Jonne Ransijn <jonne@yyny.dev>
2024-10-26 15:45:34 +0200

    LibGfx: Use `FlyString` for family name
    This value gets converted to FlyString a lot, so let's just make it
    a FlyString in the first place!

--- Userland/Libraries/LibGfx/Font/TypefaceSkia.cpp
+++ Userland/Libraries/LibGfx/Font/TypefaceSkia.cpp
@@ -114,12 +114,12 @@ void TypefaceSkia::populate_glyph_page(GlyphPage& glyph_page, size_t page_index)
     }
 }
 
-String TypefaceSkia::family() const
+FlyString TypefaceSkia::family() const
 {
     if (!m_family.has_value()) {
         SkString family_name;
         impl().skia_typeface->getFamilyName(&family_name);
-        m_family = String::from_utf8_without_validation(ReadonlyBytes { family_name.c_str(), family_name.size() });
+        m_family = FlyString::from_utf8_without_validation(ReadonlyBytes { family_name.c_str(), family_name.size() });
     }
     return m_family.value();
 }
