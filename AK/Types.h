479b562c5ca724f29e5f0756aa61f8ed8a2b1825
stasoid <stasoid@yahoo.com>
2024-10-28 19:27:10 +0500

    AK: Define pid_t for Windows
    The actual type used by Windows for process id is DWORD (unsigned long).
    I define it as long to be able to write `pid_t m_pid { -1 };`
    without a cast like in Linux.
    This is fine because -1 (0xFFFFFFFF) is invalid pid on Windows too: https://devblogs.microsoft.com/oldnewthing/20040223-00/?p=40503
    "This comes from AllowSetForegroundWindow: The value ASFW_ANY
    has special meaning, which implies that it is never a valid process ID."

--- AK/Types.h
+++ AK/Types.h
@@ -144,6 +144,7 @@ using __ptrdiff_t = __PTRDIFF_TYPE__;
 #    if defined(AK_OS_WINDOWS)
 using ssize_t = AK::Detail::MakeSigned<size_t>;
 using mode_t = unsigned short;
+using pid_t = long;
 #    endif
 #endif
 
