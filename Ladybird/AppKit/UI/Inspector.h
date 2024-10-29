53d5c7084e8ddd29dc85314274d54c30fea9bbaf
Timothy Flynn <trflynn89@pm.me>
2024-10-29 11:05:25 -0400

    UI/AppKit: Extract bare minimum WebView functionality into its own class
    When a window containing a WebView becomes visibile, we have to inform
    WebContent. This was only implemented for the Tab class (not Inspector
    or Task Manaager).
    
    This patch adds LadybirdWebViewWindow to contain the bare minimum needed
    to render a LadybirdWebView. All windows containing a WebView inherit
    from this class, to ensure their WebContent processes know they became
    visible.

--- Ladybird/AppKit/UI/Inspector.h
+++ Ladybird/AppKit/UI/Inspector.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Tim Flynn <trflynn89@serenityos.org>
+ * Copyright (c) 2023-2024, Tim Flynn <trflynn89@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -7,11 +7,12 @@
 #pragma once
 
 #import <Cocoa/Cocoa.h>
+#import <Ladybird/AppKit/UI/LadybirdWebViewWindow.h>
 
 @class LadybirdWebView;
 @class Tab;
 
-@interface Inspector : NSWindow
+@interface Inspector : LadybirdWebViewWindow
 
 - (instancetype)init:(Tab*)tab;
 
@@ -19,7 +20,5 @@
 - (void)reset;
 
 - (void)selectHoveredElement;
-
-@property (nonatomic, strong) LadybirdWebView* web_view;
 
 @end
--- Ladybird/AppKit/UI/Tab.mm
+++ Ladybird/AppKit/UI/Tab.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024, Tim Flynn <trflynn89@serenityos.org>
+ * Copyright (c) 2023-2024, Tim Flynn <trflynn89@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -77,21 +77,11 @@ static constexpr CGFloat const WINDOW_HEIGHT = 800;
     auto screen_rect = [[NSScreen mainScreen] frame];
     auto position_x = (NSWidth(screen_rect) - WINDOW_WIDTH) / 2;
     auto position_y = (NSHeight(screen_rect) - WINDOW_HEIGHT) / 2;
-
     auto window_rect = NSMakeRect(position_x, position_y, WINDOW_WIDTH, WINDOW_HEIGHT);
-    auto style_mask = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable;
 
-    self = [super initWithContentRect:window_rect
-                            styleMask:style_mask
-                              backing:NSBackingStoreBuffered
-                                defer:NO];
-
-    if (self) {
+    if (self = [super initWithWebView:web_view windowRect:window_rect]) {
         // Remember last window position
         self.frameAutosaveName = @"window";
-
-        self.web_view = web_view;
-        [self.web_view setPostsBoundsChangedNotifications:YES];
 
         self.favicon = [Tab defaultFavicon];
         self.title = @"New Tab";
@@ -103,27 +93,13 @@ static constexpr CGFloat const WINDOW_HEIGHT = 800;
         self.search_panel = [[SearchPanel alloc] init];
         [self.search_panel setHidden:YES];
 
-        auto* scroll_view = [[NSScrollView alloc] init];
-        [scroll_view setHasVerticalScroller:NO];
-        [scroll_view setHasHorizontalScroller:NO];
-        [scroll_view setLineScroll:24];
-
-        [scroll_view setContentView:self.web_view];
-        [scroll_view setDocumentView:[[NSView alloc] init]];
-
         auto* stack_view = [NSStackView stackViewWithViews:@[
             self.search_panel,
-            scroll_view,
+            self.web_view.enclosingScrollView,
         ]];
 
         [stack_view setOrientation:NSUserInterfaceLayoutOrientationVertical];
         [stack_view setSpacing:0];
-
-        [[NSNotificationCenter defaultCenter]
-            addObserver:self
-               selector:@selector(onContentScroll:)
-                   name:NSViewBoundsDidChangeNotification
-                 object:[scroll_view contentView]];
 
         [self setContentView:stack_view];
 
@@ -279,11 +255,6 @@ static constexpr CGFloat const WINDOW_HEIGHT = 800;
     VERIFY_NOT_REACHED();
 }
 
-- (void)onContentScroll:(NSNotification*)notification
-{
-    [[self web_view] handleScroll];
-}
-
 #pragma mark - LadybirdWebViewObserver
 
 - (String const&)onCreateNewTab:(Optional<URL::URL> const&)url
@@ -420,20 +391,6 @@ static constexpr CGFloat const WINDOW_HEIGHT = 800;
 {
     [self.search_panel onFindInPageResult:current_match_index
                           totalMatchCount:total_match_count];
-}
-
-#pragma mark - NSWindow
-
-- (void)setIsVisible:(BOOL)flag
-{
-    [[self web_view] handleVisibility:flag];
-    [super setIsVisible:flag];
-}
-
-- (void)setIsMiniaturized:(BOOL)flag
-{
-    [[self web_view] handleVisibility:!flag];
-    [super setIsMiniaturized:flag];
 }
 
 @end
