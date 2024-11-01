42afee564604cbbfbda1ca28358494b5d3e6f86b
Shannon Booth <shannon@serenityos.org>
2024-11-02 02:57:24 +1300

    headless-browser: Support running XHTML tests

--- Ladybird/Headless/Test.cpp
+++ Ladybird/Headless/Test.cpp
@@ -70,7 +70,7 @@ static ErrorOr<void> collect_dump_tests(Vector<Test>& tests, StringView path, St
             continue;
         }
 
-        if (!name.ends_with(".html"sv) && !name.ends_with(".svg"sv))
+        if (!name.ends_with(".html"sv) && !name.ends_with(".svg"sv) && !name.ends_with(".xhtml"sv))
             continue;
 
         auto expectation_path = ByteString::formatted("{}/expected/{}/{}.txt", path, trail, LexicalPath::title(name));
