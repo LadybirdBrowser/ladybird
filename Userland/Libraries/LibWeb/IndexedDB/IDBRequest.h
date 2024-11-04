22df2dcefa549c2f2cced76ab1907b43f8e5698e
stelar7 <dudedbz@gmail.com>
2024-11-04 19:09:21 +0100

    LibWeb: Add getters and setters to IDBRequest

--- Userland/Libraries/LibWeb/IndexedDB/IDBRequest.h
+++ Userland/Libraries/LibWeb/IndexedDB/IDBRequest.h
@@ -18,6 +18,16 @@ class IDBRequest : public DOM::EventTarget {
 public:
     virtual ~IDBRequest() override;
 
+    [[nodiscard]] JS::Value result() const { return m_result; }
+    [[nodiscard]] JS::GCPtr<WebIDL::DOMException> error() const { return m_error; }
+    [[nodiscard]] bool done() const { return m_done; }
+    [[nodiscard]] bool processed() const { return m_processed; }
+
+    void set_done(bool done) { m_done = done; }
+    void set_result(JS::Value result) { m_result = result; }
+    void set_error(JS::GCPtr<WebIDL::DOMException> error) { m_error = error; }
+    void set_processed(bool processed) { m_processed = processed; }
+
     void set_onsuccess(WebIDL::CallbackType*);
     WebIDL::CallbackType* onsuccess();
     void set_onerror(WebIDL::CallbackType*);
@@ -27,6 +37,18 @@ protected:
     explicit IDBRequest(JS::Realm&);
 
     virtual void initialize(JS::Realm&) override;
+    virtual void visit_edges(Visitor& visitor) override;
+
+private:
+    // A request has a processed flag which is initially false.
+    bool m_processed { false };
+    // A request has a done flag which is initially false.
+    bool m_done { false };
+    // A request has a result and an error
+    JS::Value m_result;
+    JS::GCPtr<WebIDL::DOMException> m_error;
+    // FIXME: A request has a source object.
+    // FIXME: A request has a transaction which is initially null.
 };
 
 }
