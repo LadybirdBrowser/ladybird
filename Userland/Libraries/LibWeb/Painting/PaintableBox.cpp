7c63b1cc331cc685d9e96cafa0a906c6292c0f60
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2024-11-06 20:54:50 +0100

    LibWeb: Fix Utf8View substring construction in paint_cursor_if_needed()
    Before this change we were incorrectly passing
    `document.cursor_position()->offset()` as byte offset while this value
    represents code unit offset. This led to incorrect cursor position in
    non-latin text.

--- Userland/Libraries/LibWeb/Painting/PaintableBox.cpp
+++ Userland/Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -561,8 +561,9 @@ void paint_cursor_if_needed(PaintContext& context, TextPaintable const& paintabl
     auto fragment_rect = fragment.absolute_rect();
 
     auto text = fragment.string_view();
+    auto utf8_view = Utf8View(text).unicode_substring_view(0, document.cursor_position()->offset() - fragment.start());
     CSSPixelRect cursor_rect {
-        fragment_rect.x() + CSSPixels::nearest_value_for(paintable.layout_node().first_available_font().width(text.substring_view(0, document.cursor_position()->offset() - fragment.start()))),
+        fragment_rect.x() + CSSPixels::nearest_value_for(paintable.layout_node().first_available_font().width(utf8_view)),
         fragment_rect.top(),
         1,
         fragment_rect.height()
