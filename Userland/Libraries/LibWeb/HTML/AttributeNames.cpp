eccdb705566cb3de39dd58d7085fbf22f58f00cf
Timothy Flynn <trflynn89@pm.me>
2024-11-06 18:31:04 -0500

    LibWeb+WebContent: Update our list of boolean attributes for WebDriver
    Chromium has a larger list of boolean attributes, and WPT depends on it.
    We must also compare attribute names case-insensitively.
    
    Note this method is only used by WebDriver.

--- Userland/Libraries/LibWeb/HTML/AttributeNames.cpp
+++ Userland/Libraries/LibWeb/HTML/AttributeNames.cpp
@@ -41,33 +41,48 @@ void initialize_strings()
 // https://html.spec.whatwg.org/#boolean-attribute
 bool is_boolean_attribute(FlyString const& attribute)
 {
-    // NOTE: This is the list of attributes from https://html.spec.whatwg.org/#attributes-3
-    //       with a Value column value of "Boolean attribute".
-    return attribute.is_one_of(
-        AttributeNames::allowfullscreen,
-        AttributeNames::async,
-        AttributeNames::autofocus,
-        AttributeNames::autoplay,
-        AttributeNames::checked,
-        AttributeNames::controls,
-        AttributeNames::default_,
-        AttributeNames::defer,
-        AttributeNames::disabled,
-        AttributeNames::formnovalidate,
-        AttributeNames::inert,
-        AttributeNames::ismap,
-        AttributeNames::itemscope,
-        AttributeNames::loop,
-        AttributeNames::multiple,
-        AttributeNames::muted,
-        AttributeNames::nomodule,
-        AttributeNames::novalidate,
-        AttributeNames::open,
-        AttributeNames::playsinline,
-        AttributeNames::readonly,
-        AttributeNames::required,
-        AttributeNames::reversed,
-        AttributeNames::selected);
+    // NOTE: For web compatibility, this matches the list of attributes which Chromium considers to be booleans,
+    //       excluding attributes that are only used by Chromium itself:
+    //       https://source.chromium.org/chromium/chromium/src/+/460b7c003cf89fc9493e721701906f19e5f6a387:chrome/test/chromedriver/element_commands.cc;l=48-94
+    return attribute.equals_ignoring_ascii_case(AttributeNames::allowfullscreen)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::async)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::autofocus)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::autoplay)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::checked)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::compact)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::controls)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::declare)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::default_)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::defaultchecked)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::defaultselected)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::defer)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::disabled)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::ended)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::formnovalidate)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::hidden)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::indeterminate)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::iscontenteditable)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::ismap)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::itemscope)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::loop)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::multiple)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::muted)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::nohref)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::nomodule)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::noresize)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::noshade)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::novalidate)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::nowrap)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::open)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::paused)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::playsinline)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::readonly)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::required)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::reversed)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::seeking)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::selected)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::truespeed)
+        || attribute.equals_ignoring_ascii_case(AttributeNames::willvalidate);
 }
 
 }
