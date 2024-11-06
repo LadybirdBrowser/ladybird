321d460a8b2499f093b0d8368c93d849310224b5
Andreas Kling <andreas@ladybird.org>
2024-11-06 19:52:49 +0100

    LibWeb: Make :nth-* selectors match children of non-elements
    This was covered by WPT, which caught us not allowing :nth-child(1)
    to match the root HTML element, and other similar issues.

--- Userland/Libraries/LibWeb/CSS/SelectorEngine.cpp
+++ Userland/Libraries/LibWeb/CSS/SelectorEngine.cpp
@@ -517,7 +517,7 @@ static inline bool matches_pseudo_class(CSS::Selector::SimpleSelector::PseudoCla
         if (step_size == 0 && offset == 0)
             return false; // "If both a and b are equal to zero, the pseudo-class represents no element in the document tree."
 
-        auto const* parent = element.parent_element();
+        auto const* parent = element.parent();
         if (!parent)
             return false;
 
