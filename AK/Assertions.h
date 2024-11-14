0de15264ab869b05399902c01509490b83408da8
Jonne Ransijn <jonne@yyny.dev>
2024-11-14 23:12:59 +0100

    AK: Remove `clang-tidy` warnings for `ASSERT(a || b)` lines
    Lines like these were getting a warning to simplify the expanded
    boolean expression from `!(a || b)` to `(a && b)`, but since the
    `!(...)` is part of the macro, that is never going to happen.

--- AK/Assertions.h
+++ AK/Assertions.h
@@ -27,7 +27,7 @@ static constexpr bool TODO = false;
 extern "C" __attribute__((noreturn)) void ak_assertion_failed(char const*);
 #ifndef NDEBUG
 #    define ASSERT(expr)                                                               \
-        (__builtin_expect(!(expr), 0)                                                  \
+        (__builtin_expect(/* NOLINT(readability-simplify-boolean-expr) */ !(expr), 0)  \
                 ? ak_assertion_failed(#expr " at " __FILE__ ":" __stringify(__LINE__)) \
                 : (void)0)
 #    define ASSERT_NOT_REACHED ASSERT(false) /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
