0d072252dafe703266a8a0d244af26b0f5175c86
Timothy Flynn <trflynn89@pm.me>
2024-11-15 13:22:01 -0500

    LibCompress: Add a forwarding header
    Currently, just with the types needed for the w3c Compression spec.

---
+++ Libraries/LibCompress/Forward.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024, Tim Flynn <trflynn89@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+namespace Compress {
+
+class DeflateCompressor;
+class DeflateDecompressor;
+class GzipCompressor;
+class GzipDecompressor;
+class ZlibCompressor;
+class ZlibDecompressor;
+
+}
