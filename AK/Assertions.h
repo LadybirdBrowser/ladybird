d372484edba3fcfe8bf6edbcc4f1d7efbdc0f0ca
Jonne Ransijn <jonne@yyny.dev>
2024-11-15 16:48:56 +0100

    AK: Fix `ASSERT_NOT_REACHED` macro when `NDEBUG` is not defined
    This is supposed to be a function-like macro just like
    `VERIFY_NOT_REACHED()` and when `NDEBUG` is defined.

--- AK/Assertions.h
+++ AK/Assertions.h
@@ -27,7 +27,7 @@ extern "C" __attribute__((noreturn)) void ak_assertion_failed(char const*);
         (__builtin_expect(!(expr), 0)                                                  \
                 ? ak_assertion_failed(#expr " at " __FILE__ ":" __stringify(__LINE__)) \
                 : (void)0)
-#    define ASSERT_NOT_REACHED ASSERT(false) /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
+#    define ASSERT_NOT_REACHED() ASSERT(false) /* NOLINT(cert-dcl03-c,misc-static-assert) No, this can't be static_assert, it's a runtime check */
 #else
 #    define ASSERT(expr)
 #    define ASSERT_NOT_REACHED() __builtin_unreachable()
