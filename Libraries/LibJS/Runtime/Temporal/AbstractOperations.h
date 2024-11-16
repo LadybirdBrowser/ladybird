3616c523416674f3fac0c69fc457b74407696088
Timothy Flynn <trflynn89@pm.me>
2024-11-16 13:14:14 -0500

    LibJS: Update spec steps for the few remaining Temporal AOs

--- Libraries/LibJS/Runtime/Temporal/AbstractOperations.h
+++ Libraries/LibJS/Runtime/Temporal/AbstractOperations.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2021, Idan Horowitz <idan.horowitz@serenityos.org>
  * Copyright (c) 2021-2023, Linus Groh <linusg@serenityos.org>
+ * Copyright (c) 2024, Tim Flynn <trflynn89@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -15,14 +16,28 @@
 
 namespace JS::Temporal {
 
+// 13.39 ToIntegerIfIntegral ( argument ), https://tc39.es/proposal-temporal/#sec-tointegerifintegral
+template<typename... Args>
+ThrowCompletionOr<double> to_integer_if_integral(VM& vm, Value argument, ErrorType error_type, Args&&... args)
+{
+    // 1. Let number be ? ToNumber(argument).
+    auto number = TRY(argument.to_number(vm));
+
+    // 2. If number is not an integral Number, throw a RangeError exception.
+    if (!number.is_integral_number())
+        return vm.throw_completion<RangeError>(error_type, forward<Args>(args)...);
+
+    // 3. Return ℝ(number).
+    return number.as_double();
+}
+
 enum class OptionType {
     Boolean,
     String,
-    Number
 };
 
-struct GetOptionRequired { };
-using OptionDefault = Variant<GetOptionRequired, Empty, bool, StringView, double>;
+struct DefaultRequired { };
+using OptionDefault = Variant<DefaultRequired, Empty, bool, StringView, double>;
 
 ThrowCompletionOr<Object*> get_options_object(VM&, Value options);
 ThrowCompletionOr<Value> get_option(VM&, Object const& options, PropertyKey const& property, OptionType type, ReadonlySpan<StringView> values, OptionDefault const&);
@@ -31,25 +46,6 @@ template<size_t Size>
 ThrowCompletionOr<Value> get_option(VM& vm, Object const& options, PropertyKey const& property, OptionType type, StringView const (&values)[Size], OptionDefault const& default_)
 {
     return get_option(vm, options, property, type, ReadonlySpan<StringView> { values }, default_);
-}
-
-// 13.41 ToIntegerIfIntegral ( argument ), https://tc39.es/proposal-temporal/#sec-tointegerifintegral
-template<typename... Args>
-ThrowCompletionOr<double> to_integer_if_integral(VM& vm, Value argument, ErrorType error_type, Args... args)
-{
-    // 1. Let number be ? ToNumber(argument).
-    auto number = TRY(argument.to_number(vm));
-
-    // 2. If number is NaN, +0𝔽, or -0𝔽, return 0.
-    if (number.is_nan() || number.is_positive_zero() || number.is_negative_zero())
-        return 0;
-
-    // 3. If IsIntegralNumber(number) is false, throw a RangeError exception.
-    if (!number.is_integral_number())
-        return vm.template throw_completion<RangeError>(error_type, args...);
-
-    // 4. Return ℝ(number).
-    return number.as_double();
 }
 
 }
--- Libraries/LibJS/Runtime/Temporal/Duration.h
+++ Libraries/LibJS/Runtime/Temporal/Duration.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2021-2023, Linus Groh <linusg@serenityos.org>
  * Copyright (c) 2024, Shannon Booth <shannon@serenityos.org>
+ * Copyright (c) 2024, Tim Flynn <trflynn89@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -11,7 +12,6 @@
 
 namespace JS::Temporal {
 
-i8 duration_sign(double years, double months, double weeks, double days, double hours, double minutes, double seconds, double milliseconds, double microseconds, double nanoseconds);
 bool is_valid_duration(double years, double months, double weeks, double days, double hours, double minutes, double seconds, double milliseconds, double microseconds, double nanoseconds);
 
 }
