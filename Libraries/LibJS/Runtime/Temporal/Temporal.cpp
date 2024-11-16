d368fcadac3feb530a950ed127abce9b574e4730
Timothy Flynn <trflynn89@pm.me>
2024-11-16 13:15:20 -0500

    LibJS: Update spec link for Temporal [ %Symbol.toStringTag% ]

--- Libraries/LibJS/Runtime/Temporal/Temporal.cpp
+++ Libraries/LibJS/Runtime/Temporal/Temporal.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021-2023, Linus Groh <linusg@serenityos.org>
+ * Copyright (c) 2024, Tim Flynn <trflynn89@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -23,7 +24,7 @@ void Temporal::initialize(Realm& realm)
 
     auto& vm = this->vm();
 
-    // 1.1.1 Temporal [ @@toStringTag ], https://tc39.es/proposal-temporal/#sec-temporal-@@tostringtag
+    // 1.1.1 Temporal [ %Symbol.toStringTag% ], https://tc39.es/proposal-temporal/#sec-temporal-%symbol.tostringtag%
     define_direct_property(vm.well_known_symbol_to_string_tag(), PrimitiveString::create(vm, "Temporal"_string), Attribute::Configurable);
 }
 
