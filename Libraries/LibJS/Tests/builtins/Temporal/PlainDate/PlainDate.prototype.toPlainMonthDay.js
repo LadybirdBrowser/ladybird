ac9ed7f518c2e53fbd4e036430e544254363f055
Timothy Flynn <trflynn89@pm.me>
2024-11-22 15:17:11 -0500

    LibJS: Implement Temporal.PlainDate.prototype.toPlainMonthDay/YearMonth

---
+++ Libraries/LibJS/Tests/builtins/Temporal/PlainDate/PlainDate.prototype.toPlainMonthDay.js
@@ -0,0 +1,17 @@
+describe("correct behavior", () => {
+    test("length is 0", () => {
+        expect(Temporal.PlainDate.prototype.toPlainMonthDay).toHaveLength(0);
+    });
+
+    test("basic functionality", () => {
+        const plainDate = new Temporal.PlainDate(2021, 7, 6);
+        const plainMonthDay = plainDate.toPlainMonthDay();
+        expect(plainMonthDay.calendar).toBe(plainDate.calendar);
+        expect(plainMonthDay.monthCode).toBe("M07");
+        expect(plainMonthDay.day).toBe(6);
+
+        const fields = plainMonthDay.toString({ calendarName: "always" });
+        const year = fields.split("-")[0];
+        expect(year).toBe("1972");
+    });
+});
