1d554f97de341008ef60ed241169365538154f82
Andreas Kling <andreas@ladybird.org>
2024-11-23 16:28:35 +0100

    LibWeb: Reset the "stop parsing" flag when entering HTML parser
    Otherwise we'll always bail after processing one token, which is not
    what we want.

--- Libraries/LibWeb/HTML/Parser/HTMLParser.cpp
+++ Libraries/LibWeb/HTML/Parser/HTMLParser.cpp
@@ -188,6 +188,8 @@ void HTMLParser::visit_edges(Cell::Visitor& visitor)
 
 void HTMLParser::run(HTMLTokenizer::StopAtInsertionPoint stop_at_insertion_point)
 {
+    m_stop_parsing = false;
+
     for (;;) {
         // FIXME: Find a better way to say that we come from Document::close() and want to process EOF.
         if (!m_tokenizer.is_eof_inserted() && m_tokenizer.is_insertion_point_reached())
