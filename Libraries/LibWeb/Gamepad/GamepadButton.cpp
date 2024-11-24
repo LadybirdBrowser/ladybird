wetrynagetgames/26rickard

LibWeb: Implement the GamepadButton interface
Also add the Gamepad namespace

Userland/Libraries/LibWeb/Gamepad/GamepadButton.cpp
@@ -1,0 +1,34 @@
+/*
+ * Copyright (c) 2024, Undefine <undefine@undefine.pl>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/Bindings/GamepadButtonPrototype.h>
+#include <LibWeb/Bindings/Intrinsics.h>
+#include <LibWeb/Gamepad/GamepadButton.h>
+
+namespace Web::Gamepad {
+
+JS_DEFINE_ALLOCATOR(GamepadButton);
+
+WebIDL::ExceptionOr<JS::NonnullGCPtr<GamepadButton>> GamepadButton::create(JS::Realm& realm, bool pressed)
+{
+    return realm.heap().allocate<GamepadButton>(realm, realm, pressed);
+}
+
+GamepadButton::GamepadButton(JS::Realm& realm, bool pressed)
+    : Web::Bindings::PlatformObject(realm)
+    , m_pressed(pressed)
+{
+}
+
+GamepadButton::~GamepadButton() = default;
+
+void GamepadButton::initialize(JS::Realm& realm)
+{
+    Base::initialize(realm);
+    WEB_SET_PROTOTYPE_FOR_INTERFACE(GamepadButton);
+}
+
+}

Userland/Libraries/LibWeb/Gamepad/GamepadButton.h
@@ -1,0 +1,37 @@
+/*
+ * Copyright (c) 2024, Undefine <undefine@undefine.pl>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/String.h>
+#include <LibJS/Forward.h>
+#include <LibJS/Heap/GCPtr.h>
+#include <LibWeb/Forward.h>
+#include <LibWeb/WebIDL/ExceptionOr.h>
+
+namespace Web::Gamepad {
+
+class GamepadButton final : public Web::Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(GamepadButton, Web::Bindings::PlatformObject);
+    JS_DECLARE_ALLOCATOR(GamepadButton);
+
+public:
+    static WebIDL::ExceptionOr<JS::NonnullGCPtr<GamepadButton>> create(JS::Realm&, bool pressed);
+    virtual ~GamepadButton() override;
+
+    bool pressed() const { return m_pressed; }
+    bool touched() const { return m_pressed; }
+    double value() const { return m_pressed ? 1.0 : 0.0; }
+
+private:
+    GamepadButton(JS::Realm&, bool pressed);
+
+    virtual void initialize(JS::Realm&) override;
+
+    bool m_pressed;
+};
+
+}

LadybirdBrowser/ladybird

Libraries/LibWeb/Gamepad/GamepadButton.cpp
@@ -1,0 +1,34 @@
+/*
+ * Copyright (c) 2024, Undefine <undefine@undefine.pl>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/Bindings/GamepadButtonPrototype.h>
+#include <LibWeb/Bindings/Intrinsics.h>
+#include <LibWeb/Gamepad/GamepadButton.h>
+
+namespace Web::Gamepad {
+
+GC_DEFINE_ALLOCATOR(GamepadButton);
+
+WebIDL::ExceptionOr<GC::Ref<GamepadButton>> GamepadButton::create(JS::Realm& realm, bool pressed)
+{
+    return realm.create<GamepadButton>(realm, pressed);
+}
+
+GamepadButton::GamepadButton(JS::Realm& realm, bool pressed)
+    : Web::Bindings::PlatformObject(realm)
+    , m_pressed(pressed)
+{
+}
+
+GamepadButton::~GamepadButton() = default;
+
+void GamepadButton::initialize(JS::Realm& realm)
+{
+    Base::initialize(realm);
+    WEB_SET_PROTOTYPE_FOR_INTERFACE(GamepadButton);
+}
+
+}

Libraries/LibWeb/Gamepad/GamepadButton.h
@@ -1,0 +1,37 @@
+/*
+ * Copyright (c) 2024, Undefine <undefine@undefine.pl>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/String.h>
+#include <LibGC/Ptr.h>
+#include <LibJS/Forward.h>
+#include <LibWeb/Forward.h>
+#include <LibWeb/WebIDL/ExceptionOr.h>
+
+namespace Web::Gamepad {
+
+class GamepadButton final : public Web::Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(GamepadButton, Web::Bindings::PlatformObject);
+    GC_DECLARE_ALLOCATOR(GamepadButton);
+
+public:
+    static WebIDL::ExceptionOr<GC::Ref<GamepadButton>> create(JS::Realm&, bool pressed);
+    virtual ~GamepadButton() override;
+
+    bool pressed() const { return m_pressed; }
+    bool touched() const { return m_pressed; }
+    double value() const { return m_pressed ? 1.0 : 0.0; }
+
+private:
+    GamepadButton(JS::Realm&, bool pressed);
+
+    virtual void initialize(JS::Realm&) override;
+
+    bool m_pressed;
+};
+
+}