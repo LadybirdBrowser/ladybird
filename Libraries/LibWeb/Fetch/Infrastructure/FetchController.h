b263cd11f708492c2b3cbfe1552b97ad3a7a08e2
Glenn Skrzypczak <glenn.skrzypczak@gmail.com>
2024-11-24 20:37:13 +0100

    LibWeb/Fetch: Deserialize abort reason
    Deserialize the fetch controllers abort reason according to the spec.
    Currently fetch does not support a scenario where this actually happens.

--- Libraries/LibWeb/Fetch/Infrastructure/FetchController.h
+++ Libraries/LibWeb/Fetch/Infrastructure/FetchController.h
@@ -44,6 +44,7 @@ public:
     void process_next_manual_redirect() const;
     [[nodiscard]] GC::Ref<FetchTimingInfo> extract_full_timing_info() const;
     void abort(JS::Realm&, Optional<JS::Value>);
+    JS::Value deserialize_a_serialized_abort_reason(JS::Realm&);
     void terminate();
 
     void set_fetch_params(Badge<FetchParams>, GC::Ref<FetchParams> fetch_params) { m_fetch_params = fetch_params; }
@@ -77,7 +78,7 @@ private:
     // https://fetch.spec.whatwg.org/#fetch-controller-report-timing-steps
     // serialized abort reason (default null)
     //     Null or a Record (result of StructuredSerialize).
-    HTML::SerializationRecord m_serialized_abort_reason;
+    Optional<HTML::SerializationRecord> m_serialized_abort_reason;
 
     // https://fetch.spec.whatwg.org/#fetch-controller-next-manual-redirect-steps
     // next manual redirect steps (default null)
@@ -88,6 +89,24 @@ private:
 
     HashMap<u64, HTML::TaskID> m_ongoing_fetch_tasks;
     u64 m_next_fetch_task_id { 0 };
+};
+
+class FetchControllerHolder : public JS::Cell {
+    GC_CELL(FetchControllerHolder, JS::Cell);
+    GC_DECLARE_ALLOCATOR(FetchControllerHolder);
+
+public:
+    static GC::Ref<FetchControllerHolder> create(JS::VM&);
+
+    [[nodiscard]] GC::Ptr<FetchController> const& controller() const { return m_controller; }
+    void set_controller(GC::Ref<FetchController> controller) { m_controller = controller; }
+
+private:
+    FetchControllerHolder();
+
+    virtual void visit_edges(Cell::Visitor&) override;
+
+    GC::Ptr<FetchController> m_controller;
 };
 
 }
