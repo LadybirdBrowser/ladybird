3cf0ba1987c3c00fca1bb8498572b7f5e8fc8d9b
Andreas Kling <andreas@ladybird.org>
2024-11-24 20:48:46 +0100

    LibWeb: Implement the exportKey algorithm for Ed25519

--- Libraries/LibWeb/Crypto/CryptoAlgorithms.h
+++ Libraries/LibWeb/Crypto/CryptoAlgorithms.h
@@ -516,6 +516,7 @@ public:
 
     virtual WebIDL::ExceptionOr<Variant<GC::Ref<CryptoKey>, GC::Ref<CryptoKeyPair>>> generate_key(AlgorithmParams const&, bool, Vector<Bindings::KeyUsage> const&) override;
     virtual WebIDL::ExceptionOr<GC::Ref<CryptoKey>> import_key(AlgorithmParams const&, Bindings::KeyFormat, CryptoKey::InternalKeyData, bool, Vector<Bindings::KeyUsage> const&) override;
+    virtual WebIDL::ExceptionOr<GC::Ref<JS::Object>> export_key(Bindings::KeyFormat, GC::Ref<CryptoKey>) override;
 
     static NonnullOwnPtr<AlgorithmMethods> create(JS::Realm& realm) { return adopt_own(*new ED25519(realm)); }
 
