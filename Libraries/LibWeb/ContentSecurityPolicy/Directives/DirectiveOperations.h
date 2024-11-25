8cfdfc85cddc8ff765f7acac43b2964ddf24fd2c
Luke Wilde <luke@ladybird.org>
2024-11-25 17:50:37 +0000

    LibWeb: Enforce Content Security Policy on navigation request/response

--- Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveOperations.h
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveOperations.h
@@ -8,6 +8,7 @@
 
 #include <AK/StringView.h>
 #include <LibGC/Ptr.h>
+#include <LibWeb/ContentSecurityPolicy/Directives/Directive.h>
 #include <LibWeb/Forward.h>
 
 namespace Web::ContentSecurityPolicy::Directives {
@@ -20,5 +21,7 @@ enum class ShouldExecute {
 [[nodiscard]] Optional<FlyString> get_the_effective_directive_for_request(GC::Ref<Fetch::Infrastructure::Request const> request);
 [[nodiscard]] Vector<StringView> get_fetch_directive_fallback_list(Optional<FlyString> directive_name);
 [[nodiscard]] ShouldExecute should_fetch_directive_execute(Optional<FlyString> effective_directive_name, FlyString const& directive_name, GC::Ref<Policy const> policy);
+
+[[nodiscard]] FlyString get_the_effective_directive_for_inline_checks(Directive::InlineType type);
 
 }
