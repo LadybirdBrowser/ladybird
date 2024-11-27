15a1b3435d55f29f5e97e0188e5259fe8d3dd15f
Timothy Flynn <trflynn89@pm.me>
2024-11-27 16:44:45 -0500

    LibJS: Implement the ECMA-402 ZonedDateTime.prototype.toLocaleString.js

--- Libraries/LibJS/Tests/builtins/Temporal/ZonedDateTime/ZonedDateTime.prototype.toLocaleString.js
+++ Libraries/LibJS/Tests/builtins/Temporal/ZonedDateTime/ZonedDateTime.prototype.toLocaleString.js
@@ -4,9 +4,20 @@ describe("correct behavior", () => {
     });
 
     test("basic functionality", () => {
-        const plainDateTime = new Temporal.PlainDateTime(2021, 11, 3, 1, 33, 5, 100, 200, 300);
+        const plainDateTime = new Temporal.PlainDateTime(
+            2021,
+            11,
+            3,
+            1,
+            33,
+            5,
+            100,
+            200,
+            300,
+            "gregory"
+        );
         const zonedDateTime = plainDateTime.toZonedDateTime("UTC");
-        expect(zonedDateTime.toLocaleString()).toBe("2021-11-03T01:33:05.1002003+00:00[UTC]");
+        expect(zonedDateTime.toLocaleString()).toBe("11/3/2021, 1:33:5 AM UTC");
     });
 });
 
@@ -15,5 +26,28 @@ describe("errors", () => {
         expect(() => {
             Temporal.ZonedDateTime.prototype.toLocaleString.call("foo");
         }).toThrowWithMessage(TypeError, "Not an object of type Temporal.ZonedDateTime");
+    });
+
+    test("Temporal object must have same calendar", () => {
+        const plainDateTime = new Temporal.PlainDateTime(
+            2021,
+            11,
+            3,
+            1,
+            33,
+            5,
+            100,
+            200,
+            300,
+            "gregory"
+        );
+        const zonedDateTime = plainDateTime.toZonedDateTime("UTC");
+
+        expect(() => {
+            zonedDateTime.toLocaleString("en", { calendar: "iso8601" });
+        }).toThrowWithMessage(
+            RangeError,
+            "Cannot format Temporal.ZonedDateTime with calendar 'gregory' in locale with calendar 'iso8601'"
+        );
     });
 });
