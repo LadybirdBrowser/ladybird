959bb5cc181f70c4148976791cfb06b85639a77a
Luke Wilde <luke@ladybird.org>
2024-11-28 16:35:12 +0000

    LibWeb/CSP: Implement the connect-src directive

--- Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
@@ -5,13 +5,18 @@
  */
 
 #include <LibJS/Runtime/Realm.h>
+#include <LibWeb/ContentSecurityPolicy/Directives/ConnectSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/Directive.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.h>
+#include <LibWeb/ContentSecurityPolicy/Directives/Names.h>
 
 namespace Web::ContentSecurityPolicy::Directives {
 
 GC::Ref<Directive> create_directive(GC::Heap& heap, String name, Vector<String> value)
 {
+    if (name == Names::ConnectSrc)
+        return heap.allocate<ConnectSourceDirective>(move(name), move(value));
+
     return heap.allocate<Directive>(move(name), move(value));
 }
 
