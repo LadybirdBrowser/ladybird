f879978cfbf6107b0b571d86b3609219e88e74c8
Luke Wilde <luke@ladybird.org>
2024-11-28 16:35:12 +0000

    LibWeb/CSP: Implement the connect-src directive

--- Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
@@ -5,13 +5,18 @@
  */
 
 #include <LibJS/Runtime/Realm.h>
+#include <LibWeb/ContentSecurityPolicy/Directives/ConnectSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/Directive.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.h>
+#include <LibWeb/ContentSecurityPolicy/Directives/Names.h>
 
 namespace Web::ContentSecurityPolicy::Directives {
 
 GC::Ref<Directive> create_directive(JS::Realm& realm, String name, Vector<String> value)
 {
+    if (name == Names::ConnectSrc)
+        return realm.create<ConnectSourceDirective>(move(name), move(value));
+
     dbgln("Potential FIXME: Creating unknown Content Security Policy directive: {}", name);
     return realm.create<Directive>(move(name), move(value));
 }
