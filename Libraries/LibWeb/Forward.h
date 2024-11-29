2cbb1393a15248b37744ef64de90d39be35e0409
Luke Wilde <luke@ladybird.org>
2024-11-29 12:10:14 +0000

    LibWeb/CSP: Implement the object-src directive

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -115,6 +115,7 @@ class FrameSourceDirective;
 class ImageSourceDirective;
 class ManifestSourceDirective;
 class MediaSourceDirective;
+class ObjectSourceDirective;
 struct SerializedDirective;
 }
 
