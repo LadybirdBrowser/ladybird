6988f795797d533d5eca78fb41a81fe4b2602d0d
Luke Wilde <luke@ladybird.org>
2024-11-29 11:21:17 +0000

    LibWeb/CSP: Implement the img-src directive

--- Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
@@ -10,6 +10,7 @@
 #include <LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/FontSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/FrameSourceDirective.h>
+#include <LibWeb/ContentSecurityPolicy/Directives/ImageSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/Names.h>
 
 namespace Web::ContentSecurityPolicy::Directives {
@@ -24,6 +25,9 @@ GC::Ref<Directive> create_directive(JS::Realm& realm, String name, Vector<String
 
     if (name == Names::FrameSrc)
         return realm.create<FrameSourceDirective>(move(name), move(value));
+
+    if (name == Names::ImgSrc)
+        return realm.create<ImageSourceDirective>(move(name), move(value));
 
     dbgln("Potential FIXME: Creating unknown Content Security Policy directive: {}", name);
     return realm.create<Directive>(move(name), move(value));
