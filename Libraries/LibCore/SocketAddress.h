eb650798d056baae14de97953dd64760dd6b9b49
stasoid <stasoid@yahoo.com>
2024-11-29 22:13:25 +0500

    LibCore: Fix SocketAddress.h compilation errors on Windows

--- Libraries/LibCore/SocketAddress.h
+++ Libraries/LibCore/SocketAddress.h
@@ -9,11 +9,15 @@
 
 #include <AK/IPv4Address.h>
 #include <AK/IPv6Address.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/un.h>
+
+#ifndef AK_OS_WINDOWS
+#    include <arpa/inet.h>
+#    include <netinet/in.h>
+#    include <sys/socket.h>
+#    include <sys/un.h>
+#else
+#    include "SocketAddressWindows.h"
+#endif
 
 namespace Core {
 
