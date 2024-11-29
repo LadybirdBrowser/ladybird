5e95e569899e2486d7759be3ca2c58c1d715c21d
Luke Wilde <luke@ladybird.org>
2024-11-29 11:45:38 +0000

    LibWeb/CSP: Implement the manifest-src directive

--- Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
@@ -11,6 +11,7 @@
 #include <LibWeb/ContentSecurityPolicy/Directives/FontSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/FrameSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/ImageSourceDirective.h>
+#include <LibWeb/ContentSecurityPolicy/Directives/ManifestSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/Names.h>
 
 namespace Web::ContentSecurityPolicy::Directives {
@@ -28,6 +29,9 @@ GC::Ref<Directive> create_directive(GC::Heap& heap, String name, Vector<String> 
 
     if (name == Names::ImgSrc)
         return heap.allocate<ImageSourceDirective>(move(name), move(value));
+
+    if (name == Names::ManifestSrc)
+        return heap.allocate<ManifestSourceDirective>(move(name), move(value));
 
     return heap.allocate<Directive>(move(name), move(value));
 }
