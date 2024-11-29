06eff2fff660b1d34f5202f01508a8416fd204e3
Luke Wilde <luke@ladybird.org>
2024-11-29 12:04:15 +0000

    LibWeb/CSP: Implement the media-src directive

--- Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
@@ -12,6 +12,7 @@
 #include <LibWeb/ContentSecurityPolicy/Directives/FrameSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/ImageSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/ManifestSourceDirective.h>
+#include <LibWeb/ContentSecurityPolicy/Directives/MediaSourceDirective.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/Names.h>
 
 namespace Web::ContentSecurityPolicy::Directives {
@@ -32,6 +33,9 @@ GC::Ref<Directive> create_directive(GC::Heap& heap, String name, Vector<String> 
 
     if (name == Names::ManifestSrc)
         return heap.allocate<ManifestSourceDirective>(move(name), move(value));
+
+    if (name == Names::MediaSrc)
+        return heap.allocate<MediaSourceDirective>(move(name), move(value));
 
     return heap.allocate<Directive>(move(name), move(value));
 }
