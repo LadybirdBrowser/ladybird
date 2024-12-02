a0e62c09925730cb8f1f6ad3cbaf2224959450c8
Lucas CHOLLET <lucas.chollet@free.fr>
2024-12-02 18:55:43 -0500

    LibGfx+LibWeb: Do some color management on images with an ICC profile
    This patch introduces the `Gfx::ColorSpace` class, this is basically a
    serializable wrapper for skia's SkColorSpace. Creation of the instances
    of this class (and thus ICC profiles parsing) is performed in the
    ImageDecoder process. Then the object is serialized and sent through
    IPC, to finally be handed to skia for rendering.
    
    However, to make sure that we're not making all LibGfx's users dependent
    on Skia as well, we need to ensure the `Gfx::ColorSpace` object has no
    dependency on objects from Skia. To that end, the only member of the
    `ColorSpace` class is the opaque `ColorSpaceImpl` struct. Though, there
    is on issue with that design, the code in `DisplayListPlayer.cpp` needs
    access to the underlying `sk_sp<SkColorSpace>`. It is provided by a
    template function, that is only specialized for this type.
    
    Doing this work allows us to pass the following WPT tests:
    - https://wpt.live/css/css-color/tagged-images-001.html
    - https://wpt.live/css/css-color/tagged-images-003.html
    - https://wpt.live/css/css-color/tagged-images-004.html
    - https://wpt.live/css/css-color/untagged-images-001.html
    
    Other test cases can also be found here:
    - https://github.com/svgeesus/PNG-ICC-tests
    
    Note that SkColorSpace support quite a limited amount of color spaces,
    so color profiles like the ones in [1] or the v4 profiles in [2] are not
    supported yet. In fact, SkColorSpace only accepts skcms_ICCProfile with
    a linear conversion to XYZ D50.
    
    [1] https://www.color.org/browsertest.xalter
    [2] https://www.color.org/version4html.xalter

---
+++ Libraries/LibGfx/ColorSpace.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2024, Lucas Chollet <lucas.chollet@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Error.h>
+#include <AK/Noncopyable.h>
+#include <AK/NonnullOwnPtr.h>
+#include <LibIPC/Forward.h>
+
+namespace Gfx {
+
+namespace Details {
+
+struct ColorSpaceImpl;
+
+}
+
+class ColorSpace {
+public:
+    ColorSpace();
+    ColorSpace(ColorSpace const&);
+    ColorSpace(ColorSpace&&);
+    ColorSpace& operator=(ColorSpace const&);
+    ColorSpace& operator=(ColorSpace&&);
+    ~ColorSpace();
+
+    static ErrorOr<ColorSpace> load_from_icc_bytes(ReadonlyBytes);
+
+    // In order to keep this file free of Skia types, this function can't return
+    // a sk_sp<ColorSpace>. To work around that issue, we define a template here
+    // and only provide a specialization for sk_sp<SkColorSpace>.
+    template<typename T>
+    T& color_space();
+
+private:
+    template<typename T>
+    friend ErrorOr<void> IPC::encode(IPC::Encoder&, T const&);
+    template<typename T>
+    friend ErrorOr<T> IPC::decode(IPC::Decoder&);
+
+    explicit ColorSpace(NonnullOwnPtr<Details::ColorSpaceImpl>&& color_pace);
+
+    NonnullOwnPtr<Details::ColorSpaceImpl> m_color_space;
+};
+
+}
+
+namespace IPC {
+
+template<>
+ErrorOr<void> encode(Encoder&, Gfx::ColorSpace const&);
+
+template<>
+ErrorOr<Gfx::ColorSpace> decode(Decoder&);
+
+}
