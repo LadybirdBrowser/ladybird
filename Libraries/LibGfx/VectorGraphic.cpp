ece646eb086f7d4edef368d58fe4081ae0b54d81
Pavel Shliak <shlyakpavel@gmail.com>
2024-12-03 00:24:34 +0400

    LibGfx: Let Painter care about TinyVG transforms
    This helps us with non-uniform scales, and makes things simple

--- Libraries/LibGfx/VectorGraphic.cpp
+++ Libraries/LibGfx/VectorGraphic.cpp
@@ -9,26 +9,25 @@
 
 namespace Gfx {
 
-void VectorGraphic::draw_into(Painter& painter, IntRect const& dest, AffineTransform transform) const
+ErrorOr<NonnullRefPtr<Gfx::Bitmap>> VectorGraphic::bitmap(IntSize size, AffineTransform transform) const
 {
+    auto bitmap = TRY(Bitmap::create(Gfx::BitmapFormat::BGRA8888, size));
+    auto painter = PainterSkia::create(bitmap);
+
     // Apply the transform then center within destination rectangle (this ignores any translation from the transform):
     // This allows you to easily rotate or flip the image before painting.
-    auto transformed_rect = transform.map(FloatRect { {}, size() });
-    auto scale = min(float(dest.width()) / transformed_rect.width(), float(dest.height()) / transformed_rect.height());
-    auto centered = FloatRect { {}, transformed_rect.size().scaled(scale) }.centered_within(dest.to_type<float>());
+    auto transformed_rect = transform.map(FloatRect { {}, this->size() });
+    auto scale = min(float(size.width()) / transformed_rect.width(), float(size.height()) / transformed_rect.height());
+    auto centered = FloatRect { {}, transformed_rect.size().scaled(scale) }.centered_within(IntRect { {}, size }.to_type<float>());
     auto view_transform = AffineTransform {}
                               .translate(centered.location())
                               .multiply(AffineTransform {}.scale(scale, scale))
                               .multiply(AffineTransform {}.translate(-transformed_rect.location()))
                               .multiply(transform);
-    draw_transformed(painter, view_transform);
-}
 
-ErrorOr<NonnullRefPtr<Gfx::Bitmap>> VectorGraphic::bitmap(IntSize size, AffineTransform transform) const
-{
-    auto bitmap = TRY(Bitmap::create(Gfx::BitmapFormat::BGRA8888, size));
-    auto painter = PainterSkia::create(bitmap);
-    draw_into(*painter, IntRect { {}, size }, transform);
+    painter->set_transform(view_transform);
+    draw(*painter);
+
     return bitmap;
 }
 
