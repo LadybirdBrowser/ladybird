0cff47828d4b7c3cf53d1bb6f9ecb15b15c5e188
Luke Wilde <luke@ladybird.org>
2024-12-02 16:01:19 +0000

    LibWeb/CSP: Implement the script-src directive

--- Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveOperations.h
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveOperations.h
@@ -35,5 +35,11 @@ MatchResult does_url_match_source_list_in_origin_with_redirect_count(URL::URL co
 
 MatchResult does_request_match_source_list(GC::Ref<Fetch::Infrastructure::Request const> request, Vector<String> const& source_list, GC::Ref<Policy const> policy);
 MatchResult does_response_match_source_list(GC::Ref<Fetch::Infrastructure::Response const> response, GC::Ref<Fetch::Infrastructure::Request const> request, Vector<String> const& source_list, GC::Ref<Policy const> policy);
+MatchResult does_nonce_match_source_list(String const& nonce, Vector<String> const& source_list);
+
+Directive::Result script_directives_pre_request_check(GC::Ref<Fetch::Infrastructure::Request const> request, GC::Ref<Directive const> directive, GC::Ref<Policy const> policy);
+Directive::Result script_directives_post_request_check(GC::Ref<Fetch::Infrastructure::Request const> request, GC::Ref<Fetch::Infrastructure::Response const> response, GC::Ref<Directive const> directive, GC::Ref<Policy const> policy);
+
+MatchResult does_element_match_source_list_for_type_and_source(GC::Ptr<DOM::Element const> element, Vector<String> const& source_list, Directive::InlineType type, String const& source);
 
 }
