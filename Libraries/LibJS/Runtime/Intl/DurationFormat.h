5cfd50224c4217bc0a04d35202c151f27b335909
Timothy Flynn <trflynn89@pm.me>
2024-12-03 10:34:33 -0500

    LibJS: Use enums instead of strings in Intl.DurationFormat AOs

--- Libraries/LibJS/Runtime/Intl/DurationFormat.h
+++ Libraries/LibJS/Runtime/Intl/DurationFormat.h
@@ -24,8 +24,10 @@ public:
         Long,
         Short,
         Narrow,
-        Digital
+        Digital,
     };
+    static Style style_from_string(StringView style);
+    static StringView style_to_string(Style);
 
     enum class ValueStyle {
         Long,
@@ -35,13 +37,31 @@ public:
         TwoDigit,
         Fractional,
     };
+    static ValueStyle value_style_from_string(StringView);
+    static StringView value_style_to_string(ValueStyle);
+
     static_assert(to_underlying(ValueStyle::Long) == to_underlying(Unicode::Style::Long));
     static_assert(to_underlying(ValueStyle::Short) == to_underlying(Unicode::Style::Short));
     static_assert(to_underlying(ValueStyle::Narrow) == to_underlying(Unicode::Style::Narrow));
 
     enum class Display {
         Auto,
-        Always
+        Always,
+    };
+    static Display display_from_string(StringView display);
+    static StringView display_to_string(Display);
+
+    enum class Unit {
+        Years,
+        Months,
+        Weeks,
+        Days,
+        Hours,
+        Minutes,
+        Seconds,
+        Milliseconds,
+        Microseconds,
+        Nanoseconds,
     };
 
     static constexpr auto relevant_extension_keys()
@@ -69,83 +89,83 @@ public:
     Style style() const { return m_style; }
     StringView style_string() const { return style_to_string(m_style); }
 
-    void set_years_style(StringView years_style) { m_years_style = date_style_from_string(years_style); }
+    void set_years_style(ValueStyle years_style) { m_years_style = years_style; }
     ValueStyle years_style() const { return m_years_style; }
     StringView years_style_string() const { return value_style_to_string(m_years_style); }
 
-    void set_years_display(StringView years_display) { m_years_display = display_from_string(years_display); }
+    void set_years_display(Display years_display) { m_years_display = years_display; }
     Display years_display() const { return m_years_display; }
     StringView years_display_string() const { return display_to_string(m_years_display); }
 
-    void set_months_style(StringView months_style) { m_months_style = date_style_from_string(months_style); }
+    void set_months_style(ValueStyle months_style) { m_months_style = months_style; }
     ValueStyle months_style() const { return m_months_style; }
     StringView months_style_string() const { return value_style_to_string(m_months_style); }
 
-    void set_months_display(StringView months_display) { m_months_display = display_from_string(months_display); }
+    void set_months_display(Display months_display) { m_months_display = months_display; }
     Display months_display() const { return m_months_display; }
     StringView months_display_string() const { return display_to_string(m_months_display); }
 
-    void set_weeks_style(StringView weeks_style) { m_weeks_style = date_style_from_string(weeks_style); }
+    void set_weeks_style(ValueStyle weeks_style) { m_weeks_style = weeks_style; }
     ValueStyle weeks_style() const { return m_weeks_style; }
     StringView weeks_style_string() const { return value_style_to_string(m_weeks_style); }
 
-    void set_weeks_display(StringView weeks_display) { m_weeks_display = display_from_string(weeks_display); }
+    void set_weeks_display(Display weeks_display) { m_weeks_display = weeks_display; }
     Display weeks_display() const { return m_weeks_display; }
     StringView weeks_display_string() const { return display_to_string(m_weeks_display); }
 
-    void set_days_style(StringView days_style) { m_days_style = date_style_from_string(days_style); }
+    void set_days_style(ValueStyle days_style) { m_days_style = days_style; }
     ValueStyle days_style() const { return m_days_style; }
     StringView days_style_string() const { return value_style_to_string(m_days_style); }
 
-    void set_days_display(StringView days_display) { m_days_display = display_from_string(days_display); }
+    void set_days_display(Display days_display) { m_days_display = days_display; }
     Display days_display() const { return m_days_display; }
     StringView days_display_string() const { return display_to_string(m_days_display); }
 
-    void set_hours_style(StringView hours_style) { m_hours_style = time_style_from_string(hours_style); }
+    void set_hours_style(ValueStyle hours_style) { m_hours_style = hours_style; }
     ValueStyle hours_style() const { return m_hours_style; }
     StringView hours_style_string() const { return value_style_to_string(m_hours_style); }
 
-    void set_hours_display(StringView hours_display) { m_hours_display = display_from_string(hours_display); }
+    void set_hours_display(Display hours_display) { m_hours_display = hours_display; }
     Display hours_display() const { return m_hours_display; }
     StringView hours_display_string() const { return display_to_string(m_hours_display); }
 
-    void set_minutes_style(StringView minutes_style) { m_minutes_style = time_style_from_string(minutes_style); }
+    void set_minutes_style(ValueStyle minutes_style) { m_minutes_style = minutes_style; }
     ValueStyle minutes_style() const { return m_minutes_style; }
     StringView minutes_style_string() const { return value_style_to_string(m_minutes_style); }
 
-    void set_minutes_display(StringView minutes_display) { m_minutes_display = display_from_string(minutes_display); }
+    void set_minutes_display(Display minutes_display) { m_minutes_display = minutes_display; }
     Display minutes_display() const { return m_minutes_display; }
     StringView minutes_display_string() const { return display_to_string(m_minutes_display); }
 
-    void set_seconds_style(StringView seconds_style) { m_seconds_style = time_style_from_string(seconds_style); }
+    void set_seconds_style(ValueStyle seconds_style) { m_seconds_style = seconds_style; }
     ValueStyle seconds_style() const { return m_seconds_style; }
     StringView seconds_style_string() const { return value_style_to_string(m_seconds_style); }
 
-    void set_seconds_display(StringView seconds_display) { m_seconds_display = display_from_string(seconds_display); }
+    void set_seconds_display(Display seconds_display) { m_seconds_display = seconds_display; }
     Display seconds_display() const { return m_seconds_display; }
     StringView seconds_display_string() const { return display_to_string(m_seconds_display); }
 
-    void set_milliseconds_style(StringView milliseconds_style) { m_milliseconds_style = sub_second_style_from_string(milliseconds_style); }
+    void set_milliseconds_style(ValueStyle milliseconds_style) { m_milliseconds_style = milliseconds_style; }
     ValueStyle milliseconds_style() const { return m_milliseconds_style; }
     StringView milliseconds_style_string() const { return value_style_to_string(m_milliseconds_style); }
 
-    void set_milliseconds_display(StringView milliseconds_display) { m_milliseconds_display = display_from_string(milliseconds_display); }
+    void set_milliseconds_display(Display milliseconds_display) { m_milliseconds_display = milliseconds_display; }
     Display milliseconds_display() const { return m_milliseconds_display; }
     StringView milliseconds_display_string() const { return display_to_string(m_milliseconds_display); }
 
-    void set_microseconds_style(StringView microseconds_style) { m_microseconds_style = sub_second_style_from_string(microseconds_style); }
+    void set_microseconds_style(ValueStyle microseconds_style) { m_microseconds_style = microseconds_style; }
     ValueStyle microseconds_style() const { return m_microseconds_style; }
     StringView microseconds_style_string() const { return value_style_to_string(m_microseconds_style); }
 
-    void set_microseconds_display(StringView microseconds_display) { m_microseconds_display = display_from_string(microseconds_display); }
+    void set_microseconds_display(Display microseconds_display) { m_microseconds_display = microseconds_display; }
     Display microseconds_display() const { return m_microseconds_display; }
     StringView microseconds_display_string() const { return display_to_string(m_microseconds_display); }
 
-    void set_nanoseconds_style(StringView nanoseconds_style) { m_nanoseconds_style = sub_second_style_from_string(nanoseconds_style); }
+    void set_nanoseconds_style(ValueStyle nanoseconds_style) { m_nanoseconds_style = nanoseconds_style; }
     ValueStyle nanoseconds_style() const { return m_nanoseconds_style; }
     StringView nanoseconds_style_string() const { return value_style_to_string(m_nanoseconds_style); }
 
-    void set_nanoseconds_display(StringView nanoseconds_display) { m_nanoseconds_display = display_from_string(nanoseconds_display); }
+    void set_nanoseconds_display(Display nanoseconds_display) { m_nanoseconds_display = nanoseconds_display; }
     Display nanoseconds_display() const { return m_nanoseconds_display; }
     StringView nanoseconds_display_string() const { return display_to_string(m_nanoseconds_display); }
 
@@ -155,15 +175,6 @@ public:
 
 private:
     explicit DurationFormat(Object& prototype);
-
-    static Style style_from_string(StringView style);
-    static StringView style_to_string(Style);
-    static ValueStyle date_style_from_string(StringView date_style);
-    static ValueStyle time_style_from_string(StringView time_style);
-    static ValueStyle sub_second_style_from_string(StringView sub_second_style);
-    static StringView value_style_to_string(ValueStyle);
-    static Display display_from_string(StringView display);
-    static StringView display_to_string(Display);
 
     String m_locale;                  // [[Locale]]
     String m_numbering_system;        // [[NumberingSystem]]
@@ -211,13 +222,12 @@ struct DurationRecord {
 struct DurationInstanceComponent {
     double DurationRecord::*value_slot;
     DurationFormat::ValueStyle (DurationFormat::*get_style_slot)() const;
-    void (DurationFormat::*set_style_slot)(StringView);
+    void (DurationFormat::*set_style_slot)(DurationFormat::ValueStyle);
     DurationFormat::Display (DurationFormat::*get_display_slot)() const;
-    void (DurationFormat::*set_display_slot)(StringView);
-    StringView unit;
-    StringView number_format_unit;
+    void (DurationFormat::*set_display_slot)(DurationFormat::Display);
+    DurationFormat::Unit unit;
     ReadonlySpan<StringView> values;
-    StringView digital_default;
+    DurationFormat::ValueStyle digital_default;
 };
 
 // Table 2: DurationFormat instance internal slots and properties relevant to PartitionDurationFormatPattern, https://tc39.es/proposal-intl-duration-format/#table-partition-duration-format-pattern
@@ -225,22 +235,23 @@ struct DurationInstanceComponent {
 static constexpr auto date_values = AK::Array { "long"sv, "short"sv, "narrow"sv };
 static constexpr auto time_values = AK::Array { "long"sv, "short"sv, "narrow"sv, "numeric"sv, "2-digit"sv };
 static constexpr auto sub_second_values = AK::Array { "long"sv, "short"sv, "narrow"sv, "numeric"sv };
+
 static constexpr auto duration_instances_components = to_array<DurationInstanceComponent>({
-    { &DurationRecord::years, &DurationFormat::years_style, &DurationFormat::set_years_style, &DurationFormat::years_display, &DurationFormat::set_years_display, "years"sv, "year"sv, date_values, "short"sv },
-    { &DurationRecord::months, &DurationFormat::months_style, &DurationFormat::set_months_style, &DurationFormat::months_display, &DurationFormat::set_months_display, "months"sv, "month"sv, date_values, "short"sv },
-    { &DurationRecord::weeks, &DurationFormat::weeks_style, &DurationFormat::set_weeks_style, &DurationFormat::weeks_display, &DurationFormat::set_weeks_display, "weeks"sv, "week"sv, date_values, "short"sv },
-    { &DurationRecord::days, &DurationFormat::days_style, &DurationFormat::set_days_style, &DurationFormat::days_display, &DurationFormat::set_days_display, "days"sv, "day"sv, date_values, "short"sv },
-    { &DurationRecord::hours, &DurationFormat::hours_style, &DurationFormat::set_hours_style, &DurationFormat::hours_display, &DurationFormat::set_hours_display, "hours"sv, "hour"sv, time_values, "numeric"sv },
-    { &DurationRecord::minutes, &DurationFormat::minutes_style, &DurationFormat::set_minutes_style, &DurationFormat::minutes_display, &DurationFormat::set_minutes_display, "minutes"sv, "minute"sv, time_values, "numeric"sv },
-    { &DurationRecord::seconds, &DurationFormat::seconds_style, &DurationFormat::set_seconds_style, &DurationFormat::seconds_display, &DurationFormat::set_seconds_display, "seconds"sv, "second"sv, time_values, "numeric"sv },
-    { &DurationRecord::milliseconds, &DurationFormat::milliseconds_style, &DurationFormat::set_milliseconds_style, &DurationFormat::milliseconds_display, &DurationFormat::set_milliseconds_display, "milliseconds"sv, "millisecond"sv, sub_second_values, "numeric"sv },
-    { &DurationRecord::microseconds, &DurationFormat::microseconds_style, &DurationFormat::set_microseconds_style, &DurationFormat::microseconds_display, &DurationFormat::set_microseconds_display, "microseconds"sv, "microsecond"sv, sub_second_values, "numeric"sv },
-    { &DurationRecord::nanoseconds, &DurationFormat::nanoseconds_style, &DurationFormat::set_nanoseconds_style, &DurationFormat::nanoseconds_display, &DurationFormat::set_nanoseconds_display, "nanoseconds"sv, "nanosecond"sv, sub_second_values, "numeric"sv },
+    { &DurationRecord::years, &DurationFormat::years_style, &DurationFormat::set_years_style, &DurationFormat::years_display, &DurationFormat::set_years_display, DurationFormat::Unit::Years, date_values, DurationFormat::ValueStyle::Short },
+    { &DurationRecord::months, &DurationFormat::months_style, &DurationFormat::set_months_style, &DurationFormat::months_display, &DurationFormat::set_months_display, DurationFormat::Unit::Months, date_values, DurationFormat::ValueStyle::Short },
+    { &DurationRecord::weeks, &DurationFormat::weeks_style, &DurationFormat::set_weeks_style, &DurationFormat::weeks_display, &DurationFormat::set_weeks_display, DurationFormat::Unit::Weeks, date_values, DurationFormat::ValueStyle::Short },
+    { &DurationRecord::days, &DurationFormat::days_style, &DurationFormat::set_days_style, &DurationFormat::days_display, &DurationFormat::set_days_display, DurationFormat::Unit::Days, date_values, DurationFormat::ValueStyle::Short },
+    { &DurationRecord::hours, &DurationFormat::hours_style, &DurationFormat::set_hours_style, &DurationFormat::hours_display, &DurationFormat::set_hours_display, DurationFormat::Unit::Hours, time_values, DurationFormat::ValueStyle::Numeric },
+    { &DurationRecord::minutes, &DurationFormat::minutes_style, &DurationFormat::set_minutes_style, &DurationFormat::minutes_display, &DurationFormat::set_minutes_display, DurationFormat::Unit::Minutes, time_values, DurationFormat::ValueStyle::Numeric },
+    { &DurationRecord::seconds, &DurationFormat::seconds_style, &DurationFormat::set_seconds_style, &DurationFormat::seconds_display, &DurationFormat::set_seconds_display, DurationFormat::Unit::Seconds, time_values, DurationFormat::ValueStyle::Numeric },
+    { &DurationRecord::milliseconds, &DurationFormat::milliseconds_style, &DurationFormat::set_milliseconds_style, &DurationFormat::milliseconds_display, &DurationFormat::set_milliseconds_display, DurationFormat::Unit::Milliseconds, sub_second_values, DurationFormat::ValueStyle::Numeric },
+    { &DurationRecord::microseconds, &DurationFormat::microseconds_style, &DurationFormat::set_microseconds_style, &DurationFormat::microseconds_display, &DurationFormat::set_microseconds_display, DurationFormat::Unit::Microseconds, sub_second_values, DurationFormat::ValueStyle::Numeric },
+    { &DurationRecord::nanoseconds, &DurationFormat::nanoseconds_style, &DurationFormat::set_nanoseconds_style, &DurationFormat::nanoseconds_display, &DurationFormat::set_nanoseconds_display, DurationFormat::Unit::Nanoseconds, sub_second_values, DurationFormat::ValueStyle::Numeric },
 });
 
 struct DurationUnitOptions {
-    String style;
-    String display;
+    DurationFormat::ValueStyle style;
+    DurationFormat::Display display;
 };
 
 struct DurationFormatPart {
@@ -251,13 +262,13 @@ struct DurationFormatPart {
 
 ThrowCompletionOr<DurationRecord> to_duration_record(VM&, Value input);
 i8 duration_sign(DurationRecord const&);
-ThrowCompletionOr<DurationUnitOptions> get_duration_unit_options(VM&, String const& unit, Object const& options, StringView base_style, ReadonlySpan<StringView> styles_list, StringView digital_base, StringView previous_style, bool two_digit_hours);
+ThrowCompletionOr<DurationUnitOptions> get_duration_unit_options(VM&, DurationFormat::Unit unit, Object const& options, DurationFormat::Style base_style, ReadonlySpan<StringView> styles_list, DurationFormat::ValueStyle digital_base, Optional<DurationFormat::ValueStyle> previous_style, bool two_digit_hours);
 double compute_fractional_digits(DurationFormat const&, DurationRecord const&);
-bool next_unit_fractional(DurationFormat const&, StringView unit);
+bool next_unit_fractional(DurationFormat const&, DurationFormat::Unit unit);
 Vector<DurationFormatPart> format_numeric_hours(VM&, DurationFormat const&, double hours_value, bool sign_displayed);
 Vector<DurationFormatPart> format_numeric_minutes(VM&, DurationFormat const&, double minutes_value, bool hours_displayed, bool sign_displayed);
 Vector<DurationFormatPart> format_numeric_seconds(VM&, DurationFormat const&, double seconds_value, bool minutes_displayed, bool sign_displayed);
-Vector<DurationFormatPart> format_numeric_units(VM&, DurationFormat const&, DurationRecord const&, StringView first_numeric_unit, bool sign_displayed);
+Vector<DurationFormatPart> format_numeric_units(VM&, DurationFormat const&, DurationRecord const&, DurationFormat::Unit first_numeric_unit, bool sign_displayed);
 Vector<DurationFormatPart> list_format_parts(VM&, DurationFormat const&, Vector<Vector<DurationFormatPart>>& partitioned_parts_list);
 Vector<DurationFormatPart> partition_duration_format_pattern(VM&, DurationFormat const&, DurationRecord const&);
 
