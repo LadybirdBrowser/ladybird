f382bccc3dc406891425672b571fffb3b6d148b1
Luke Wilde <luke@ladybird.org>
2024-12-03 15:24:07 +0000

    LibWeb/CSP: Implement the script-src-elem directive

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -141,6 +141,7 @@ class ManifestSourceDirective;
 class MediaSourceDirective;
 class ObjectSourceDirective;
 class ScriptSourceDirective;
+class ScriptSourceElementDirective;
 struct SerializedDirective;
 
 }
