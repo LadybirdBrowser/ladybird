798123023647459e86442c8f76139d33a4d11bcb
Luke Wilde <luke@ladybird.org>
2024-12-04 12:36:18 +0000

    LibWeb/CSP: Implement the frame-ancestors directive

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -108,6 +108,7 @@ class DefaultSourceDirective;
 class Directive;
 class FontSourceDirective;
 class FormActionDirective;
+class FrameAncestorsDirective;
 class FrameSourceDirective;
 class ImageSourceDirective;
 class ManifestSourceDirective;
