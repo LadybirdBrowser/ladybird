bc62bf09e1cb1ff8ba60259a64590a5bdf1254dd
Luke Warlow <luke@warlow.dev>
2024-12-06 23:10:03 +0000

    LibWeb: Implement popover beforetoggle and toggle events

--- Libraries/LibWeb/HTML/HTMLElement.h
+++ Libraries/LibWeb/HTML/HTMLElement.h
@@ -6,10 +6,12 @@
 
 #pragma once
 
+#include <AK/Optional.h>
 #include <LibWeb/DOM/Element.h>
 #include <LibWeb/HTML/EventNames.h>
 #include <LibWeb/HTML/GlobalEventHandlers.h>
 #include <LibWeb/HTML/HTMLOrSVGElement.h>
+#include <LibWeb/HTML/ToggleTaskTracker.h>
 #include <LibWeb/HTML/TokenizedFeatures.h>
 
 namespace Web::HTML {
@@ -128,8 +130,6 @@ public:
     WebIDL::ExceptionOr<void> show_popover(ThrowExceptions throw_exceptions, GC::Ptr<HTMLElement> invoker);
     WebIDL::ExceptionOr<void> hide_popover(FocusPreviousElement focus_previous_element, FireEvents fire_events, ThrowExceptions throw_exceptions);
 
-    WebIDL::ExceptionOr<bool> check_popover_validity(ExpectedToBeShowing expected_to_be_showing, ThrowExceptions throw_exceptions, GC::Ptr<DOM::Document>);
-
 protected:
     HTMLElement(DOM::Document&, DOM::QualifiedName);
 
@@ -156,6 +156,10 @@ private:
 
     GC::Ptr<DOM::NodeList> m_labels;
 
+    WebIDL::ExceptionOr<bool> check_popover_validity(ExpectedToBeShowing expected_to_be_showing, ThrowExceptions throw_exceptions, GC::Ptr<DOM::Document>);
+
+    void queue_a_popover_toggle_event_task(String old_state, String new_state);
+
     // https://html.spec.whatwg.org/multipage/custom-elements.html#attached-internals
     GC::Ptr<ElementInternals> m_attached_internals;
 
@@ -175,6 +179,9 @@ private:
 
     // https://html.spec.whatwg.org/multipage/popover.html#popover-showing-or-hiding
     bool m_popover_showing_or_hiding { false };
+
+    // https://html.spec.whatwg.org/multipage/popover.html#the-popover-attribute:toggle-task-tracker
+    Optional<ToggleTaskTracker> m_popover_toggle_task_tracker;
 };
 
 }
