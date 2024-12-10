495006ddb58de490a685f8ce2e3312c91c8316ef
Jelle Raaijmakers <jelle@ladybird.org>
2024-12-10 16:16:56 +0100

    LibWeb: Implement document.execCommand('insertLinebreak')

--- Libraries/LibWeb/Editing/Commands.h
+++ Libraries/LibWeb/Editing/Commands.h
@@ -24,6 +24,7 @@ Optional<CommandDefinition const&> find_command_definition(FlyString const&);
 bool command_default_paragraph_separator_action(DOM::Document&, String const&);
 String command_default_paragraph_separator_value(DOM::Document const&);
 bool command_delete_action(DOM::Document&, String const&);
+bool command_insert_linebreak_action(DOM::Document&, String const&);
 bool command_insert_paragraph_action(DOM::Document&, String const&);
 bool command_style_with_css_action(DOM::Document&, String const&);
 bool command_style_with_css_state(DOM::Document const&);
