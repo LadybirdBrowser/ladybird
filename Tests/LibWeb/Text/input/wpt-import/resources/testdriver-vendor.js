1bd10a5443982d5cad1da1183b988f4a18c2da81
Tim Ledbetter <tim.ledbetter@ladybird.org>
2024-12-10 16:41:37 +0000

    Tests: Add vendor-specific testdriver `click()` function
    This uses `Internals.click()` which doesn't do as much as the
    equivalent WebDriver method, but should be enough to satify most tests
    that use it.

--- Tests/LibWeb/Text/input/wpt-import/resources/testdriver-vendor.js
+++ Tests/LibWeb/Text/input/wpt-import/resources/testdriver-vendor.js
@@ -1,3 +1,13 @@
+window.test_driver_internal.click = function(element) {
+    const boundingRect = element.getBoundingClientRect();
+    const centerPoint = {
+        x: boundingRect.left + boundingRect.width / 2,
+        y: boundingRect.top + boundingRect.height / 2
+    };
+    window.internals.click(centerPoint.x, centerPoint.y);
+    return Promise.resolve();
+};
+
 window.test_driver_internal.get_computed_label = async function(element) {
     return await window.internals.getComputedLabel(element);
 };
