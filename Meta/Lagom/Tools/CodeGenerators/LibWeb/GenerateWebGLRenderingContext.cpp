7f8660261cc4aac07bf7abe7e1d6d15b85e153a9
Luke Wilde <luke@ladybird.org>
2024-12-13 18:47:41 +0000

    LibWeb/WebGL2: Implement texSubImage3D with ArrayBufferView and offset

--- Meta/Lagom/Tools/CodeGenerators/LibWeb/GenerateWebGLRenderingContext.cpp
+++ Meta/Lagom/Tools/CodeGenerators/LibWeb/GenerateWebGLRenderingContext.cpp
@@ -670,6 +670,19 @@ public:
             continue;
         }
 
+        if (function.name == "texSubImage3D"sv && function.overload_index == 0) {
+            function_impl_generator.append(R"~~~(
+    void const* pixels_ptr = nullptr;
+    if (src_data) {
+        auto const& viewed_array_buffer = src_data->viewed_array_buffer();
+        auto const& byte_buffer = viewed_array_buffer->buffer();
+        pixels_ptr = byte_buffer.data() + src_offset;
+    }
+    glTexSubImage3D(target, level, xoffset, yoffset, zoffset, width, height, depth, format, type, pixels_ptr);
+)~~~");
+            continue;
+        }
+
         if (function.name == "getShaderParameter"sv) {
             function_impl_generator.append(R"~~~(
     GLint result = 0;
