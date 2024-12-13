0b1b2ed9aa759e28377862a1bf56ed7ab3eb53ae
Luke Wilde <luke@ladybird.org>
2024-12-13 19:37:26 +0000

    LibWeb/WebGL2: Implement MAX_VERTEX_UNIFORM_BLOCKS parameter

--- Meta/Lagom/Tools/CodeGenerators/LibWeb/GenerateWebGLRenderingContext.cpp
+++ Meta/Lagom/Tools/CodeGenerators/LibWeb/GenerateWebGLRenderingContext.cpp
@@ -178,6 +178,7 @@ static void generate_get_parameter(SourceGenerator& generator, int webgl_version
         { "MAX_UNIFORM_BUFFER_BINDINGS"sv, { "GLint"sv }, 2 },
         { "UNIFORM_BUFFER_OFFSET_ALIGNMENT"sv, { "GLint"sv }, 2 },
         { "MAX_DRAW_BUFFERS"sv, { "GLint"sv }, 2 },
+        { "MAX_VERTEX_UNIFORM_BLOCKS"sv, { "GLint"sv }, 2 },
     };
 
     auto is_primitive_type = [](StringView type) {
