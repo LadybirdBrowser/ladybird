2ad73dcb55aa210d333356cb4b4208924d7bb31e
Pavel Shliak <shlyakpavel@gmail.com>
2024-12-20 03:22:48 +0400

    LibWeb: Make is_identifier always return false the simple way

---
+++ Libraries/LibWeb/WebAssembly/Error.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2024, Pavel Shliak <shlyakpavel@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibJS/Runtime/Error.h>
+
+namespace Web::WebAssembly {
+
+class CompileError final : public JS::Error {
+    JS_OBJECT(CompileError, Error);
+    GC_DECLARE_ALLOCATOR(CompileError);
+
+public:
+    static GC::Ref<CompileError> create(JS::Realm&);
+    static GC::Ref<CompileError> create(JS::Realm&, StringView message);
+    virtual ~CompileError() override = default;
+
+private:
+    explicit CompileError(JS::Object& prototype);
+};
+
+class LinkError final : public JS::Error {
+    JS_OBJECT(LinkError, Error);
+    GC_DECLARE_ALLOCATOR(LinkError);
+
+public:
+    static GC::Ref<LinkError> create(JS::Realm&);
+    static GC::Ref<LinkError> create(JS::Realm&, StringView message);
+    virtual ~LinkError() override = default;
+
+private:
+    explicit LinkError(JS::Object& prototype);
+};
+
+class RuntimeError final : public JS::Error {
+    JS_OBJECT(RuntimeError, Error);
+    GC_DECLARE_ALLOCATOR(RuntimeError);
+
+public:
+    static GC::Ref<RuntimeError> create(JS::Realm&);
+    static GC::Ref<RuntimeError> create(JS::Realm&, StringView message);
+    virtual ~RuntimeError() override = default;
+
+private:
+    explicit RuntimeError(JS::Object& prototype);
+};
+
+} // namespace Web::WebAssembly
