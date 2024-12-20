c9105955f6b03233d28e151ee708884e2a337c73
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2024-12-20 11:26:22 +0100

    LibWeb/WebGL: Add support for ANGLEInstancedArrays extension
    This is only WebGL 1.0 extension. It's enabled in ANGLE by default.
    
    Progress on https://ciechanow.ski/curves-and-surfaces/ that relies on
    this extension.

---
+++ Libraries/LibWeb/WebGL/ANGLEInstancedArrays.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024, Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/Bindings/PlatformObject.h>
+#include <LibWeb/Forward.h>
+#include <LibWeb/WebGL/Types.h>
+
+namespace Web::WebGL {
+
+class ANGLEInstancedArrays : public Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(ANGLEInstancedArrays, Bindings::PlatformObject);
+    GC_DECLARE_ALLOCATOR(ANGLEInstancedArrays);
+
+public:
+    static JS::ThrowCompletionOr<GC::Ptr<ANGLEInstancedArrays>> create(JS::Realm&);
+
+    void draw_arrays_instanced_angle(GLenum mode, GLint first, GLsizei count, GLsizei primcount);
+    void draw_elements_instanced_angle(GLenum mode, GLsizei count, GLenum type, GLintptr offset, GLsizei primcount);
+    void vertex_attrib_divisor_angle(GLuint index, GLuint divisor);
+
+protected:
+    void initialize(JS::Realm&) override;
+
+private:
+    ANGLEInstancedArrays(JS::Realm&);
+};
+
+}
--- Libraries/LibWeb/WebGL/Types.h
+++ Libraries/LibWeb/WebGL/Types.h
@@ -14,6 +14,8 @@ namespace Web::WebGL {
 using GLenum = unsigned int;
 using GLuint = unsigned int;
 using GLint = int;
+using GLsizei = int;
+using GLintptr = int;
 
 // FIXME: This should really be "struct __GLsync*", but the linker doesn't recognise it.
 //        Since this conflicts with the original definition of GLsync, the suffix "Internal" has been added.
