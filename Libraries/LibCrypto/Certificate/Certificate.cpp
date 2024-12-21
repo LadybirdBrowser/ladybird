c5a8930a0c4d3b4bc1a2170488e1644d131effc4
devgianlu <altomanigianluca@gmail.com>
2024-12-21 14:44:15 +0100

    LibCrypto: Add Ed448 OID to know algorithm identifiers

--- Libraries/LibCrypto/Certificate/Certificate.cpp
+++ Libraries/LibCrypto/Certificate/Certificate.cpp
@@ -82,7 +82,7 @@ static ErrorOr<AlgorithmIdentifier> parse_algorithm_identifier(Crypto::ASN1::Dec
     READ_OBJECT(ObjectIdentifier, Vector<int>, algorithm);
     POP_SCOPE();
 
-    constexpr static Array<Span<int const>, 12> known_algorithm_identifiers {
+    constexpr static Array<Span<int const>, 13> known_algorithm_identifiers {
         ASN1::rsa_encryption_oid,
         ASN1::rsa_md5_encryption_oid,
         ASN1::rsa_sha1_encryption_oid,
@@ -95,6 +95,7 @@ static ErrorOr<AlgorithmIdentifier> parse_algorithm_identifier(Crypto::ASN1::Dec
         ASN1::x25519_oid,
         ASN1::ed25519_oid,
         ASN1::x448_oid,
+        ASN1::ed448_oid,
     };
 
     bool is_known_algorithm = false;
