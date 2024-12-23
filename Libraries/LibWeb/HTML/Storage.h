5b3b5ed0d865383a79b6b58a1561410ebefb8f26
Shannon Booth <shannon@serenityos.org>
2024-12-23 22:32:49 +1300

    LibWeb/HTML: Store the type of the storage object
    This will be needed by the storage broadcast implementation to perform
    different steps if the object is for session storage.

--- Libraries/LibWeb/HTML/Storage.h
+++ Libraries/LibWeb/HTML/Storage.h
@@ -13,12 +13,20 @@
 
 namespace Web::HTML {
 
+// https://html.spec.whatwg.org/multipage/webstorage.html#storage-2
 class Storage : public Bindings::PlatformObject {
     WEB_PLATFORM_OBJECT(Storage, Bindings::PlatformObject);
     GC_DECLARE_ALLOCATOR(Storage);
 
 public:
-    [[nodiscard]] static GC::Ref<Storage> create(JS::Realm&, u64 quota_bytes);
+    // https://html.spec.whatwg.org/multipage/webstorage.html#concept-storage-type
+    enum class Type {
+        Local,
+        Session,
+    };
+
+    [[nodiscard]] static GC::Ref<Storage> create(JS::Realm&, Type, u64 quota_bytes);
+
     ~Storage();
 
     size_t length() const;
@@ -29,11 +37,12 @@ public:
     void clear();
 
     auto const& map() const { return m_map; }
+    Type type() const { return m_type; }
 
     void dump() const;
 
 private:
-    explicit Storage(JS::Realm&, u64 quota_limit);
+    Storage(JS::Realm&, Type, u64 quota_limit);
 
     virtual void initialize(JS::Realm&) override;
 
@@ -49,6 +58,7 @@ private:
     void broadcast(StringView key, StringView old_value, StringView new_value);
 
     OrderedHashMap<String, String> m_map;
+    Type m_type {};
     u64 m_quota_bytes { 0 };
     u64 m_stored_bytes { 0 };
 };
