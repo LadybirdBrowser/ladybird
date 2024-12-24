efdf020066f42bcff657901b51ca9c6e5abe19c8
Luke Wilde <luke@ladybird.org>
2024-12-24 18:22:44 +0000

    LibWeb/WebGL: Enforce minimum size when resizing existing context
    Previously the enforcement was only done on creation. Not enforcing it
    on change would cause a crash if the canvas width/height was set to
    zero or less.

--- Libraries/LibWeb/WebGL/WebGLRenderingContext.cpp
+++ Libraries/LibWeb/WebGL/WebGLRenderingContext.cpp
@@ -146,7 +146,10 @@ Optional<WebGLContextAttributes> WebGLRenderingContext::get_context_attributes()
 
 void WebGLRenderingContext::set_size(Gfx::IntSize const& size)
 {
-    context().set_size(size);
+    Gfx::IntSize final_size;
+    final_size.set_width(max(size.width(), 1));
+    final_size.set_height(max(size.height(), 1));
+    context().set_size(final_size);
 }
 
 void WebGLRenderingContext::reset_to_default_state()
