706cfcb36add85f7067c97543e4fc2adc37c6406
Andrew Kaster <andrew@ladybird.org>
2024-12-25 14:55:14 -0700

    Devcontainer: Add a Fedora 41 devcontainer for use in CI

---
+++ .devcontainer/features/ladybird/install-fedora.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+# Git and GitHub CLI
+dnf install -y git gh
+
+# Ladybird dev dependencies
+dnf install -y autoconf-archive automake ccache cmake curl google-noto-sans-mono-fonts liberation-sans-fonts libglvnd-devel \
+    nasm ninja-build perl-FindBin perl-IPC-Cmd perl-lib qt6-qtbase-devel qt6-qtmultimedia-devel qt6-qttools-devel \
+    qt6-qtwayland-devel tar unzip zip zlib-ng-compat-static
