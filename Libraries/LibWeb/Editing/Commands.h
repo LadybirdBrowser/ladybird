abb1774aed2adfff0933754e8fc23f01c860b138
Jelle Raaijmakers <jelle@ladybird.org>
2025-01-07 16:51:16 +0100

    LibWeb: Implement the "forwardDelete" editing command

--- Libraries/LibWeb/Editing/Commands.h
+++ Libraries/LibWeb/Editing/Commands.h
@@ -33,6 +33,7 @@ bool command_bold_action(DOM::Document&, String const&);
 bool command_default_paragraph_separator_action(DOM::Document&, String const&);
 String command_default_paragraph_separator_value(DOM::Document const&);
 bool command_delete_action(DOM::Document&, String const&);
+bool command_forward_delete_action(DOM::Document&, String const&);
 bool command_insert_linebreak_action(DOM::Document&, String const&);
 bool command_insert_paragraph_action(DOM::Document&, String const&);
 bool command_style_with_css_action(DOM::Document&, String const&);
