043a28baebe2d4b86e4163d67d8f2cf572a8ede6
Jelle Raaijmakers <jelle@ladybird.org>
2025-01-08 17:26:06 +0100

    LibWeb: Implement the "underline" editing command

--- Libraries/LibWeb/Editing/Commands.h
+++ Libraries/LibWeb/Editing/Commands.h
@@ -51,5 +51,6 @@ bool command_subscript_action(DOM::Document&, String const&);
 bool command_subscript_indeterminate(DOM::Document const&);
 bool command_superscript_action(DOM::Document&, String const&);
 bool command_superscript_indeterminate(DOM::Document const&);
+bool command_underline_action(DOM::Document&, String const&);
 
 }
