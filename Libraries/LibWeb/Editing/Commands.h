0ce7d409b313e7328b9235bf7d6b65d1629409ef
Jelle Raaijmakers <jelle@ladybird.org>
2025-01-08 17:38:02 +0100

    LibWeb: Implement the "unlink" editing command

--- Libraries/LibWeb/Editing/Commands.h
+++ Libraries/LibWeb/Editing/Commands.h
@@ -52,5 +52,6 @@ bool command_subscript_indeterminate(DOM::Document const&);
 bool command_superscript_action(DOM::Document&, String const&);
 bool command_superscript_indeterminate(DOM::Document const&);
 bool command_underline_action(DOM::Document&, String const&);
+bool command_unlink_action(DOM::Document&, String const&);
 
 }
