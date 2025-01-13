0398627e645198dfc545314c71422b7decc0e1d9
Shannon Booth <shannon@serenityos.org>
2025-01-13 22:24:18 +1300

    LibURL: Add a representation of a URL Pattern 'result'
    This is the return value of a URLPattern after `exec` is called on it.
    It conveys information about the named (or unammed) regex groups
    matched for each component of the URL. For example,
    
    ```
    let p = new URLPattern({ hostname: "{:subdomain.}*example.com" });
    const result = pattern.exec({ hostname: "foo.bar.example.com" });
    console.log(result.hostname.groups.subdomain);
    ```
    
    Will log 'foo.bar'.

--- Libraries/LibURL/Pattern/Pattern.h
+++ Libraries/LibURL/Pattern/Pattern.h
@@ -6,7 +6,10 @@
 
 #pragma once
 
+#include <AK/HashMap.h>
+#include <AK/String.h>
 #include <AK/Variant.h>
+#include <AK/Vector.h>
 #include <LibURL/Pattern/Init.h>
 
 namespace URL::Pattern {
@@ -17,6 +20,26 @@ using Input = Variant<String, Init>;
 // https://urlpattern.spec.whatwg.org/#dictdef-urlpatternoptions
 struct Options {
     bool ignore_case { false };
+};
+
+// https://urlpattern.spec.whatwg.org/#dictdef-urlpatterncomponentresult
+struct ComponentResult {
+    String input;
+    OrderedHashMap<String, Variant<String, Empty>> groups;
+};
+
+// https://urlpattern.spec.whatwg.org/#dictdef-urlpatternresult
+struct Result {
+    Vector<Input> inputs;
+
+    ComponentResult protocol;
+    ComponentResult username;
+    ComponentResult password;
+    ComponentResult hostname;
+    ComponentResult port;
+    ComponentResult pathname;
+    ComponentResult search;
+    ComponentResult hash;
 };
 
 }
