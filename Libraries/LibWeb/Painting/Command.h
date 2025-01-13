3a2f3beacb23801936bd20644e78f15f37b188c5
Psychpsyo <psychpsyo@gmail.com>
2025-01-14 00:04:01 +0100

    LibWeb: Fix CSS clip-path ignoring scroll position

--- Libraries/LibWeb/Painting/Command.h
+++ Libraries/LibWeb/Painting/Command.h
@@ -128,6 +128,9 @@ struct PushStackingContext {
     {
         source_paintable_rect.translate_by(offset);
         transform.origin.translate_by(offset.to_type<float>());
+        if (clip_path.has_value()) {
+            clip_path.value().transform(Gfx::AffineTransform().translate(offset.to_type<float>()));
+        }
     }
 };
 
