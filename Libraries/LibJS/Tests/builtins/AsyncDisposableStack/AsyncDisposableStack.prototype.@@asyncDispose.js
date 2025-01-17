80a9863b8d72d648e3b060a0b1aa40d075f5d65a
Timothy Flynn <trflynn89@pm.me>
2025-01-17 10:09:01 -0500

    LibJS: Implement the AsyncDisposableStack interface
    This is very similar to the DisposableStack interface, except disposal
    of resources is promise-based.

---
+++ Libraries/LibJS/Tests/builtins/AsyncDisposableStack/AsyncDisposableStack.prototype.@@asyncDispose.js
@@ -0,0 +1,9 @@
+test("length is 0", () => {
+    expect(AsyncDisposableStack.prototype[Symbol.asyncDispose]).toHaveLength(0);
+});
+
+test("is the same as disposeAsync", () => {
+    expect(AsyncDisposableStack.prototype[Symbol.asyncDispose]).toBe(
+        AsyncDisposableStack.prototype.disposeAsync
+    );
+});
