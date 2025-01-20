546fd0ac16669fd4ce7ffdd2673811c273b20b96
Timothy Flynn <trflynn89@pm.me>
2025-01-20 11:43:41 -0500

    LibJS: Do not consume "with" tokens in import statements as identifiers
    The "with" statement is its own token (TokenType::With), and thus would
    fail to parse as an identifier. We've already asserted that the token
    we are parsing is "with" or "assert", so just consume it.

--- Libraries/LibJS/Tests/modules/json-modules.js
+++ Libraries/LibJS/Tests/modules/json-modules.js
@@ -1,10 +1,43 @@
 describe("basic behavior", () => {
-    test("can import json modules", () => {
+    test("can import json modules (with import function)", () => {
         let passed = false;
         let error = null;
         let result = null;
 
         import("./json-module.json", { assert: { type: "json" } })
+            .then(jsonObj => {
+                passed = true;
+                result = jsonObj;
+            })
+            .catch(err => {
+                error = err;
+            });
+
+        runQueuedPromiseJobs();
+
+        if (error) throw error;
+
+        console.log(JSON.stringify(result));
+        expect(passed).toBeTrue();
+
+        expect(result).not.toBeNull();
+        expect(result).not.toBeUndefined();
+
+        const jsonResult = result.default;
+        expect(jsonResult).not.toBeNull();
+        expect(jsonResult).not.toBeUndefined();
+
+        expect(jsonResult).toHaveProperty("value", "value");
+        expect(jsonResult).toHaveProperty("array", [1, 2, 3]);
+        expect(jsonResult).toHaveProperty("map", { innerValue: "innerValue" });
+    });
+
+    test("can import json modules (with import statement)", () => {
+        let passed = false;
+        let error = null;
+        let result = null;
+
+        import("./json-module.mjs")
             .then(jsonObj => {
                 passed = true;
                 result = jsonObj;
