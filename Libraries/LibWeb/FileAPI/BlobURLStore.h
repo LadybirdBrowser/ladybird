e5473b63b0b507fcdc4bd598355d7c91347d3862
Shannon Booth <shannon@serenityos.org>
2025-01-22 17:21:14 +1300

    LibWeb: Update revokeObjectURL for reported spec bugs
    The spec intends to pass through a URL record object as it needs to
    be serialized on removal. This has no functional impact on our
    implementation other than the double parsing of every URL being
    revoked.
    
    It is also missing an error check for an invalid URL being passed
    through. This does not impact our implementation currently as we
    just end up using an empty URL which is not part of the blob entry
    map. This will cause problems once DOMURL::parse is updated to
    return an Optional<URL::URL> however.

--- Libraries/LibWeb/FileAPI/BlobURLStore.h
+++ Libraries/LibWeb/FileAPI/BlobURLStore.h
@@ -30,7 +30,7 @@ ErrorOr<String> add_entry_to_blob_url_store(GC::Ref<Blob> object);
 bool check_for_same_partition_blob_url_usage(URL::BlobURLEntry const&, GC::Ref<HTML::Environment>);
 struct NavigationEnvironment { };
 Optional<URL::BlobURLEntry::Object> obtain_a_blob_object(URL::BlobURLEntry const&, Variant<GC::Ref<HTML::Environment>, NavigationEnvironment> environment);
-void remove_entry_from_blob_url_store(StringView url);
+void remove_entry_from_blob_url_store(URL::URL const& url);
 Optional<BlobURLEntry const&> resolve_a_blob_url(URL::URL const&);
 
 void run_unloading_cleanup_steps(GC::Ref<DOM::Document>);
