b4df14ebe5ee75f1c4a8e10b476d51a270b31348
Jess <jess@jessie.cafe>
2025-02-03 05:30:43 +1300

    Nix: Fix OpenGL linking issue in the devshell
    https://github.com/LadybirdBrowser/ladybird/issues/371#issuecomment-2616415434
    https://github.com/NixOS/nixpkgs/commit/049a854b4be087eaa3a09012b9c452fbc838dd41

--- shell.nix
+++ shell.nix
@@ -22,6 +22,10 @@ pkgs.mkShell {
       prettier
     ];
 
+  # Fix for: https://github.com/LadybirdBrowser/ladybird/issues/371#issuecomment-2616415434
+  # https://github.com/NixOS/nixpkgs/commit/049a854b4be087eaa3a09012b9c452fbc838dd41
+  NIX_LDFLAGS = "-lGL";
+
   shellHook = ''
     # NOTE: This is required to make it find the wayland platform plugin installed
     #       above, but should probably be fixed upstream.
