cf7b775709e78e954d3bb293b24a160ad6cbfb34
Shannon Booth <shannon@serenityos.org>
2025-02-04 15:25:19 +1300

    LibWeb/URLPattern: Implement the URLPattern IDL getters
    These simply return the compiled URLPattern pattern strings for
    each component, and whether any of the components contained any
    regexp groups.

--- Libraries/LibWeb/URLPattern/URLPattern.cpp
+++ Libraries/LibWeb/URLPattern/URLPattern.cpp
@@ -43,4 +43,71 @@ Optional<URLPatternResult> URLPattern::exec(URLPatternInput const&, Optional<Str
     return {};
 }
 
+// https://urlpattern.spec.whatwg.org/#dom-urlpattern-protocol
+String const& URLPattern::protocol() const
+{
+    // 1. Return this's associated URL pattern's protocol component's pattern string.
+    return m_url_pattern.protocol_component().pattern_string;
+}
+
+// https://urlpattern.spec.whatwg.org/#dom-urlpattern-username
+String const& URLPattern::username() const
+{
+    // 1. Return this's associated URL pattern's username component's pattern string.
+    return m_url_pattern.username_component().pattern_string;
+}
+
+// https://urlpattern.spec.whatwg.org/#dom-urlpattern-password
+String const& URLPattern::password() const
+{
+    // 1. Return this's associated URL pattern's password component's pattern string.
+    return m_url_pattern.password_component().pattern_string;
+}
+
+// https://urlpattern.spec.whatwg.org/#dom-urlpattern-hostname
+String const& URLPattern::hostname() const
+{
+    // 1. Return this's associated URL pattern's hostname component's pattern string.
+    return m_url_pattern.hostname_component().pattern_string;
+}
+
+// https://urlpattern.spec.whatwg.org/#dom-urlpattern-port
+String const& URLPattern::port() const
+{
+    // 1. Return this's associated URL pattern's port component's pattern string.
+    return m_url_pattern.port_component().pattern_string;
+}
+
+// https://urlpattern.spec.whatwg.org/#dom-urlpattern-pathname
+String const& URLPattern::pathname() const
+{
+    // 1. Return this's associated URL pattern's pathname component's pattern string.
+    return m_url_pattern.pathname_component().pattern_string;
+}
+
+// https://urlpattern.spec.whatwg.org/#dom-urlpattern-search
+String const& URLPattern::search() const
+{
+    // 1. Return this's associated URL pattern's search component's pattern string.
+    return m_url_pattern.search_component().pattern_string;
+}
+
+// https://urlpattern.spec.whatwg.org/#dom-urlpattern-hash
+String const& URLPattern::hash() const
+{
+    // 1. Return this's associated URL pattern's hash component's pattern string.
+    return m_url_pattern.hash_component().pattern_string;
+}
+
+// https://urlpattern.spec.whatwg.org/#dom-urlpattern-hasregexpgroups
+bool URLPattern::has_reg_exp_groups() const
+{
+    // 1. If this's associated URL pattern's has regexp groups, then return true.
+    if (m_url_pattern.has_regexp_groups())
+        return true;
+
+    // 2. Return false.
+    return false;
+}
+
 }
