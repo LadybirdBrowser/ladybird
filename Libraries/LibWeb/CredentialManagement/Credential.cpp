c726be47adc39b4df9ef03d46872a6d15513b9b8
devgianlu <altomanigianluca@gmail.com>
2025-02-07 16:58:35 +0100

    LibWeb: Introduce `CredentialInterface` for internal credentials methods
    The abstract `CredentialInterface` class serves to implement what is
    mentioned in the spec as "interface objects". They hold just internal
    methods, nothing is actually exposed to JS from them.
    
    We cannot implement the internal methods on the `Credential` class
    because the spec requires to treat them as concrete objects.

--- Libraries/LibWeb/CredentialManagement/Credential.cpp
+++ Libraries/LibWeb/CredentialManagement/Credential.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Altomani Gianluca <altomanigianluca@gmail.com>
+ * Copyright (c) 2026, Altomani Gianluca <altomanigianluca@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -8,6 +8,30 @@
 #include <LibWeb/CredentialManagement/Credential.h>
 
 namespace Web::CredentialManagement {
+
+WebIDL::ExceptionOr<Variant<Empty, GC::Ref<Credential>, GC::Ref<CreateCredentialAlgorithm>>> CredentialInterface::create(JS::Realm&, URL::Origin const&, CredentialCreationOptions const&, bool) const
+{
+    // 1. Return null.
+    return Empty {};
+}
+
+WebIDL::ExceptionOr<void> CredentialInterface::store(JS::Realm& realm, bool) const
+{
+    // 1. Throw a NotSupportedError.
+    return JS::throw_completion(WebIDL::NotSupportedError::create(realm, "store"_utf16));
+}
+
+WebIDL::ExceptionOr<Variant<Empty, GC::Ref<Credential>>> CredentialInterface::discover_from_external_source(JS::Realm&, URL::Origin const&, CredentialRequestOptions const&, bool) const
+{
+    // 1. Return null.
+    return Empty {};
+}
+
+WebIDL::ExceptionOr<Vector<Credential>> CredentialInterface::collect_from_credential_store(JS::Realm&, URL::Origin const&, CredentialRequestOptions const&, bool) const
+{
+    // 1. Return an empty set.
+    return Vector<Credential> {};
+}
 
 // https://www.w3.org/TR/credential-management-1/#dom-credential-isconditionalmediationavailable
 GC::Ref<WebIDL::Promise> Credential::is_conditional_mediation_available(JS::VM& vm)
