f14d9ffbd5666274b9d580d2e6826a44e56caf1e
Timothy Flynn <trflynn89@pm.me>
2025-02-14 10:31:44 -0500

    LibWeb: Move DOM::NodeType to its own file
    This is to allow using these values in libraries that otherwise do not
    need to depend on LibWeb.

---
+++ Libraries/LibWeb/DOM/NodeType.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2025, Tim Flynn <trflynn89@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+
+namespace Web::DOM {
+
+enum class NodeType : u16 {
+    INVALID = 0,
+    ELEMENT_NODE = 1,
+    ATTRIBUTE_NODE = 2,
+    TEXT_NODE = 3,
+    CDATA_SECTION_NODE = 4,
+    ENTITY_REFERENCE_NODE = 5,
+    ENTITY_NODE = 6,
+    PROCESSING_INSTRUCTION_NODE = 7,
+    COMMENT_NODE = 8,
+    DOCUMENT_NODE = 9,
+    DOCUMENT_TYPE_NODE = 10,
+    DOCUMENT_FRAGMENT_NODE = 11,
+    NOTATION_NODE = 12
+};
+
+}
