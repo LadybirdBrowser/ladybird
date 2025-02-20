1aa50ac34a335c325d06b0d754f8536df0289ae2
Jelle Raaijmakers <jelle@ladybird.org>
2025-02-20 11:10:49 +0100

    LibWeb: Combine SVG tag names into a single list
    We have no need to separate these tags into "SVG graphics tags" and "SVG
    tags".

--- Libraries/LibWeb/SVG/TagNames.h
+++ Libraries/LibWeb/SVG/TagNames.h
@@ -10,37 +10,34 @@
 
 namespace Web::SVG::TagNames {
 
-#define ENUMERATE_SVG_GRAPHICS_TAGS \
-    __ENUMERATE_SVG_TAG(a)          \
-    __ENUMERATE_SVG_TAG(circle)     \
-    __ENUMERATE_SVG_TAG(ellipse)    \
-    __ENUMERATE_SVG_TAG(g)          \
-    __ENUMERATE_SVG_TAG(image)      \
-    __ENUMERATE_SVG_TAG(line)       \
-    __ENUMERATE_SVG_TAG(path)       \
-    __ENUMERATE_SVG_TAG(polygon)    \
-    __ENUMERATE_SVG_TAG(polyline)   \
-    __ENUMERATE_SVG_TAG(rect)       \
-    __ENUMERATE_SVG_TAG(svg)        \
-    __ENUMERATE_SVG_TAG(text)       \
-    __ENUMERATE_SVG_TAG(textPath)   \
-    __ENUMERATE_SVG_TAG(tspan)
-
 #define ENUMERATE_SVG_TAGS              \
-    ENUMERATE_SVG_GRAPHICS_TAGS         \
+    __ENUMERATE_SVG_TAG(a)              \
+    __ENUMERATE_SVG_TAG(circle)         \
     __ENUMERATE_SVG_TAG(clipPath)       \
     __ENUMERATE_SVG_TAG(defs)           \
     __ENUMERATE_SVG_TAG(desc)           \
+    __ENUMERATE_SVG_TAG(ellipse)        \
     __ENUMERATE_SVG_TAG(foreignObject)  \
+    __ENUMERATE_SVG_TAG(g)              \
+    __ENUMERATE_SVG_TAG(image)          \
+    __ENUMERATE_SVG_TAG(line)           \
     __ENUMERATE_SVG_TAG(linearGradient) \
     __ENUMERATE_SVG_TAG(mask)           \
     __ENUMERATE_SVG_TAG(metadata)       \
+    __ENUMERATE_SVG_TAG(path)           \
+    __ENUMERATE_SVG_TAG(polygon)        \
+    __ENUMERATE_SVG_TAG(polyline)       \
     __ENUMERATE_SVG_TAG(radialGradient) \
+    __ENUMERATE_SVG_TAG(rect)           \
     __ENUMERATE_SVG_TAG(script)         \
     __ENUMERATE_SVG_TAG(stop)           \
     __ENUMERATE_SVG_TAG(style)          \
+    __ENUMERATE_SVG_TAG(svg)            \
     __ENUMERATE_SVG_TAG(symbol)         \
+    __ENUMERATE_SVG_TAG(text)           \
+    __ENUMERATE_SVG_TAG(textPath)       \
     __ENUMERATE_SVG_TAG(title)          \
+    __ENUMERATE_SVG_TAG(tspan)          \
     __ENUMERATE_SVG_TAG(use)
 
 #define __ENUMERATE_SVG_TAG(name) extern FlyString name;
