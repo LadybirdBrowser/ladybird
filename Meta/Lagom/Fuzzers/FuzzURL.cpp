5b2bebcc16af4e30a9dd2ff19a7cf0a87334f7e7
Shannon Booth <shannon@serenityos.org>
2025-02-22 22:16:42 +1300

    Meta: Use URL basic parse for URL Fuzzer

--- Meta/Lagom/Fuzzers/FuzzURL.cpp
+++ Meta/Lagom/Fuzzers/FuzzURL.cpp
@@ -4,12 +4,13 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <LibURL/URL.h>
+#include <LibURL/Parser.h>
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
 {
     AK::set_debug_enabled(false);
     auto string_view = StringView(data, size);
-    auto url = URL::URL(string_view);
+    auto url = URL::Parser::basic_parse(string_view);
+    (void)url;
     return 0;
 }
