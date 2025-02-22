94de31ff3b4950912397320b93bd8615ec779fdc
R-Goc <ryszardgoc@gmail.com>
2025-02-22 23:57:34 +0100

    AK: Remove fast_u32_xxx apis from Memory.h
    This commit removes the fast_u32_fill and fast_u32_copy functions,
    as they were only used in one place, and are not optimal.

--- AK/Memory.h
+++ AK/Memory.h
@@ -10,32 +10,6 @@
 #include <AK/Types.h>
 #include <string.h>
 
-ALWAYS_INLINE void fast_u32_copy(u32* dest, u32 const* src, size_t count)
-{
-#if ARCH(X86_64)
-    asm volatile(
-        "rep movsl\n"
-        : "+S"(src), "+D"(dest), "+c"(count)::"memory");
-#else
-    __builtin_memcpy(dest, src, count * 4);
-#endif
-}
-
-ALWAYS_INLINE void fast_u32_fill(u32* dest, u32 value, size_t count)
-{
-#if ARCH(X86_64)
-    asm volatile(
-        "rep stosl\n"
-        : "=D"(dest), "=c"(count)
-        : "D"(dest), "c"(count), "a"(value)
-        : "memory");
-#else
-    for (auto* p = dest; p < (dest + count); ++p) {
-        *p = value;
-    }
-#endif
-}
-
 namespace AK {
 
 inline void secure_zero(void* ptr, size_t size)
