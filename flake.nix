2c7835bf429ae3273940c7cd51b38e2f3a490861
NotAShelf <raf@notashelf.dev>
2025-03-02 16:26:10 +0300

    refactor(nix): Remove flake-utils
    Motive: flake-utils is an unnecessary abstraction library that manages
    to creep into every project under the guise of making things "easier."
    There is no reason to use flake-utils here, as Nix is a powerful enough
    DSL to handle system abstractions in-house without relying on more 3rd
    party code, especially in a flake as small as Ladybird's.

--- flake.nix
+++ flake.nix
@@ -2,25 +2,31 @@
   description = "Ladybird";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs =
     {
+      systems,
       nixpkgs,
-      flake-utils,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = import ./shell.nix { inherit pkgs; };
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.callPackage ./shell.nix;
+        }
+      );
 
-        formatter = pkgs.nixfmt-rfc-style;
-      }
-    );
+      # Provide a formatter for `nix fmt`
+      formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+    };
 }
