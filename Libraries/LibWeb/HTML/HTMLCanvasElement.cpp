0e231047c5828925cf3bc9bf98e3c2a53ff29224
Totto16 <tobiausgais@gmail.com>
2025-03-04 15:51:42 +0100

    LibWeb: Implement basics for OffscreenCanvas
    This implements the basic interface, classes and functions for
    OffscreenCanvas. Many are still stubbed out and have many FIXMEs in
    them, but it is a basic skeleton

--- Libraries/LibWeb/HTML/HTMLCanvasElement.cpp
+++ Libraries/LibWeb/HTML/HTMLCanvasElement.cpp
@@ -19,6 +19,7 @@
 #include <LibWeb/CSS/StyleValues/RatioStyleValue.h>
 #include <LibWeb/CSS/StyleValues/StyleValueList.h>
 #include <LibWeb/DOM/Document.h>
+#include <LibWeb/HTML/Canvas/SerializeBitmap.h>
 #include <LibWeb/HTML/CanvasRenderingContext2D.h>
 #include <LibWeb/HTML/HTMLCanvasElement.h>
 #include <LibWeb/HTML/Numbers.h>
@@ -285,33 +286,6 @@ Gfx::IntSize HTMLCanvasElement::bitmap_size_for_canvas(size_t minimum_width, siz
         return {};
     }
     return Gfx::IntSize(width, height);
-}
-
-struct SerializeBitmapResult {
-    ByteBuffer buffer;
-    StringView mime_type;
-};
-
-// https://html.spec.whatwg.org/multipage/canvas.html#a-serialisation-of-the-bitmap-as-a-file
-static ErrorOr<SerializeBitmapResult> serialize_bitmap(Gfx::Bitmap const& bitmap, StringView type, JS::Value quality)
-{
-    // If type is an image format that supports variable quality (such as "image/jpeg"), quality is given, and type is not "image/png", then,
-    // if quality is a Number in the range 0.0 to 1.0 inclusive, the user agent must treat quality as the desired quality level.
-    // Otherwise, the user agent must use its default quality value, as if the quality argument had not been given.
-    bool valid_quality = quality.is_number() && quality.as_double() >= 0.0 && quality.as_double() <= 1.0;
-
-    if (type.equals_ignoring_ascii_case("image/jpeg"sv)) {
-        AllocatingMemoryStream file;
-        Gfx::JPEGWriter::Options jpeg_options;
-        if (valid_quality)
-            jpeg_options.quality = static_cast<int>(quality.as_double() * 100);
-        TRY(Gfx::JPEGWriter::encode(file, bitmap, jpeg_options));
-        return SerializeBitmapResult { TRY(file.read_until_eof()), "image/jpeg"sv };
-    }
-
-    // User agents must support PNG ("image/png"). User agents may support other types.
-    // If the user agent does not support the requested type, then it must create the file using the PNG format. [PNG]
-    return SerializeBitmapResult { TRY(Gfx::PNGWriter::encode(bitmap)), "image/png"sv };
 }
 
 // https://html.spec.whatwg.org/multipage/canvas.html#dom-canvas-todataurl
