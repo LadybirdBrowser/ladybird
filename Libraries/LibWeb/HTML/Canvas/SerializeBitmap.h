49500ac3860fde0ce1445805f323b0febf4e6c23
Totto16 <tobiausgais@gmail.com>
2025-03-06 23:37:13 +0100

    LibWeb: Factor out canvas serialization algorihtm
    Factor out canvas serialization algorihtm from HTMLCanvasElement to
    seperate file. This makes it usable by other things too.

---
+++ Libraries/LibWeb/HTML/Canvas/SerializeBitmap.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025, Ladybird contributors
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/ByteBuffer.h>
+#include <LibGfx/Bitmap.h>
+#include <LibWeb/DOM/Document.h>
+
+namespace Web::HTML {
+
+struct SerializeBitmapResult {
+    ByteBuffer buffer;
+    StringView mime_type;
+};
+
+// https://html.spec.whatwg.org/multipage/canvas.html#a-serialisation-of-the-bitmap-as-a-file
+ErrorOr<SerializeBitmapResult> serialize_bitmap(Gfx::Bitmap const& bitmap, StringView type, Optional<double> quality);
+
+}
