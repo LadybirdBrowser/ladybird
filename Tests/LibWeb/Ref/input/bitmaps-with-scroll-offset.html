LadybirdBrowser/ladybird

commit e055927eadc8b051f580587da6cf83fe2ab64c6c

LibWeb: Do not deform bitmaps partially outside the img-box
Instead of trying to manually determine which parts of a bitmap fall
within the box of the `<img>` element, just draw the whole bitmap and
let Skia clip the draw-area to the correct rectangle.

This fixes a bug where the entire bitmap was squashed into the rectangle
of the image box instead of being clipped.

With this change, image rendering is now correct enough to import some
of the WPT tests for object-fit and object-position. To get some good
coverage I have imported all tests for the `<img>` tag. I also wanted to
import a subset of the tests for the `<object>` tag, since those are
passing as well now. Unfortunately, they are flaky for unknown reasons.

Libraries/LibWeb/CSS/StyleValues/ImageStyleValue.cpp
@@ -149,7 +149,8 @@ void ImageStyleValue::paint(PaintContext& context, DevicePixelRect const& dest_r
 {
     if (auto const* b = bitmap(m_current_frame_index, dest_rect.size().to_type<int>()); b != nullptr) {
         auto scaling_mode = to_gfx_scaling_mode(image_rendering, b->rect(), dest_rect.to_type<int>());
-        context.display_list_recorder().draw_scaled_immutable_bitmap(dest_rect.to_type<int>(), *b, b->rect(), scaling_mode);
+        auto dest_int_rect = dest_rect.to_type<int>();
+        context.display_list_recorder().draw_scaled_immutable_bitmap(dest_int_rect, dest_int_rect, *b, scaling_mode);
     }
 }
 

Libraries/LibWeb/Painting/Command.h
@@ -72,8 +72,8 @@ struct DrawPaintingSurface {
 
 struct DrawScaledImmutableBitmap {
     Gfx::IntRect dst_rect;
+    Gfx::IntRect clip_rect;
     NonnullRefPtr<Gfx::ImmutableBitmap> bitmap;
-    Gfx::IntRect src_rect;
     Gfx::ScalingMode scaling_mode;
 
     [[nodiscard]] Gfx::IntRect bounding_rect() const { return dst_rect; }

Libraries/LibWeb/Painting/DisplayListPlayerSkia.cpp
@@ -135,11 +135,14 @@ void DisplayListPlayerSkia::draw_painting_surface(DrawPaintingSurface const& com
 
 void DisplayListPlayerSkia::draw_scaled_immutable_bitmap(DrawScaledImmutableBitmap const& command)
 {
-    auto src_rect = to_skia_rect(command.src_rect);
     auto dst_rect = to_skia_rect(command.dst_rect);
+    auto clip_rect = to_skia_rect(command.clip_rect);
     auto& canvas = surface().canvas();
     SkPaint paint;
-    canvas.drawImageRect(command.bitmap->sk_image(), src_rect, dst_rect, to_skia_sampling_options(command.scaling_mode), &paint, SkCanvas::kStrict_SrcRectConstraint);
+    canvas.save();
+    canvas.clipRect(clip_rect);
+    canvas.drawImageRect(command.bitmap->sk_image(), dst_rect, to_skia_sampling_options(command.scaling_mode), &paint);
+    canvas.restore();
 }
 
 void DisplayListPlayerSkia::draw_repeated_immutable_bitmap(DrawRepeatedImmutableBitmap const& command)

Libraries/LibWeb/Painting/DisplayListRecorder.cpp
@@ -194,14 +194,14 @@ void DisplayListRecorder::draw_painting_surface(Gfx::IntRect const& dst_rect, No
     });
 }
 
-void DisplayListRecorder::draw_scaled_immutable_bitmap(Gfx::IntRect const& dst_rect, Gfx::ImmutableBitmap const& bitmap, Gfx::IntRect const& src_rect, Gfx::ScalingMode scaling_mode)
+void DisplayListRecorder::draw_scaled_immutable_bitmap(Gfx::IntRect const& dst_rect, Gfx::IntRect const& clip_rect, Gfx::ImmutableBitmap const& bitmap, Gfx::ScalingMode scaling_mode)
 {
     if (dst_rect.is_empty())
         return;
     append(DrawScaledImmutableBitmap {
         .dst_rect = dst_rect,
+        .clip_rect = clip_rect,
         .bitmap = bitmap,
-        .src_rect = src_rect,
         .scaling_mode = scaling_mode,
     });
 }

Libraries/LibWeb/Painting/DisplayListRecorder.h
@@ -95,7 +95,7 @@ public:
     void draw_rect(Gfx::IntRect const& rect, Color color, bool rough = false);
 
     void draw_painting_surface(Gfx::IntRect const& dst_rect, NonnullRefPtr<Gfx::PaintingSurface>, Gfx::IntRect const& src_rect, Gfx::ScalingMode scaling_mode = Gfx::ScalingMode::NearestNeighbor);
-    void draw_scaled_immutable_bitmap(Gfx::IntRect const& dst_rect, Gfx::ImmutableBitmap const& bitmap, Gfx::IntRect const& src_rect, Gfx::ScalingMode scaling_mode = Gfx::ScalingMode::NearestNeighbor);
+    void draw_scaled_immutable_bitmap(Gfx::IntRect const& dst_rect, Gfx::IntRect const& clip_rect, Gfx::ImmutableBitmap const& bitmap, Gfx::ScalingMode scaling_mode = Gfx::ScalingMode::NearestNeighbor);
 
     void draw_repeated_immutable_bitmap(Gfx::IntRect dst_rect, Gfx::IntRect clip_rect, NonnullRefPtr<Gfx::ImmutableBitmap> bitmap, Gfx::ScalingMode scaling_mode, DrawRepeatedImmutableBitmap::Repeat);
 

Libraries/LibWeb/Painting/ImagePaintable.cpp
@@ -79,7 +79,6 @@ void ImagePaintable::paint(PaintContext& context, PaintPhase phase) const
 
             auto scale_x = 0.0f;
             auto scale_y = 0.0f;
-            Gfx::IntRect bitmap_intersect = bitmap_rect;
 
             // https://drafts.csswg.org/css-images/#the-object-fit
             auto object_fit = m_is_svg_image ? CSS::ObjectFit::Contain : computed_values().object_fit();
@@ -109,11 +108,9 @@ void ImagePaintable::paint(PaintContext& context, PaintPhase phase) const
                 if (bitmap_aspect_ratio >= image_aspect_ratio) {
                     scale_x = (float)image_rect.width() / bitmap_rect.width();
                     scale_y = scale_x;
-                    bitmap_intersect.set_height(bitmap_rect.width() * image_aspect_ratio);
                 } else {
                     scale_x = (float)image_rect.height() / bitmap_rect.height();
                     scale_y = scale_x;
-                    bitmap_intersect.set_width(bitmap_rect.height() / image_aspect_ratio);
                 }
                 break;
             case CSS::ObjectFit::ScaleDown:
@@ -121,7 +118,6 @@ void ImagePaintable::paint(PaintContext& context, PaintPhase phase) const
             case CSS::ObjectFit::None:
                 scale_x = 1;
                 scale_y = 1;
-                bitmap_intersect.set_size(image_rect.size().to_type<int>());
             }
 
             auto scaled_bitmap_width = CSSPixels::nearest_value_for(bitmap_rect.width() * scale_x);
@@ -130,31 +126,22 @@ void ImagePaintable::paint(PaintContext& context, PaintPhase phase) const
             auto residual_horizontal = image_rect.width() - scaled_bitmap_width;
             auto residual_vertical = image_rect.height() - scaled_bitmap_height;
 
-            bitmap_intersect.set_x((bitmap_rect.width() - bitmap_intersect.width()) / 2);
-            bitmap_intersect.set_y((bitmap_rect.height() - bitmap_intersect.height()) / 2);
-
             // https://drafts.csswg.org/css-images/#the-object-position
             auto const& object_position = computed_values().object_position();
 
             auto offset_x = CSSPixels::from_raw(0);
             if (object_position.edge_x == CSS::PositionEdge::Left) {
                 offset_x = object_position.offset_x.to_px(layout_node(), residual_horizontal);
-                bitmap_intersect.set_x(0);
             } else if (object_position.edge_x == CSS::PositionEdge::Right) {
                 offset_x = residual_horizontal - object_position.offset_x.to_px(layout_node(), residual_horizontal);
             }
-            if (image_rect.width() < scaled_bitmap_width)
-                bitmap_intersect.set_x(-(offset_x / scale_x));
 
             auto offset_y = CSSPixels::from_raw(0);
             if (object_position.edge_y == CSS::PositionEdge::Top) {
                 offset_y = object_position.offset_y.to_px(layout_node(), residual_vertical);
-                bitmap_intersect.set_y(0);
             } else if (object_position.edge_y == CSS::PositionEdge::Bottom) {
                 offset_y = residual_vertical - object_position.offset_y.to_px(layout_node(), residual_vertical);
             }
-            if (image_rect.height() < scaled_bitmap_height)
-                bitmap_intersect.set_y(-(offset_y / scale_y));
 
             Gfx::IntRect draw_rect = {
                 image_int_rect_device_pixels.x() + context.rounded_device_pixels(offset_x).value(),
@@ -163,7 +150,7 @@ void ImagePaintable::paint(PaintContext& context, PaintPhase phase) const
                 context.rounded_device_pixels(scaled_bitmap_height).value()
             };
 
-            context.display_list_recorder().draw_scaled_immutable_bitmap(draw_rect.intersected(image_int_rect_device_pixels), *bitmap, bitmap_rect.intersected(bitmap_intersect), scaling_mode);
+            context.display_list_recorder().draw_scaled_immutable_bitmap(draw_rect, image_int_rect_device_pixels, *bitmap, scaling_mode);
         }
     }
 }

Libraries/LibWeb/Painting/VideoPaintable.cpp
@@ -130,7 +130,8 @@ void VideoPaintable::paint(PaintContext& context, PaintPhase phase) const
 
     auto paint_frame = [&](auto const& frame) {
         auto scaling_mode = to_gfx_scaling_mode(computed_values().image_rendering(), frame->rect(), video_rect.to_type<int>());
-        context.display_list_recorder().draw_scaled_immutable_bitmap(video_rect.to_type<int>(), Gfx::ImmutableBitmap::create(*frame), frame->rect(), scaling_mode);
+        auto dst_rect = video_rect.to_type<int>();
+        context.display_list_recorder().draw_scaled_immutable_bitmap(dst_rect, dst_rect, Gfx::ImmutableBitmap::create(*frame), scaling_mode);
     };
 
     auto paint_transparent_black = [&]() {

Tests/LibWeb/Ref/expected/wpt-import/css/css-images/object-fit-contain-png-001-ref.html
@@ -1,0 +1,79 @@
+<!DOCTYPE html>
+<!--
+     Any copyright is dedicated to the Public Domain.
+     http://creativecommons.org/publicdomain/zero/1.0/
+-->
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CSS Reftest Reference</title>
+    <link rel="author" title="Daniel Holbert" href="mailto:dholbert@mozilla.com">
+    <style type="text/css">
+      .objectOuter {
+        border: 1px dashed gray;
+        padding: 1px;
+        float: left;
+      }
+      .objectOuter > * {
+        background-image: url("support/colors-16x8.png");
+        background-size: contain;
+        background-repeat: no-repeat;
+        image-rendering: crisp-edges;
+      }
+      .bigWide {
+        width: 48px;
+        height: 32px;
+      }
+      .bigTall {
+        width: 32px;
+        height: 48px;
+      }
+      .small {
+        width: 8px;
+        height: 8px;
+      }
+
+      br { clear: both; }
+
+      .tr { background-position: top right }
+      .bl { background-position: bottom left }
+      .tl { background-position: top 25% left 25% }
+      .br { background-position: bottom 1px right 2px }
+
+      .tc { background-position: top 3px center }
+      .cr { background-position: center right 25% }
+      .default { background-position: 50% 50% }
+    </style>
+  </head>
+  <body>
+    <!-- big/wide: -->
+    <div class="objectOuter"><div class="bigWide tr"></div></div>
+    <div class="objectOuter"><div class="bigWide bl"></div></div>
+    <div class="objectOuter"><div class="bigWide tl"></div></div>
+    <div class="objectOuter"><div class="bigWide br"></div></div>
+    <div class="objectOuter"><div class="bigWide tc"></div></div>
+    <div class="objectOuter"><div class="bigWide cr"></div></div>
+    <div class="objectOuter"><div class="bigWide default"></div></div>
+    <br>
+    <!-- big/tall: -->
+    <div class="objectOuter"><div class="bigTall tr"></div></div>
+    <div class="objectOuter"><div class="bigTall bl"></div></div>
+    <div class="objectOuter"><div class="bigTall tl"></div></div>
+    <div class="objectOuter"><div class="bigTall br"></div></div>
+    <div class="objectOuter"><div class="bigTall tc"></div></div>
+    <div class="objectOuter"><div class="bigTall cr"></div></div>
+    <div class="objectOuter"><div class="bigTall default"></div></div>
+    <br>
+    <!-- small: -->
+    <div class="objectOuter"><div class="small tr"></div></div>
+    <div class="objectOuter"><div class="small bl"></div></div>
+    <div class="objectOuter"><div class="small tl"></div></div>
+    <div class="objectOuter"><div class="small br"></div></div>
+    <div class="objectOuter"><div class="small tc"></div></div>
+    <div class="objectOuter"><div class="small cr"></div></div>
+    <div class="objectOuter"><div class="small default"></div></div>
+    <br>
+    <!-- FIXME: Workaround to ensure CSS background-image is loaded before taking screenshot: https://github.com/LadybirdBrowser/ladybird/issues/3448 -->
+    <img style="display: none;" src="support/colors-16x8.png" />
+  </body>
+</html>

commit 25b27eab99d80f10e55f6f8ad96b4a48c85fedcf

Tests: Regression test recent image rendering bug when scrolling
e055927ead introduced a regression that caused images to not be rendered
when the page was scrolled. This was subsequently reverted in
16b14273d1. To ensure this mistake cannot happen again, this commit
introduces a regression test for that error case.

Tests/LibWeb/Ref/expected/bitmaps-with-scroll-offset-ref.html
@@ -1,0 +1,19 @@
+<!DOCTYPE html>
+<style>
+html { scrollbar-width: none; }
+body {
+  position: relative;
+  height: 400vh;
+}
+#background-image {
+  display: inline-block;
+  width: 120px;
+  height: 120px;
+  background-image: url("../../Assets/120.png");
+  background-repeat: no-repeat;
+}
+</style>
+<div id="container">
+  <img src="../../Assets/120.png" />
+  <div id="background-image"></div>
+</div>

Tests/LibWeb/Ref/input/bitmaps-with-scroll-offset.html
@@ -1,0 +1,27 @@
+<!DOCTYPE html>
+<link rel="match" href="../expected/bitmaps-with-scroll-offset-ref.html" />
+<style>
+html { scrollbar-width: none; }
+body {
+  position: relative;
+  height: 400vh;
+}
+#container {
+  position: absolute;
+  top: 200px;
+}
+#background-image {
+  display: inline-block;
+  width: 120px;
+  height: 120px;
+  background-image: url("../../Assets/120.png");
+  background-repeat: no-repeat;
+}
+</style>
+<div id="container">
+  <img src="../../Assets/120.png" />
+  <div id="background-image"></div>
+</div>
+<script>
+window.scrollTo(0, 200);
+</script>