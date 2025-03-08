afebc7224cba691d45947b805d5a8d71729d91a7
Shannon Booth <shannon@serenityos.org>
2025-03-08 18:05:39 +1300

    LibWeb/DOM: Remove incorrect optimization assigning slots for a tree
    It turns out that this optimization does not hold, and results in
    the algorithm not running in "Moving a slot runs the assign
    slottables algorithm" WPT subtest of `slotchange-events.html`,
    resulting in a timeout.
    
    We will need to find another method of making this optimization

--- Libraries/LibWeb/DOM/Slottable.cpp
+++ Libraries/LibWeb/DOM/Slottable.cpp
@@ -178,12 +178,6 @@ void assign_slottables(GC::Ref<HTML::HTMLSlotElement> slot)
 // https://dom.spec.whatwg.org/#assign-slotables-for-a-tree
 void assign_slottables_for_a_tree(GC::Ref<Node> root)
 {
-    // AD-HOC: This method iterates over the root's entire subtree. That iteration does nothing if the root is not a
-    //         shadow root (see `find_slottables`). This iteration can be very expensive as the HTML parser inserts
-    //         nodes, especially on sites with many elements. So we skip it if we know it's going to be a no-op anyways.
-    if (!root->is_shadow_root())
-        return;
-
     // To assign slottables for a tree, given a node root, run assign slottables for each slot slot in root’s inclusive
     // descendants, in tree order.
     root->for_each_in_inclusive_subtree_of_type<HTML::HTMLSlotElement>([](auto& slot) {
