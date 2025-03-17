c0f7acd20af74cdeb5a419a7619a2ee2080bc4f5
Andrew Kaster <andrew@ladybird.org>
2025-03-17 17:46:42 -0600

    LibCore+Meta: Un-break Swift build on Linux
    LibCore's list of ignored header files for Swift was missing the Apple
    only files on non-Apple platforms. Additionally, any generic glue code
    cannot use -fobjc-arc, so we need to rely on -fblocks only.

--- Libraries/LibCore/EventSwift.mm
+++ Libraries/LibCore/EventSwift.mm
@@ -7,16 +7,10 @@
 #include <LibCore/EventLoop.h>
 #include <LibCore/EventSwift.h>
 
-#if !__has_feature(objc_arc)
-#    error "This file requires ARC"
-#endif
-
 namespace Core {
 void deferred_invoke_block(EventLoop& event_loop, void (^invokee)(void))
 {
-    event_loop.deferred_invoke([invokee = move(invokee)] {
-        invokee();
-    });
+    event_loop.deferred_invoke(invokee);
 }
 
 }
