77dfdebadb9b503f92c6b5f7d96e009ada09f363
Shannon Booth <shannon@serenityos.org>
2025-03-18 19:00:45 +1300

    LibURL/Pattern: Implement ability to generate a pattern string
    Compiling a URLPattern component will generate a 'parts list' which
    is used for generating the regular expression that is used for
    matching against URLs.
    
    This parts list is also used to generate (through this function) a
    pattern string. The pattern string of a URL component is what is
    exposed on the USVString getters of the URLPattern class itself.
    
    As an example, the following:
    
    ```
    let pattern = new URLPattern({ "pathname": "/foo/(.*)*" });
    console.log(pattern.pathname);
    ```
    
    Will log the pattern string of: '/foo/**'.

--- Libraries/LibURL/Pattern/String.h
+++ Libraries/LibURL/Pattern/String.h
@@ -7,9 +7,14 @@
 #pragma once
 
 #include <AK/String.h>
+#include <LibURL/Pattern/Options.h>
+#include <LibURL/Pattern/Part.h>
 
 namespace URL::Pattern {
 
 String escape_a_pattern_string(String const&);
+String escape_a_regexp_string(String const&);
+String generate_a_segment_wildcard_regexp(Options const&);
+String generate_a_pattern_string(ReadonlySpan<Part>, Options const&);
 
 }
