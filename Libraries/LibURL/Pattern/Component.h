404e151bf648f57fa4c73b804b2d6f43ebfe049e
Shannon Booth <shannon@serenityos.org>
2025-03-18 19:45:08 +1300

    LibURL/Pattern: Implement generating a component match result

--- Libraries/LibURL/Pattern/Component.h
+++ Libraries/LibURL/Pattern/Component.h
@@ -15,6 +15,16 @@ namespace URL::Pattern {
 
 // https://urlpattern.spec.whatwg.org/#component
 struct Component {
+    static PatternErrorOr<Component> compile(Utf8View const& input, PatternParser::EncodingCallback, Options const&);
+
+    // https://urlpattern.spec.whatwg.org/#dictdef-urlpatterncomponentresult
+    struct Result {
+        String input;
+        OrderedHashMap<String, Variant<String, Empty>> groups;
+    };
+
+    Result create_match_result(String const& input, regex::RegexResult const& exec_result) const;
+
     // https://urlpattern.spec.whatwg.org/#component-pattern-string
     // pattern string, a well formed pattern string
     String pattern_string;
@@ -30,8 +40,6 @@ struct Component {
     // https://urlpattern.spec.whatwg.org/#component-has-regexp-groups
     // has regexp groups, a boolean
     bool has_regexp_groups {};
-
-    static PatternErrorOr<Component> compile(Utf8View const& input, PatternParser::EncodingCallback, Options const&);
 };
 
 bool protocol_component_matches_a_special_scheme(Component const& protocol_component);
