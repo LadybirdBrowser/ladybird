6d48a01b5732da3e2e3fde375be2392b436a3a24
stelar7 <dudedbz@gmail.com>
2025-03-24 20:50:52 +0100

    LibWeb/IDB: Implement IDBObjectStore::keyPath

--- Libraries/LibWeb/IndexedDB/IDBObjectStore.cpp
+++ Libraries/LibWeb/IndexedDB/IDBObjectStore.cpp
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/String.h>
+#include <LibJS/Runtime/Array.h>
 #include <LibWeb/Bindings/IDBObjectStorePrototype.h>
 #include <LibWeb/Bindings/Intrinsics.h>
 #include <LibWeb/HTML/EventNames.h>
@@ -38,6 +40,23 @@ void IDBObjectStore::visit_edges(Visitor& visitor)
     Base::visit_edges(visitor);
     visitor.visit(m_store);
     visitor.visit(m_transaction);
+}
+
+// https://w3c.github.io/IndexedDB/#dom-idbobjectstore-keypath
+JS::Value IDBObjectStore::key_path() const
+{
+    if (!m_store->key_path().has_value())
+        return JS::js_null();
+
+    return m_store->key_path().value().visit(
+        [&](String const& value) -> JS::Value {
+            return JS::PrimitiveString::create(realm().vm(), value);
+        },
+        [&](Vector<String> const& value) -> JS::Value {
+            return JS::Array::create_from<String>(realm(), value.span(), [&](auto const& entry) -> JS::Value {
+                return JS::PrimitiveString::create(realm().vm(), entry);
+            });
+        });
 }
 
 }
