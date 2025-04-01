fba7ad6969530d05fbb443410a1a91c3c43392f1
stelar7 <dudedbz@gmail.com>
2025-04-01 18:38:55 +0200

    LibWeb/IDB: Implement IDBObjectStore::indexNames

--- Libraries/LibWeb/IndexedDB/IDBObjectStore.h
+++ Libraries/LibWeb/IndexedDB/IDBObjectStore.h
@@ -40,6 +40,7 @@ public:
     AK::HashMap<String, GC::Ref<Index>>& index_set() { return m_indexes; }
 
     WebIDL::ExceptionOr<GC::Ref<IDBIndex>> create_index(String const&, KeyPath, IDBIndexParameters options);
+    [[nodiscard]] GC::Ref<HTML::DOMStringList> index_names();
 
 protected:
     explicit IDBObjectStore(JS::Realm&, GC::Ref<ObjectStore>, GC::Ref<IDBTransaction>);
