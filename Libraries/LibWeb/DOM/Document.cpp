201f1bfac023780ccf8d35dae9c372c2709b23e8
Simon Farre <simon.farre.cx@gmail.com>
2025-04-01 14:24:55 +0200

    LibWeb: Add `removing steps` and `unloading document cleanup steps`
    When a node is removed, this ensures that the "removing steps" for
    fullscreen API is run.
    
    The Fullscreen API specification also states that when a document is
    running it's unloading document cleanup steps, it should exit fullscreen
    fully.

--- Libraries/LibWeb/DOM/Document.cpp
+++ Libraries/LibWeb/DOM/Document.cpp
@@ -3972,6 +3972,7 @@ void Document::run_unloading_cleanup_steps()
     }
 
     FileAPI::run_unloading_cleanup_steps(*this);
+    exit_fullscreen_fully();
 }
 
 // https://html.spec.whatwg.org/multipage/document-lifecycle.html#destroy-a-document
