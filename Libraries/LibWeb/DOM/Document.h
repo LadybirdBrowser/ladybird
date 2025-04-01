53ca4eeb27a615cab780fecae2e6fb98ca3bcb54
Simon Farre <simon.farre.cx@gmail.com>
2025-04-01 14:23:04 +0200

    LibWeb: Implement exitFullscreen algorithm
    Exiting fullscreen from the UI will be added in future commits.

--- Libraries/LibWeb/DOM/Document.h
+++ Libraries/LibWeb/DOM/Document.h
@@ -992,6 +992,11 @@ public:
     bool fullscreen() const;
     bool fullscreen_enabled() const;
 
+    void fully_exit_fullscreen();
+    GC::Ref<WebIDL::Promise> exit_fullscreen();
+
+    void unfullscreen_element(GC::Ref<Element> element);
+
     auto& script_blocking_style_sheet_set() { return m_script_blocking_style_sheet_set; }
     auto const& script_blocking_style_sheet_set() const { return m_script_blocking_style_sheet_set; }
 
@@ -1037,6 +1042,9 @@ private:
 
     void evaluate_media_rules();
 
+    bool is_simple_fullscreen_document() const;
+    GC::Ref<GC::HeapVector<GC::Ref<Document>>> collect_documents_to_unfullscreen();
+
     enum class AddLineFeed {
         Yes,
         No,
@@ -1071,6 +1079,8 @@ private:
     void build_registered_properties_cache();
 
     void ensure_cookie_version_index(URL::URL const& new_url, URL::URL const& old_url = {});
+
+    void unfullscreen();
 
     GC::Ref<Page> m_page;
     GC::Ptr<CSS::StyleComputer> m_style_computer;
