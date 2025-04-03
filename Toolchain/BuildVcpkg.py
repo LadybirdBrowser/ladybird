20352cc05b41fc734708e6314a5aaf7438c545c8
Andrew Kaster <andrew@ladybird.org>
2025-04-03 07:24:10 -0600

    Meta: Update vcpkg to latest main revision
    This should resolve CMake 4.0 issues on macOS using the fix upstream
    vcpkg came up with.

--- Toolchain/BuildVcpkg.py
+++ Toolchain/BuildVcpkg.py
@@ -10,7 +10,7 @@ def main() -> int:
     script_dir = pathlib.Path(__file__).parent.resolve()
 
     git_repo = "https://github.com/microsoft/vcpkg.git"
-    git_rev = "ea2a964f9303270322cf3f2d51c265ba146c422d"  # main on 2025-04-01
+    git_rev = "533a5fda5c0646d1771345fb572e759283444d5f"  # main on 2025-04-03
 
     build_dir = script_dir.parent / "Build"
     build_dir.mkdir(parents=True, exist_ok=True)
