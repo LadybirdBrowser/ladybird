5436d5803715bcf6fcafea7bb263022917ca61c3
rmg-x <git@rmgx.io>
2025-04-05 11:17:13 -0500

    AK+Tests: Add support for URI syntax in `IPv6Address::from_string`
    This supports IPv6 strings that start with `[` and end with `]` in
    accordance with RFC3986 which states:
    
    A host identified by an Internet Protocol literal address, version 6
    [RFC3513] or later, is distinguished by enclosing the IP literal
    within square brackets ("[" and "]").  This is the only place where
    square bracket characters are allowed in the URI syntax.

--- AK/IPv6Address.h
+++ AK/IPv6Address.h
@@ -111,6 +111,18 @@ public:
         if (string.is_null())
             return {};
 
+        // NOTE: This supports URI syntax (square brackets) for IPv6 addresses.
+        //       See: https://datatracker.ietf.org/doc/html/rfc3986#section-3.2.2
+        auto const starts_with_bracket = string.starts_with('[');
+        auto const ends_with_bracket = string.ends_with(']');
+
+        if (starts_with_bracket || ends_with_bracket) {
+            if (!starts_with_bracket || !ends_with_bracket)
+                return {};
+
+            string = string.substring_view(1, string.length() - 2);
+        }
+
         auto const parts = string.split_view(':', SplitBehavior::KeepEmpty);
         if (parts.is_empty())
             return {};
