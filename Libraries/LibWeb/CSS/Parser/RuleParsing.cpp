1033bf5cd2c7f3893d0f4d5d7972ed94e77c3b64
Sam Atkins <sam@ladybird.org>
2025-04-08 16:50:51 +0100

    LibWeb/CSS: Allow CSSImportRule's document to be null on construction
    It's possible to parse an `@import` rule that isn't attached to a
    document. We only actually need it to have one when fetching the linked
    style sheet, and that should only happen when the CSSImportRule is
    attached to a document. So, we can just accept a null pointer when
    constructing it.
    
    We relied on that Document to get the Realm, so pass that in as a
    separate parameter.

--- Libraries/LibWeb/CSS/Parser/RuleParsing.cpp
+++ Libraries/LibWeb/CSS/Parser/RuleParsing.cpp
@@ -198,7 +198,7 @@ GC::Ptr<CSSImportRule> Parser::convert_to_import_rule(AtRule const& rule)
         return {};
     }
 
-    return CSSImportRule::create(resolved_url.release_value(), const_cast<DOM::Document&>(*document()), supports, move(media_query_list));
+    return CSSImportRule::create(realm(), resolved_url.release_value(), const_cast<DOM::Document*>(m_document.ptr()), supports, move(media_query_list));
 }
 
 Optional<FlyString> Parser::parse_layer_name(TokenStream<ComponentValue>& tokens, AllowBlankLayerName allow_blank_layer_name)
