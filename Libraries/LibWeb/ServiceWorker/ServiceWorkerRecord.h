73b981e339ac1e4af1a07ceb4f810b9f11731dc7
Shannon Booth <shannon@serenityos.org>
2025-04-08 17:29:17 +1200

    LibWeb/ServiceWorker: Adjust comments for ServiceWorkerRecord

--- Libraries/LibWeb/ServiceWorker/ServiceWorkerRecord.h
+++ Libraries/LibWeb/ServiceWorker/ServiceWorkerRecord.h
@@ -17,9 +17,17 @@ namespace Web::ServiceWorker {
 // FIXME: This should be owned and managed at the user agent level
 // FIXME: A lot of the fields for this struct actually need to live in the Agent for the service worker in the WebWorker process
 struct ServiceWorkerRecord {
-    Bindings::ServiceWorkerState state = Bindings::ServiceWorkerState::Parsed; // https://w3c.github.io/ServiceWorker/#dfn-state
-    URL::URL script_url;                                                       // https://w3c.github.io/ServiceWorker/#dfn-script-url
-    Bindings::WorkerType worker_type = Bindings::WorkerType::Classic;          // https://w3c.github.io/ServiceWorker/#dfn-type
+    // https://w3c.github.io/ServiceWorker/#dfn-state
+    // A service worker has an associated state, which is one of "parsed", "installing", "installed", "activating", "activated", and "redundant". It is initially "parsed".
+    Bindings::ServiceWorkerState state = Bindings::ServiceWorkerState::Parsed;
+
+    // https://w3c.github.io/ServiceWorker/#dfn-script-url
+    // A service worker has an associated script url (a URL).
+    URL::URL script_url;
+
+    // https://w3c.github.io/ServiceWorker/#dfn-type
+    // A service worker has an associated type which is either "classic" or "module". Unless stated otherwise, it is "classic".
+    Bindings::WorkerType worker_type = Bindings::WorkerType::Classic;
 
     // FIXME: A lot more fields after this...
 };
