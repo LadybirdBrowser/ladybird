80c36a90ee65c20628472053b7cd368f995cb93a
stelar7 <dudedbz@gmail.com>
2025-04-11 11:38:29 +0200

    LibWeb/IDB: Implement store_a_record_into_an_object_store

--- Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
+++ Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
@@ -40,5 +40,6 @@ ErrorOr<u64> generate_a_key(GC::Ref<ObjectStore>);
 void possibly_update_the_key_generator(GC::Ref<ObjectStore>, GC::Ref<Key>);
 void inject_a_key_into_a_value_using_a_key_path(JS::Realm&, JS::Value, GC::Ref<Key>, KeyPath const&);
 void delete_records_from_an_object_store(GC::Ref<ObjectStore>, GC::Ref<IDBKeyRange>);
+WebIDL::ExceptionOr<GC::Ptr<Key>> store_a_record_into_an_object_store(JS::Realm&, GC::Ref<ObjectStore>, JS::Value, GC::Ptr<Key>, bool);
 
 }
