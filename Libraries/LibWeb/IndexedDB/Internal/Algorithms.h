d9a0f1b03dd832265b3fdb2e7562035e491589ac
stelar7 <dudedbz@gmail.com>
2025-04-11 10:54:23 +0200

    LibWeb/IDB: Implement check_that_a_key_could_be_injected_into_a_value

--- Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
+++ Libraries/LibWeb/IndexedDB/Internal/Algorithms.h
@@ -32,5 +32,6 @@ WebIDL::ExceptionOr<JS::Value> clone_in_realm(JS::Realm&, JS::Value, GC::Ref<IDB
 WebIDL::ExceptionOr<ErrorOr<GC::Ref<Key>>> convert_a_value_to_a_multi_entry_key(JS::Realm&, JS::Value);
 WebIDL::ExceptionOr<ErrorOr<JS::Value>> evaluate_key_path_on_a_value(JS::Realm&, JS::Value, KeyPath const&);
 WebIDL::ExceptionOr<ErrorOr<GC::Ref<Key>>> extract_a_key_from_a_value_using_a_key_path(JS::Realm&, JS::Value, KeyPath const&, bool = false);
+bool check_that_a_key_could_be_injected_into_a_value(JS::Realm&, JS::Value, KeyPath const&);
 
 }
