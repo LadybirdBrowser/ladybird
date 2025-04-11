eff9d4881c7516b03ffcc9768528b18dee3c439c
Jonne Ransijn <jonne@yyny.dev>
2025-04-11 15:10:51 +0200

    AK: Add `IsScalar` to `StdLibExtraDetails.h`
    This matches the behaviour of `std::is_scalar_v`

--- AK/StdLibExtraDetails.h
+++ AK/StdLibExtraDetails.h
@@ -407,6 +407,9 @@ inline constexpr bool IsArithmetic = IsIntegral<T> || IsFloatingPoint<T>;
 template<typename T>
 inline constexpr bool IsFundamental = IsArithmetic<T> || IsVoid<T> || IsNullPointer<T>;
 
+template<typename T>
+inline constexpr bool IsScalar = IsArithmetic<T> || IsEnum<T> || IsPointer<T> || IsNullPointer<T> || IsMemberPointer<T>;
+
 template<typename T, T... Ts>
 struct IntegerSequence {
     using Type = T;
@@ -705,6 +708,7 @@ using AK::Detail::IsRvalueReference;
 using AK::Detail::IsSame;
 using AK::Detail::IsSameIgnoringCV;
 using AK::Detail::IsSameIgnoringCVReference;
+using AK::Detail::IsScalar;
 using AK::Detail::IsSigned;
 using AK::Detail::IsSpecializationOf;
 using AK::Detail::IsTemplateBaseOf;
