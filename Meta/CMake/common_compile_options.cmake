8cdc484c8c69296ddb30dff98e3d2a9d455db4ac
Andrew Kaster <andrew@ladybird.org>
2025-04-14 16:18:16 -0600

    CMake: Remove warning suppression for unknown attributes
    We should take care of these with macros or other means instead of
    suppressing them.

--- Meta/CMake/common_compile_options.cmake
+++ Meta/CMake/common_compile_options.cmake
@@ -49,7 +49,6 @@ endmacro()
 
 # FIXME: Rework these flags to remove the suspicious ones.
 if (WIN32)
-  add_compile_options(-Wno-unknown-attributes) # [[no_unique_address]] is broken in MSVC ABI until next ABI break
   add_compile_options(-Wno-reinterpret-base-class)
   add_compile_options(-Wno-microsoft-unqualified-friend) # MSVC doesn't support unqualified friends
   add_compile_definitions(_CRT_SECURE_NO_WARNINGS) # _s replacements not desired (or implemented on any other platform other than VxWorks)
