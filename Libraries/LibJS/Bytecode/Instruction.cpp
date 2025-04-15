3abed01f9698fccb1c4f3a5fac40585e1821cbc2
Andrew Kaster <andrew@ladybird.org>
2025-04-15 15:58:42 -0600

    LibJS: Remove unused InstructionStreamIterator::source_code getter

--- Libraries/LibJS/Bytecode/Instruction.cpp
+++ Libraries/LibJS/Bytecode/Instruction.cpp
@@ -105,11 +105,6 @@ UnrealizedSourceRange InstructionStreamIterator::source_range() const
     };
 }
 
-RefPtr<SourceCode> InstructionStreamIterator::source_code() const
-{
-    return m_executable ? m_executable->source_code.ptr() : nullptr;
-}
-
 Operand::Operand(Register reg)
     : m_type(Type::Register)
     , m_index(reg.index())
