3ca38505fa8f3bc65c388f674d798364872c7c6a
Andreas Kling <andreas@ladybird.org>
2025-04-20 20:46:52 +0200

    LibWeb: Rebuild parent element's layout tree on <object> update
    These elements are quite special, so let's treat them like we do for
    substantial CSS display changes and rebuild the layout tree starting
    from the parent element instead of self.

--- Libraries/LibWeb/HTML/HTMLObjectElement.cpp
+++ Libraries/LibWeb/HTML/HTMLObjectElement.cpp
@@ -562,7 +562,9 @@ void HTMLObjectElement::update_layout_and_child_objects(Representation represent
 
     m_representation = representation;
     invalidate_style(DOM::StyleInvalidationReason::HTMLObjectElementUpdateLayoutAndChildObjects);
-    set_needs_layout_tree_update(true);
+
+    if (auto parent_element = this->parent_element())
+        parent_element->set_needs_layout_tree_update(true);
 }
 
 // https://html.spec.whatwg.org/multipage/interaction.html#dom-tabindex
