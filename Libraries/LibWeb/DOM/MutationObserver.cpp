084cceab5cf1e027bd5e12abfaee078810c9734e
Shannon Booth <shannon@serenityos.org>
2025-04-24 15:04:13 +1200

    LibWeb: Split out SimilarOriginWindowAgent from HTML::Agent
    To allow for adding the concept of a WorkerAgent to be reused
    between shared and dedicated workers. An event loop is the
    commonality between the different agent types, though, there
    are some differences between those event loops which we customize
    on the construction of the HTML::EventLoop.

--- Libraries/LibWeb/DOM/MutationObserver.cpp
+++ Libraries/LibWeb/DOM/MutationObserver.cpp
@@ -9,7 +9,7 @@
 #include <LibWeb/Bindings/MutationObserverPrototype.h>
 #include <LibWeb/DOM/MutationObserver.h>
 #include <LibWeb/DOM/Node.h>
-#include <LibWeb/HTML/Scripting/Agent.h>
+#include <LibWeb/HTML/Scripting/SimilarOriginWindowAgent.h>
 
 namespace Web::DOM {
 
@@ -30,7 +30,7 @@ MutationObserver::MutationObserver(JS::Realm& realm, GC::Ptr<WebIDL::CallbackTyp
     // 1. Set this’s callback to callback.
 
     // 2. Append this to this’s relevant agent’s mutation observers.
-    HTML::relevant_agent(*this).mutation_observers.append(*this);
+    HTML::relevant_similar_origin_window_agent(*this).mutation_observers.append(*this);
 }
 
 MutationObserver::~MutationObserver()
@@ -39,7 +39,7 @@ MutationObserver::~MutationObserver()
 
 void MutationObserver::finalize()
 {
-    HTML::relevant_agent(*this).mutation_observers.remove(*this);
+    HTML::relevant_similar_origin_window_agent(*this).mutation_observers.remove(*this);
 }
 
 void MutationObserver::initialize(JS::Realm& realm)
--- Libraries/LibWeb/HTML/Scripting/Agent.h
+++ Libraries/LibWeb/HTML/Scripting/Agent.h
@@ -6,44 +6,19 @@
 
 #pragma once
 
-#include <AK/OwnPtr.h>
-#include <AK/Vector.h>
 #include <LibGC/Root.h>
 #include <LibJS/Forward.h>
 #include <LibJS/Runtime/Agent.h>
-#include <LibWeb/DOM/MutationObserver.h>
 #include <LibWeb/Forward.h>
-#include <LibWeb/HTML/CustomElements/CustomElementReactionsStack.h>
 
 namespace Web::HTML {
 
-// https://html.spec.whatwg.org/multipage/webappapis.html#similar-origin-window-agent
 struct Agent : public JS::Agent {
+    // https://html.spec.whatwg.org/multipage/webappapis.html#window-event-loop
+    // The event loop of a similar-origin window agent is known as a window event loop.
+    // The event loop of a dedicated worker agent, shared worker agent, or service worker agent is known as a worker event loop.
+    // And the event loop of a worklet agent is known as a worklet event loop.
     GC::Root<HTML::EventLoop> event_loop;
-
-    // FIXME: These should only be on similar-origin window agents, but we don't currently differentiate agent types.
-
-    // https://dom.spec.whatwg.org/#mutation-observer-compound-microtask-queued-flag
-    bool mutation_observer_microtask_queued { false };
-
-    // https://dom.spec.whatwg.org/#mutation-observer-list
-    DOM::MutationObserver::List mutation_observers;
-
-    // https://html.spec.whatwg.org/multipage/custom-elements.html#custom-element-reactions-stack
-    // Each similar-origin window agent has a custom element reactions stack, which is initially empty.
-    CustomElementReactionsStack custom_element_reactions_stack {};
-
-    // https://dom.spec.whatwg.org/#signal-slot-list
-    // Each similar-origin window agent has signal slots (a set of slots), which is initially empty. [HTML]
-    Vector<GC::Root<HTML::HTMLSlotElement>> signal_slots;
-
-    // https://html.spec.whatwg.org/multipage/custom-elements.html#current-element-queue
-    // A similar-origin window agent's current element queue is the element queue at the top of its custom element reactions stack.
-    Vector<GC::Root<DOM::Element>>& current_element_queue() { return custom_element_reactions_stack.element_queue_stack.last(); }
-    Vector<GC::Root<DOM::Element>> const& current_element_queue() const { return custom_element_reactions_stack.element_queue_stack.last(); }
-
-    // [[CanBlock]]
-    virtual bool can_block() const override;
 
     virtual void spin_event_loop_until(GC::Root<GC::Function<bool()>> goal_condition) override;
 };
