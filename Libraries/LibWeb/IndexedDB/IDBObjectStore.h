d5cf2cee41c823539a3a3907d39babe0479cc6e5
stelar7 <dudedbz@gmail.com>
2025-04-28 15:52:54 +0200

    LibWeb/IDB: Implement IDBObjectStore::count

--- Libraries/LibWeb/IndexedDB/IDBObjectStore.h
+++ Libraries/LibWeb/IndexedDB/IDBObjectStore.h
@@ -47,6 +47,7 @@ public:
     [[nodiscard]] WebIDL::ExceptionOr<GC::Ref<IDBRequest>> add_or_put(GC::Ref<IDBObjectStore>, JS::Value, Optional<JS::Value> const&, bool);
     [[nodiscard]] WebIDL::ExceptionOr<GC::Ref<IDBRequest>> add(JS::Value value, Optional<JS::Value> const& key);
     [[nodiscard]] WebIDL::ExceptionOr<GC::Ref<IDBRequest>> put(JS::Value value, Optional<JS::Value> const& key);
+    [[nodiscard]] WebIDL::ExceptionOr<GC::Ref<IDBRequest>> count(Optional<JS::Value>);
 
 protected:
     explicit IDBObjectStore(JS::Realm&, GC::Ref<ObjectStore>, GC::Ref<IDBTransaction>);
