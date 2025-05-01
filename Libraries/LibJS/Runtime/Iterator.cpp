0eeec6756b062d9d341522886f88567d991ad066
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2025-05-01 18:27:55 +0300

    LibJS: Skip "return" method call while closing built-in iterators
    "return" method is not defined on any of builtin iterators, so we could
    skip it, avoiding method lookup.
    
    I measured 10% improvement in array-destructuring-assignment.js micro
    benchmark with this change.

--- Libraries/LibJS/Runtime/Iterator.cpp
+++ Libraries/LibJS/Runtime/Iterator.cpp
@@ -271,6 +271,10 @@ static Completion iterator_close_impl(VM& vm, IteratorRecord const& iterator_rec
     // 2. Let iterator be iteratorRecord.[[Iterator]].
     auto iterator = iterator_record.iterator;
 
+    // OPTIMIZATION: "return" method is not defined on any of iterators we treat as built-in.
+    if (iterator->as_builtin_iterator())
+        return completion;
+
     // 3. Let innerResult be Completion(GetMethod(iterator, "return")).
     auto inner_result = ThrowCompletionOr<Value> { js_undefined() };
     auto get_method_result = Value(iterator).get_method(vm, vm.names.return_);
