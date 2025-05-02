1c26b4cd45291f3f3995bc78ad3681a0a37d9e4a
stelar7 <dudedbz@gmail.com>
2025-05-02 14:31:58 +0200

    LibWeb/IDB: Dont move away the name when creating an Index

--- Libraries/LibWeb/IndexedDB/Internal/Index.cpp
+++ Libraries/LibWeb/IndexedDB/Internal/Index.cpp
@@ -13,14 +13,14 @@ GC_DEFINE_ALLOCATOR(Index);
 
 Index::~Index() = default;
 
-GC::Ref<Index> Index::create(JS::Realm& realm, GC::Ref<ObjectStore> store, String name, KeyPath const& key_path, bool unique, bool multi_entry)
+GC::Ref<Index> Index::create(JS::Realm& realm, GC::Ref<ObjectStore> store, String const& name, KeyPath const& key_path, bool unique, bool multi_entry)
 {
     return realm.create<Index>(store, name, key_path, unique, multi_entry);
 }
 
-Index::Index(GC::Ref<ObjectStore> store, String name, KeyPath const& key_path, bool unique, bool multi_entry)
+Index::Index(GC::Ref<ObjectStore> store, String const& name, KeyPath const& key_path, bool unique, bool multi_entry)
     : m_object_store(store)
-    , m_name(move(name))
+    , m_name(name)
     , m_unique(unique)
     , m_multi_entry(multi_entry)
     , m_key_path(key_path)
