31576185f111606010dd782577e6f17ddd63dce5
Andreas Kling <andreas@ladybird.org>
2025-05-03 15:40:29 +0200

    LibJS: Use PrimitiveString more instead of Utf16String in RegExp code
    PrimitiveString has an internal UTF-16 string cache anyway, and so this
    actually avoids repeatedly converting between UTF-8 and UTF-16.

--- Libraries/LibJS/Runtime/RegExpStringIterator.h
+++ Libraries/LibJS/Runtime/RegExpStringIterator.h
@@ -6,9 +6,8 @@
 
 #pragma once
 
-#include <AK/Utf16View.h>
 #include <LibJS/Runtime/Object.h>
-#include <LibJS/Runtime/Utf16String.h>
+#include <LibJS/Runtime/PrimitiveString.h>
 
 namespace JS {
 
@@ -17,12 +16,12 @@ class RegExpStringIterator final : public Object {
     GC_DECLARE_ALLOCATOR(RegExpStringIterator);
 
 public:
-    static GC::Ref<RegExpStringIterator> create(Realm&, Object& regexp_object, Utf16String string, bool global, bool unicode);
+    static GC::Ref<RegExpStringIterator> create(Realm&, Object& regexp_object, GC::Ref<PrimitiveString> string, bool global, bool unicode);
 
     virtual ~RegExpStringIterator() override = default;
 
     Object& regexp_object() { return m_regexp_object; }
-    Utf16String const& string() const { return m_string; }
+    GC::Ref<PrimitiveString> string() const { return m_string; }
     bool global() const { return m_global; }
     bool unicode() const { return m_unicode; }
 
@@ -30,12 +29,12 @@ public:
     void set_done() { m_done = true; }
 
 private:
-    explicit RegExpStringIterator(Object& prototype, Object& regexp_object, Utf16String string, bool global, bool unicode);
+    explicit RegExpStringIterator(Object& prototype, Object& regexp_object, GC::Ref<PrimitiveString> string, bool global, bool unicode);
 
     virtual void visit_edges(Cell::Visitor&) override;
 
     GC::Ref<Object> m_regexp_object;
-    Utf16String m_string;
+    GC::Ref<PrimitiveString> m_string;
     bool m_global { false };
     bool m_unicode { false };
     bool m_done { false };
