LadybirdBrowser/ladybird

commit 6a549f62702e8b733dd4fc0caf1fa3e114243070

LibWeb: Replace InlinePaintable with PaintableWithLines created per line
InlinePaintable was an ad-hoc paintable type required to support the
fragmentation of inline nodes across multiple lines. It existed because
there was no way to associate multiple paintables with a single layout
node. This resulted in a lot of duplicated code between PaintableBox and
InlinePaintable. For example, most of the CSS properties like
background, border, shadows, etc. and hit-testing are almost identical
for both of them. However, the code had to be duplicated to account for
the fact that InlinePaintable creates a box for each line. And we had
quite many places that operate on paintables with a code like:
```
if (box.is_paintable_box()) {
  // do something
} else (box.is_inline_paintable()) {
  // do exactly the same as for paintable box but using InlinePaintable
}
```

This change replaces the usage of `InlinePaintable` with
`PaintableWithLines` created for each line, which is now possible
because we support having multiple paintables per layout node. By doing
that, we remove lots of duplicated code and bring our implementation
closer to the spec.

Userland/Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2022-2023, Andreas Kling <andreas@ladybird.org>
  * Copyright (c) 2022-2023, Sam Atkins <atkinssj@serenityos.org>
+ * Copyright (c) 2024, Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -34,12 +35,27 @@ JS::NonnullGCPtr<PaintableWithLines> PaintableWithLines::create(Layout::BlockCon
     return block_container.heap().allocate_without_realm<PaintableWithLines>(block_container);
 }
 
+JS::NonnullGCPtr<PaintableWithLines> PaintableWithLines::create(Layout::InlineNode const& inline_node, size_t line_index)
+{
+    return inline_node.heap().allocate_without_realm<PaintableWithLines>(inline_node, line_index);
+}
+
 JS::NonnullGCPtr<PaintableBox> PaintableBox::create(Layout::Box const& layout_box)
 {
     return layout_box.heap().allocate_without_realm<PaintableBox>(layout_box);
 }
 
+JS::NonnullGCPtr<PaintableBox> PaintableBox::create(Layout::InlineNode const& layout_box)
+{
+    return layout_box.heap().allocate_without_realm<PaintableBox>(layout_box);
+}
+
 PaintableBox::PaintableBox(Layout::Box const& layout_box)
+    : Paintable(layout_box)
+{
+}
+
+PaintableBox::PaintableBox(Layout::InlineNode const& layout_box)
     : Paintable(layout_box)
 {
 }
@@ -50,6 +66,12 @@ PaintableBox::~PaintableBox()
 
 PaintableWithLines::PaintableWithLines(Layout::BlockContainer const& layout_box)
     : PaintableBox(layout_box)
+{
+}
+
+PaintableWithLines::PaintableWithLines(Layout::InlineNode const& inline_node, size_t line_index)
+    : PaintableBox(inline_node)
+    , m_line_index(line_index)
 {
 }
 
@@ -112,7 +134,7 @@ void PaintableBox::set_scroll_offset(CSSPixelPoint offset)
     // the user agent must run these steps:
 
     // 1. Let doc be the element’s node document.
-    auto& document = layout_box().document();
+    auto& document = layout_node().document();
 
     // FIXME: 2. If the element is a snap container, run the steps to update snapchanging targets for the element with
     //           the element’s eventual snap target in the block axis as newBlockTarget and the element’s eventual snap
@@ -125,7 +147,7 @@ void PaintableBox::set_scroll_offset(CSSPixelPoint offset)
         return;
 
     // 4. Append the element to doc’s pending scroll event targets.
-    document.pending_scroll_event_targets().append(*layout_box().dom_node());
+    document.pending_scroll_event_targets().append(*layout_node_with_style_and_box_metrics().dom_node());
 
     set_needs_display(InvalidateDisplayList::No);
 }
@@ -143,7 +165,9 @@ void PaintableBox::set_offset(CSSPixelPoint offset)
 void PaintableBox::set_content_size(CSSPixelSize size)
 {
     m_content_size = size;
-    layout_box().did_set_content_size();
+    if (is<Layout::Box>(Paintable::layout_node())) {
+        static_cast<Layout::Box&>(layout_node_with_style_and_box_metrics()).did_set_content_size();
+    }
 }
 
 CSSPixelPoint PaintableBox::offset() const
@@ -197,7 +221,7 @@ CSSPixelRect PaintableBox::absolute_paint_rect() const
 Optional<CSSPixelRect> PaintableBox::get_clip_rect() const
 {
     auto clip = computed_values().clip();
-    if (clip.is_rect() && layout_box().is_absolutely_positioned()) {
+    if (clip.is_rect() && layout_node_with_style_and_box_metrics().is_absolutely_positioned()) {
         auto border_box = absolute_border_box_rect();
         return clip.to_rect().resolved(layout_node(), border_box);
     }
@@ -238,7 +262,10 @@ void PaintableBox::after_paint(PaintContext& context, [[maybe_unused]] PaintPhas
 bool PaintableBox::is_scrollable(ScrollDirection direction) const
 {
     auto overflow = direction == ScrollDirection::Horizontal ? computed_values().overflow_x() : computed_values().overflow_y();
-    auto scrollable_overflow_size = direction == ScrollDirection::Horizontal ? scrollable_overflow_rect()->width() : scrollable_overflow_rect()->height();
+    auto scrollable_overflow_rect = this->scrollable_overflow_rect();
+    if (!scrollable_overflow_rect.has_value())
+        return false;
+    auto scrollable_overflow_size = direction == ScrollDirection::Horizontal ? scrollable_overflow_rect->width() : scrollable_overflow_rect->height();
     auto scrollport_size = direction == ScrollDirection::Horizontal ? absolute_padding_box_rect().width() : absolute_padding_box_rect().height();
     if ((is_viewport() && overflow != CSS::Overflow::Hidden) || overflow == CSS::Overflow::Auto)
         return scrollable_overflow_size > scrollport_size;
@@ -363,7 +390,7 @@ void PaintableBox::paint(PaintContext& context, PaintPhase phase) const
         }
     }
 
-    if (phase == PaintPhase::Overlay && layout_box().document().inspected_layout_node() == &layout_box()) {
+    if (phase == PaintPhase::Overlay && layout_node().document().inspected_layout_node() == &layout_node_with_style_and_box_metrics()) {
         auto content_rect = absolute_rect();
 
         auto margin_box = box_model().margin_box();
@@ -390,10 +417,10 @@ void PaintableBox::paint(PaintContext& context, PaintPhase phase) const
         auto& font = Platform::FontPlugin::the().default_font();
 
         StringBuilder builder;
-        if (layout_box().dom_node())
-            builder.append(layout_box().dom_node()->debug_description());
+        if (layout_node_with_style_and_box_metrics().dom_node())
+            builder.append(layout_node_with_style_and_box_metrics().dom_node()->debug_description());
         else
-            builder.append(layout_box().debug_description());
+            builder.append(layout_node_with_style_and_box_metrics().debug_description());
         builder.appendff(" {}x{} @ {},{}", border_rect.width(), border_rect.height(), border_rect.x(), border_rect.y());
         auto size_text = MUST(builder.to_string());
         auto size_text_rect = border_rect;
@@ -445,10 +472,10 @@ void PaintableBox::paint_backdrop_filter(PaintContext& context) const
 void PaintableBox::paint_background(PaintContext& context) const
 {
     // If the body's background properties were propagated to the root element, do no re-paint the body's background.
-    if (layout_box().is_body() && document().html_element()->should_use_body_background_properties())
+    if (layout_node_with_style_and_box_metrics().is_body() && document().html_element()->should_use_body_background_properties())
         return;
 
-    Painting::paint_background(context, layout_box(), computed_values().image_rendering(), m_resolved_background, normalized_border_radii_data());
+    Painting::paint_background(context, layout_node_with_style_and_box_metrics(), computed_values().image_rendering(), m_resolved_background, normalized_border_radii_data());
 }
 
 void PaintableBox::paint_box_shadow(PaintContext& context) const
@@ -787,14 +814,14 @@ bool PaintableBox::handle_mousewheel(Badge<EventHandler>, CSSPixelPoint, unsigne
     return true;
 }
 
-Layout::BlockContainer const& PaintableWithLines::layout_box() const
+Layout::NodeWithStyleAndBoxModelMetrics const& PaintableWithLines::layout_node_with_style_and_box_metrics() const
 {
-    return static_cast<Layout::BlockContainer const&>(PaintableBox::layout_box());
+    return static_cast<Layout::NodeWithStyleAndBoxModelMetrics const&>(PaintableBox::layout_node_with_style_and_box_metrics());
 }
 
-Layout::BlockContainer& PaintableWithLines::layout_box()
+Layout::NodeWithStyleAndBoxModelMetrics& PaintableWithLines::layout_node_with_style_and_box_metrics()
 {
-    return static_cast<Layout::BlockContainer&>(PaintableBox::layout_box());
+    return static_cast<Layout::NodeWithStyleAndBoxModelMetrics&>(PaintableBox::layout_node_with_style_and_box_metrics());
 }
 
 TraversalDecision PaintableBox::hit_test_scrollbars(CSSPixelPoint position, Function<TraversalDecision(HitTestResult)> const& callback) const
@@ -822,7 +849,7 @@ TraversalDecision PaintableBox::hit_test(CSSPixelPoint position, HitTestType typ
     if (hit_test_scrollbars(position_adjusted_by_scroll_offset, callback) == TraversalDecision::Break)
         return TraversalDecision::Break;
 
-    if (layout_box().is_viewport()) {
+    if (layout_node_with_style_and_box_metrics().is_viewport()) {
         auto& viewport_paintable = const_cast<ViewportPaintable&>(static_cast<ViewportPaintable const&>(*this));
         viewport_paintable.build_stacking_context_tree_if_needed();
         viewport_paintable.document().update_paint_and_hit_testing_properties_if_needed();
@@ -874,7 +901,7 @@ TraversalDecision PaintableWithLines::hit_test(CSSPixelPoint position, HitTestTy
     auto position_adjusted_by_scroll_offset = position;
     position_adjusted_by_scroll_offset.translate_by(-cumulative_offset_of_enclosing_scroll_frame());
 
-    if (!layout_box().children_are_inline() || m_fragments.is_empty()) {
+    if (!layout_node_with_style_and_box_metrics().children_are_inline() || m_fragments.is_empty()) {
         return PaintableBox::hit_test(position, type, callback);
     }
 
@@ -1102,7 +1129,7 @@ void PaintableBox::resolve_paint_properties()
     CSSPixelRect background_rect;
     Color background_color = computed_values.background_color();
     auto const* background_layers = &computed_values.background_layers();
-    if (layout_box().is_root_element()) {
+    if (layout_node_with_style_and_box_metrics().is_root_element()) {
         background_rect = navigable()->viewport_rect();
 
         // Section 2.11.2: If the computed value of background-image on the root element is none and its background-color is transparent,
@@ -1122,7 +1149,7 @@ void PaintableBox::resolve_paint_properties()
 
     m_resolved_background.layers.clear();
     if (background_layers) {
-        m_resolved_background = resolve_background_layers(*background_layers, layout_box(), background_color, background_rect, normalized_border_radii_data());
+        m_resolved_background = resolve_background_layers(*background_layers, layout_node_with_style_and_box_metrics(), background_color, background_rect, normalized_border_radii_data());
     };
 }
 

Tests/LibWeb/Text/input/hit_testing/inline-stacking-context.html
@@ -18,6 +18,6 @@ div {
 <script src="../include.js"></script>
 <script>
     test(() => {
-        println(internals.hitTest(50, 50).node.parentNode === document.getElementById("inline-stacking-context"));
+        println(internals.hitTest(50, 50).node === document.getElementById("inline-stacking-context"));
     });
 </script>

Userland/Libraries/LibWeb/DOM/Element.cpp
@@ -1364,7 +1364,7 @@ void Element::set_scroll_left(double x)
     if (!paintable_box())
         return;
 
-    if (!paintable_box()->layout_box().is_scroll_container())
+    if (!paintable_box()->layout_node_with_style_and_box_metrics().is_scroll_container())
         return;
 
     // FIXME: or the element has no overflow.
@@ -1421,7 +1421,7 @@ void Element::set_scroll_top(double y)
     if (!paintable_box())
         return;
 
-    if (!paintable_box()->layout_box().is_scroll_container())
+    if (!paintable_box()->layout_node_with_style_and_box_metrics().is_scroll_container())
         return;
 
     // FIXME: or the element has no overflow.

Userland/Libraries/LibWeb/Dump.cpp
@@ -383,11 +383,13 @@ void dump_tree(StringBuilder& builder, Layout::Node const& layout_node, bool sho
 
     if (is<Layout::InlineNode>(layout_node) && layout_node.first_paintable()) {
         auto const& inline_node = static_cast<Layout::InlineNode const&>(layout_node);
-        auto const& inline_paintable = static_cast<Painting::InlinePaintable const&>(*inline_node.first_paintable());
-        auto const& fragments = inline_paintable.fragments();
-        for (size_t fragment_index = 0; fragment_index < fragments.size(); ++fragment_index) {
-            auto const& fragment = fragments[fragment_index];
-            dump_fragment(fragment, fragment_index);
+        for (auto const& paintable : inline_node.paintables()) {
+            auto const& paintable_with_lines = static_cast<Painting::PaintableWithLines const&>(paintable);
+            auto const& fragments = paintable_with_lines.fragments();
+            for (size_t fragment_index = 0; fragment_index < fragments.size(); ++fragment_index) {
+                auto const& fragment = fragments[fragment_index];
+                dump_fragment(fragment, fragment_index);
+            }
         }
     }
 

Userland/Libraries/LibWeb/Forward.h
@@ -574,6 +574,7 @@ class CheckBox;
 class FlexFormattingContext;
 class FormattingContext;
 class ImageBox;
+class InlineNode;
 class InlineFormattingContext;
 class Label;
 class LabelableNode;

Userland/Libraries/LibWeb/Layout/Box.cpp
@@ -34,37 +34,6 @@ void Box::visit_edges(Cell::Visitor& visitor)
     visitor.visit(m_contained_abspos_children);
 }
 
-// https://www.w37.org/TR/css-overflow-3/#overflow-control
-static bool overflow_value_makes_box_a_scroll_container(CSS::Overflow overflow)
-{
-    switch (overflow) {
-    case CSS::Overflow::Clip:
-    case CSS::Overflow::Visible:
-        return false;
-    case CSS::Overflow::Auto:
-    case CSS::Overflow::Hidden:
-    case CSS::Overflow::Scroll:
-        return true;
-    }
-    VERIFY_NOT_REACHED();
-}
-
-// https://www.w3.org/TR/css-overflow-3/#scroll-container
-bool Box::is_scroll_container() const
-{
-    // NOTE: This isn't in the spec, but we want the viewport to behave like a scroll container.
-    if (is_viewport())
-        return true;
-
-    return overflow_value_makes_box_a_scroll_container(computed_values().overflow_x())
-        || overflow_value_makes_box_a_scroll_container(computed_values().overflow_y());
-}
-
-bool Box::is_body() const
-{
-    return dom_node() && dom_node() == document().body();
-}
-
 JS::GCPtr<Painting::Paintable> Box::create_paintable() const
 {
     return Painting::PaintableBox::create(*this);

Userland/Libraries/LibWeb/Layout/Box.h
@@ -25,8 +25,6 @@ public:
     Painting::PaintableBox const* paintable_box() const;
     Painting::PaintableBox* paintable_box();
 
-    bool is_body() const;
-
     // https://www.w3.org/TR/css-images-3/#natural-dimensions
     Optional<CSSPixels> natural_width() const { return m_natural_width; }
     Optional<CSSPixels> natural_height() const { return m_natural_height; }
@@ -49,8 +47,6 @@ public:
     virtual void did_set_content_size() { }
 
     virtual JS::GCPtr<Painting::Paintable> create_paintable() const override;
-
-    bool is_scroll_container() const;
 
     void add_contained_abspos_child(JS::NonnullGCPtr<Node> child) { m_contained_abspos_children.append(child); }
     void clear_contained_abspos_children() { m_contained_abspos_children.clear(); }

Userland/Libraries/LibWeb/Layout/InlineNode.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2018-2022, Andreas Kling <andreas@ladybird.org>
  * Copyright (c) 2021, Sam Atkins <atkinssj@serenityos.org>
+ * Copyright (c) 2024, Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -23,9 +24,17 @@ InlineNode::InlineNode(DOM::Document& document, DOM::Element* element, NonnullRe
 
 InlineNode::~InlineNode() = default;
 
-JS::GCPtr<Painting::Paintable> InlineNode::create_paintable() const
+JS::GCPtr<Painting::PaintableWithLines> InlineNode::create_paintable_for_line_with_index(size_t line_index) const
 {
-    return Painting::InlinePaintable::create(*this);
+    for (auto const& paintable : paintables()) {
+        if (is<Painting::PaintableWithLines>(paintable)) {
+            auto const& paintable_with_lines = static_cast<Painting::PaintableWithLines const&>(paintable);
+            if (paintable_with_lines.line_index() == line_index) {
+                return const_cast<Painting::PaintableWithLines&>(paintable_with_lines);
+            }
+        }
+    }
+    return Painting::PaintableWithLines::create(*this, line_index);
 }
 
 }

commit 0068e12d45cda03bf3c4485a874b1f0d0b3c2a38

LibWeb/Painting: Only paint left/right borders of inlines when we should
Inline nodes only paint their left border for the first line, and the
right border for their last line.

We don't have an easy way of tracking which line is the last one without
walking the list of child paintables and counting them, but I've tried
to make sure we only do these checks when we need to: If there's a >0
border-width or the right side has corner radii.

This seems to have regressed in 6a549f62702e8b733dd4fc0caf1fa3e114243070
when we stopped using the logic for it in InlinePaintable. It wasn't
caught because we had no test, so now we do. :^)

Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -559,7 +559,33 @@ void PaintableBox::paint_border(PaintContext& context) const
         .bottom = box_model().border.bottom == 0 ? CSS::BorderData() : computed_values().border_bottom(),
         .left = box_model().border.left == 0 ? CSS::BorderData() : computed_values().border_left(),
     };
-    paint_all_borders(context.display_list_recorder(), context.rounded_device_rect(absolute_border_box_rect()), normalized_border_radii_data().as_corners(context), borders_data.to_device_pixels(context));
+
+    auto corners_data = normalized_border_radii_data().as_corners(context);
+
+    if (is_paintable_with_lines() && is_inline()) {
+        // For multi-line inline nodes, only draw the inline-start border on the first line, and the inline-end border
+        // on the last line.
+        // FIXME: Actually use inline-start/end instead of left/right.
+        auto& paintable_with_lines = as<PaintableWithLines>(*this);
+        if (paintable_with_lines.line_index() > 0) {
+            borders_data.left = CSS::BorderData();
+            corners_data.top_left = {};
+            corners_data.bottom_left = {};
+        }
+        if (borders_data.right.width != 0
+            || corners_data.top_right.horizontal_radius != 0 || corners_data.top_right.vertical_radius != 0
+            || corners_data.bottom_right.horizontal_radius != 0 || corners_data.bottom_right.vertical_radius != 0) {
+            // FIXME: Find a way of doing this that doesn't involve walking the tree each time.
+            auto parent_line_count = paintable_with_lines.layout_node().paintables().size_slow();
+            if (paintable_with_lines.line_index() < (parent_line_count - 1)) {
+                borders_data.right = CSS::BorderData();
+                corners_data.top_right = {};
+                corners_data.bottom_right = {};
+            }
+        }
+    }
+
+    paint_all_borders(context.display_list_recorder(), context.rounded_device_rect(absolute_border_box_rect()), corners_data, borders_data.to_device_pixels(context));
 }
 
 void PaintableBox::paint_backdrop_filter(PaintContext& context) const

Tests/LibWeb/Screenshot/expected/inline-node-borders-ref.html
@@ -1,0 +1,10 @@
+<!DOCTYPE html>
+<style>
+    * {
+        margin: 0;
+    }
+    body {
+        background-color: white;
+    }
+</style>
+<img src="../images/inline-node-borders-ref.png">

Tests/LibWeb/Screenshot/input/inline-node-borders.html
@@ -1,0 +1,20 @@
+<!DOCTYPE html>
+<link rel="match" href="../expected/inline-node-borders-ref.html" />
+<style>
+  #pseudos::before {
+    content: 'This text is very long so that it will break onto multiple lines and showcase the bug.';
+    border: 2px solid red;
+    border-radius: 5px;
+  }
+  div {
+    max-width: 100px;
+    line-height: 3em;
+  }
+  span {
+    border: 2px solid blue;
+    border-radius: 5px;
+  }
+</style>
+<div id="pseudos"></div>
+<br>
+<div><span>This text is very long so that it will break onto multiple lines and showcase the bug.</span></div>