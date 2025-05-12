LadybirdBrowser/ladybird

commit 81b6a1100ed0d0bcb3a9e1e23fce497f46d0c7e6

LibJS: Skip iterator result object allocation in for..of and for..in
Introduce special instruction for `for..of` and `for..in` loop that
skips `{ value, done }` result object allocation if iterator is builtin
(array, map, set, string). This reduces GC pressure significantly and
avoids extracting the `value` and `done` properties.

This change makes this micro benchmark 48% faster on my computer:
```js
const arr = new Array(10_000_000);
let counter = 0;
for (let _ of arr) {
    counter++;
}
```

Libraries/LibJS/Bytecode/Interpreter.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021-2025, Andreas Kling <andreas@ladybird.org>
+ * Copyright (c) 2025, Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -611,6 +612,7 @@ FLATTEN_ON_CLANG void Interpreter::run_bytecode(size_t entry_point)
             HANDLE_INSTRUCTION_WITHOUT_EXCEPTION_CHECK(Dump);
             HANDLE_INSTRUCTION(EnterObjectEnvironment);
             HANDLE_INSTRUCTION(Exp);
+            HANDLE_INSTRUCTION(ForOfNext);
             HANDLE_INSTRUCTION(GetById);
             HANDLE_INSTRUCTION(GetByIdWithThis);
             HANDLE_INSTRUCTION(GetByValue);
@@ -2980,6 +2982,27 @@ ThrowCompletionOr<void> IteratorNext::execute_impl(Bytecode::Interpreter& interp
     return {};
 }
 
+ThrowCompletionOr<void> ForOfNext::execute_impl(Bytecode::Interpreter& interpreter) const
+{
+    auto& vm = interpreter.vm();
+    auto& iterator_record = static_cast<IteratorRecord&>(interpreter.get(m_iterator_record).as_cell());
+
+    Value value;
+    bool done = false;
+    if (auto* builtin_iterator = iterator_record.iterator->as_builtin_iterator()) {
+        TRY(builtin_iterator->next(vm, done, value));
+    } else {
+        auto result = TRY(iterator_next(vm, iterator_record));
+        value = TRY(result->internal_get(vm.names.value, {}));
+        done = TRY(result->internal_get(vm.names.done, {})).to_boolean();
+    }
+
+    interpreter.set(dst_done(), Value(done));
+    interpreter.set(dst_value(), value);
+
+    return {};
+}
+
 ThrowCompletionOr<void> NewClass::execute_impl(Bytecode::Interpreter& interpreter) const
 {
     Value super_class;
@@ -3751,6 +3774,14 @@ ByteString IteratorNext::to_byte_string_impl(Executable const& executable) const
 {
     return ByteString::formatted("IteratorNext {}, {}",
         format_operand("dst"sv, m_dst, executable),
+        format_operand("iterator_record"sv, m_iterator_record, executable));
+}
+
+ByteString ForOfNext::to_byte_string_impl(Executable const& executable) const
+{
+    return ByteString::formatted("ForOfNext {}, {}, {}",
+        format_operand("dst_value"sv, m_dst_value, executable),
+        format_operand("dst_done"sv, m_dst_done, executable),
         format_operand("iterator_record"sv, m_iterator_record, executable));
 }
 

Libraries/LibJS/Bytecode/ASTCodegen.cpp
@@ -3183,11 +3183,23 @@ static Bytecode::CodeGenerationErrorOr<Optional<ScopedOperand>> for_in_of_body_e
     generator.begin_continuable_scope(Bytecode::Label { loop_update }, label_set);
 
     // a. Let nextResult be ? Call(iteratorRecord.[[NextMethod]], iteratorRecord.[[Iterator]]).
-    auto next_result = generator.allocate_register();
-    generator.emit<Bytecode::Op::IteratorNext>(next_result, *head_result.iterator);
+    auto next_value = generator.allocate_register();
+    auto done = generator.allocate_register();
 
-    // b. If iteratorKind is async, set nextResult to ? Await(nextResult).
-    if (iterator_kind == IteratorHint::Async) {
+    if (iterator_kind == IteratorHint::Sync) {
+        generator.emit<Bytecode::Op::ForOfNext>(next_value, done, *head_result.iterator);
+
+        auto& loop_continue = generator.make_block();
+        generator.emit_jump_if(
+            done,
+            Bytecode::Label { loop_end },
+            Bytecode::Label { loop_continue });
+        generator.switch_to_basic_block(loop_continue);
+    } else {
+        auto next_result = generator.allocate_register();
+        generator.emit<Bytecode::Op::IteratorNext>(next_result, *head_result.iterator);
+
+        // b. If iteratorKind is async, set nextResult to ? Await(nextResult).
         auto received_completion = generator.allocate_register();
         auto received_completion_type = generator.allocate_register();
         auto received_completion_value = generator.allocate_register();
@@ -3195,26 +3207,24 @@ static Bytecode::CodeGenerationErrorOr<Optional<ScopedOperand>> for_in_of_body_e
         generator.emit_mov(received_completion, generator.accumulator());
         auto new_result = generate_await(generator, next_result, received_completion, received_completion_type, received_completion_value);
         generator.emit_mov(next_result, new_result);
+
+        // c. If Type(nextResult) is not Object, throw a TypeError exception.
+        generator.emit<Bytecode::Op::ThrowIfNotObject>(next_result);
+
+        // d. Let done be ? IteratorComplete(nextResult).
+        generator.emit_iterator_complete(done, next_result);
+
+        // e. If done is true, return V.
+        auto& loop_continue = generator.make_block();
+        generator.emit_jump_if(
+            done,
+            Bytecode::Label { loop_end },
+            Bytecode::Label { loop_continue });
+        generator.switch_to_basic_block(loop_continue);
+
+        // f. Let nextValue be ? IteratorValue(nextResult).
+        generator.emit_iterator_value(next_value, next_result);
     }
-
-    // c. If Type(nextResult) is not Object, throw a TypeError exception.
-    generator.emit<Bytecode::Op::ThrowIfNotObject>(next_result);
-
-    // d. Let done be ? IteratorComplete(nextResult).
-    auto done = generator.allocate_register();
-    generator.emit_iterator_complete(done, next_result);
-
-    // e. If done is true, return V.
-    auto& loop_continue = generator.make_block();
-    generator.emit_jump_if(
-        done,
-        Bytecode::Label { loop_end },
-        Bytecode::Label { loop_continue });
-    generator.switch_to_basic_block(loop_continue);
-
-    // f. Let nextValue be ? IteratorValue(nextResult).
-    auto next_value = generator.allocate_register();
-    generator.emit_iterator_value(next_value, next_result);
 
     // g. If lhsKind is either assignment or varBinding, then
     if (head_result.lhs_kind != LHSKind::LexicalBinding) {

Libraries/LibJS/Bytecode/Instruction.h
@@ -51,6 +51,7 @@
     O(EnterObjectEnvironment)          \
     O(EnterUnwindContext)              \
     O(Exp)                             \
+    O(ForOfNext)                       \
     O(GetById)                         \
     O(GetByIdWithThis)                 \
     O(GetByValue)                      \

Libraries/LibJS/Bytecode/Op.h
@@ -2721,6 +2721,35 @@ private:
     Operand m_iterator_record;
 };
 
+class ForOfNext final : public Instruction {
+public:
+    ForOfNext(Operand dst_value, Operand dst_done, Operand iterator_record)
+        : Instruction(Type::ForOfNext)
+        , m_dst_value(dst_value)
+        , m_dst_done(dst_done)
+        , m_iterator_record(iterator_record)
+    {
+    }
+
+    ThrowCompletionOr<void> execute_impl(Bytecode::Interpreter&) const;
+    ByteString to_byte_string_impl(Bytecode::Executable const&) const;
+    void visit_operands_impl(Function<void(Operand&)> visitor)
+    {
+        visitor(m_dst_value);
+        visitor(m_dst_done);
+        visitor(m_iterator_record);
+    }
+
+    Operand dst_value() const { return m_dst_value; }
+    Operand dst_done() const { return m_dst_done; }
+    Operand iterator_record() const { return m_iterator_record; }
+
+private:
+    Operand m_dst_value;
+    Operand m_dst_done;
+    Operand m_iterator_record;
+};
+
 class ResolveThisBinding final : public Instruction {
 public:
     ResolveThisBinding()

commit d0f661269690e4baf2ce1ba41d97e7dbdbcd0bb6

LibJS: Disable optimization in IteratorNextUnpack if next() is redefined
81b6a11 regressed correctness by always bypassing the `next()` method
resolution for built-in iterators, causing incorrect behavior when
`next()` was redefined on built-in prototypes. This change fixes the
issue by storing a flag on built-in prototypes indicating whether
`next()` has ever been redefined.

Libraries/LibJS/Bytecode/Interpreter.cpp
@@ -1769,7 +1769,7 @@ class PropertyNameIterator final
 public:
     virtual ~PropertyNameIterator() override = default;
 
-    BuiltinIterator* as_builtin_iterator() override { return this; }
+    BuiltinIterator* as_builtin_iterator_if_next_is_not_redefined() override { return this; }
     ThrowCompletionOr<void> next(VM&, bool& done, Value& value) override
     {
         while (true) {
@@ -3175,7 +3175,7 @@ ThrowCompletionOr<void> IteratorNextUnpack::execute_impl(Bytecode::Interpreter& 
 
     Value value;
     bool done = false;
-    if (auto* builtin_iterator = iterator_record.iterator->as_builtin_iterator()) {
+    if (auto* builtin_iterator = iterator_record.iterator->as_builtin_iterator_if_next_is_not_redefined()) {
         TRY(builtin_iterator->next(vm, done, value));
     } else {
         auto result = TRY(iterator_next(vm, iterator_record));

Libraries/LibJS/Runtime/ArrayIterator.cpp
@@ -6,6 +6,7 @@
 
 #include <LibJS/Runtime/Array.h>
 #include <LibJS/Runtime/ArrayIterator.h>
+#include <LibJS/Runtime/ArrayIteratorPrototype.h>
 #include <LibJS/Runtime/TypedArray.h>
 
 namespace JS {
@@ -28,6 +29,8 @@ ArrayIterator::ArrayIterator(Value array, Object::PropertyKind iteration_kind, O
     , m_array(array)
     , m_iteration_kind(iteration_kind)
 {
+    auto& array_iterator_prototype = as<ArrayIteratorPrototype>(prototype);
+    m_next_method_was_redefined = array_iterator_prototype.next_method_was_redefined();
 }
 
 void ArrayIterator::visit_edges(Cell::Visitor& visitor)

Libraries/LibJS/Runtime/ArrayIterator.h
@@ -21,7 +21,12 @@ public:
 
     virtual ~ArrayIterator() override = default;
 
-    BuiltinIterator* as_builtin_iterator() override { return this; }
+    BuiltinIterator* as_builtin_iterator_if_next_is_not_redefined() override
+    {
+        if (m_next_method_was_redefined)
+            return nullptr;
+        return this;
+    }
     ThrowCompletionOr<void> next(VM&, bool& done, Value& value) override;
 
 private:

Libraries/LibJS/Runtime/ArrayIteratorPrototype.h
@@ -19,10 +19,20 @@ public:
     virtual void initialize(Realm&) override;
     virtual ~ArrayIteratorPrototype() override = default;
 
+    bool next_method_was_redefined() const { return m_next_method_was_redefined; }
+    void set_next_method_was_redefined() { m_next_method_was_redefined = true; }
+
+    virtual bool is_array_iterator_prototype() const override { return true; }
+
 private:
     explicit ArrayIteratorPrototype(Realm&);
 
     JS_DECLARE_NATIVE_FUNCTION(next);
+
+    bool m_next_method_was_redefined { false };
 };
+
+template<>
+inline bool Object::fast_is<ArrayIteratorPrototype>() const { return is_array_iterator_prototype(); }
 
 }

Libraries/LibJS/Runtime/Iterator.cpp
@@ -272,7 +272,7 @@ static Completion iterator_close_impl(VM& vm, IteratorRecord const& iterator_rec
     auto iterator = iterator_record.iterator;
 
     // OPTIMIZATION: "return" method is not defined on any of iterators we treat as built-in.
-    if (iterator->as_builtin_iterator())
+    if (iterator->as_builtin_iterator_if_next_is_not_redefined())
         return completion;
 
     // 3. Let innerResult be Completion(GetMethod(iterator, "return")).

Libraries/LibJS/Runtime/Iterator.h
@@ -71,6 +71,9 @@ class BuiltinIterator {
 public:
     virtual ~BuiltinIterator() = default;
     virtual ThrowCompletionOr<void> next(VM&, bool& done, Value& value) = 0;
+
+protected:
+    bool m_next_method_was_redefined { false };
 };
 
 // 7.4.12 IfAbruptCloseIterator ( value, iteratorRecord ), https://tc39.es/ecma262/#sec-ifabruptcloseiterator

Libraries/LibJS/Runtime/MapIterator.cpp
@@ -6,6 +6,7 @@
 
 #include <LibJS/Runtime/Array.h>
 #include <LibJS/Runtime/MapIterator.h>
+#include <LibJS/Runtime/MapIteratorPrototype.h>
 
 namespace JS {
 
@@ -22,6 +23,8 @@ MapIterator::MapIterator(Map& map, Object::PropertyKind iteration_kind, Object& 
     , m_iteration_kind(iteration_kind)
     , m_iterator(static_cast<Map const&>(map).begin())
 {
+    auto& map_iterator_prototype = as<MapIteratorPrototype>(prototype);
+    m_next_method_was_redefined = map_iterator_prototype.next_method_was_redefined();
 }
 
 void MapIterator::visit_edges(Cell::Visitor& visitor)

Libraries/LibJS/Runtime/MapIterator.h
@@ -22,7 +22,12 @@ public:
 
     virtual ~MapIterator() override = default;
 
-    BuiltinIterator* as_builtin_iterator() override { return this; }
+    BuiltinIterator* as_builtin_iterator_if_next_is_not_redefined() override
+    {
+        if (m_next_method_was_redefined)
+            return nullptr;
+        return this;
+    }
     ThrowCompletionOr<void> next(VM&, bool& done, Value& value) override;
 
 private:

Libraries/LibJS/Runtime/MapIteratorPrototype.h
@@ -19,10 +19,20 @@ public:
     virtual void initialize(Realm&) override;
     virtual ~MapIteratorPrototype() override = default;
 
+    bool next_method_was_redefined() const { return m_next_method_was_redefined; }
+    void set_next_method_was_redefined() { m_next_method_was_redefined = true; }
+
+    virtual bool is_map_iterator_prototype() const override { return true; }
+
 private:
     MapIteratorPrototype(Realm&);
 
     JS_DECLARE_NATIVE_FUNCTION(next);
+
+    bool m_next_method_was_redefined { false };
 };
+
+template<>
+inline bool Object::fast_is<MapIteratorPrototype>() const { return is_map_iterator_prototype(); }
 
 }

Libraries/LibJS/Runtime/Object.cpp
@@ -10,15 +10,19 @@
 #include <LibJS/Runtime/AbstractOperations.h>
 #include <LibJS/Runtime/Accessor.h>
 #include <LibJS/Runtime/Array.h>
+#include <LibJS/Runtime/ArrayIteratorPrototype.h>
 #include <LibJS/Runtime/ClassFieldDefinition.h>
 #include <LibJS/Runtime/ECMAScriptFunctionObject.h>
 #include <LibJS/Runtime/Error.h>
 #include <LibJS/Runtime/GlobalObject.h>
+#include <LibJS/Runtime/MapIteratorPrototype.h>
 #include <LibJS/Runtime/NativeFunction.h>
 #include <LibJS/Runtime/Object.h>
 #include <LibJS/Runtime/PropertyDescriptor.h>
 #include <LibJS/Runtime/ProxyObject.h>
+#include <LibJS/Runtime/SetIteratorPrototype.h>
 #include <LibJS/Runtime/Shape.h>
+#include <LibJS/Runtime/StringIteratorPrototype.h>
 #include <LibJS/Runtime/Value.h>
 
 namespace JS {
@@ -956,6 +960,18 @@ ThrowCompletionOr<bool> Object::internal_set(PropertyKey const& property_key, Va
 {
     VERIFY(!value.is_special_empty_value());
     VERIFY(!receiver.is_special_empty_value());
+
+    if (receiver.is_object() && property_key == vm().names.next) {
+        auto& receiver_object = receiver.as_object();
+        if (auto* array_iterator_prototype = as_if<ArrayIteratorPrototype>(receiver_object))
+            array_iterator_prototype->set_next_method_was_redefined();
+        else if (auto* map_iterator_prototype = as_if<MapIteratorPrototype>(receiver_object))
+            map_iterator_prototype->set_next_method_was_redefined();
+        else if (auto* set_iterator_prototype = as_if<SetIteratorPrototype>(receiver_object))
+            set_iterator_prototype->set_next_method_was_redefined();
+        else if (auto* string_iterator_prototype = as_if<StringIteratorPrototype>(receiver_object))
+            string_iterator_prototype->set_next_method_was_redefined();
+    }
 
     // 2. Let ownDesc be ? O.[[GetOwnProperty]](P).
     auto own_descriptor = TRY(internal_get_own_property(property_key));

Libraries/LibJS/Runtime/Object.h
@@ -212,7 +212,12 @@ public:
     virtual bool is_array_iterator() const { return false; }
     virtual bool is_raw_json_object() const { return false; }
 
-    virtual BuiltinIterator* as_builtin_iterator() { return nullptr; }
+    virtual BuiltinIterator* as_builtin_iterator_if_next_is_not_redefined() { return nullptr; }
+
+    virtual bool is_array_iterator_prototype() const { return false; }
+    virtual bool is_map_iterator_prototype() const { return false; }
+    virtual bool is_set_iterator_prototype() const { return false; }
+    virtual bool is_string_iterator_prototype() const { return false; }
 
     // B.3.7 The [[IsHTMLDDA]] Internal Slot, https://tc39.es/ecma262/#sec-IsHTMLDDA-internal-slot
     virtual bool is_htmldda() const { return false; }

Libraries/LibJS/Runtime/SetIterator.cpp
@@ -6,6 +6,7 @@
 
 #include <LibJS/Runtime/Array.h>
 #include <LibJS/Runtime/SetIterator.h>
+#include <LibJS/Runtime/SetIteratorPrototype.h>
 
 namespace JS {
 
@@ -22,6 +23,8 @@ SetIterator::SetIterator(Set& set, Object::PropertyKind iteration_kind, Object& 
     , m_iteration_kind(iteration_kind)
     , m_iterator(static_cast<Set const&>(set).begin())
 {
+    auto& set_iterator_prototype = as<SetIteratorPrototype>(prototype);
+    m_next_method_was_redefined = set_iterator_prototype.next_method_was_redefined();
 }
 
 void SetIterator::visit_edges(Cell::Visitor& visitor)

Libraries/LibJS/Runtime/SetIterator.h
@@ -22,7 +22,13 @@ public:
 
     virtual ~SetIterator() override = default;
 
-    BuiltinIterator* as_builtin_iterator() override { return this; }
+    BuiltinIterator* as_builtin_iterator_if_next_is_not_redefined() override
+    {
+        if (m_next_method_was_redefined)
+            return nullptr;
+        return this;
+    }
+
     ThrowCompletionOr<void> next(VM&, bool& done, Value& value) override;
 
 private:

Libraries/LibJS/Runtime/SetIteratorPrototype.h
@@ -19,10 +19,20 @@ public:
     virtual void initialize(Realm&) override;
     virtual ~SetIteratorPrototype() override = default;
 
+    bool next_method_was_redefined() const { return m_next_method_was_redefined; }
+    void set_next_method_was_redefined() { m_next_method_was_redefined = true; }
+
+    virtual bool is_set_iterator_prototype() const override { return true; }
+
 private:
     explicit SetIteratorPrototype(Realm&);
 
     JS_DECLARE_NATIVE_FUNCTION(next);
+
+    bool m_next_method_was_redefined { false };
 };
+
+template<>
+inline bool Object::fast_is<SetIteratorPrototype>() const { return is_set_iterator_prototype(); }
 
 }

Libraries/LibJS/Runtime/StringIterator.cpp
@@ -7,6 +7,7 @@
 #include <AK/Utf8View.h>
 #include <LibJS/Runtime/GlobalObject.h>
 #include <LibJS/Runtime/StringIterator.h>
+#include <LibJS/Runtime/StringIteratorPrototype.h>
 
 namespace JS {
 
@@ -22,6 +23,8 @@ StringIterator::StringIterator(String string, Object& prototype)
     , m_string(move(string))
     , m_iterator(Utf8View(m_string).begin())
 {
+    auto& string_iterator_prototype = as<StringIteratorPrototype>(prototype);
+    m_next_method_was_redefined = string_iterator_prototype.next_method_was_redefined();
 }
 
 ThrowCompletionOr<void> StringIterator::next(VM& vm, bool& done, Value& value)

Libraries/LibJS/Runtime/StringIterator.h
@@ -23,7 +23,12 @@ public:
 
     virtual ~StringIterator() override = default;
 
-    BuiltinIterator* as_builtin_iterator() override { return this; }
+    BuiltinIterator* as_builtin_iterator_if_next_is_not_redefined() override
+    {
+        if (m_next_method_was_redefined)
+            return nullptr;
+        return this;
+    }
     ThrowCompletionOr<void> next(VM&, bool& done, Value& value) override;
 
 private:

Libraries/LibJS/Runtime/StringIteratorPrototype.h
@@ -20,10 +20,20 @@ public:
     virtual void initialize(Realm&) override;
     virtual ~StringIteratorPrototype() override = default;
 
+    bool next_method_was_redefined() const { return m_next_method_was_redefined; }
+    void set_next_method_was_redefined() { m_next_method_was_redefined = true; }
+
+    virtual bool is_string_iterator_prototype() const override { return true; }
+
 private:
     explicit StringIteratorPrototype(Realm&);
 
     JS_DECLARE_NATIVE_FUNCTION(next);
+
+    bool m_next_method_was_redefined { false };
 };
+
+template<>
+inline bool Object::fast_is<StringIteratorPrototype>() const { return is_string_iterator_prototype(); }
 
 }

Libraries/LibJS/Tests/redefine-next-in-builtin-iterators.js
@@ -1,0 +1,46 @@
+describe("redefine next() in built in iterators", () => {
+    test("redefine next() in ArrayIteratorPrototype", () => {
+        let arrayIteratorPrototype = Object.getPrototypeOf([].values());
+        let originalNext = arrayIteratorPrototype.next;
+        let counter = 0;
+        arrayIteratorPrototype.next = function () {
+            counter++;
+            return originalNext.apply(this, arguments);
+        };
+        for (let i of [1, 2, 3]) {
+        }
+        expect(counter).toBe(4);
+    });
+
+    test("redefine next() in MapIteratorPrototype", () => {
+        let m = new Map([
+            [1, 1],
+            [2, 2],
+            [3, 3],
+        ]);
+        let mapIteratorPrototype = Object.getPrototypeOf(m.values());
+        let originalNext = mapIteratorPrototype.next;
+        let counter = 0;
+        mapIteratorPrototype.next = function () {
+            counter++;
+            return originalNext.apply(this, arguments);
+        };
+        for (let v of m.values()) {
+        }
+        expect(counter).toBe(4);
+    });
+
+    test("redefine next() in SetIteratorPrototype", () => {
+        let s = new Set([1, 2, 3]);
+        let setIteratorPrototype = Object.getPrototypeOf(s.values());
+        let originalNext = setIteratorPrototype.next;
+        let counter = 0;
+        setIteratorPrototype.next = function () {
+            counter++;
+            return originalNext.apply(this, arguments);
+        };
+        for (let v of s.values()) {
+        }
+        expect(counter).toBe(4);
+    });
+});