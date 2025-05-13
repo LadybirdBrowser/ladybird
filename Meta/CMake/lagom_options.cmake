7234a94db641f81ffe52f48a2bc5e249f5f2c5fb
Andrew Kaster <andrew@ladybird.org>
2025-05-13 03:22:20 -0600

    CI: Disable LTO by default for static GCC builds
    lto1 uses more memory than exists in the known universe when building
    ladybird, so disable LTO for static GCC builds.

--- Meta/CMake/lagom_options.cmake
+++ Meta/CMake/lagom_options.cmake
@@ -5,6 +5,14 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common_options.cmake NO_POLICY_SCOPE)
 include(${CMAKE_CURRENT_LIST_DIR}/lagom_install_options.cmake)
 
+# lto1 uses a crazy amount of RAM in static builds.
+# Disable LTO for static gcc builds unless explicitly asked for.
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT BUILD_SHARED_LIBS)
+    set(RELEASE_LTO_DEFAULT OFF)
+else()
+    set(RELEASE_LTO_DEFAULT ON)
+endif()
+
 serenity_option(ENABLE_ADDRESS_SANITIZER OFF CACHE BOOL "Enable address sanitizer testing in gcc/clang")
 serenity_option(ENABLE_MEMORY_SANITIZER OFF CACHE BOOL "Enable memory sanitizer testing in gcc/clang")
 serenity_option(ENABLE_FUZZERS OFF CACHE BOOL "Build fuzzing targets")
@@ -14,7 +22,7 @@ serenity_option(LAGOM_TOOLS_ONLY OFF CACHE BOOL "Don't build libraries, utilitie
 serenity_option(ENABLE_LAGOM_CCACHE ON CACHE BOOL "Enable ccache for Lagom builds")
 serenity_option(LAGOM_USE_LINKER "" CACHE STRING "The linker to use (e.g. lld, mold) instead of the system default")
 serenity_option(LAGOM_LINK_POOL_SIZE "" CACHE STRING "The maximum number of parallel jobs to use for linking")
-serenity_option(ENABLE_LTO_FOR_RELEASE ON CACHE BOOL "Enable link-time optimization for release builds")
+serenity_option(ENABLE_LTO_FOR_RELEASE ${RELEASE_LTO_DEFAULT} CACHE BOOL "Enable link-time optimization for release builds")
 serenity_option(ENABLE_LAGOM_COVERAGE_COLLECTION OFF CACHE STRING "Enable code coverage instrumentation for lagom binaries in clang")
 
 if (ANDROID OR APPLE)
