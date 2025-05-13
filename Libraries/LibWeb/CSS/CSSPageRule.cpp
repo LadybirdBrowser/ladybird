d989e772b70338dd221081942b8cbc97d97bb28c
Sam Atkins <sam@ladybird.org>
2025-05-13 13:05:08 +0100

    LibWeb/CSS: Parse `@page` selectors
    Ideally we'd be able to share the code between page selectors and style
    ones, but given how simple page selectors are, some code duplication is
    the simpler option.

--- Libraries/LibWeb/CSS/CSSPageRule.cpp
+++ Libraries/LibWeb/CSS/CSSPageRule.cpp
@@ -15,12 +15,56 @@ namespace Web::CSS {
 
 GC_DEFINE_ALLOCATOR(CSSPageRule);
 
-GC::Ref<CSSPageRule> CSSPageRule::create(JS::Realm& realm, SelectorList&& selectors, GC::Ref<CSSPageDescriptors> style, CSSRuleList& rules)
+Optional<PagePseudoClass> page_pseudo_class_from_string(StringView input)
+{
+    if (input.equals_ignoring_ascii_case("blank"sv))
+        return PagePseudoClass::Blank;
+    if (input.equals_ignoring_ascii_case("first"sv))
+        return PagePseudoClass::First;
+    if (input.equals_ignoring_ascii_case("left"sv))
+        return PagePseudoClass::Left;
+    if (input.equals_ignoring_ascii_case("right"sv))
+        return PagePseudoClass::Right;
+    return {};
+}
+
+StringView to_string(PagePseudoClass pseudo_class)
+{
+    switch (pseudo_class) {
+    case PagePseudoClass::Blank:
+        return "blank"sv;
+    case PagePseudoClass::First:
+        return "first"sv;
+    case PagePseudoClass::Left:
+        return "left"sv;
+    case PagePseudoClass::Right:
+        return "right"sv;
+    }
+    VERIFY_NOT_REACHED();
+}
+
+PageSelector::PageSelector(Optional<FlyString> name, Vector<PagePseudoClass> pseudo_classes)
+    : m_name(move(name))
+    , m_pseudo_classes(move(pseudo_classes))
+{
+}
+
+String PageSelector::serialize() const
+{
+    StringBuilder builder;
+    if (m_name.has_value())
+        builder.append(m_name.value());
+    for (auto pseudo_class : m_pseudo_classes)
+        builder.appendff(":{}", to_string(pseudo_class));
+    return builder.to_string_without_validation();
+}
+
+GC::Ref<CSSPageRule> CSSPageRule::create(JS::Realm& realm, PageSelectorList&& selectors, GC::Ref<CSSPageDescriptors> style, CSSRuleList& rules)
 {
     return realm.create<CSSPageRule>(realm, move(selectors), style, rules);
 }
 
-CSSPageRule::CSSPageRule(JS::Realm& realm, SelectorList&& selectors, GC::Ref<CSSPageDescriptors> style, CSSRuleList& rules)
+CSSPageRule::CSSPageRule(JS::Realm& realm, PageSelectorList&& selectors, GC::Ref<CSSPageDescriptors> style, CSSRuleList& rules)
     : CSSGroupingRule(realm, rules, Type::Page)
     , m_selectors(move(selectors))
     , m_style(style)
@@ -38,7 +82,10 @@ void CSSPageRule::initialize(JS::Realm& realm)
 String CSSPageRule::selector_text() const
 {
     // The selectorText attribute, on getting, must return the result of serializing the associated selector list.
-    return serialize_a_group_of_selectors(m_selectors);
+
+    // https://www.w3.org/TR/cssom/#serialize-a-group-of-selectors
+    // To serialize a group of selectors serialize each selector in the group of selectors and then serialize a comma-separated list of these serializations.
+    return MUST(String::join(", "sv, m_selectors));
 }
 
 // https://drafts.csswg.org/cssom/#dom-csspagerule-selectortext
--- Libraries/LibWeb/CSS/CSSPageRule.h
+++ Libraries/LibWeb/CSS/CSSPageRule.h
@@ -8,9 +8,31 @@
 
 #include <LibWeb/CSS/CSSGroupingRule.h>
 #include <LibWeb/CSS/CSSPageDescriptors.h>
-#include <LibWeb/CSS/Selector.h>
 
 namespace Web::CSS {
+
+enum class PagePseudoClass : u8 {
+    Left,
+    Right,
+    First,
+    Blank,
+};
+Optional<PagePseudoClass> page_pseudo_class_from_string(StringView);
+StringView to_string(PagePseudoClass);
+
+class PageSelector {
+public:
+    PageSelector(Optional<FlyString> name, Vector<PagePseudoClass>);
+
+    Optional<FlyString> name() const { return m_name; }
+    Vector<PagePseudoClass> const& pseudo_classes() const { return m_pseudo_classes; }
+    String serialize() const;
+
+private:
+    Optional<FlyString> m_name;
+    Vector<PagePseudoClass> m_pseudo_classes;
+};
+using PageSelectorList = Vector<PageSelector>;
 
 // https://drafts.csswg.org/css-page-3/#at-ruledef-page
 class CSSPageRule final : public CSSGroupingRule {
@@ -18,7 +40,7 @@ class CSSPageRule final : public CSSGroupingRule {
     GC_DECLARE_ALLOCATOR(CSSPageRule);
 
 public:
-    [[nodiscard]] static GC::Ref<CSSPageRule> create(JS::Realm&, SelectorList&&, GC::Ref<CSSPageDescriptors>, CSSRuleList&);
+    [[nodiscard]] static GC::Ref<CSSPageRule> create(JS::Realm&, PageSelectorList&&, GC::Ref<CSSPageDescriptors>, CSSRuleList&);
 
     virtual ~CSSPageRule() override = default;
 
@@ -29,14 +51,26 @@ public:
     GC::Ref<CSSPageDescriptors const> descriptors() const { return m_style; }
 
 private:
-    CSSPageRule(JS::Realm&, SelectorList&&, GC::Ref<CSSPageDescriptors>, CSSRuleList&);
+    CSSPageRule(JS::Realm&, PageSelectorList&&, GC::Ref<CSSPageDescriptors>, CSSRuleList&);
 
     virtual void initialize(JS::Realm&) override;
     virtual String serialized() const override;
     virtual void visit_edges(Visitor&) override;
 
-    SelectorList m_selectors;
+    PageSelectorList m_selectors;
     GC::Ref<CSSPageDescriptors> m_style;
+};
+
+}
+
+namespace AK {
+
+template<>
+struct Formatter<Web::CSS::PageSelector> : Formatter<StringView> {
+    ErrorOr<void> format(FormatBuilder& builder, Web::CSS::PageSelector const& selector)
+    {
+        return Formatter<StringView>::format(builder, selector.serialize());
+    }
 };
 
 }
