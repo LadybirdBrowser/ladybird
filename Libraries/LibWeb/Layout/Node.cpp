4db280ed060ec29dda112626aa1b19980b880ae6
Callum Law <callumlaw1709@outlook.com>
2025-05-16 18:32:31 +1200

    LibWeb: Parse and propagate `white-space-collapse` CSS property

--- Libraries/LibWeb/Layout/Node.cpp
+++ Libraries/LibWeb/Layout/Node.cpp
@@ -643,6 +643,7 @@ void NodeWithStyle::apply_style(CSS::ComputedProperties const& computed_style)
     computed_values.set_tab_size(computed_style.tab_size());
 
     computed_values.set_white_space(computed_style.white_space());
+    computed_values.set_white_space_collapse(computed_style.white_space_collapse());
     computed_values.set_word_break(computed_style.word_break());
     if (auto word_spacing = computed_style.word_spacing(); word_spacing.has_value())
         computed_values.set_word_spacing(word_spacing.value());
