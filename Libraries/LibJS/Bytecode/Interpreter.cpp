fa386c7dbe3c9122702c7dc7a11b040ce15b9b3a
Shannon Booth <shannon@serenityos.org>
2025-05-18 11:21:33 +1200

    LibJS: Avoid exception check for Not and Typeof instructions

--- Libraries/LibJS/Bytecode/Interpreter.cpp
+++ Libraries/LibJS/Bytecode/Interpreter.cpp
@@ -657,7 +657,7 @@ FLATTEN_ON_CLANG void Interpreter::run_bytecode(size_t entry_point)
             HANDLE_INSTRUCTION_WITHOUT_EXCEPTION_CHECK(NewPrimitiveArray);
             HANDLE_INSTRUCTION_WITHOUT_EXCEPTION_CHECK(NewRegExp);
             HANDLE_INSTRUCTION_WITHOUT_EXCEPTION_CHECK(NewTypeError);
-            HANDLE_INSTRUCTION(Not);
+            HANDLE_INSTRUCTION_WITHOUT_EXCEPTION_CHECK(Not);
             HANDLE_INSTRUCTION_WITHOUT_EXCEPTION_CHECK(PrepareYield);
             HANDLE_INSTRUCTION(PostfixDecrement);
             HANDLE_INSTRUCTION(PostfixIncrement);
@@ -683,7 +683,7 @@ FLATTEN_ON_CLANG void Interpreter::run_bytecode(size_t entry_point)
             HANDLE_INSTRUCTION(ThrowIfNotObject);
             HANDLE_INSTRUCTION(ThrowIfNullish);
             HANDLE_INSTRUCTION(ThrowIfTDZ);
-            HANDLE_INSTRUCTION(Typeof);
+            HANDLE_INSTRUCTION_WITHOUT_EXCEPTION_CHECK(Typeof);
             HANDLE_INSTRUCTION(TypeofBinding);
             HANDLE_INSTRUCTION(UnaryMinus);
             HANDLE_INSTRUCTION(UnaryPlus);
@@ -2182,12 +2182,12 @@ ThrowCompletionOr<void> GreaterThanEquals::execute_impl(Bytecode::Interpreter& i
     return {};
 }
 
-static ThrowCompletionOr<Value> not_(VM&, Value value)
+static ALWAYS_INLINE Value not_(VM&, Value value)
 {
     return Value(!value.to_boolean());
 }
 
-static ThrowCompletionOr<Value> typeof_(VM& vm, Value value)
+static ALWAYS_INLINE Value typeof_(VM& vm, Value value)
 {
     return value.typeof_(vm);
 }
@@ -2207,6 +2207,21 @@ static ThrowCompletionOr<Value> typeof_(VM& vm, Value value)
     }
 
 JS_ENUMERATE_COMMON_UNARY_OPS(JS_DEFINE_COMMON_UNARY_OP)
+
+#define JS_DEFINE_COMMON_UNARY_OP_WITHOUT_EXCEPTION_CHECK(OpTitleCase, op_snake_case)         \
+    void OpTitleCase::execute_impl(Bytecode::Interpreter& interpreter) const                  \
+    {                                                                                         \
+        auto& vm = interpreter.vm();                                                          \
+        interpreter.set(dst(), op_snake_case(vm, interpreter.get(src())));                    \
+    }                                                                                         \
+    ByteString OpTitleCase::to_byte_string_impl(Bytecode::Executable const& executable) const \
+    {                                                                                         \
+        return ByteString::formatted(#OpTitleCase " {}, {}",                                  \
+            format_operand("dst"sv, dst(), executable),                                       \
+            format_operand("src"sv, src(), executable));                                      \
+    }
+
+JS_ENUMERATE_COMMON_UNARY_OPS_WITHOUT_EXCEPTION_CHECK(JS_DEFINE_COMMON_UNARY_OP_WITHOUT_EXCEPTION_CHECK)
 
 void NewArray::execute_impl(Bytecode::Interpreter& interpreter) const
 {
