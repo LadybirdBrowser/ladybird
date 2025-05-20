bfc0715455722d4eda5b07e8ea7d74fa62d4158e
Ashton <ayeteadoe@gmail.com>
2025-05-20 05:11:18 -0700

    Meta: Add vcpkg command to ladybird.py

--- Meta/ladybird.py
+++ Meta/ladybird.py
@@ -110,6 +110,9 @@ def main(platform):
     subparsers.add_parser('install', help='Installs the target binary',
                           parents=[preset_parser, compiler_parser, target_parser])
 
+    subparsers.add_parser('vcpkg', help='Ensure that dependencies are available',
+                          parents=[preset_parser, compiler_parser])
+
     args = parser.parse_args()
     kwargs = vars(args)
     command = kwargs.pop('command', None)
@@ -156,6 +159,9 @@ def main(platform):
         _build_main(build_dir, **kwargs)
         kwargs['target'] = 'install'
         _build_main(build_dir, **kwargs)
+    elif command == 'vcpkg':
+        _configure_build_env(**kwargs)
+        _build_vcpkg()
 
 
 def _configure_main(platform, **kwargs):
