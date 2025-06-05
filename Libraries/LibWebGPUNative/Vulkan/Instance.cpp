b6eff9079ffe1ff6eab34e25710b6e1a6c209275
ayeteadoe <ayeteadoe@gmail.com>
2025-06-05 02:33:37 -0700

    LibWebGPUNative: Initial Instance Vulkan implementation

---
+++ Libraries/LibWebGPUNative/Vulkan/Instance.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2025, ayeteadoe <ayeteadoe@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWebGPUNative/Vulkan/Error.h>
+#include <LibWebGPUNative/Vulkan/InstanceImpl.h>
+
+namespace WebGPUNative {
+
+Instance::Instance()
+    : m_impl(make<Impl>())
+{
+}
+
+Instance::~Instance() = default;
+
+ErrorOr<void> Instance::initialize()
+{
+    return m_impl->initialize();
+}
+
+}
