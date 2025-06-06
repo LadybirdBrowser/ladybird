8f21fa2f4332d097237ce92fbcbf6af2ff8c307f
ayeteadoe <ayeteadoe@gmail.com>
2025-06-06 01:22:09 -0700

    LibWeb/WebGPU: Initial GPU implementation and GPUAdapter initialization

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -1165,6 +1165,18 @@ class WebGLVertexArrayObjectOES;
 
 }
 
+namespace Web::WebGPU {
+
+class GPU;
+class GPUAdapter;
+class GPUAdapterInfo;
+struct GPURequestAdapterOptions;
+class GPUSupportedFeatures;
+class GPUSupportedLimits;
+class WGSLLanguageFeatures;
+
+}
+
 namespace Web::WebIDL {
 
 class ArrayBufferView;
--- Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/IDLGenerators.cpp
+++ Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/IDLGenerators.cpp
@@ -71,6 +71,8 @@ static bool is_platform_object(Type const& type)
         "FileList"sv,
         "FontFace"sv,
         "FormData"sv,
+        "GPU"sv,
+        "GPUAdapter"sv,
         "HTMLCollection"sv,
         "IDBCursor"sv,
         "IDBCursorWithValue"sv,
@@ -145,6 +147,7 @@ static bool is_platform_object(Type const& type)
         "WebGLUniformLocation"sv,
         "WebGLVertexArrayObject"sv,
         "WebGLVertexArrayObjectOES"sv,
+        "WGSLLanguageFeatures"sv,
         "Window"sv,
         "WindowProxy"sv,
         "WritableStream"sv,
@@ -4896,6 +4899,7 @@ using namespace Web::WebAssembly;
 using namespace Web::WebAudio;
 using namespace Web::WebGL;
 using namespace Web::WebGL::Extensions;
+using namespace Web::WebGPU;
 using namespace Web::WebIDL;
 using namespace Web::WebVTT;
 using namespace Web::XHR;
