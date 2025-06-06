ad4cfedceb4fd80ccfcf6e0601912fc6b2571a7e
ayeteadoe <ayeteadoe@gmail.com>
2025-06-06 01:22:09 -0700

    LibWeb/WebGPU: Initial GPU implementation and GPUAdapter initialization

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -1186,6 +1186,18 @@ class WebGLVertexArrayObjectOES;
 
 }
 
+namespace Web::WebGPU {
+
+class GPU;
+class GPUAdapter;
+class GPUAdapterInfo;
+struct GPURequestAdapterOptions;
+class GPUSupportedFeatures;
+class GPUSupportedLimits;
+class WGSLLanguageFeatures;
+
+}
+
 namespace Web::WebIDL {
 
 class ArrayBufferView;
