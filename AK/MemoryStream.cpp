0894b068eb5dce1bac6dc8f4e45a082155289fc9
Ali Mohammad Pur <ali.mpfard@gmail.com>
2025-06-06 14:19:14 +0200

    AK: Simplify some stream reading logic
    These do the same thing in a less convoluted way. NFC.

--- AK/MemoryStream.cpp
+++ AK/MemoryStream.cpp
@@ -46,13 +46,9 @@ ErrorOr<void> FixedMemoryStream::truncate(size_t)
 
 ErrorOr<Bytes> FixedMemoryStream::read_some(Bytes bytes)
 {
-    auto to_read = min(remaining(), bytes.size());
-    if (to_read == 0)
-        return Bytes {};
-
-    m_bytes.slice(m_offset, to_read).copy_to(bytes);
-    m_offset += to_read;
-    return bytes.trim(to_read);
+    auto read = m_bytes.slice(m_offset).copy_trimmed_to(bytes);
+    m_offset += read;
+    return bytes.trim(read);
 }
 
 ErrorOr<void> FixedMemoryStream::read_until_filled(AK::Bytes bytes)
