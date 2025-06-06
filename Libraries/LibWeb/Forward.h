cbf44ed894a0e0e6cefe34a0c3b9b64d48e65901
ayeteadoe <ayeteadoe@gmail.com>
2025-06-06 01:22:09 -0700

    LibWeb/WebGPU: Initial GPU implementation

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -1075,6 +1075,12 @@ class WebGLVertexArrayObjectOES;
 
 }
 
+namespace Web::WebGPU {
+
+class GPU;
+
+}
+
 namespace Web::WebIDL {
 
 class ArrayBufferView;
---
+++ Libraries/LibWeb/WebGPU/GPU.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2025, ayeteadoe <ayeteadoe@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/Bindings/GPUPrototype.h>
+#include <LibWeb/Bindings/PlatformObject.h>
+#include <LibWeb/Forward.h>
+#include <LibWebGPUNative/Instance.h>
+
+namespace Web::WebGPU {
+
+class GPU final : public Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(GPU, Bindings::PlatformObject);
+    GC_DECLARE_ALLOCATOR(GPU);
+
+public:
+    static JS::ThrowCompletionOr<GC::Ref<GPU>> create(JS::Realm&);
+
+    ~GPU() override;
+
+private:
+    explicit GPU(JS::Realm&);
+
+    void initialize(JS::Realm&) override;
+
+    void visit_edges(Visitor&) override;
+
+    WebGPUNative::Instance m_native_gpu;
+};
+
+}
