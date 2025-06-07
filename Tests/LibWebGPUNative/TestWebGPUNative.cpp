cca95957cbb796e4157ef193b8ffbf5955374484
ayeteadoe <ayeteadoe@gmail.com>
2025-06-07 15:59:28 -0700

    Tests/LibWebGPUNative: Extend clear test to initialize Adapter

--- Tests/LibWebGPUNative/TestWebGPUNative.cpp
+++ Tests/LibWebGPUNative/TestWebGPUNative.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <LibTest/TestCase.h>
+#include <LibWebGPUNative/Adapter.h>
 #include <LibWebGPUNative/Instance.h>
 
 // FIXME: Complete enough of the implementation to test a "clear value" render pass into to a Gfx::Bitmap for headless/offscreen verification
@@ -15,4 +16,18 @@ TEST_CASE(clear)
         FAIL("Failed to initialize Instance");
         return;
     }
+
+    Core::EventLoop loop;
+    WebGPUNative::Adapter adapter = instance.adapter();
+    NonnullRefPtr const adapter_promise = instance.request_adapter();
+    loop.deferred_invoke([=, &adapter] {
+        MUST(adapter.initialize());
+        adapter_promise->resolve(std::move(adapter));
+    });
+    auto adapter_result = adapter_promise->await();
+    if (adapter_result.is_error()) {
+        FAIL("Adapter initialization failed");
+        return;
+    }
+    adapter = std::move(adapter_result.value());
 }
