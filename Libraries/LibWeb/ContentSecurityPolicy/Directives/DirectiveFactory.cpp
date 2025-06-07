25e9b56e21a1ea5354dbc9be04694f0445dc72b7
Oliver Hemsted <oliver@hemsted.co>
2025-06-07 22:34:23 +0100

    LibWeb: Only log unknown CSP directives when not listed in Names.h

--- Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.cpp
@@ -7,12 +7,26 @@
 #include <LibJS/Runtime/Realm.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/Directive.h>
 #include <LibWeb/ContentSecurityPolicy/Directives/DirectiveFactory.h>
+#include <LibWeb/ContentSecurityPolicy/Directives/Names.h>
 
 namespace Web::ContentSecurityPolicy::Directives {
 
+static bool is_known_directive(String const& name)
+{
+#define __ENUMERATE_DIRECTIVE_NAME(enum_name, string_value) \
+    if (name == Names::enum_name) {                        \
+        return true;                                       \
+    }
+    ENUMERATE_DIRECTIVE_NAMES
+#undef __ENUMERATE_DIRECTIVE_NAME
+    return false;
+}
+
 GC::Ref<Directive> create_directive(GC::Heap& heap, String name, Vector<String> value)
 {
-    dbgln("Potential FIXME: Creating unknown Content Security Policy directive: {}", name);
+    if (!is_known_directive(name))
+        dbgln("Potential FIXME: Creating unknown Content Security Policy directive: {}", name);
+
     return heap.allocate<Directive>(move(name), move(value));
 }
 
