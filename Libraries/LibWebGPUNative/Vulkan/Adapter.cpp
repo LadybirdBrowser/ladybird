0cc3d48a833fe4eed59c18a9e77ab47c89a3a169
ayeteadoe <ayeteadoe@gmail.com>
2025-06-07 15:57:15 -0700

    LibWebGPUNative: Initial Adapter Vulkan implementation

---
+++ Libraries/LibWebGPUNative/Vulkan/Adapter.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025, ayeteadoe <ayeteadoe@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWebGPUNative/Adapter.h>
+#include <LibWebGPUNative/Vulkan/AdapterImpl.h>
+
+namespace WebGPUNative {
+
+Adapter::Adapter(Instance const& gpu)
+    : m_impl(make<Impl>(gpu))
+{
+}
+
+Adapter::Adapter(Adapter&&) noexcept = default;
+Adapter& Adapter::operator=(Adapter&&) noexcept = default;
+Adapter::~Adapter() = default;
+
+ErrorOr<void> Adapter::initialize()
+{
+    return m_impl->initialize();
+}
+
+}
