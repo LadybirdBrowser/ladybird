53d944f09c9f287ed5f18701633a42a27c73651d
ayeteadoe <ayeteadoe@gmail.com>
2025-06-07 21:19:35 -0700

    LibWebGPUNative: Initial CommandBuffer Vulkan implementation

--- Libraries/LibWebGPUNative/Vulkan/QueueImpl.cpp
+++ Libraries/LibWebGPUNative/Vulkan/QueueImpl.cpp
@@ -14,4 +14,37 @@ Queue::Impl::Impl(Device const& gpu_device)
 {
 }
 
+// FIXME: Add spec comments
+//  https://www.w3.org/TR/webgpu/#dom-gpuqueue-submit
+ErrorOr<void> Queue::Impl::submit(Vector<NonnullRawPtr<CommandBuffer>> const& gpu_command_buffers)
+{
+    Vector<VkCommandBuffer> command_buffers;
+    for (auto const& command_buffer : gpu_command_buffers) {
+        command_buffers.append(command_buffer->m_impl->command_buffer());
+    }
+
+    VkSubmitInfo submit_info = {};
+    submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submit_info.commandBufferCount = static_cast<uint32_t>(command_buffers.size());
+    submit_info.pCommandBuffers = command_buffers.data();
+    if (VkResult const queue_submit_result = vkQueueSubmit(m_queue, 1, &submit_info, VK_NULL_HANDLE); queue_submit_result != VK_SUCCESS)
+        return make_error(queue_submit_result, "Unable to submit queue");
+
+    // FIXME: Queue submission should be asynchronous
+    //  https://www.w3.org/TR/webgpu/#dom-gpuqueue-onsubmittedworkdone
+    if (VkResult const queue_wait_idle_result = vkQueueWaitIdle(m_queue); queue_wait_idle_result != VK_SUCCESS)
+        return make_error(queue_wait_idle_result, "Unable to wait for queue to be idle");
+
+    // FIXME: Consult spec for the recommended method of notifying the GPUCanvasContext that is needs to update it's HTMLCanvasElement surface
+    if (m_submitted_callback)
+        m_submitted_callback();
+
+    return {};
+}
+
+void Queue::Impl::on_submitted(Function<void()> callback)
+{
+    m_submitted_callback = std::move(callback);
+}
+
 }
