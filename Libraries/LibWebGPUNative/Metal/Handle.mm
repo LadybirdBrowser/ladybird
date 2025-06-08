ceaad0a0956923748faede6fbfba3ff5fa6e1dc0
ayeteadoe <ayeteadoe@gmail.com>
2025-06-07 20:14:30 -0700

    LibWebGPUNative: Initial Device Metal implementation

---
+++ Libraries/LibWebGPUNative/Metal/Handle.mm
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2025, ayeteadoe <ayeteadoe@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWebGPUNative/Metal/Handle.h>
+#import <Metal/Metal.h>
+
+namespace WebGPUNative {
+
+MetalDeviceHandle::MetalDeviceHandle(id device)
+    : m_device(device)
+{
+    [m_device retain];
+}
+
+MetalDeviceHandle::~MetalDeviceHandle()
+{
+    if (m_device) {
+        [m_device release];
+        m_device = nullptr;
+    }
+}
+
+MetalDeviceHandle::MetalDeviceHandle(MetalDeviceHandle&& other) noexcept
+    : m_device(other.m_device)
+{
+    other.m_device = nullptr;
+}
+
+MetalDeviceHandle& MetalDeviceHandle::operator=(MetalDeviceHandle&& other) noexcept
+{
+    if (this != &other) {
+        if (m_device) {
+            [m_device release];
+        }
+        m_device = other.m_device;
+        other.m_device = nullptr;
+    }
+    return *this;
+}
+
+MetalCommandQueueHandle::MetalCommandQueueHandle(id command_queue)
+    : m_command_queue(command_queue)
+{
+    [m_command_queue retain];
+}
+
+MetalCommandQueueHandle::~MetalCommandQueueHandle()
+{
+    if (m_command_queue) {
+        [m_command_queue release];
+        m_command_queue = nullptr;
+    }
+}
+
+MetalCommandQueueHandle::MetalCommandQueueHandle(MetalCommandQueueHandle&& other) noexcept
+    : m_command_queue(other.m_command_queue)
+{
+    other.m_command_queue = nullptr;
+}
+
+MetalCommandQueueHandle& MetalCommandQueueHandle::operator=(MetalCommandQueueHandle&& other) noexcept
+{
+    if (this != &other) {
+        if (m_command_queue) {
+            [m_command_queue release];
+        }
+        m_command_queue = other.m_command_queue;
+        other.m_command_queue = nullptr;
+    }
+    return *this;
+}
+
+}
