8fade4110678d0792ebb518c4745cf9fa3d7ad39
ayeteadoe <ayeteadoe@gmail.com>
2025-06-08 08:58:07 -0700

    LibWebGPUNative: Initial CommandEncoder Metal implementation

---
+++ Libraries/LibWebGPUNative/Metal/CommandEncoder.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025, ayeteadoe <ayeteadoe@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWebGPUNative/CommandBuffer.h>
+#include <LibWebGPUNative/CommandEncoder.h>
+#include <LibWebGPUNative/Metal/CommandEncoderImpl.h>
+
+namespace WebGPUNative {
+
+CommandEncoder::CommandEncoder(Device const& gpu_device)
+    : m_impl(make<Impl>(gpu_device))
+{
+}
+
+CommandEncoder::CommandEncoder(CommandEncoder&&) noexcept = default;
+CommandEncoder& CommandEncoder::operator=(CommandEncoder&&) noexcept = default;
+CommandEncoder::~CommandEncoder() = default;
+
+ErrorOr<void> CommandEncoder::initialize()
+{
+    return m_impl->initialize();
+}
+
+}
