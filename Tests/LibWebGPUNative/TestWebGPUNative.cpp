d0748456f4b4f9fbb1046ec85a7b21e5d3dedf9a
ayeteadoe <ayeteadoe@gmail.com>
2025-06-08 11:59:25 -0700

    Tests/LibWebGPUNative: Extend clear test to initialize Texture

--- Tests/LibWebGPUNative/TestWebGPUNative.cpp
+++ Tests/LibWebGPUNative/TestWebGPUNative.cpp
@@ -4,12 +4,14 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibGfx/Color.h>
 #include <LibTest/TestCase.h>
 #include <LibWebGPUNative/Adapter.h>
 #include <LibWebGPUNative/CommandEncoder.h>
 #include <LibWebGPUNative/Device.h>
 #include <LibWebGPUNative/Instance.h>
 #include <LibWebGPUNative/Queue.h>
+#include <LibWebGPUNative/Texture.h>
 
 // FIXME: Complete enough of the implementation to test a "clear value" render pass into to a Gfx::Bitmap for headless/offscreen verification
 TEST_CASE(clear)
@@ -54,4 +56,31 @@ TEST_CASE(clear)
         return;
     }
 
+    constexpr Gfx::IntSize texture_size = { 800, 600 };
+    WebGPUNative::Texture texture = device.texture(texture_size);
+    auto texture_result = texture.initialize();
+    if (texture_result.is_error()) {
+        FAIL("Texture initialization failed");
+        return;
+    }
+
+    // Verify initial texture clear value is transparent black
+    {
+        auto mapped_buffer_result = texture.map_buffer();
+        if (mapped_buffer_result.is_error()) {
+            FAIL("Mapped buffer initialization failed");
+            return;
+        }
+        auto const mapped_buffer = std::move(mapped_buffer_result.value());
+        for (auto const& [pixel, x, y] : mapped_buffer->pixels()) {
+            if (pixel != Color::Transparent) {
+                auto const r = pixel.red();
+                auto const g = pixel.green();
+                auto const b = pixel.blue();
+                auto const a = pixel.alpha();
+                FAIL(String::formatted("Unexpected clear pixel colour ({}, {}, {}, {}) at ({}, {})", r, g, b, a, x, y));
+                return;
+            }
+        }
+    }
 }
