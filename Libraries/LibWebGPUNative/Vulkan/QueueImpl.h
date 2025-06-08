eeb21ed11f26d6c9bf51bafe01baef29e8497da5
ayeteadoe <ayeteadoe@gmail.com>
2025-06-07 21:19:35 -0700

    LibWebGPUNative: Initial CommandBuffer Vulkan implementation

--- Libraries/LibWebGPUNative/Vulkan/QueueImpl.h
+++ Libraries/LibWebGPUNative/Vulkan/QueueImpl.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include <AK/Function.h>
 #include <LibWebGPUNative/Queue.h>
 #include <vulkan/vulkan_core.h>
 
@@ -14,8 +15,12 @@ namespace WebGPUNative {
 struct Queue::Impl {
     explicit Impl(Device const& gpu_device);
 
+    ErrorOr<void> submit(Vector<NonnullRawPtr<CommandBuffer>> const&);
+    void on_submitted(Function<void()> callback);
+
 private:
     VkQueue m_queue = { VK_NULL_HANDLE };
+    Function<void()> m_submitted_callback = { nullptr };
 };
 
 }
