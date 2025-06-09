c204149bbd0a4196057b3f86f9c60365ae3fd7d3
Timothy Flynn <trflynn89@pm.me>
2025-06-09 09:32:47 -0400

    Meta+CI: Enforce python linting with ruff
    We could use flake8 for linting, but ruff is compatible with black
    formatting out-of-the-box. It also seems to catch more than flake8,
    such as unnecessary f-strings.

--- Meta/lint-python.sh
+++ Meta/lint-python.sh
@@ -5,11 +5,11 @@ set -e
 script_path=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd "${script_path}/.." || exit 1
 
-check_argument="--check"
+overwrite=0
 
 if [ "$#" -gt "0" ]; then
     if  [ "--overwrite-inplace" = "$1" ] ; then
-        check_argument=""
+        overwrite=1
         shift
     fi
 fi
@@ -31,12 +31,18 @@ else
 fi
 
 if (( ${#files[@]} )); then
-    if ! command -v black >/dev/null 2>&1 ; then
-        echo "black is not available, but python files need linting! Either skip this script, or install black."
+    if ! command -v ruff >/dev/null 2>&1 ; then
+        echo "Please install ruff: pip3 install ruff"
         exit 1
     fi
 
-    black ${check_argument} "${files[@]}"
+    if [[ ${overwrite} -eq 0 ]] ; then
+        ruff check "${files[@]}"
+        ruff format --check "${files[@]}"
+    else
+        ruff check --fix "${files[@]}"
+        ruff format "${files[@]}"
+    fi
 else
     echo "No py files to check."
 fi
