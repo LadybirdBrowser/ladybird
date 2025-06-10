335190e925d88af81b7bbc885ba98fe858516013
Callum Law <callumlaw1709@outlook.com>
2025-06-10 17:38:55 +1200

    LibWeb: Handle nested shorthands in all-same-CSS-wide-keyword to_string
    Previously we only checked direct sub-properties, not accounting for the
    nested case.

--- Libraries/LibWeb/CSS/StyleValues/ShorthandStyleValue.cpp
+++ Libraries/LibWeb/CSS/StyleValues/ShorthandStyleValue.cpp
@@ -8,6 +8,7 @@
 #include "ShorthandStyleValue.h"
 #include <LibGfx/Font/FontWeight.h>
 #include <LibWeb/CSS/PropertyID.h>
+#include <LibWeb/CSS/StyleComputer.h>
 #include <LibWeb/CSS/StyleValues/BorderRadiusStyleValue.h>
 #include <LibWeb/CSS/StyleValues/CSSKeywordValue.h>
 #include <LibWeb/CSS/StyleValues/GridTemplateAreaStyleValue.h>
@@ -43,23 +44,25 @@ String ShorthandStyleValue::to_string(SerializationMode mode) const
     // If all the longhands are the same CSS-wide keyword, just return that once.
     Optional<Keyword> built_in_keyword;
     bool all_same_keyword = true;
-    for (auto& value : m_properties.values) {
-        if (!value->is_css_wide_keyword()) {
+    StyleComputer::for_each_property_expanding_shorthands(m_properties.shorthand_property, *this, [&](PropertyID name, CSSStyleValue const& value) {
+        (void)name;
+        if (!value.is_css_wide_keyword()) {
             all_same_keyword = false;
-            break;
+            return;
         }
-        auto keyword = value->to_keyword();
+        auto keyword = value.to_keyword();
         if (!built_in_keyword.has_value()) {
             built_in_keyword = keyword;
-            continue;
+            return;
         }
         if (built_in_keyword != keyword) {
             all_same_keyword = false;
-            break;
+            return;
         }
-    }
+    });
+
     if (all_same_keyword && built_in_keyword.has_value())
-        return m_properties.values.first()->to_string(mode);
+        return MUST(String::from_utf8(string_from_keyword(built_in_keyword.value())));
 
     auto default_to_string = [&]() {
         auto all_properties_same_value = true;
