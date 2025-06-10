98e63e3dd55912d69d6ac8a7647c88324bc83744
Tim Ledbetter <tim.ledbetter@ladybird.org>
2025-06-10 23:54:48 +0100

    LibWeb: Parse the `border-image-outset` property

--- Meta/Lagom/Tools/CodeGenerators/LibWeb/GenerateCSSPseudoElement.cpp
+++ Meta/Lagom/Tools/CodeGenerators/LibWeb/GenerateCSSPseudoElement.cpp
@@ -316,6 +316,7 @@ bool pseudo_element_supports_property(PseudoElement pseudo_element, PropertyID p
                 append_property("border-bottom-style"sv);
                 append_property("border-bottom-width"sv);
                 append_property("border-color"sv);
+                append_property("border-image-outset"sv);
                 append_property("border-image-slice"sv);
                 append_property("border-image-source"sv);
                 append_property("border-image-width"sv);
