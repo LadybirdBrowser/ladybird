52d18a2923c78c927e057f994d12981d257043f5
Timothy Flynn <trflynn89@pm.me>
2025-06-13 10:15:08 -0400

    AK: Define a formatter for char16_t

--- AK/Format.h
+++ AK/Format.h
@@ -547,8 +547,12 @@ struct Formatter<char> : StandardFormatter {
     ErrorOr<void> format(FormatBuilder&, char);
 };
 template<>
+struct Formatter<char16_t> : StandardFormatter {
+    ErrorOr<void> format(FormatBuilder&, char16_t);
+};
+template<>
 struct Formatter<char32_t> : StandardFormatter {
-    ErrorOr<void> format(FormatBuilder& builder, char32_t);
+    ErrorOr<void> format(FormatBuilder&, char32_t);
 };
 template<>
 struct Formatter<bool> : StandardFormatter {
