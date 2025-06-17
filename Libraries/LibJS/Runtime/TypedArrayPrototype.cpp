edeef940b6dc52cbb5a4138118543cef2db7d53a
Daniel Bertalan <dani@danielbertalan.dev>
2025-06-17 12:04:16 +0200

    LibJS: Fix pointer authentication failure in `TypedArray`
    `TypedArray` objects need to know their own constructor objects to allow
    copying. This was implemented by storing a function pointer to the
    `Intrinsic` object's method which returns the constructor object.
    
    The problem is that function pointers aren't polymorphic, we can't
    legally just cast e.g. a `Derived* (*ptr)(void)` to `Base*
    (*ptr)(void)` (this is why the code needed a `bit_cast` to even
    compile). But this wasn't actually a problem in practice because their
    ABIs were the same. But with pointer authentication (Apple's `arm64e`
    ABI) this signature mismatch becomes a hard failure and crashes the
    process.
    
    Fix this by adding a virtual function that returns the intrinsic
    constructor (actually, a `NativeFunction`, as typed arrays constructors
    don't inherit from the base `TypedArray` constructor) instead of the
    function pointer.
    
    With this, test-js passes and Ladybird launches correctly when built
    (with a lot of vcpkg hacks) for arm64e.

--- Libraries/LibJS/Runtime/TypedArrayPrototype.cpp
+++ Libraries/LibJS/Runtime/TypedArrayPrototype.cpp
@@ -97,7 +97,7 @@ static ThrowCompletionOr<TypedArrayBase*> typed_array_species_create(VM& vm, Typ
     auto& realm = *vm.current_realm();
 
     // 1. Let defaultConstructor be the intrinsic object listed in column one of Table 72 for exemplar.[[TypedArrayName]].
-    auto default_constructor = (realm.intrinsics().*exemplar.intrinsic_constructor())();
+    auto default_constructor = exemplar.intrinsic_constructor(realm);
 
     // 2. Let constructor be ? SpeciesConstructor(exemplar, defaultConstructor).
     auto* constructor = TRY(species_constructor(vm, exemplar, *default_constructor));
