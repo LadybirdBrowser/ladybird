ea21480b5bab98e9406951f0b745a15a93cc4348
Totto16 <tobiausgais@gmail.com>
2025-06-18 02:51:52 +0200

    LibWeb: Factor out canvas rendering options algorihtms
    Factor out canvas parsing algorihtm for CanvasRenderingContext2DSettings
    from JS::Value. This was only used in one place but needs to be usable
    from other places too in the future.

--- Libraries/LibWeb/HTML/CanvasRenderingContext2D.cpp
+++ Libraries/LibWeb/HTML/CanvasRenderingContext2D.cpp
@@ -37,7 +37,7 @@ GC_DEFINE_ALLOCATOR(CanvasRenderingContext2D);
 
 JS::ThrowCompletionOr<GC::Ref<CanvasRenderingContext2D>> CanvasRenderingContext2D::create(JS::Realm& realm, HTMLCanvasElement& element, JS::Value options)
 {
-    auto context_attributes = TRY(context_attributes_from_options(realm.vm(), options));
+    auto context_attributes = TRY(CanvasRenderingContext2DSettings::from_js_value(realm.vm(), options));
     return realm.create<CanvasRenderingContext2D>(realm, element, context_attributes);
 }
 
@@ -62,52 +62,6 @@ void CanvasRenderingContext2D::visit_edges(Cell::Visitor& visitor)
 {
     Base::visit_edges(visitor);
     visitor.visit(m_element);
-}
-
-// https://html.spec.whatwg.org/multipage/canvas.html#canvasrenderingcontext2dsettings
-JS::ThrowCompletionOr<CanvasRenderingContext2DSettings> CanvasRenderingContext2D::context_attributes_from_options(JS::VM& vm, JS::Value value)
-{
-    if (!value.is_nullish() && !value.is_object())
-        return vm.throw_completion<JS::TypeError>(JS::ErrorType::NotAnObjectOfType, "CanvasRenderingContext2DSettings");
-
-    CanvasRenderingContext2DSettings settings;
-    if (value.is_nullish())
-        return settings;
-
-    auto& value_object = value.as_object();
-
-    JS::Value alpha = TRY(value_object.get("alpha"_fly_string));
-    settings.alpha = alpha.is_undefined() ? true : alpha.to_boolean();
-
-    JS::Value desynchronized = TRY(value_object.get("desynchronized"_fly_string));
-    settings.desynchronized = desynchronized.is_undefined() ? false : desynchronized.to_boolean();
-
-    JS::Value color_space = TRY(value_object.get("colorSpace"_fly_string));
-    if (!color_space.is_undefined()) {
-        auto color_space_string = TRY(color_space.to_string(vm));
-        if (color_space_string == "srgb"sv)
-            settings.color_space = Bindings::PredefinedColorSpace::Srgb;
-        else if (color_space_string == "display-p3"sv)
-            settings.color_space = Bindings::PredefinedColorSpace::DisplayP3;
-        else
-            return vm.throw_completion<JS::TypeError>(JS::ErrorType::InvalidEnumerationValue, color_space_string, "colorSpace");
-    }
-
-    JS::Value color_type = TRY(value_object.get("colorType"_fly_string));
-    if (!color_type.is_undefined()) {
-        auto color_type_string = TRY(color_type.to_string(vm));
-        if (color_type_string == "unorm8"sv)
-            settings.color_type = Bindings::CanvasColorType::Unorm8;
-        else if (color_type_string == "float16"sv)
-            settings.color_type = Bindings::CanvasColorType::Float16;
-        else
-            return vm.throw_completion<JS::TypeError>(JS::ErrorType::InvalidEnumerationValue, color_type_string, "colorType");
-    }
-
-    JS::Value will_read_frequently = TRY(value_object.get("willReadFrequently"_fly_string));
-    settings.will_read_frequently = will_read_frequently.is_undefined() ? false : will_read_frequently.to_boolean();
-
-    return settings;
 }
 
 HTMLCanvasElement& CanvasRenderingContext2D::canvas_element()
