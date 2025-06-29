8640a3a6e8462daec167a08dc026cd98ecd6b8e4
Callum Law <callumlaw1709@outlook.com>
2025-06-29 17:18:05 +1200

    LibWeb: Support relative lengths in `calc` color values
    Gains us ~40 WPT tests.

--- Libraries/LibWeb/Editing/Internal/Algorithms.cpp
+++ Libraries/LibWeb/Editing/Internal/Algorithms.cpp
@@ -1174,7 +1174,8 @@ Optional<String> effective_command_value(GC::Ptr<DOM::Node> node, FlyString cons
             if (!background_color.has_value())
                 return NumericLimits<u8>::max();
             VERIFY(is<Layout::NodeWithStyle>(node->layout_node()));
-            return background_color.value()->to_color(*static_cast<Layout::NodeWithStyle*>(node->layout_node()), {}).alpha();
+            auto& layout_node = *static_cast<Layout::NodeWithStyle*>(node->layout_node());
+            return background_color.value()->to_color(layout_node, { .length_resolution_context = CSS::Length::ResolutionContext::for_layout_node(layout_node) }).alpha();
         };
         while (resolved_background_alpha() == 0 && node->parent() && is<DOM::Element>(*node->parent()))
             node = node->parent();
