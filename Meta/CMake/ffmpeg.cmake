5ec816c4c3b36c26427a5e6f5a9101e16892ab40
Olekoop <mlglol360xd@gmail.com>
2025-07-03 19:25:39 +0200

    LibMedia+Meta: Enable FFmpeg under Android and remove the stubs
    The licensing issue seems to be resolved (#2214).
    The stubs are not longer necessary. Even if they were, they do not
    compile properly.

--- Meta/CMake/ffmpeg.cmake
+++ Meta/CMake/ffmpeg.cmake
@@ -1,6 +1,10 @@
 include_guard()
 
+if (NOT ANDROID)
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(AVCODEC REQUIRED IMPORTED_TARGET libavcodec)
 pkg_check_modules(AVFORMAT REQUIRED IMPORTED_TARGET libavformat)
 pkg_check_modules(AVUTIL REQUIRED IMPORTED_TARGET libavutil)
+else()
+    find_package(FFMPEG REQUIRED)
+endif()
