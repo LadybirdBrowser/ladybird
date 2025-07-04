LadybirdBrowser/ladybird

commit bc8870d019ee7a7087cc2c784058ed1313dc6011

LibWeb: Make `recompute_selection_states()` go faster
...by avoiding `is_before()` call on every loop iteration in step 5.

This change makes switching channels on Discord go a lot faster.

Libraries/LibWeb/Painting/ViewportPaintable.cpp
@@ -374,7 +374,7 @@ void ViewportPaintable::recompute_selection_states(DOM::Range& range)
     }
 
     // 5. Mark the nodes between start node and end node (in tree order) as Full.
-    for (auto* node = start_container->next_in_pre_order(); node && (node->is_before(end_container) || node->is_descendant_of(end_container)); node = node->next_in_pre_order()) {
+    for (auto* node = start_container->next_in_pre_order(); node && node != end_container; node = node->next_in_pre_order()) {
         if (node->is_inert())
             continue;
         if (auto* paintable = node->paintable())

commit 16609e609dedba78e9b486c77bc4d82cb2efd0a4

LibWeb: Fix selection when start node is inside end node
Fixes a regression introduced in
bc8870d019ee7a7087cc2c784058ed1313dc6011 (in a performant way this
time)

Libraries/LibWeb/Painting/ViewportPaintable.cpp
@@ -362,19 +362,23 @@ void ViewportPaintable::recompute_selection_states(DOM::Range& range)
 
     // 4. Mark the selection end node as End (if text) or Full (if anything else).
     if (auto* paintable = end_container->paintable(); paintable && !range.end().node->is_inert()) {
-        if (is<DOM::Text>(*end_container) || end_container->is_ancestor_of(start_container)) {
+        if (is<DOM::Text>(*end_container))
             paintable->set_selection_state(SelectionState::End);
-        } else {
-            paintable->for_each_in_inclusive_subtree([&](auto& layout_node) {
-                if (!layout_node.dom_node() || !layout_node.dom_node()->is_inert())
-                    layout_node.set_selection_state(SelectionState::Full);
-                return TraversalDecision::Continue;
-            });
-        }
+        else
+            paintable->set_selection_state(SelectionState::Full);
     }
 
     // 5. Mark the nodes between start node and end node (in tree order) as Full.
-    for (auto* node = start_container->next_in_pre_order(); node && node != end_container; node = node->next_in_pre_order()) {
+    // NOTE: If the start node is a descendant of the end node, we cannot traverse from it to the end node since the end node is before it in tree order.
+    //       Instead, we need to stop traversal somewhere inside the end node, or right after it.
+    DOM::Node* stop_at;
+    if (start_container->is_descendant_of(end_container)) {
+        stop_at = end_container->child_at_index(range.end_offset());
+    } else {
+        stop_at = end_container;
+    }
+
+    for (auto* node = start_container->next_in_pre_order(); node && node != stop_at; node = node->next_in_pre_order(end_container)) {
         if (node->is_inert())
             continue;
         if (auto* paintable = node->paintable())

Tests/LibWeb/Screenshot/expected/selection-start-in-end-node-ref.html
@@ -1,0 +1,7 @@
+<!DOCTYPE html>
+<style>
+* {
+    margin: 0;
+}
+</style>
+<img src="../images/selection-start-in-end-node-ref.png">

Tests/LibWeb/Screenshot/input/selection-start-in-end-node-2.html
@@ -1,0 +1,10 @@
+<!DOCTYPE html>
+<link rel="match" href="../expected/selection-start-in-end-node-ref.html" />
+<!-- The space after the start node, as well as "NOT SELECTED" must not be selected. -->
+<span id="end">End Node <span id="start">Start Node</span> </span>NOT SELECTED
+<script>
+  let range = document.createRange();
+  range.setStart(start, 0);
+  range.setEnd(end, 2);
+  window.getSelection().addRange(range);
+</script>

Tests/LibWeb/Screenshot/input/selection-start-in-end-node-3.html
@@ -1,0 +1,10 @@
+<!DOCTYPE html>
+<link rel="match" href="../expected/selection-start-in-end-node-ref.html" />
+<!-- here the end node contains the start node, as well as some further selected content and some non-selected content. -->
+<span id="end">End Node <span id="start">Start</span> <span>Node</span> </span>NOT SELECTED
+<script>
+  let range = document.createRange();
+  range.setStart(start, 0);
+  range.setEnd(end, 4);
+  window.getSelection().addRange(range);
+</script>

Tests/LibWeb/Screenshot/input/selection-start-in-end-node.html
@@ -1,0 +1,10 @@
+<!DOCTYPE html>
+<link rel="match" href="../expected/selection-start-in-end-node-ref.html" />
+<!-- The text after the end node must not be selected. -->
+<span id="end">End Node <span id="start">Start Node</span></span> NOT SELECTED
+<script>
+  let range = document.createRange();
+  range.setStart(start, 0);
+  range.setEnd(end, 2);
+  window.getSelection().addRange(range);
+</script>