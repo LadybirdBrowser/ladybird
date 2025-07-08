b05fe0127bebb4a948318f92d4a749647b2b8ff9
Sam Atkins <sam@ladybird.org>
2025-07-08 14:38:42 +0100

    LibWeb/HTML: Allow more characters in custom element names
    Corresponds to https://github.com/whatwg/html/commit/78d2678789d2d45cc0fb2ad3d056323d7a3a355b
    
    And import a related test.

--- Libraries/LibWeb/HTML/CustomElements/CustomElementName.h
+++ Libraries/LibWeb/HTML/CustomElements/CustomElementName.h
@@ -1,15 +1,21 @@
 /*
  * Copyright (c) 2023, Srikavin Ramkumar <me@srikavin.me>
+ * Copyright (c) 2025, Sam Atkins <sam@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-#include <AK/StringView.h>
+#include <AK/String.h>
 
 namespace Web::HTML {
 
-bool is_valid_custom_element_name(StringView name);
+bool is_valid_custom_element_name(String const& name);
+
+inline bool is_valid_custom_element_name(FlyString const& name)
+{
+    return is_valid_custom_element_name(name.to_string());
+}
 
 }
