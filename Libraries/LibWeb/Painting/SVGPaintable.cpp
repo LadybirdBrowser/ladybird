64d11b6183029b34c5350d5af375bbe388447a41
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2025-07-12 00:17:38 +0200

    LibWeb: Allow `<svg>` to establish a stacking context
    83b6bc4 went too far by forbidding SVGSVGElement from establishing a
    stacking context. This element type does follow the behavior of CSS
    boxes, unlike inner SVG elements like `<rect>`, `<circle>`, etc., which
    are not supposed to be aware of concepts like stacking contexts,
    overflow clipping, scroll offsets, etc.
    
    This change allows us to delete overrides of `before_paint()` and
    `after_paint()` in SVGPaintable and SVGSVGPaintable, because display
    list recording code has been rearranged to take care of clipping and
    scrolling before recursing into SVGSVGPaintable descendants.
    
    `Screenshot/images/css-transform-box-ref.png` expectation is updated and
    fixes a bug where a rectangle at the very bottom of the page was not
    clipped correctly.
    `Screenshot/images/svg-filters-lb-website-ref.png` has a more subtle
    difference, but if you look closely, you’ll see it matches other
    browsers more closely now.

--- Libraries/LibWeb/Painting/SVGPaintable.cpp
+++ Libraries/LibWeb/Painting/SVGPaintable.cpp
@@ -17,26 +17,6 @@ SVGPaintable::SVGPaintable(Layout::SVGBox const& layout_box)
 {
 }
 
-void SVGPaintable::before_paint(PaintContext& context, PaintPhase phase) const
-{
-    if (!is_visible())
-        return;
-    if (!has_css_transform()) {
-        apply_clip_overflow_rect(context, phase);
-    }
-    apply_scroll_offset(context);
-}
-
-void SVGPaintable::after_paint(PaintContext& context, PaintPhase phase) const
-{
-    if (!is_visible())
-        return;
-    reset_scroll_offset(context);
-    if (!has_css_transform()) {
-        clear_clip_overflow_rect(context, phase);
-    }
-}
-
 Layout::SVGBox const& SVGPaintable::layout_box() const
 {
     return static_cast<Layout::SVGBox const&>(layout_node());
