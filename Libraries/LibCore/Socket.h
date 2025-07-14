2300c115c936336211f12084ebdd2283a92564cd
ayeteadoe <ayeteadoe@gmail.com>
2025-07-14 15:27:18 -0700

    Tests/LibCore: Enable TestLibCoreStream in Windows CI

--- Libraries/LibCore/Socket.h
+++ Libraries/LibCore/Socket.h
@@ -243,21 +243,7 @@ public:
         return *this;
     }
 
-    virtual ErrorOr<Bytes> read_some(Bytes buffer) override
-    {
-        auto pending_bytes = TRY(this->pending_bytes());
-        if (pending_bytes > buffer.size()) {
-            // With UDP datagrams, reading a datagram into a buffer that's
-            // smaller than the datagram's size will cause the rest of the
-            // datagram to be discarded. That's not very nice, so let's bail
-            // early, telling the caller that he should allocate a bigger
-            // buffer.
-            return Error::from_errno(EMSGSIZE);
-        }
-
-        return m_helper.read(buffer, default_flags());
-    }
-
+    virtual ErrorOr<Bytes> read_some(Bytes buffer) override;
     virtual ErrorOr<size_t> write_some(ReadonlyBytes buffer) override { return m_helper.write(buffer, default_flags()); }
     virtual bool is_eof() const override { return m_helper.is_eof(); }
     virtual bool is_open() const override { return m_helper.is_open(); }
