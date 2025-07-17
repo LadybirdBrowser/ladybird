7669abcc22fe7cf2aff1f716d6349fb203c57f62
zac <zac@zacoons.com>
2025-07-18 07:59:15 +1000

    LibWeb: Remove redundant for_each_fragment method in PaintableBox

--- Libraries/LibWeb/Painting/PaintableBox.h
+++ Libraries/LibWeb/Painting/PaintableBox.h
@@ -372,15 +372,6 @@ public:
 
     void set_fragments(Vector<PaintableFragment>&& fragments) { m_fragments = move(fragments); }
 
-    template<typename Callback>
-    void for_each_fragment(Callback callback) const
-    {
-        for (auto& fragment : m_fragments) {
-            if (callback(fragment) == IterationDecision::Break)
-                return;
-        }
-    }
-
     virtual void paint(DisplayListRecordingContext&, PaintPhase) const override;
 
     [[nodiscard]] virtual TraversalDecision hit_test(CSSPixelPoint position, HitTestType type, Function<TraversalDecision(HitTestResult)> const& callback) const override;
