5e6e5bdda7e4876697b012701e460fe2ba152766
Shannon Booth <shannon@serenityos.org>
2025-07-18 15:00:12 +1200

    LibWeb: Use Bindings::InterfaceName for serializing types
    While this does cost us an extra byte to serialize as it
    contains _all_ interface names instead of the set of serializable
    types, doing this will allow us to remove to use the same
    enum for checking whether that interface is exposed in a future
    commit.

--- Libraries/LibWeb/Geometry/DOMRect.h
+++ Libraries/LibWeb/Geometry/DOMRect.h
@@ -33,7 +33,7 @@ public:
     void set_width(double width) { m_rect.set_width(width); }
     void set_height(double height) { m_rect.set_height(height); }
 
-    virtual HTML::SerializeType serialize_type() const override { return HTML::SerializeType::DOMRect; }
+    virtual Bindings::InterfaceName serialize_type() const override { return Bindings::InterfaceName::DOMRect; }
 
 private:
     DOMRect(JS::Realm&, double x, double y, double width, double height);
