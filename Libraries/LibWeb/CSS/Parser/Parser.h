866e12f6889a9e3dee43797e8cd735ac40b6109f
InvalidUsernameException <InvalidUsernameException@users.noreply.github.com>
2025-07-18 20:36:11 +0200

    LibWeb/CSS: Parse mask-size property

--- Libraries/LibWeb/CSS/Parser/Parser.h
+++ Libraries/LibWeb/CSS/Parser/Parser.h
@@ -421,7 +421,7 @@ private:
     RefPtr<CSSStyleValue const> parse_aspect_ratio_value(TokenStream<ComponentValue>&);
     RefPtr<CSSStyleValue const> parse_background_value(TokenStream<ComponentValue>&);
     RefPtr<CSSStyleValue const> parse_single_background_position_x_or_y_value(TokenStream<ComponentValue>&, PropertyID);
-    RefPtr<CSSStyleValue const> parse_single_background_size_value(TokenStream<ComponentValue>&);
+    RefPtr<CSSStyleValue const> parse_single_background_size_value(PropertyID, TokenStream<ComponentValue>&);
     RefPtr<CSSStyleValue const> parse_border_value(PropertyID, TokenStream<ComponentValue>&);
     RefPtr<CSSStyleValue const> parse_border_image_value(TokenStream<ComponentValue>&);
     RefPtr<CSSStyleValue const> parse_border_image_slice_value(TokenStream<ComponentValue>&);
