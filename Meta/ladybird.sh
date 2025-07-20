98b45b4137da20125579c3a842f3a34ccc862f09
rmgx <git@rmgx.io>
2025-07-20 13:36:56 -0500

    Meta: Remove `ladybird.sh`

--- Meta/ladybird.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo -e "\033[33;1mWARNING\033[0m: ladybird.sh is deprecated and will be removed soon. Please use ladybird.py instead."
-"${DIR}/ladybird.py" "$@"
