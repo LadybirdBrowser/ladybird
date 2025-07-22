33c3c3324901ead5af72a624f465a019942458d6
Jan Koudijs <theappgineer@gmail.com>
2025-07-22 10:58:41 -0600

    Meta: Add offline build for ANGLE flatpak module
    At the same time, build with gcc instead of bundled clang.
    
    Co-Authored-By: Andrew Kaster <andrew@ladybird.org>

--- Meta/CMake/flatpak/angle/angle-build.sh
+++ Meta/CMake/flatpak/angle/angle-build.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-export PATH=$PWD/depot_tools:$PATH
 cd angle
 
 ninja -j"$(nproc)" -C out
--- Meta/CMake/flatpak/angle/angle-configure.sh
+++ Meta/CMake/flatpak/angle/angle-configure.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-export PATH=$PWD/depot_tools:$PATH
 cd angle
 
 gn gen out --args='
+    is_clang=false
     is_official_build=true
     is_component_build=true
     is_debug=false
     angle_build_tests=false
+    angle_enable_abseil=true
     angle_enable_renderdoc=false
     angle_enable_swiftshader=false
     angle_enable_vulkan=true
@@ -18,8 +19,11 @@ gn gen out --args='
     angle_use_wayland=true
     angle_use_x11=false
     build_angle_deqp_tests=false
+    treat_warnings_as_errors=false
     use_custom_libcxx=false
     use_safe_libstdcxx=true
+    use_siso=false
+    use_sysroot=false
     chrome_pgo_phase=0
     is_cfi = false
 '
