fdfba1e43b1322ebd724bfe5729478b77c7ae962
stelar7 <dudedbz@gmail.com>
2025-07-25 19:51:45 +0200

    LibWeb/IDB: Implement create_a_request_to_retrieve_multiple_items

--- Libraries/LibWeb/IndexedDB/IDBObjectStore.cpp
+++ Libraries/LibWeb/IndexedDB/IDBObjectStore.cpp
@@ -562,68 +562,9 @@ WebIDL::ExceptionOr<GC::Ref<IDBRequest>> IDBObjectStore::get_key(JS::Value query
 // https://w3c.github.io/IndexedDB/#dom-idbobjectstore-getall
 WebIDL::ExceptionOr<GC::Ref<IDBRequest>> IDBObjectStore::get_all(Optional<JS::Value> query_or_options, Optional<WebIDL::UnsignedLong> count)
 {
-    auto& realm = this->realm();
-    auto& vm = realm.vm();
-
-    // 1. Let transaction be this’s transaction.
-    auto transaction = this->transaction();
-
-    // 2. Let store be this’s object store.
-    auto store = this->store();
-
-    // FIXME: 3. If store has been deleted, throw an "InvalidStateError" DOMException.
-
-    // 4. If transaction’s state is not active, then throw a "TransactionInactiveError" DOMException.
-    if (!transaction->is_active())
-        return WebIDL::TransactionInactiveError::create(realm, "Transaction is not active while getting all"_utf16);
-
-    // 5. Let range be a key range.
-    GC::Ptr<IDBKeyRange> range;
-
-    // 6. Let direction be a cursor direction.
-    Bindings::IDBCursorDirection direction = Bindings::IDBCursorDirection::Next;
-
-    // 7. If running is a potentially valid key range with query_or_options is true, then:
-    if (is_a_potentially_valid_key_range(realm, *query_or_options)) {
-
-        // 1. Set range to the result of converting a value to a key range with query_or_options. Rethrow any exceptions.
-        range = TRY(convert_a_value_to_a_key_range(realm, query_or_options));
-
-        // 2. Set direction to "next".
-        direction = Bindings::IDBCursorDirection::Next;
-    }
-
-    // 8. Else:
-    else {
-        // 1. Set range to the result of converting a value to a key range with query_or_options["query"]. Rethrow any exceptions.
-        range = TRY(convert_a_value_to_a_key_range(realm, TRY(query_or_options->get(vm, "query"_utf16))));
-
-        // 2. Set count to query_or_options["count"].
-        count = TRY(TRY(query_or_options->get(vm, "count"_utf16)).to_u32(vm));
-
-        // 3. Set direction to query_or_options["direction"].
-        auto direction_value = TRY(TRY(query_or_options->get(vm, "direction"_utf16)).to_string(vm));
-        if (direction_value == "next")
-            direction = Bindings::IDBCursorDirection::Next;
-        else if (direction_value == "nextunique")
-            direction = Bindings::IDBCursorDirection::Nextunique;
-        else if (direction_value == "prev")
-            direction = Bindings::IDBCursorDirection::Prev;
-        else if (direction_value == "prevunique")
-            direction = Bindings::IDBCursorDirection::Prevunique;
-        else
-            return WebIDL::SimpleException { WebIDL::SimpleExceptionType::TypeError, "Invalid direction value"_string };
-    }
-
-    // 9. Let operation be an algorithm to run retrieve multiple items from an object store with the current Realm record, store, range, "value", direction, and count if given.
-    auto operation = GC::Function<WebIDL::ExceptionOr<JS::Value>()>::create(realm.heap(), [&realm, store, range, direction, count] -> WebIDL::ExceptionOr<JS::Value> {
-        return retrieve_multiple_items_from_an_object_store(realm, store, GC::Ref(*range), RecordKind::Value, direction, count);
-    });
-
-    // 10. Return the result (an IDBRequest) of running asynchronously execute a request with this and operation.
-    auto result = asynchronously_execute_a_request(realm, GC::Ref(*this), operation);
-    dbgln_if(IDB_DEBUG, "Executing request for get all with uuid {}", result->uuid());
-    return result;
+    // 1. Return the result of creating a request to retrieve multiple items with the current Realm record, this,
+    //    "value", queryOrOptions, and count if given. Rethrow any exceptions.
+    return create_a_request_to_retrieve_multiple_items(realm(), GC::Ref(*this), RecordKind::Value, *query_or_options, count);
 }
 
 // https://w3c.github.io/IndexedDB/#dom-idbobjectstore-openkeycursor
@@ -668,95 +609,23 @@ WebIDL::ExceptionOr<GC::Ref<IDBRequest>> IDBObjectStore::open_key_cursor(JS::Val
 // https://w3c.github.io/IndexedDB/#dom-idbobjectstore-getallkeys
 WebIDL::ExceptionOr<GC::Ref<IDBRequest>> IDBObjectStore::get_all_keys(Optional<JS::Value> query_or_options, Optional<WebIDL::UnsignedLong> count)
 {
-    auto& realm = this->realm();
-    auto& vm = realm.vm();
-
-    // 1. Let transaction be this’s transaction.
-    auto transaction = this->transaction();
-
-    // 2. Let store be this’s object store.
-    auto store = this->store();
-
-    // FIXME: 3. If store has been deleted, throw an "InvalidStateError" DOMException.
-
-    // 4. If transaction’s state is not active, then throw a "TransactionInactiveError" DOMException.
-    if (!transaction->is_active())
-        return WebIDL::TransactionInactiveError::create(realm, "Transaction is not active while getting all keys"_utf16);
-
-    // 5. Let range be a key range.
-    GC::Ptr<IDBKeyRange> range;
-
-    // 6. Let direction be a cursor direction.
-    Bindings::IDBCursorDirection direction = Bindings::IDBCursorDirection::Next;
-
-    // 7. If running is a potentially valid key range with query_or_options is true, then:
-    if (is_a_potentially_valid_key_range(realm, *query_or_options)) {
-        // 1. Set range to the result of converting a value to a key range with query_or_options. Rethrow any exceptions.
-        range = TRY(convert_a_value_to_a_key_range(realm, query_or_options));
-
-        // 2. Set direction to "next".
-        direction = Bindings::IDBCursorDirection::Next;
-    }
-
-    // 8. Else:
-    else {
-        // 1. Set range to the result of converting a value to a key range with query_or_options["query"]. Rethrow any exceptions.
-        range = TRY(convert_a_value_to_a_key_range(realm, TRY(query_or_options->get(vm, "query"_utf16))));
-
-        // 2. Set count to query_or_options["count"].
-        count = TRY(TRY(query_or_options->get(vm, "count"_utf16)).to_u32(vm));
-
-        // 3. Set direction to query_or_options["direction"].
-        auto direction_value = TRY(TRY(query_or_options->get(vm, "direction"_utf16)).to_string(vm));
-        if (direction_value == "next")
-            direction = Bindings::IDBCursorDirection::Next;
-        else if (direction_value == "nextunique")
-            direction = Bindings::IDBCursorDirection::Nextunique;
-        else if (direction_value == "prev")
-            direction = Bindings::IDBCursorDirection::Prev;
-        else if (direction_value == "prevunique")
-            direction = Bindings::IDBCursorDirection::Prevunique;
-        else
-            return WebIDL::SimpleException { WebIDL::SimpleExceptionType::TypeError, "Invalid direction value"_string };
-    }
-
-    // 9. Let operation be an algorithm to run retrieve multiple items from an object store with the current Realm record, store, range, "key", direction, and count if given.
-    auto operation = GC::Function<WebIDL::ExceptionOr<JS::Value>()>::create(realm.heap(), [&realm, store, range, direction, count] -> WebIDL::ExceptionOr<JS::Value> {
-        return retrieve_multiple_items_from_an_object_store(realm, store, GC::Ref(*range), RecordKind::Key, direction, count);
-    });
-
-    // 10. Return the result (an IDBRequest) of running asynchronously execute a request with this and operation.
-    auto result = asynchronously_execute_a_request(realm, GC::Ref(*this), operation);
-    dbgln_if(IDB_DEBUG, "Executing request for get all keys with uuid {}", result->uuid());
-    return result;
+    // 1. Return the result of creating a request to retrieve multiple items with the current Realm record, this, "key",
+    //    queryOrOptions, and count if given. Rethrow any exceptions.
+    return create_a_request_to_retrieve_multiple_items(realm(), GC::Ref(*this), RecordKind::Key, *query_or_options, count);
 }
 
+// https://pr-preview.s3.amazonaws.com/w3c/IndexedDB/pull/461.html#dom-idbobjectstore-getallrecords
 WebIDL::ExceptionOr<GC::Ref<IDBRequest>> IDBObjectStore::get_all_records(IDBGetAllOptions const& options)
 {
-    auto& realm = this->realm();
+    // 1. Return the result of creating a request to retrieve multiple items with the current Realm record, this,
+    //    "record", and options. Rethrow any exceptions.
 
-    // 1. Let transaction be this’s transaction.
-    auto transaction = this->transaction();
+    auto converted_options = JS::Object::create(realm(), nullptr);
+    MUST(converted_options->create_data_property("query"_utf16_fly_string, options.query));
+    MUST(converted_options->create_data_property("count"_utf16_fly_string, options.count.has_value() ? JS::Value(options.count.value()) : JS::js_undefined()));
+    MUST(converted_options->create_data_property("direction"_utf16_fly_string, JS::PrimitiveString::create(realm().vm(), idl_enum_to_string(options.direction))));
 
-    // 2. Let store be this’s object store.
-    auto store = this->store();
-
-    // FIXME: 3. If store has been deleted, throw an "InvalidStateError" DOMException.
-
-    // 4. If transaction’s state is not active, then throw a "TransactionInactiveError" DOMException.
-    if (!transaction->is_active())
-        return WebIDL::TransactionInactiveError::create(realm, "Transaction is not active while getting all records"_utf16);
-
-    // 5. Let range be the result of converting a value to a key range with options["query"]. Rethrow any exceptions.
-    auto range = TRY(convert_a_value_to_a_key_range(realm, options.query));
-
-    // 6. Let operation be an algorithm to run retrieve multiple items from an object store with the current Realm record, store, range, "record", options["direction"], and options["count"] if given.
-    auto operation = GC::Function<WebIDL::ExceptionOr<JS::Value>()>::create(realm.heap(), [&realm, store, range, options] -> WebIDL::ExceptionOr<JS::Value> {
-        return retrieve_multiple_items_from_an_object_store(realm, store, range, RecordKind::Record, options.direction, options.count);
-    });
-
-    // 7. Return the result (an IDBRequest) of running asynchronously execute a request with this and operation.
-    return asynchronously_execute_a_request(realm, GC::Ref(*this), operation);
+    return create_a_request_to_retrieve_multiple_items(realm(), GC::Ref(*this), RecordKind::Record, converted_options, {});
 }
 
 }
