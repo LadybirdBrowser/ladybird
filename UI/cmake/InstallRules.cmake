1b1eae4409c831f6b9d394d95e9c77253d63446e
Jan Koudijs <theappgineer@gmail.com>
2025-07-25 11:50:41 +0200

    Meta: Add a freedesktop appstream/metainfo manifest

--- UI/cmake/InstallRules.cmake
+++ UI/cmake/InstallRules.cmake
@@ -112,6 +112,14 @@ endif()
 
 if (ENABLE_INSTALL_FREEDESKTOP_FILES)
     set(FREEDESKTOP_RESOURCE_DIR "${LADYBIRD_SOURCE_DIR}/Meta/CMake/freedesktop")
+    string(TIMESTAMP DATE "%Y-%m-%d" UTC)
+    execute_process(
+        COMMAND git rev-parse --short=10 HEAD
+        WORKING_DIRECTORY ${LADYBIRD_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    configure_file(${FREEDESKTOP_RESOURCE_DIR}/org.ladybird.Ladybird.metainfo.xml.in org.ladybird.Ladybird.metainfo.xml @ONLY)
     install(FILES
         "${FREEDESKTOP_RESOURCE_DIR}/org.ladybird.Ladybird.svg"
         DESTINATION "${CMAKE_INSTALL_DATADIR}/icons/hicolor/scalable/apps"
@@ -125,6 +133,11 @@ if (ENABLE_INSTALL_FREEDESKTOP_FILES)
     install(FILES
         "${FREEDESKTOP_RESOURCE_DIR}/org.ladybird.Ladybird.service"
         DESTINATION "${CMAKE_INSTALL_DATADIR}/dbus-1/services"
+        COMPONENT ladybird_Runtime
+    )
+    install(FILES
+        "org.ladybird.Ladybird.metainfo.xml"
+        DESTINATION "${CMAKE_INSTALL_DATADIR}/metainfo"
         COMPONENT ladybird_Runtime
     )
 endif()
