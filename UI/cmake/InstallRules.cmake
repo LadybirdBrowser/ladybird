LadybirdBrowser/ladybird

commit 1b1eae4409c831f6b9d394d95e9c77253d63446e

Meta: Add a freedesktop appstream/metainfo manifest

UI/cmake/InstallRules.cmake
@@ -112,6 +112,14 @@ endif()
 
 if (ENABLE_INSTALL_FREEDESKTOP_FILES)
     set(FREEDESKTOP_RESOURCE_DIR "${LADYBIRD_SOURCE_DIR}/Meta/CMake/freedesktop")
+    string(TIMESTAMP DATE "%Y-%m-%d" UTC)
+    execute_process(
+        COMMAND git rev-parse --short=10 HEAD
+        WORKING_DIRECTORY ${LADYBIRD_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    configure_file(${FREEDESKTOP_RESOURCE_DIR}/org.ladybird.Ladybird.metainfo.xml.in org.ladybird.Ladybird.metainfo.xml @ONLY)
     install(FILES
         "${FREEDESKTOP_RESOURCE_DIR}/org.ladybird.Ladybird.svg"
         DESTINATION "${CMAKE_INSTALL_DATADIR}/icons/hicolor/scalable/apps"
@@ -125,6 +133,11 @@ if (ENABLE_INSTALL_FREEDESKTOP_FILES)
     install(FILES
         "${FREEDESKTOP_RESOURCE_DIR}/org.ladybird.Ladybird.service"
         DESTINATION "${CMAKE_INSTALL_DATADIR}/dbus-1/services"
+        COMPONENT ladybird_Runtime
+    )
+    install(FILES
+        "org.ladybird.Ladybird.metainfo.xml"
+        DESTINATION "${CMAKE_INSTALL_DATADIR}/metainfo"
         COMPONENT ladybird_Runtime
     )
 endif()

Meta/CMake/freedesktop/org.ladybird.Ladybird.desktop
@@ -9,7 +9,8 @@ Exec=Ladybird --force-new-process %U
 Icon=org.ladybird.Ladybird
 DBusActivatable=false
 Type=Application
-Categories=Network;WebBrowser;Qt;KDE;
+Categories=Network;WebBrowser;
+Keywords=Qt;KDE;
 StartupNotify=false
 MimeType=text/html;application/xhtml+xml;x-scheme-handler/http;x-scheme-handler/https;
 Actions=new-window;

Meta/CMake/freedesktop/org.ladybird.Ladybird.metainfo.xml.in
@@ -1,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Copyright 2025, the Ladybird developers. -->
+<component type="desktop-application">
+  <id>org.ladybird.Ladybird</id>
+
+  <name>Ladybird</name>
+  <summary>Truly independent web browser</summary>
+
+  <developer id="org.ladybird">
+    <name>Ladybird Browser</name>
+  </developer>
+
+  <url type="homepage">https://ladybird.org</url>
+  <url type="bugtracker">https://github.com/LadybirdBrowser/ladybird/issues</url>
+  <url type="faq">https://ladybird.org/#faq</url>
+  <url type="donation">https://donorbox.org/ladybird</url>
+  <url type="contact">https://ladybird.org/organization/#intiative</url>
+  <url type="vcs-browser">https://github.com/LadybirdBrowser/ladybird</url>
+  <url type="contribute">https://ladybird.org/#gi</url>
+
+  <metadata_license>0BSD</metadata_license>
+  <project_license>BSD-2-Clause</project_license>
+  <content_rating type="oars-1.1" />
+
+  <description>
+    <p>
+      Ladybird is a brand-new browser and web engine.
+      Driven by a web standards first approach, Ladybird aims to render the modern web with good performance, stability and security.
+    </p>
+  </description>
+
+  <provides>
+    <mediatype>text/html</mediatype>
+    <mediatype>text/xml</mediatype>
+    <mediatype>application/xhtml+xml</mediatype>
+    <mediatype>x-scheme-handler/http</mediatype>
+    <mediatype>x-scheme-handler/https</mediatype>
+  </provides>
+
+  <launchable type="desktop-id">org.ladybird.Ladybird.desktop</launchable>
+  <releases>
+    <release version="@CMAKE_PROJECT_VERSION@-snapshot-@GIT_HASH@" type="snapshot" date="@DATE@">
+      <description>
+        <p>
+          Pre-Alpha.
+        </p>
+      </description>
+    </release>
+  </releases>
+
+  <branding>
+    <color type="primary" scheme_preference="light">#9d7cf2</color>
+    <color type="primary" scheme_preference="dark">#8a64e5</color>
+  </branding>
+
+</component>

commit 58f8ca47fd9f2f8a22173ed82b160b8b6dac9c71

CMake: Install AppStream metainfo file with absolute path
Fixes CMake install with a separate build directory breakage introduced
by 1b1eae4409c831f6b9d394d95e9c77253d63446e

UI/cmake/InstallRules.cmake
@@ -119,7 +119,7 @@ if (ENABLE_INSTALL_FREEDESKTOP_FILES)
         OUTPUT_VARIABLE GIT_HASH
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    configure_file(${FREEDESKTOP_RESOURCE_DIR}/org.ladybird.Ladybird.metainfo.xml.in org.ladybird.Ladybird.metainfo.xml @ONLY)
+    configure_file("${FREEDESKTOP_RESOURCE_DIR}/org.ladybird.Ladybird.metainfo.xml.in" "${CMAKE_CURRENT_BINARY_DIR}/org.ladybird.Ladybird.metainfo.xml" @ONLY)
     install(FILES
         "${FREEDESKTOP_RESOURCE_DIR}/org.ladybird.Ladybird.svg"
         DESTINATION "${CMAKE_INSTALL_DATADIR}/icons/hicolor/scalable/apps"
@@ -136,7 +136,7 @@ if (ENABLE_INSTALL_FREEDESKTOP_FILES)
         COMPONENT ladybird_Runtime
     )
     install(FILES
-        "org.ladybird.Ladybird.metainfo.xml"
+        "${CMAKE_CURRENT_BINARY_DIR}/org.ladybird.Ladybird.metainfo.xml"
         DESTINATION "${CMAKE_INSTALL_DATADIR}/metainfo"
         COMPONENT ladybird_Runtime
     )