da70f82152553651ebd1996b685e1e6576f3ce10
Kenneth Myhra <kennethmyhra@gmail.com>
2025-07-30 12:19:12 +0200

    LibWeb: Implement FederatedCredential ctor and corresponding AO
    This implements the following AO:
    - Create a FederatedCredential from FederatedCredentialInit.
    
    Which corresponds to this FederatedCredential ctor:
    - constructor(FederatedCredentialInit)

--- Libraries/LibWeb/CredentialManagement/FederatedCredential.cpp
+++ Libraries/LibWeb/CredentialManagement/FederatedCredential.cpp
@@ -6,6 +6,7 @@
 
 #include <LibWeb/Bindings/Intrinsics.h>
 #include <LibWeb/CredentialManagement/FederatedCredential.h>
+#include <LibWeb/CredentialManagement/FederatedCredentialOperations.h>
 
 namespace Web::CredentialManagement {
 
@@ -17,9 +18,12 @@ GC::Ref<FederatedCredential> FederatedCredential::create(JS::Realm& realm)
 }
 
 // https://www.w3.org/TR/credential-management-1/#dom-federatedcredential-federatedcredential
-WebIDL::ExceptionOr<GC::Ref<FederatedCredential>> FederatedCredential::construct_impl(JS::Realm& realm, FederatedCredentialInit const&)
+WebIDL::ExceptionOr<GC::Ref<FederatedCredential>> FederatedCredential::construct_impl(JS::Realm& realm, FederatedCredentialInit const& data)
 {
-    return realm.vm().throw_completion<JS::InternalError>(JS::ErrorType::NotImplemented, "construct"sv);
+    // 1. Let r be the result of executing Create a FederatedCredential from FederatedCredentialInit on data. If that
+    // threw an exception, rethrow that exception.
+    // 2. Return r.
+    return create_federated_credential(realm, data);
 }
 
 FederatedCredential::~FederatedCredential()
@@ -28,6 +32,14 @@ FederatedCredential::~FederatedCredential()
 
 FederatedCredential::FederatedCredential(JS::Realm& realm)
     : Credential(realm)
+{
+}
+
+FederatedCredential::FederatedCredential(JS::Realm& realm, FederatedCredentialInit const& init)
+    : Credential(realm, init.id)
+    , CredentialUserData(init.name.value_or(String {}), init.icon_url.value_or(String {}))
+    , m_provider(init.provider)
+    , m_origin(init.origin)
 {
 }
 
