4e1e49170d5c4e1952fe6c69a1c98a8052889e86
Tete17 <miguel_tete17@hotmail.com>
2025-07-31 00:55:34 +0200

    LibWeb: Implement is_html method for TrustedTypePolicyFactory
    This is used to check the legitimacy of a TrustedHTML object.

--- Libraries/LibWeb/TrustedTypes/TrustedTypePolicyFactory.cpp
+++ Libraries/LibWeb/TrustedTypes/TrustedTypePolicyFactory.cpp
@@ -15,6 +15,7 @@
 #include <LibWeb/HTML/WindowEventHandlers.h>
 #include <LibWeb/Namespace.h>
 #include <LibWeb/SVG/TagNames.h>
+#include <LibWeb/TrustedTypes/TrustedHTML.h>
 
 namespace Web::TrustedTypes {
 
@@ -130,6 +131,13 @@ void TrustedTypePolicyFactory::initialize(JS::Realm& realm)
 {
     WEB_SET_PROTOTYPE_FOR_INTERFACE(TrustedTypePolicyFactory);
     Base::initialize(realm);
+}
+
+// https://w3c.github.io/trusted-types/dist/spec/#dom-trustedtypepolicyfactory-ishtml
+bool TrustedTypePolicyFactory::is_html(JS::Value value)
+{
+    // 1. Returns true if value is an instance of TrustedHTML and has an associated data value set, false otherwise.
+    return value.is_object() && is<TrustedHTML>(value.as_object());
 }
 
 // https://w3c.github.io/trusted-types/dist/spec/#abstract-opdef-get-trusted-type-data-for-attribute
--- Libraries/LibWeb/TrustedTypes/TrustedTypePolicyFactory.h
+++ Libraries/LibWeb/TrustedTypes/TrustedTypePolicyFactory.h
@@ -21,6 +21,8 @@ public:
 
     virtual ~TrustedTypePolicyFactory() override { }
 
+    bool is_html(JS::Value);
+
     Optional<String> get_attribute_type(String const& tag_name, String& attribute, Optional<String> element_ns, Optional<String> attr_ns);
     Optional<String> get_property_type(String const& tag_name, String const& property, Optional<String> element_ns);
 
