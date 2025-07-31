df8ea537acd9f719054d4523f48d1ac35dec03b2
Tete17 <miguel_tete17@hotmail.com>
2025-07-31 02:03:29 +0200

    LibWeb: Add TrustedTypePolicy class
    It is mostly a skeleton with no actual implementation.

---
+++ Libraries/LibWeb/TrustedTypes/TrustedTypePolicy.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025, Miguel Sacristán Izcue <miguel_tete17@hotmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/TrustedTypes/TrustedTypePolicy.h>
+
+#include <LibGC/Ptr.h>
+#include <LibJS/Runtime/Realm.h>
+#include <LibWeb/Bindings/Intrinsics.h>
+
+namespace Web::TrustedTypes {
+
+GC_DEFINE_ALLOCATOR(TrustedTypePolicy);
+
+TrustedTypePolicy::TrustedTypePolicy(JS::Realm& realm)
+    : PlatformObject(realm)
+{
+}
+
+void TrustedTypePolicy::initialize(JS::Realm& realm)
+{
+    WEB_SET_PROTOTYPE_FOR_INTERFACE(TrustedTypePolicy);
+    Base::initialize(realm);
+}
+
+}
---
+++ Libraries/LibWeb/TrustedTypes/TrustedTypePolicy.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2025, Miguel Sacristán Izcue <miguel_tete17@hotmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibJS/Forward.h>
+#include <LibWeb/Bindings/PlatformObject.h>
+#include <LibWeb/Bindings/TrustedTypePolicyPrototype.h>
+
+namespace Web::TrustedTypes {
+
+struct TrustedTypePolicyOptions {
+    GC::Root<WebIDL::CallbackType> create_html;
+    GC::Root<WebIDL::CallbackType> create_script;
+    GC::Root<WebIDL::CallbackType> create_script_url;
+};
+
+class TrustedTypePolicy final : public Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(TrustedTypePolicy, Bindings::PlatformObject);
+    GC_DECLARE_ALLOCATOR(TrustedTypePolicy);
+
+public:
+    virtual ~TrustedTypePolicy() override = default;
+
+private:
+    explicit TrustedTypePolicy(JS::Realm&);
+    virtual void initialize(JS::Realm&) override;
+};
+
+}
