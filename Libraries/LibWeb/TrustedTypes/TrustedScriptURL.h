adaad653ca9bc866b872c3469252fe31936dac9d
Tete17 <miguel_tete17@hotmail.com>
2025-07-31 17:08:30 +0200

    LibWeb: Implement TrustedScriptURL class

---
+++ Libraries/LibWeb/TrustedTypes/TrustedScriptURL.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2025, Miguel Sacristán Izcue <miguel_tete17@hotmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibJS/Forward.h>
+#include <LibWeb/Bindings/PlatformObject.h>
+#include <LibWeb/Bindings/TrustedScriptURLPrototype.h>
+
+namespace Web::TrustedTypes {
+
+class TrustedScriptURL final : public Bindings::PlatformObject {
+    WEB_PLATFORM_OBJECT(TrustedScriptURL, Bindings::PlatformObject);
+    GC_DECLARE_ALLOCATOR(TrustedScriptURL);
+
+public:
+    virtual ~TrustedScriptURL() override = default;
+
+    Utf16String const& to_string() const;
+    Utf16String const& to_json() const;
+
+private:
+    explicit TrustedScriptURL(JS::Realm&, Utf16String);
+    virtual void initialize(JS::Realm&) override;
+
+    Utf16String const m_data;
+};
+
+}
