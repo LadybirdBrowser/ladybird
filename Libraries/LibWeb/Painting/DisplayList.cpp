f28b7064ee6785e26b8ac3097c3a11e56aa30df0
Jelle Raaijmakers <jelle@ladybird.org>
2025-08-01 11:33:53 +0200

    LibWeb: Add indentation to display list dumps
    Output display list dumps with an indentation level to show balanced
    commands. It makes it much easier to see what is happening between e.g.
    PushStackingContext and PopStackingContext, or SaveLayer and Restore.

--- Libraries/LibWeb/Painting/DisplayList.cpp
+++ Libraries/LibWeb/Painting/DisplayList.cpp
@@ -18,9 +18,28 @@ void DisplayList::append(DisplayListCommand&& command, Optional<i32> scroll_fram
 String DisplayList::dump() const
 {
     StringBuilder builder;
-    for (auto const& command : m_commands) {
-        command.command.visit([&builder](auto const& cmd) { cmd.dump(builder); });
-        builder.appendff("\n");
+    int indentation = 0;
+    for (auto const& command_list_item : m_commands) {
+        auto const& command = command_list_item.command;
+
+        command.visit([&indentation](auto const& command) {
+            if constexpr (requires { command.nesting_level_change; }) {
+                if (command.nesting_level_change < 0 && indentation >= -command.nesting_level_change)
+                    indentation += command.nesting_level_change;
+            }
+        });
+
+        if (indentation > 0)
+            builder.append(MUST(String::repeated("  "_string, indentation)));
+        command.visit([&builder](auto const& cmd) { cmd.dump(builder); });
+        builder.append('\n');
+
+        command.visit([&indentation](auto const& command) {
+            if constexpr (requires { command.nesting_level_change; }) {
+                if (command.nesting_level_change > 0)
+                    indentation += command.nesting_level_change;
+            }
+        });
     }
     return builder.to_string_without_validation();
 }
