e2e6af620a643b5ec68165aeeeab8cd757aec186
Callum Law <callumlaw1709@outlook.com>
2025-08-03 22:46:50 +1200

    LibWeb: Serialize NumberStyleValue with 6 decimal places of precision

--- Libraries/LibWeb/CSS/StyleValues/NumberStyleValue.cpp
+++ Libraries/LibWeb/CSS/StyleValues/NumberStyleValue.cpp
@@ -13,7 +13,13 @@ namespace Web::CSS {
 
 String NumberStyleValue::to_string(SerializationMode) const
 {
-    return String::number(m_value);
+    // FIXME: This should be moved into Serialize.cpp and used by applicable dimensions as well.
+    // https://drafts.csswg.org/cssom/#serialize-a-css-value
+    // <number>
+    // A base-ten number using digits 0-9 (U+0030 to U+0039) in the shortest form possible, using "." to separate
+    // decimals (if any), rounding the value if necessary to not produce more than 6 decimals, preceded by "-" (U+002D)
+    // if it is negative.
+    return MUST(String::formatted("{:.6}", m_value));
 }
 
 Vector<Parser::ComponentValue> NumberStyleValue::tokenize() const
