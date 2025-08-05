4c54c0142c790fa91a34f6927a99b99c039144a5
Tete17 <miguel_tete17@hotmail.com>
2025-08-05 14:36:11 +0200

    LibWeb: Add require-trusted-types-for Directive
    This is meant to configure the behaviour of an injection sinks when a
    string is passed.

---
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/RequireTrustedTypesFor.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2025, Miguel Sacristán Izcue <miguel_tete17@hotmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/ContentSecurityPolicy/Directives/RequireTrustedTypesFor.h>
+
+#include <LibWeb/ContentSecurityPolicy/Directives/Names.h>
+#include <LibWeb/DOMURL/DOMURL.h>
+#include <LibWeb/Fetch/Infrastructure/HTTP/Requests.h>
+#include <LibWeb/TrustedTypes/TrustedScript.h>
+#include <LibWeb/TrustedTypes/TrustedTypePolicy.h>
+
+namespace Web::ContentSecurityPolicy::Directives {
+
+GC_DEFINE_ALLOCATOR(RequireTrustedTypesFor);
+
+RequireTrustedTypesFor::RequireTrustedTypesFor(String name, Vector<String> value)
+    : Directive(move(name), move(value))
+{
+}
+
+// https://www.w3.org/TR/trusted-types/#require-trusted-types-for-pre-navigation-check
+Directive::Result RequireTrustedTypesFor::pre_navigation_check(GC::Ref<Fetch::Infrastructure::Request> request, NavigationType, GC::Ref<Policy const>) const
+{
+    // 1. If request’s url’s scheme is not "javascript", return "Allowed" and abort further steps.
+    if (request->url().scheme() != "javascript"sv)
+        return Result::Allowed;
+
+    // 2. Let urlString be the result of running the URL serializer on request’s url.
+    auto url_string = request->url().serialize();
+
+    // 3. Let encodedScriptSource be the result of removing the leading "javascript:" from urlString.
+    auto const encoded_script_source = MUST(url_string.substring_from_byte_offset_with_shared_superstring("javascript:"sv.length()));
+
+    // 4. Let convertedScriptSource be the result of executing Process value with a default policy algorithm, with the following arguments:
+    //    expectedType:
+    //      TrustedScript
+    //    global:
+    //      request’s clients’s global object:
+    //    input:
+    //      encodedScriptSource:
+    //    sink:
+    //      "Location href":
+    auto converted_script_source = TrustedTypes::process_value_with_a_default_policy(
+        TrustedTypes::TrustedTypeName::TrustedScript,
+        request->client()->global_object(),
+        Utf16String::from_utf8(encoded_script_source),
+        TrustedTypes::InjectionSink::Locationhref);
+
+    // If that algorithm threw an error or convertedScriptSource is not a TrustedScript object, return "Blocked" and abort further steps.
+    if (converted_script_source.is_error() || !converted_script_source.value().is_object())
+        return Result::Blocked;
+
+    auto const converted_script_source_value = as_if<TrustedTypes::TrustedScript>(converted_script_source.value().as_object());
+
+    if (!converted_script_source_value)
+        return Result::Blocked;
+
+    // 5. Set urlString to be the result of prepending "javascript:" to stringified convertedScriptSource.
+    url_string = MUST(String::formatted("javascript:{}", converted_script_source_value->to_string()));
+
+    // 6. Let newURL be the result of running the URL parser on urlString. If the parser returns a failure, return "Blocked" and abort further steps.
+    auto const new_url = DOMURL::parse(url_string);
+    if (!new_url.has_value())
+        return Result::Blocked;
+
+    // 7. Set request’s url to newURL.
+    request->set_url(new_url.value());
+
+    // 8. Return "Allowed".
+    return Result::Allowed;
+}
+
+}
---
+++ Libraries/LibWeb/ContentSecurityPolicy/Directives/RequireTrustedTypesFor.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025, Miguel Sacristán Izcue <miguel_tete17@hotmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/ContentSecurityPolicy/Directives/Directive.h>
+
+namespace Web::ContentSecurityPolicy::Directives {
+
+// https://www.w3.org/TR/trusted-types/#require-trusted-types-for-csp-directive
+class RequireTrustedTypesFor final : public Directive {
+    GC_CELL(RequireTrustedTypesFor, Directive)
+    GC_DECLARE_ALLOCATOR(RequireTrustedTypesFor);
+
+public:
+    virtual ~RequireTrustedTypesFor() = default;
+
+    virtual Result pre_navigation_check(GC::Ref<Fetch::Infrastructure::Request>, NavigationType, GC::Ref<Policy const>) const override;
+
+private:
+    RequireTrustedTypesFor(String name, Vector<String> value);
+};
+
+}
