cf211cf99ac5b27c733f22cc3a9ec463a718975f
Tete17 <miguel_tete17@hotmail.com>
2025-08-05 23:26:38 +0200

    LibWeb: Migrate TrustedTypes to Utf16String

--- Libraries/LibWeb/TrustedTypes/TrustedTypePolicy.h
+++ Libraries/LibWeb/TrustedTypes/TrustedTypePolicy.h
@@ -41,21 +41,21 @@ class TrustedTypePolicy final : public Bindings::PlatformObject {
 public:
     virtual ~TrustedTypePolicy() override = default;
 
-    String const& name() const { return m_name; }
+    Utf16String const& name() const { return m_name; }
 
-    WebIDL::ExceptionOr<GC::Root<TrustedHTML>> create_html(String const&, GC::RootVector<JS::Value> const&);
-    WebIDL::ExceptionOr<GC::Root<TrustedScript>> create_script(String const&, GC::RootVector<JS::Value> const&);
-    WebIDL::ExceptionOr<GC::Root<TrustedScriptURL>> create_script_url(String const&, GC::RootVector<JS::Value> const&);
+    WebIDL::ExceptionOr<GC::Root<TrustedHTML>> create_html(Utf16String const&, GC::RootVector<JS::Value> const&);
+    WebIDL::ExceptionOr<GC::Root<TrustedScript>> create_script(Utf16String const&, GC::RootVector<JS::Value> const&);
+    WebIDL::ExceptionOr<GC::Root<TrustedScriptURL>> create_script_url(Utf16String const&, GC::RootVector<JS::Value> const&);
 
 private:
-    explicit TrustedTypePolicy(JS::Realm&, String const&, TrustedTypePolicyOptions const&);
+    explicit TrustedTypePolicy(JS::Realm&, Utf16String const&, TrustedTypePolicyOptions const&);
     virtual void initialize(JS::Realm&) override;
 
-    TrustedTypesVariants create_a_trusted_type(TrustedTypeName, String const&, GC::RootVector<JS::Value> const& values);
+    TrustedTypesVariants create_a_trusted_type(TrustedTypeName, Utf16String const&, GC::RootVector<JS::Value> const& values);
 
-    WebIDL::ExceptionOr<JS::Value> get_trusted_type_policy_value(TrustedTypeName, String const& value, GC::RootVector<JS::Value> const& values, ThrowIfCallbackMissing throw_if_missing);
+    WebIDL::ExceptionOr<JS::Value> get_trusted_type_policy_value(TrustedTypeName, Utf16String const& value, GC::RootVector<JS::Value> const& values, ThrowIfCallbackMissing throw_if_missing);
 
-    String const m_name;
+    Utf16String const m_name;
     TrustedTypePolicyOptions const m_options;
 };
 
