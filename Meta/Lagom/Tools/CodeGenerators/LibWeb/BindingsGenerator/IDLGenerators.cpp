dc1b7b19254865661bfd7c8f88812594d09f67ee
Idan Horowitz <idan.horowitz@gmail.com>
2025-08-05 20:24:27 +0300

    IDLGenerators: Support generating dictionary to value converter helpers
    This is useful when returning dictionaries as a Promise.

--- Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/IDLGenerators.cpp
+++ Meta/Lagom/Tools/CodeGenerators/LibWeb/BindingsGenerator/IDLGenerators.cpp
@@ -2953,6 +2953,33 @@ static ByteString get_best_value_for_underlying_enum_type(size_t size)
     VERIFY_NOT_REACHED();
 }
 
+static void generate_dictionaries(SourceGenerator& generator, IDL::Interface const& interface)
+{
+    for (auto const& it : interface.dictionaries) {
+        if (!it.value.is_original_definition)
+            continue;
+        if (!it.value.extended_attributes.contains("GenerateToValue"sv))
+            continue;
+        auto dictionary_generator = generator.fork();
+        dictionary_generator.set("dictionary.name", make_input_acceptable_cpp(it.key));
+        dictionary_generator.set("dictionary.name:snakecase", make_input_acceptable_cpp(it.key.to_snakecase()));
+        dictionary_generator.append(R"~~~(
+JS::Value @dictionary.name:snakecase@_to_value(JS::Realm&, @dictionary.name@ const&);
+JS::Value @dictionary.name:snakecase@_to_value(JS::Realm& realm, @dictionary.name@ const& dictionary)
+{
+    auto& vm = realm.vm();
+    @dictionary.name@ copy = dictionary;
+)~~~");
+        // FIXME: Support generating wrap statements for lvalues and get rid of the copy above
+        auto dictionary_type = adopt_ref(*new Type(it.key, false));
+        generate_wrap_statement(dictionary_generator, "copy", dictionary_type, interface, "return"sv);
+
+        dictionary_generator.append(R"~~~(
+}
+)~~~");
+    }
+}
+
 static void generate_enumerations(HashMap<ByteString, Enumeration> const& enumerations, StringBuilder& builder)
 {
     SourceGenerator generator { builder };
@@ -4748,6 +4775,8 @@ JS_DEFINE_NATIVE_FUNCTION(@class_name@::clear)
 )~~~");
         }
     }
+
+    generate_dictionaries(generator, interface);
 }
 
 void generate_namespace_header(IDL::Interface const& interface, StringBuilder& builder)
