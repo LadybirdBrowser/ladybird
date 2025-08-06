16f44b9cf721a5e6304f744c9eafddeb05f65770
Callum Law <callumlaw1709@outlook.com>
2025-08-06 16:16:43 +1200

    LibWeb: Return an intermediate struct from `CSV::resolve_value`
    Returning this struct will allow us to modify the underlying value of
    the `CalculationResult` without requiring us to go through the process
    of constructing a whole new `CalculationResult` to return.

--- Libraries/LibWeb/CSS/StyleValues/CalculatedStyleValue.h
+++ Libraries/LibWeb/CSS/StyleValues/CalculatedStyleValue.h
@@ -72,8 +72,6 @@ public:
     virtual ValueComparingNonnullRefPtr<CSSStyleValue const> absolutized(CSSPixelRect const& viewport_rect, Length::FontMetrics const& font_metrics, Length::FontMetrics const& root_font_metrics) const override;
     virtual bool equals(CSSStyleValue const& other) const override;
 
-    Optional<CalculationResult> resolve_value(CalculationResolutionContext const&) const;
-
     bool resolves_to_angle() const { return m_resolved_type.matches_angle(m_context.percentages_resolve_as); }
     bool resolves_to_angle_percentage() const { return m_resolved_type.matches_angle_percentage(m_context.percentages_resolve_as); }
     Optional<Angle> resolve_angle_deprecated(CalculationResolutionContext const&) const;
@@ -126,6 +124,12 @@ private:
         , m_context(move(context))
     {
     }
+
+    struct ResolvedValue {
+        double value;
+        Optional<CSSNumericType> type;
+    };
+    Optional<ResolvedValue> resolve_value(CalculationResolutionContext const&) const;
 
     Optional<ValueType> percentage_resolved_type() const;
 
