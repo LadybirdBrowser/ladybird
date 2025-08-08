e554b2a128f47e6f09d42a008638e7cfc6ac720e
zac <zac@zacoons.com>
2025-08-08 21:09:42 +1000

    LibWeb: Set `is_first_chunk` to false as needed in InlineLevelIterator
    `m_text_node_context->is_first_chunk` was always true because
    `InlineLevelIterator::next_without_lookahead` never set it to false.

--- Libraries/LibWeb/Layout/InlineLevelIterator.cpp
+++ Libraries/LibWeb/Layout/InlineLevelIterator.cpp
@@ -493,6 +493,8 @@ Optional<InlineLevelIterator::Item> InlineLevelIterator::next_without_lookahead(
     if (auto* text_node = as_if<Layout::TextNode>(*m_current_node)) {
         if (!m_text_node_context.has_value())
             enter_text_node(*text_node);
+        else
+            m_text_node_context->is_first_chunk = false;
 
         auto chunk_opt = m_text_node_context->chunk_iterator.next();
         if (!chunk_opt.has_value()) {
