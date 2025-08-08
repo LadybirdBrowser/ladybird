541fb1ac2cba21befc0a5d8f74a3d4a8db283a8e
Timothy Flynn <trflynn89@pm.me>
2025-08-08 09:05:39 -0400

    LibWeb: Add missing includes to SharedResourceRequest
    Missing Cell.h and Ptr.h were causing a bunch of clangd errors. Also
    remove unused includes while we're here.

--- Libraries/LibWeb/HTML/SharedResourceRequest.cpp
+++ Libraries/LibWeb/HTML/SharedResourceRequest.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <AK/HashTable.h>
 #include <LibGfx/Bitmap.h>
 #include <LibWeb/Bindings/PrincipalHostDefined.h>
 #include <LibWeb/Fetch/Fetching/Fetching.h>
