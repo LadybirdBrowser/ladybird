acda1f6b7af5424cdc931913899066c2d882da36
Tete17 <miguel_tete17@hotmail.com>
2025-08-09 02:11:32 +0200

    LibWeb: Enumerate all injection sinks relevant to the TrustedTypes spec
    This enables us to more strictly control the available sinks we support.

--- Libraries/LibWeb/TrustedTypes/InjectionSink.h
+++ Libraries/LibWeb/TrustedTypes/InjectionSink.h
@@ -6,13 +6,32 @@
 
 #pragma once
 
+#include <AK/Utf16String.h>
+#include <LibWeb/HTML/GlobalEventHandlers.h>
+#include <LibWeb/HTML/WindowEventHandlers.h>
+
 namespace Web::TrustedTypes {
 
+#define EVENT_HANDLERS_INJECTION_SINKS(attribute_name, event_name) \
+    __ENUMERATE_INJECTION_SINKS(Element##attribute_name, "Element " #attribute_name)
+
 // https://w3c.github.io/trusted-types/dist/spec/#injection-sink
+#define ENUMERATE_INJECTION_SINKS                                                    \
+    __ENUMERATE_INJECTION_SINKS(Documentwrite, "Document write")                     \
+    __ENUMERATE_INJECTION_SINKS(Documentwriteln, "Document writeln")                 \
+    __ENUMERATE_INJECTION_SINKS(Function, "Function")                                \
+    __ENUMERATE_INJECTION_SINKS(HTMLIFrameElementsrcdoc, "HTMLIFrameElement srcdoc") \
+    __ENUMERATE_INJECTION_SINKS(HTMLScriptElementsrc, "HTMLScriptElement src")       \
+    __ENUMERATE_INJECTION_SINKS(SVGScriptElementhref, "SVGScriptElement href")       \
+    ENUMERATE_GLOBAL_EVENT_HANDLERS(EVENT_HANDLERS_INJECTION_SINKS)                  \
+    ENUMERATE_WINDOW_EVENT_HANDLERS(EVENT_HANDLERS_INJECTION_SINKS)
+
 enum class InjectionSink {
-    DocumentWrite,
-    DocumentWriteln,
-    Function,
+#define __ENUMERATE_INJECTION_SINKS(name, value) name,
+    ENUMERATE_INJECTION_SINKS
+#undef __ENUMERATE_INJECTION_SINKS
 };
+
+Utf16String to_string(InjectionSink sink);
 
 }
