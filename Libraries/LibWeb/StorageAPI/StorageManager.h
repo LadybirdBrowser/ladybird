9915ca0961d59643162f34de1f0d2f09348cba6f
Tim Ledbetter <tim.ledbetter@ladybird.org>
2025-08-10 22:07:10 +0100

    LibWeb: Implement `StorageManager::estimate()`
    This asks the StorageManager to return how much storage is currently
    being used and how much space is available.

--- Libraries/LibWeb/StorageAPI/StorageManager.h
+++ Libraries/LibWeb/StorageAPI/StorageManager.h
@@ -18,10 +18,15 @@ public:
     static WebIDL::ExceptionOr<GC::Ref<StorageManager>> create(JS::Realm&);
     virtual ~StorageManager() override = default;
 
+    GC::Ref<WebIDL::Promise> estimate() const;
+
 private:
     StorageManager(JS::Realm&);
 
     virtual void initialize(JS::Realm&) override;
+
+    static void queue_a_storage_task(JS::Realm&, JS::Object& global, Function<void()>);
+    static GC::Ptr<StorageShelf> obtain_a_local_storage_shelf(HTML::EnvironmentSettingsObject&);
 };
 
 }
