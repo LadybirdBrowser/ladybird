LadybirdBrowser/ladybird

commit c57975c9fdb98e03da0f8098c324577f8ecc753c

LibWeb: Move and rename CSSStyleValue to StyleValues/StyleValue.{h,cpp}
This reverts 0e3487b9ab455a7648185995363bb3b487551d40.

Back when I made that change, I thought we could make our StyleValue
classes match the typed-om definitions directly. However, they have
different requirements. Typed-om types need to be mutable and GCed,
whereas StyleValues are immutable and ideally wouldn't require a JS VM.

While I was already making such a cataclysmic change, I've moved it into
the StyleValues directory, because it *not* being there has bothered me
for a long time. 😅

Services/WebContent/WebDriverConnection.cpp
@@ -8,6 +8,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <../../Libraries/LibWeb/CSS/StyleValues/StyleValue.h>
 #include <AK/JsonObject.h>
 #include <AK/JsonValue.h>
 #include <AK/LexicalPath.h>
@@ -16,7 +17,6 @@
 #include <LibCore/File.h>
 #include <LibJS/Runtime/Value.h>
 #include <LibURL/Parser.h>
-#include <LibWeb/CSS/CSSStyleValue.h>
 #include <LibWeb/CSS/ComputedProperties.h>
 #include <LibWeb/CSS/PropertyID.h>
 #include <LibWeb/Cookie/Cookie.h>

commit 0f63cddfd35f8c8033c2a96549c9b8c83ad8f33a

WebContent: Correct StyleValue.h include
Regressed in c57975c9fdb98e03da0f8098c324577f8ecc753c, oops!

Services/WebContent/WebDriverConnection.cpp
@@ -8,7 +8,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-#include <../../Libraries/LibWeb/CSS/StyleValues/StyleValue.h>
 #include <AK/JsonObject.h>
 #include <AK/JsonValue.h>
 #include <AK/LexicalPath.h>
@@ -19,6 +18,7 @@
 #include <LibURL/Parser.h>
 #include <LibWeb/CSS/ComputedProperties.h>
 #include <LibWeb/CSS/PropertyID.h>
+#include <LibWeb/CSS/StyleValues/StyleValue.h>
 #include <LibWeb/Cookie/Cookie.h>
 #include <LibWeb/Cookie/ParsedCookie.h>
 #include <LibWeb/Crypto/Crypto.h>