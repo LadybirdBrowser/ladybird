6b8f7c85f15ae9f7b9b9c1f7fb7dfcb523d4f2fc
Undefine <undefine@undefine.pl>
2025-08-12 17:00:32 +0200

    AK+Everywhere: Remove ifdefs for WASM and Emscripten
    Building the Ladybird code for Emscripten doesn't really make sense
    and I doubt it is even possible with all the dependencies now.

--- Utilities/test262-runner.cpp
+++ Utilities/test262-runner.cpp
@@ -27,7 +27,7 @@
 #include <signal.h>
 #include <unistd.h>
 
-#if !defined(AK_OS_MACOS) && !defined(AK_OS_EMSCRIPTEN) && !defined(AK_OS_GNU_HURD)
+#if !defined(AK_OS_MACOS) && !defined(AK_OS_GNU_HURD)
 // Only used to disable core dumps
 #    include <sys/prctl.h>
 #endif
@@ -592,7 +592,7 @@ int main(int argc, char** argv)
 #ifdef AK_OS_GNU_HURD
     if (disable_core_dumping)
         setenv("CRASHSERVER", "/servers/crash-kill", true);
-#elif !defined(AK_OS_MACOS) && !defined(AK_OS_EMSCRIPTEN)
+#elif !defined(AK_OS_MACOS)
     if (disable_core_dumping && prctl(PR_SET_DUMPABLE, 0, 0, 0) < 0) {
         perror("prctl(PR_SET_DUMPABLE)");
         return exit_wrong_arguments;
