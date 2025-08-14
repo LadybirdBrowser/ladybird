71d81d95663c2e7faf96520aae84a19229ebb1b3
Erik Kurzinger <ekurzinger@gmail.com>
2025-08-14 19:00:44 -0400

    LibGfx: Implement Vulkan image allocation
    This introduces the ability to allocate Vulkan images which can be
    shared across graphics APIs or across processes using the Linux dma-buf
    interface.
    
    The create_shareable_vulkan_image function takes a VulkanContext, image
    width, height, and format, and an array of DRM format modifiers
    representing image memory layouts accepted by the caller. The function
    will intersect this list with the list of modifiers supported by the
    Vulkan implementation, ensuring the resulting image uses one of those
    layouts (exactly which one is up to the implementation). The function
    will return a VulkanImage object, which is reference-counted and
    encapsulates the VkImage itself as well as the backing memory
    allocation. It also stores various image parameters such as usage,
    tiling, etc.
    
    The VulkanImage::get_dma_buf_fd function will create a file descriptor
    representing the image's backing dma-buf which can then be imported by a
    different API or sent over a unix domain socket.

--- Libraries/LibGfx/VulkanContext.h
+++ Libraries/LibGfx/VulkanContext.h
@@ -8,6 +8,10 @@
 
 #ifdef USE_VULKAN
 
+#    include <AK/Assertions.h>
+#    include <AK/NonnullRefPtr.h>
+#    include <AK/RefCounted.h>
+#    include <libdrm/drm_fourcc.h>
 #    include <vulkan/vulkan.h>
 
 namespace Gfx {
@@ -21,9 +25,51 @@ struct VulkanContext {
     uint32_t graphics_queue_family { 0 };
     VkCommandPool command_pool { VK_NULL_HANDLE };
     VkCommandBuffer command_buffer { VK_NULL_HANDLE };
+    struct
+    {
+        PFN_vkGetMemoryFdKHR get_memory_fd { nullptr };
+        PFN_vkGetImageDrmFormatModifierPropertiesEXT get_image_drm_format_modifier_properties { nullptr };
+    } ext_procs;
 };
 
+struct VulkanImage : public RefCounted<VulkanImage> {
+    VkImage image { VK_NULL_HANDLE };
+    VkDeviceMemory memory { VK_NULL_HANDLE };
+    struct {
+        VkFormat format;
+        VkExtent3D extent;
+        VkImageTiling tiling;
+        VkImageUsageFlags usage;
+        VkSharingMode sharing_mode;
+        VkImageLayout layout;
+        VkDeviceSize row_pitch; // for tiled images this is some implementation-specific value
+        uint64_t modifier { DRM_FORMAT_MOD_INVALID };
+    } info;
+    VulkanContext const& context;
+
+    int get_dma_buf_fd();
+    void transition_layout(VkImageLayout old_layout, VkImageLayout new_layout);
+    VulkanImage(VulkanContext const& context)
+        : context(context)
+    {
+    }
+    ~VulkanImage();
+};
+
+static inline uint32_t vk_format_to_drm_format(VkFormat format)
+{
+    switch (format) {
+    case VK_FORMAT_B8G8R8A8_UNORM:
+        return DRM_FORMAT_ARGB8888;
+    // add more as needed
+    default:
+        VERIFY_NOT_REACHED();
+        return DRM_FORMAT_INVALID;
+    }
+}
+
 ErrorOr<VulkanContext> create_vulkan_context();
+ErrorOr<NonnullRefPtr<VulkanImage>> create_shared_vulkan_image(VulkanContext const& context, uint32_t width, uint32_t height, VkFormat format, uint32_t num_modifiers, uint64_t const* modifiers);
 
 }
 
