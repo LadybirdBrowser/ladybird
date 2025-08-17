695bbcb991828344a3fa11d84fc258b69b9d929f
Timothy Flynn <trflynn89@pm.me>
2025-08-17 18:37:47 -0400

    LibJS: Revert common error types to only hold a single string
    Before porting to UTF-16, these instances held a String. The port to
    UTF-16 changed them to hold the original string as a StringView, and
    lazily allocated the UTF-16 message as needed. This somehow negatively
    impacting the zlib.js benchmark in the Octane suite.

--- Libraries/LibJS/Runtime/ErrorTypes.cpp
+++ Libraries/LibJS/Runtime/ErrorTypes.cpp
@@ -9,15 +9,8 @@
 namespace JS {
 
 #define __ENUMERATE_JS_ERROR(name, message) \
-    const ErrorType ErrorType::name = ErrorType(message##sv);
+    const ErrorType ErrorType::name = ErrorType(message##_utf16);
 JS_ENUMERATE_ERROR_TYPES(__ENUMERATE_JS_ERROR)
 #undef __ENUMERATE_JS_ERROR
-
-Utf16String const& ErrorType::message() const
-{
-    if (m_message.is_empty())
-        m_message = Utf16String::from_utf8_without_validation(m_format);
-    return m_message;
-}
 
 }
