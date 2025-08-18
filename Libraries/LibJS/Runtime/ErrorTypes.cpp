282b463208d86500e1a33c4980010d14e1765a56
Timothy Flynn <trflynn89@pm.me>
2025-08-18 06:21:59 -0400

    Revert "LibJS: Revert common error types to only hold a single string"
    This reverts commit 695bbcb991828344a3fa11d84fc258b69b9d929f.
    
    Despite improving performance on my Linux machine, this managed to tank
    performance on the Linux benchmark machine.

--- Libraries/LibJS/Runtime/ErrorTypes.cpp
+++ Libraries/LibJS/Runtime/ErrorTypes.cpp
@@ -9,8 +9,15 @@
 namespace JS {
 
 #define __ENUMERATE_JS_ERROR(name, message) \
-    const ErrorType ErrorType::name = ErrorType(message##_utf16);
+    const ErrorType ErrorType::name = ErrorType(message##sv);
 JS_ENUMERATE_ERROR_TYPES(__ENUMERATE_JS_ERROR)
 #undef __ENUMERATE_JS_ERROR
+
+Utf16String const& ErrorType::message() const
+{
+    if (m_message.is_empty())
+        m_message = Utf16String::from_utf8_without_validation(m_format);
+    return m_message;
+}
 
 }
