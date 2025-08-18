LadybirdBrowser/ladybird

commit 80c8e787a825da323b2419db962de65efbae1bcd

LibWeb: Fix abspos layout when box is contained by grid area
Before this change, `layout_absolutely_positioned_element()` in GFC
had an assumption that all contained by grid container abspos boxes were
also direct children of the grid container. This change adds handling
for the cases when it's not true and, in order to identify grid area
abspos box belongs to, we have to find ancestor grid item.

Libraries/LibWeb/Layout/GridFormattingContext.cpp
@@ -2109,27 +2109,48 @@ void GridFormattingContext::run(AvailableSpace const& available_space)
     m_grid_container_used_values.set_grid_template_rows(CSS::GridTrackSizeListStyleValue::create(move(grid_track_rows)));
 }
 
+// https://www.w3.org/TR/css-grid-2/#abspos-items
 void GridFormattingContext::layout_absolutely_positioned_element(Box const& box)
 {
     auto& box_state = m_state.get_mutable(box);
     auto const& computed_values = box.computed_values();
 
-    auto is_auto_row = is_auto_positioned_track(computed_values.grid_row_start(), computed_values.grid_row_end());
-    auto is_auto_column = is_auto_positioned_track(computed_values.grid_column_start(), computed_values.grid_column_end());
+    auto grid_area_rect = [&] -> CSSPixelRect {
+        // NOTE: Grid areas form containing blocks for abspos elements, but
+        //       `Node::containing_block()` is not aware of that. Therefore, we need to
+        //       find the closest grid item ancestor in order to identify grid area it belongs to.
+        NodeWithStyle const* containing_grid_item = &box;
+        while (containing_grid_item->parent() && !containing_grid_item->parent()->display().is_grid_inside())
+            containing_grid_item = containing_grid_item->parent();
 
-    GridItem item { box, box_state, {}, {}, {}, {} };
-    if (!is_auto_row) {
+        auto const& computed_values = containing_grid_item->computed_values();
+        VERIFY(containing_grid_item);
+
+        // NOTE: If abspos box is contained by in-flow grid item its grid position is already determined.
+        if (containing_grid_item->is_grid_item()) {
+            auto item = *m_grid_items.find_if([containing_grid_item](GridItem const& grid_item) {
+                return grid_item.box == containing_grid_item;
+            });
+            return get_grid_area_rect(item);
+        }
+
+        GridItem item { as<Box>(*containing_grid_item), box_state, {}, {}, {}, {} };
+        auto is_auto_row = is_auto_positioned_track(computed_values.grid_row_start(), computed_values.grid_row_end());
+        auto is_auto_column = is_auto_positioned_track(computed_values.grid_column_start(), computed_values.grid_column_end());
+
         auto row_placement_position = resolve_grid_position(box, GridDimension::Row);
-        item.row = row_placement_position.start;
-        item.row_span = row_placement_position.span;
-    }
-    if (!is_auto_column) {
         auto column_placement_position = resolve_grid_position(box, GridDimension::Column);
-        item.column = column_placement_position.start;
-        item.column_span = column_placement_position.span;
-    }
+        if (!is_auto_row) {
+            item.row = row_placement_position.start;
+            item.row_span = row_placement_position.span;
+        }
+        if (!is_auto_column) {
+            item.column = column_placement_position.start;
+            item.column_span = column_placement_position.span;
+        }
+        return get_grid_area_rect(item);
+    }();
 
-    auto grid_area_rect = get_grid_area_rect(item);
     auto available_width = AvailableSize::make_definite(grid_area_rect.width());
     auto available_height = AvailableSize::make_definite(grid_area_rect.height());
     AvailableSpace available_space { available_width, available_height };

Tests/LibWeb/Layout/input/grid/abspos-item-contained-by-grid-area.html
@@ -1,0 +1,19 @@
+<!DOCTYPE html><style>
+    .gfc {
+        display: grid;
+        grid-template-columns: 1fr [right-start] 1fr [grid-end];
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-color: lightpink;
+    }
+    .grid-item {
+        grid-column: right-start/grid-end;
+        background-color: magenta;
+    }
+    .abspos {
+        position: absolute;
+        width: 100px;
+        background-color: orange;
+    }
+</style><div class="gfc"><div class="grid-item"><div class="abspos">Hello</div></div></div>

commit 344958459ca833d1c08a40296382c09adc7b520f

LibWeb: Handle correctly case when abspos and relpos GFC is interleaved
...by another GFC. When searching for grid item that contains an
abspos box positioned relative to GFC, it's incorrect to assume that the
closest ancestor box whose parent establishes GFC is always the one we
are looking for, because there may be non-positioned GFC in between.

Fixes regression introduced in 80c8e78.

Libraries/LibWeb/Layout/GridFormattingContext.cpp
@@ -2120,7 +2120,7 @@ void GridFormattingContext::layout_absolutely_positioned_element(Box const& box)
         //       `Node::containing_block()` is not aware of that. Therefore, we need to
         //       find the closest grid item ancestor in order to identify grid area it belongs to.
         NodeWithStyle const* containing_grid_item = &box;
-        while (containing_grid_item->parent() && !containing_grid_item->parent()->display().is_grid_inside())
+        while (containing_grid_item->parent() && containing_grid_item->parent() != &grid_container())
             containing_grid_item = containing_grid_item->parent();
 
         auto const& computed_values = containing_grid_item->computed_values();

Tests/LibWeb/Layout/input/grid/gfc-between-relpos-gfc-and-abspos-item.html
@@ -1,0 +1,5 @@
+<!DOCTYPE html><style>
+.outer { display: grid; position: relative; }
+.middle { display: grid; }
+.abs { position: absolute; }
+</style><div class="outer"><div class="middle"><div><div class="abs"></div>