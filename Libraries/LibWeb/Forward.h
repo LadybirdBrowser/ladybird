ee575fd447a6d629e06511f099b8d5d2f47317bb
Sam Atkins <sam@ladybird.org>
2025-08-19 15:11:43 +0100

    LibWeb/CSS: Implement CSSMathSum

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -242,6 +242,7 @@ class CSSKeywordValue;
 class CSSLayerBlockRule;
 class CSSLayerStatementRule;
 class CSSMarginRule;
+class CSSMathSum;
 class CSSMathValue;
 class CSSMediaRule;
 class CSSNamespaceRule;
