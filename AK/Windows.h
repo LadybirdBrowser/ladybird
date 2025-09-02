901e1abe129bf2611ba1937af0650e583536adbd
R-Goc <ryszardgoc@gmail.com>
2025-09-02 17:14:30 +0200

    LibCore: Use IOCP for the event loop on Windows
    This commit changes the event loop to use IOCPs instead of
    WaitForMultipleObjects to wait on events. This is done through the Nt
    kernel api NtAssociateWaitCompletionPacket which associates an event
    with a completion packet. Each completion packet notifies only once, as
    they are normally used to signal completion of an operation so to use
    them for notifiers they are associated again after each time they are
    triggered.
    There are more optimizations that can be done, such as reusing the
    EventLoopNotifier and EventLoopTimer structures to reduce the number of
    allocations and context switches for timer and notifier registration.

--- AK/Windows.h
+++ AK/Windows.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2024, stasoid <stasoid@yahoo.com>
  * Copyright (c) 2025, ayeteadoe <ayeteadoe@gmail.com>
+ * Copyright (c) 2025, Ryszard Goc <ryszardgoc@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -11,11 +12,63 @@
 #pragma once
 
 #include <AK/Assertions.h>
+#include <AK/Diagnostics.h>
 #include <AK/Platform.h>
 
 #ifdef AK_OS_WINDOWS // needed for Swift
 #    define timeval dummy_timeval
+#    include <ntstatus.h>
 #    include <winsock2.h>
+#    include <winternl.h>
+
+extern "C" {
+// NOTE: These are documented here: https://learn.microsoft.com/en-us/windows/win32/devnotes/-win32-misclowlevelclientsupport
+// If the function signature changes, we should catch it with GetProcAddress failing in most cases.
+// None of these are marked deprecated and they seem to be used internally in the kernel.
+
+NTAPI NTSTATUS NtAssociateWaitCompletionPacket(
+    _In_ HANDLE WaitCompletionPacketHandle,
+    _In_ HANDLE IoCompletionHandle,
+    _In_ HANDLE TargetObjectHandle,
+    _In_opt_ PVOID KeyContext,
+    _In_opt_ PVOID ApcContext,
+    _In_ NTSTATUS IoStatus,
+    _In_ ULONG_PTR IoStatusInformation,
+    _Out_opt_ PBOOLEAN AlreadySignaled);
+
+NTAPI NTSTATUS NtCancelWaitCompletionPacket(
+    _In_ HANDLE WaitCompletionPacketHandle,
+    _In_ BOOLEAN RemoveSignaledPacket);
+
+NTAPI NTSTATUS NtCreateWaitCompletionPacket(
+    _Out_ PHANDLE WaitCompletionPacketHandle,
+    _In_ ACCESS_MASK DesiredAccess,
+    _In_opt_ POBJECT_ATTRIBUTES ObjectAttributes);
+}
+
+using PFN_NtCreateWaitCompletionPacket = decltype(&NtCreateWaitCompletionPacket);
+using PFN_NtCancelWaitCompletionPacket = decltype(&NtCancelWaitCompletionPacket);
+using PFN_NtAssociateWaitCompletionPacket = decltype(&NtAssociateWaitCompletionPacket);
+
+inline struct SystemApi {
+    PFN_NtAssociateWaitCompletionPacket NtAssociateWaitCompletionPacket = NULL;
+    PFN_NtCancelWaitCompletionPacket NtCancelWaitCompletionPacket = NULL;
+    PFN_NtCreateWaitCompletionPacket NtCreateWaitCompletionPacket = NULL;
+
+    SystemApi()
+    {
+        HMODULE hNtdll = GetModuleHandleW(L"ntdll.dll");
+        VERIFY(hNtdll);
+        AK_IGNORE_DIAGNOSTIC("-Wcast-function-type-mismatch",
+            NtAssociateWaitCompletionPacket = (PFN_NtAssociateWaitCompletionPacket)GetProcAddress(hNtdll, "NtAssociateWaitCompletionPacket");
+            NtCancelWaitCompletionPacket = (PFN_NtCancelWaitCompletionPacket)GetProcAddress(hNtdll, "NtCancelWaitCompletionPacket");
+            NtCreateWaitCompletionPacket = (PFN_NtCreateWaitCompletionPacket)GetProcAddress(hNtdll, "NtCreateWaitCompletionPacket");)
+        VERIFY(NtAssociateWaitCompletionPacket);
+        VERIFY(NtCancelWaitCompletionPacket);
+        VERIFY(NtCreateWaitCompletionPacket);
+    }
+} g_system;
+
 #    undef timeval
 #    undef IN
 #    pragma comment(lib, "ws2_32.lib")
