b057bad1023d9e6c690b16fa68e3bced31b08055
InvalidUsernameException <InvalidUsernameException@users.noreply.github.com>
2025-09-05 16:22:17 +0200

    LibWeb/CSS: Migrate some call sites to non-deprecated resolve_* methods
    This ensures that we clamp values for properties like padding-* to valid
    ranges (non-negative in this case) if they are specified with `calc()`.
    
    The length-related changes in this commit combined with the ones from
    the previous commit fix the primary layout issue on https://lwn.net
    (yes, not the first place I would have expected problems either).

--- Libraries/LibWeb/CSS/Time.cpp
+++ Libraries/LibWeb/CSS/Time.cpp
@@ -90,12 +90,11 @@ Optional<Time::Type> Time::unit_from_name(StringView name)
 
 Time Time::resolve_calculated(NonnullRefPtr<CalculatedStyleValue const> const& calculated, Layout::Node const& layout_node, Time const& reference_value)
 {
-    return calculated->resolve_time_deprecated(
-                         {
-                             .percentage_basis = reference_value,
-                             .length_resolution_context = Length::ResolutionContext::for_layout_node(layout_node),
-                         })
-        .value();
+    CalculationResolutionContext context {
+        .percentage_basis = reference_value,
+        .length_resolution_context = Length::ResolutionContext::for_layout_node(layout_node),
+    };
+    return calculated->resolve_time(context).value();
 }
 
 }
