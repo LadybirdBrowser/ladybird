7846fb5e3db564323b9d4efda3d06b9028ceff74
mikiubo <michele.uboldi@gmail.com>
2025-09-11 00:27:13 +0200

    LibWeb: Implement XMLFragmentParser
    Implement XMLFragmentParser based on the specification:
    https://html.spec.whatwg.org/multipage/xhtml.html
    
    Fixes one WPT in:
    domparsing/insert_adjacent_html-xhtml.xhtml

---
+++ Libraries/LibWeb/XML/XMLFragmentParser.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025, mikiubo <michele.uboldi@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibWeb/DOM/Document.h>
+#include <LibWeb/DOM/Element.h>
+#include <LibWeb/HTML/Parser/HTMLParser.h>
+#include <LibWeb/XML/XMLDocumentBuilder.h>
+
+namespace Web {
+
+class XMLFragmentParser final {
+public:
+    static WebIDL::ExceptionOr<Vector<GC::Root<DOM::Node>>> parse_xml_fragment(DOM::Element& context, StringView markup, HTML::HTMLParser::AllowDeclarativeShadowRoots = HTML::HTMLParser::AllowDeclarativeShadowRoots::No);
+};
+
+}
