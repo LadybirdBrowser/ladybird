4559d5f6f6183eaa8fb1eef85196262a37d8bb30
Tim Ledbetter <tim.ledbetter@ladybird.org>
2025-09-10 13:49:02 +0100

    LibWeb: Always round `text-decoration-thickness` value up to 1px

--- Libraries/LibWeb/Painting/PaintableBox.cpp
+++ Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -1589,7 +1589,8 @@ void PaintableWithLines::resolve_paint_properties()
                     return max(glyph_height.scaled(0.1), 1);
                 },
                 [&](CSS::LengthPercentage const& length_percentage) {
-                    return length_percentage.resolved(text_node, CSS::Length(1, CSS::Length::Type::Em).to_px(text_node)).to_px(*fragment.m_layout_node);
+                    auto resolved_length = length_percentage.resolved(text_node, CSS::Length(1, CSS::Length::Type::Em).to_px(text_node)).to_px(*fragment.m_layout_node);
+                    return max(resolved_length, 1);
                 });
         }();
         fragment.set_text_decoration_thickness(css_line_thickness);
