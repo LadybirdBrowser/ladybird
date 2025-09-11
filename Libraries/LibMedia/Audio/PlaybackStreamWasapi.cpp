41fc6565fee89da7dcb22192fce850a6198f7a99
R-Goc <ryszardgoc@gmail.com>
2025-09-11 21:55:03 +0200

    LibMedia: Add a WASAPI playback stream for Windows
    WIP.

---
+++ Libraries/LibMedia/Audio/PlaybackStreamWasapi.cpp
@@ -0,0 +1,441 @@
+/*
+ * Copyright (c) 2025, Ryszard Goc <ryszardgoc@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <AK/Assertions.h>
+#include <AK/Atomic.h>
+#include <AK/AtomicRefCounted.h>
+#include <AK/Error.h>
+#include <AK/Forward.h>
+#include <AK/NonnullRefPtr.h>
+#include <AK/RefPtr.h>
+#include <AK/ScopeGuard.h>
+#include <AK/Time.h>
+#include <AK/UFixedBigInt.h>
+#include <AK/Vector.h>
+#include <AK/Windows.h>
+#include <LibCore/ThreadedPromise.h>
+#include <LibMedia/Audio/PlaybackStreamWasapi.h>
+#include <LibMedia/Audio/SampleFormats.h>
+#include <LibThreading/Mutex.h>
+#include <LibThreading/Thread.h>
+
+#include <audioclient.h>
+#include <avrt.h>
+#include <mmdeviceapi.h>
+
+// NOTE: Not using the newer winrt that supersedes wrl as that uses exceptions for error handling
+#include <wrl/client.h>
+
+namespace Audio {
+
+using namespace Microsoft::WRL;
+
+// GUID for the playback session. That way all render streams have a single volume slider in the OS interface
+constexpr GUID PlaybackSessionGUID = { // 22f2ca89-210a-492c-a0aa-f25b1d2f33a1
+    0x22f2ca89,
+    0x210a,
+    0x492c,
+    { 0xa0, 0xaa, 0xf2, 0x5b, 0x1d, 0x2f, 0x33, 0xa1 }
+};
+
+struct AudioState : public AtomicRefCounted<AudioState> {
+    AudioState();
+    ~AudioState();
+
+    ComPtr<IMMDeviceEnumerator> enumerator;
+    ComPtr<IMMDevice> device;
+    ComPtr<IAudioClient> audio_client;
+    ComPtr<IAudioRenderClient> render_client;
+    ComPtr<IAudioStreamVolume> audio_stream_volume;
+    ComPtr<IAudioClock> clock;
+
+    NonnullOwnPtr<WAVEFORMATEXTENSIBLE> wave_format;
+    UINT32 buffer_frame_count;
+    HANDLE buffer_event;
+
+    PlaybackStreamWasapi::AudioDataRequestCallback data_request_callback;
+    Function<void()> underrun_callback;
+
+    Threading::Mutex mutex;
+    Atomic<bool> playing = false;
+    Atomic<bool> drain_and_suspend = false;
+    Atomic<bool> exit_requested = false;
+
+    static int render_thread_loop(AudioState* state);
+    RefPtr<Threading::Thread> audio_thread;
+    RefPtr<Core::ThreadedPromise<AK::Duration>> resume_promise;
+    RefPtr<Core::ThreadedPromise<void>> suspend_promise;
+};
+
+AudioState::AudioState()
+    : wave_format(make<WAVEFORMATEXTENSIBLE>())
+{
+}
+
+AudioState::~AudioState()
+{
+    CloseHandle(buffer_event);
+}
+
+PlaybackStreamWasapi::PlaybackStreamWasapi(NonnullRefPtr<AudioState> state)
+    : m_state(move(state))
+{
+}
+
+PlaybackStreamWasapi::~PlaybackStreamWasapi()
+{
+    m_state->exit_requested.store(true, MemoryOrder::memory_order_release);
+    // Poke the event to wake the thread up from wait
+    if (m_state->buffer_event)
+        SetEvent(m_state->buffer_event);
+
+    MUST(m_state->audio_thread->join());
+}
+
+ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStream::create(OutputState initial_output_state, u32 sample_rate, u8 channels, u32 target_latency_ms, AudioDataRequestCallback&& callback)
+{
+    return PlaybackStreamWasapi::create(initial_output_state, sample_rate, channels, target_latency_ms, move(callback));
+}
+
+ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStreamWasapi::create(OutputState initial_output_state, u32 sample_rate, u8 channels, u32, AudioDataRequestCallback&& data_request_callback)
+{
+    HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+    ScopeGuard uninitizalize_com = []() { CoUninitialize(); };
+    auto state = make_ref_counted<AudioState>();
+
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, IID_PPV_ARGS(&state->enumerator));
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+    dbgln("Created instance");
+    hr = state->enumerator->GetDefaultAudioEndpoint(eRender, eConsole, &state->device);
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+    dbgln("Got default audio endpoint");
+    hr = state->device->Activate(__uuidof(IAudioClient), CLSCTX_ALL, nullptr, &state->audio_client);
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+    dbgln("Activated audio device");
+    state->data_request_callback = move(data_request_callback);
+
+    WAVEFORMATEXTENSIBLE* ideal_format;
+    state->audio_client->GetMixFormat(reinterpret_cast<WAVEFORMATEX**>(&ideal_format));
+    dbgln("Mixing engine audio format:\n"
+          "wFormatTag: {:x}\n"
+          "nChannels: {}\n"
+          "nSamplesPerSec: {}\n"
+          "nAvgBytesPerSec: {}\n"
+          "nBlockAlign: {}\n"
+          "wBitsPerSample: {}\n"
+          "cbSize: {}\n"
+          "Samples.wValidBitsPerSample: {}\n"
+          "dwChannelMask: {:b}\n"
+          "SubFormat: {}",
+        ideal_format->Format.wFormatTag,
+        ideal_format->Format.nChannels,
+        ideal_format->Format.nSamplesPerSec,
+        ideal_format->Format.nAvgBytesPerSec,
+        ideal_format->Format.nBlockAlign,
+        ideal_format->Format.wBitsPerSample,
+        ideal_format->Format.cbSize,
+        ideal_format->Samples.wValidBitsPerSample,
+        ideal_format->dwChannelMask,
+        Span<u8> { reinterpret_cast<char*>(&ideal_format->SubFormat), 16 });
+    CoTaskMemFree(ideal_format);
+    // Set up 32bit float PCM
+    u16 bits_per_sample = 32;
+    u16 block_align = (channels * bits_per_sample) / 8;
+    state->wave_format->Format.wFormatTag = WAVE_FORMAT_EXTENSIBLE;
+    state->wave_format->Format.nChannels = channels;
+    state->wave_format->Format.nSamplesPerSec = sample_rate;
+    state->wave_format->Format.nAvgBytesPerSec = sample_rate * block_align;
+    state->wave_format->Format.nBlockAlign = block_align;
+    state->wave_format->Format.wBitsPerSample = bits_per_sample;
+    state->wave_format->Format.cbSize = 22;
+    state->wave_format->Samples.wValidBitsPerSample = bits_per_sample;
+    // FIXME: Support more than 2 channels
+    VERIFY(channels = 2);
+    state->wave_format->dwChannelMask = 0b0011;
+    state->wave_format->SubFormat = KSDATAFORMAT_SUBTYPE_PCM;
+
+    WAVEFORMATEXTENSIBLE* closest_match;
+    hr = state->audio_client->IsFormatSupported(AUDCLNT_SHAREMODE_SHARED, &state->wave_format->Format, reinterpret_cast<WAVEFORMATEX**>(&closest_match));
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+    if (hr == S_FALSE) {
+        dbgln("Audio format not supported. Current format:\n"
+              "wFormatTag: {:x}\n"
+              "nChannels: {}\n"
+              "nSamplesPerSec: {}\n"
+              "nAvgBytesPerSec: {}\n"
+              "nBlockAlign: {}\n"
+              "wBitsPerSample: {}\n"
+              "cbSize: {}\n"
+              "Samples.wValidBitsPerSample: {}\n"
+              "dwChannelMask: {:b}\n"
+              "SubFormat: {}",
+            state->wave_format->Format.wFormatTag,
+            state->wave_format->Format.nChannels,
+            state->wave_format->Format.nSamplesPerSec,
+            state->wave_format->Format.nAvgBytesPerSec,
+            state->wave_format->Format.nBlockAlign,
+            state->wave_format->Format.wBitsPerSample,
+            state->wave_format->Format.cbSize,
+            state->wave_format->Samples.wValidBitsPerSample,
+            state->wave_format->dwChannelMask,
+            Span<u8> { reinterpret_cast<char*>(&closest_match->SubFormat), 16 });
+        dbgln("Supported audio format:\n"
+              "wFormatTag: {:x}\n"
+              "nChannels: {}\n"
+              "nSamplesPerSec: {}\n"
+              "nAvgBytesPerSec: {}\n"
+              "nBlockAlign: {}\n"
+              "wBitsPerSample: {}\n"
+              "cbSize: {}\n"
+              "Samples.wValidBitsPerSample: {}\n"
+              "dwChannelMask: {:b}\n"
+              "SubFormat: {}",
+            closest_match->Format.wFormatTag,
+            closest_match->Format.nChannels,
+            closest_match->Format.nSamplesPerSec,
+            closest_match->Format.nAvgBytesPerSec,
+            closest_match->Format.nBlockAlign,
+            closest_match->Format.wBitsPerSample,
+            closest_match->Format.cbSize,
+            closest_match->Samples.wValidBitsPerSample,
+            closest_match->dwChannelMask,
+            Span<u8> { reinterpret_cast<char*>(&closest_match->SubFormat), 16 });
+        CoTaskMemFree(closest_match);
+        VERIFY_NOT_REACHED();
+    }
+    // We make the audio engine convert the format to what it needs
+    // TODO: check the actual format of the engine and use it if possible to reduce overhead
+    DWORD stream_flags
+        = AUDCLNT_STREAMFLAGS_EVENTCALLBACK | AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM | AUDCLNT_STREAMFLAGS_SRC_DEFAULT_QUALITY | AUDCLNT_STREAMFLAGS_NOPERSIST;
+    // For event driven buffering we can't specify the buffer duration.
+    hr = state->audio_client->Initialize(AUDCLNT_SHAREMODE_SHARED, stream_flags, 0, 0, &state->wave_format->Format, &PlaybackSessionGUID);
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+    dbgln("Initialized audio client");
+    hr = state->audio_client->GetBufferSize(&state->buffer_frame_count);
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+
+    hr = state->audio_client->GetService(IID_PPV_ARGS(&state->render_client));
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+
+    hr = state->audio_client->GetService(IID_PPV_ARGS(&state->audio_stream_volume));
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+
+    hr = state->audio_client->GetService(IID_PPV_ARGS(&state->clock));
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+
+    state->buffer_event = CreateEvent(NULL, FALSE, FALSE, NULL);
+    if (!state->buffer_event)
+        return Error::from_windows_error(hr);
+
+    hr = state->audio_client->SetEventHandle(state->buffer_event);
+    if (FAILED(hr))
+        return Error::from_windows_error(hr);
+
+    state->audio_thread = Threading::Thread::construct([state] { return AudioState::render_thread_loop(state.ptr()); });
+    state->audio_thread->start();
+
+    if (initial_output_state == OutputState::Playing) {
+        state->playing.store(true, MemoryOrder::memory_order_relaxed);
+        hr = state->audio_client->Start();
+        if (FAILED(hr))
+            return Error::from_windows_error(hr);
+    }
+
+    return TRY(adopt_nonnull_ref_or_enomem(new (nothrow) PlaybackStreamWasapi(move(state))));
+}
+
+int AudioState::render_thread_loop(AudioState* state)
+{
+    HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+    if (FAILED(hr))
+        VERIFY_NOT_REACHED();
+    DWORD buffer_flags = 0;
+    WORD block_align = state->wave_format->Format.nBlockAlign;
+
+    ScopeGuard uninitialize_com = [] { CoUninitialize(); };
+
+    DWORD task_index = 0;
+    HANDLE task_handle = AvSetMmThreadCharacteristicsW(L"Pro Audio", &task_index);
+
+    while (!state->exit_requested.load(MemoryOrder::memory_order_acquire)) {
+        WaitForSingleObject(state->buffer_event, INFINITE);
+        // TODO: Verify the memory order here
+        if (state->exit_requested.load(MemoryOrder::memory_order_acquire))
+            break;
+        if (!state->playing.load(MemoryOrder::memory_order_acquire))
+            continue;
+
+        u32 padding = 0;
+        hr = state->audio_client->GetCurrentPadding(&padding);
+        // TODO: Have sane action on failing. This is for testing only.
+        if (FAILED(hr)) {
+            dbgln("GetCurrentPadding failed with: {}", Error::from_windows_error(hr));
+            VERIFY_NOT_REACHED();
+        }
+        u32 frames_available = state->buffer_frame_count - padding;
+
+        if (frames_available == 0)
+            continue;
+        BYTE* buffer;
+        hr = state->render_client->GetBuffer(frames_available, &buffer);
+        if (FAILED(hr)) {
+            dbgln("GetBuffer failed with: {}", Error::from_windows_error(hr));
+            VERIFY_NOT_REACHED();
+        }
+
+        u32 buffer_size = frames_available * block_align;
+        dbgln("Request buffer of size: {}\n, frames_available: {}", buffer_size, frames_available);
+        ReadonlyBytes bytes_written = state->data_request_callback({ buffer, buffer_size }, PcmSampleFormat::Float32, frames_available);
+        if (bytes_written.is_empty()) {
+            if (state->underrun_callback)
+                state->underrun_callback();
+            buffer_flags |= AUDCLNT_BUFFERFLAGS_SILENT;
+        }
+        // TODO: Have some heuristic on how many bytes need to be in the buffer before silence needs to be added to the end.
+        // If there was just one byte written we would quickly run out.
+        frames_available = bytes_written.size() / block_align;
+        hr = state->render_client->ReleaseBuffer(frames_available, buffer_flags);
+        if (FAILED(hr)) {
+            dbgln("ReleaseBuffer failed with: {}", Error::from_windows_error(hr));
+            VERIFY_NOT_REACHED();
+        }
+    }
+
+    AvRevertMmThreadCharacteristics(task_handle);
+
+    return 0;
+}
+
+void PlaybackStreamWasapi::set_underrun_callback(Function<void()> underrun_callback)
+{
+    m_state->underrun_callback = move(underrun_callback);
+}
+
+NonnullRefPtr<Core::ThreadedPromise<AK::Duration>> PlaybackStreamWasapi::resume()
+{
+    // should this be using a separate promise or not?
+    m_state->resume_promise = Core::ThreadedPromise<AK::Duration>::create();
+    HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+    if (FAILED(hr)) {
+        m_state->resume_promise->reject(Error::from_windows_error(hr));
+        return m_state->resume_promise.release_nonnull();
+    }
+    Threading::MutexLocker locker(m_state->mutex);
+    if (m_state->playing.load(MemoryOrder::memory_order_relaxed)) {
+        m_state->resume_promise->resolve(total_time_played());
+        return m_state->resume_promise.release_nonnull();
+    }
+
+    m_state->playing.store(true, MemoryOrder::memory_order_relaxed);
+    m_state->drain_and_suspend.store(false, MemoryOrder::memory_order_relaxed);
+    hr = m_state->audio_client->Start();
+    if (FAILED(hr)) {
+        m_state->resume_promise->reject(Error::from_windows_error(hr));
+        m_state->playing.store(false, MemoryOrder::memory_order_relaxed);
+        return m_state->resume_promise.release_nonnull();
+    }
+
+    // TODO: check if this is close enough
+    m_state->resume_promise->resolve(total_time_played());
+    return m_state->resume_promise.release_nonnull();
+}
+
+NonnullRefPtr<Core::ThreadedPromise<void>> PlaybackStreamWasapi::drain_buffer_and_suspend()
+{
+    auto promise = Core::ThreadedPromise<void>::create();
+    HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+    if (FAILED(hr)) {
+        promise->reject(Error::from_windows_error(hr));
+        return promise;
+    }
+    Threading::MutexLocker locker(m_state->mutex);
+    m_state->suspend_promise = promise;
+    if (m_state->playing.load(MemoryOrder::memory_order_relaxed)) {
+        m_state->drain_and_suspend.store(true, MemoryOrder::memory_order_relaxed);
+    } else {
+        m_state->audio_client->Reset();
+        m_state->suspend_promise->resolve();
+    }
+    return *m_state->suspend_promise;
+}
+
+NonnullRefPtr<Core::ThreadedPromise<void>> PlaybackStreamWasapi::discard_buffer_and_suspend()
+{
+    auto promise = Core::ThreadedPromise<void>::create();
+    HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+    if (FAILED(hr)) {
+        promise->reject(Error::from_windows_error(hr));
+        return promise;
+    }
+    ScopeGuard uninitialize_com = [] { CoUninitialize(); };
+    Threading::MutexLocker locker(m_state->mutex);
+    if (m_state->playing.load(MemoryOrder::memory_order_relaxed)) {
+        m_state->playing.store(false, MemoryOrder::memory_order_relaxed);
+        m_state->audio_client->Stop();
+    }
+
+    m_state->audio_client->Reset();
+    promise->resolve();
+    return promise;
+}
+
+AK::Duration PlaybackStreamWasapi::total_time_played() const
+{
+    HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+    if (FAILED(hr))
+        VERIFY_NOT_REACHED();
+    ScopeGuard uninitialize_com = [] { CoUninitialize(); };
+
+    UINT64 frequency;
+    UINT64 position;
+    hr = m_state->clock->GetFrequency(&frequency);
+    if (FAILED(hr))
+        VERIFY_NOT_REACHED();
+    hr = m_state->clock->GetPosition(&position, NULL);
+    if (FAILED(hr))
+        VERIFY_NOT_REACHED();
+    // FIXME: This is a narrowing conversion. We might want a u64 overload of from_nanoseconds.
+    return AK::Duration::from_nanoseconds(static_cast<i64>(position * 1'000'000'000ull / frequency));
+}
+
+NonnullRefPtr<Core::ThreadedPromise<void>> PlaybackStreamWasapi::set_volume(double volume)
+{
+    auto promise = Core::ThreadedPromise<void>::create();
+    HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+    if (FAILED(hr)) {
+        promise->reject(Error::from_windows_error(hr));
+        return promise;
+    }
+    ScopeGuard uninitialize_com = [] { CoUninitialize(); };
+
+    float clamped_volume = static_cast<float>(clamp(volume, 0.0, 1.0));
+    auto channels = m_state->wave_format->Format.nChannels;
+    Vector<float> volumes;
+    volumes.resize(channels);
+    for (u64 i = 0; i < channels; i++)
+        volumes[i] = clamped_volume;
+
+    hr = m_state->audio_stream_volume->SetAllVolumes(channels, volumes.data());
+    if (FAILED(hr)) {
+        promise->reject(Error::from_windows_error(hr));
+    } else {
+        promise->resolve();
+    }
+    return promise;
+}
+
+}
