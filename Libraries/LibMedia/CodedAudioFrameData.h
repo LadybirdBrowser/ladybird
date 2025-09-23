72ba10d91c475e832cd664e0b3b7889495600d26
Zaggy1024 <Zaggy1024@gmail.com>
2025-09-23 18:30:41 -0500

    LibMedia: Support coded audio frames in our demuxers
    This adds a new variant of the metadata storage in CodedFrame for audio
    frames, called CodedAudioFrameData.

---
+++ Libraries/LibMedia/CodedAudioFrameData.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025, Gregory Bertilson <gregory@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+namespace Media {
+
+class CodedAudioFrameData {
+public:
+    CodedAudioFrameData()
+    {
+    }
+};
+
+}
