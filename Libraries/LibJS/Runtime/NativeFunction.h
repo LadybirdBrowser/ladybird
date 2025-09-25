88d838092043e1f0ba17c57cc567ee9756036522
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2025-09-25 18:13:11 +0200

    LibJS: Add `FunctionObject::fast_is<NativeFunction>()`

--- Libraries/LibJS/Runtime/NativeFunction.h
+++ Libraries/LibJS/Runtime/NativeFunction.h
@@ -72,4 +72,7 @@ private:
 template<>
 inline bool Object::fast_is<NativeFunction>() const { return is_native_function(); }
 
+template<>
+inline bool FunctionObject::fast_is<NativeFunction>() const { return is_native_function(); }
+
 }
