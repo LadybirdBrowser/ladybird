05063b5a36d7cdae56ef586be9883b9781bd9384
Adam Patterson <adam@adamrt.com>
2025-10-02 09:52:42 -0700

    LibJS: Remove files from .prettierignore
    .prettierignore had files listed for an issue with 'using' declarations,
    this is no longer an issue for prettier (tested on 3.6.2).

--- Libraries/LibJS/Tests/modules/top-level-dispose.mjs
+++ Libraries/LibJS/Tests/modules/top-level-dispose.mjs
@@ -1,14 +1,20 @@
 export let passed = false;
 let failed = false;
 
-if (passed)
-    failed = true;
+if (passed) failed = true;
 
-using a = { [Symbol.dispose]() { if (!failed) passed = true; } }
+using a = {
+    [Symbol.dispose]() {
+        if (!failed) passed = true;
+    },
+};
 
-if (passed)
-    failed = true;
+if (passed) failed = true;
 
 failed = true;
 // Should trigger before
-using b = { [Symbol.dispose]() { if (!passed) failed = false; } }
+using b = {
+    [Symbol.dispose]() {
+        if (!passed) failed = false;
+    },
+};
--- Libraries/LibJS/Tests/object-basic.js
+++ Libraries/LibJS/Tests/object-basic.js
@@ -27,6 +27,8 @@ describe("correct behavior", () => {
 
         expect(o.foo).toBe("bar");
         expect(o["foo"]).toBe("bar");
+        // prettier-ignore
+        // Ignored so the expressions arent wrapped in parens
         expect(o.qux).toBe(10), expect(o.hello).toBe("friends");
         expect(o["hello"]).toBe("friends");
     });
