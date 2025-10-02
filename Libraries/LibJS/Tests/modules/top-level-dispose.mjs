ac76f3354bfd4988988f2e9628c69dd8b6f9e983
Adam Patterson <adam@adamrt.com>
2025-10-02 09:52:42 -0700

    LibJS: Remove files from .prettierignore
    .prettierignore had files listed for an issue with 'using' declarations,
    this is no longer an issue for prettier (tested on 3.6.2).

--- Libraries/LibJS/Tests/modules/top-level-dispose.mjs
+++ Libraries/LibJS/Tests/modules/top-level-dispose.mjs
@@ -1,14 +1,28 @@
 export let passed = false;
 let failed = false;
 
-if (passed)
+if (passed) {
     failed = true;
+}
 
-using a = { [Symbol.dispose]() { if (!failed) passed = true; } }
+using a = {
+    [Symbol.dispose]() {
+        if (!failed) {
+            passed = true;
+        }
+    },
+};
 
-if (passed)
+if (passed) {
     failed = true;
+}
 
 failed = true;
 // Should trigger before
-using b = { [Symbol.dispose]() { if (!passed) failed = false; } }
+using b = {
+    [Symbol.dispose]() {
+        if (!passed) {
+            failed = false;
+        }
+    },
+};
