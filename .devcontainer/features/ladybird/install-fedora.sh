e48f5bbc7793c8e9af09dad7e29480b2667c5462
Jelle Raaijmakers <jelle@ladybird.org>
2025-10-03 14:31:53 +0200

    Devcontainer: Add libtool to dependencies

--- .devcontainer/features/ladybird/install-fedora.sh
+++ .devcontainer/features/ladybird/install-fedora.sh
@@ -5,6 +5,6 @@ set -e
 dnf install -y git gh
 
 # Ladybird dev dependencies
-dnf install -y autoconf-archive automake ccache cmake curl google-noto-sans-mono-fonts liberation-sans-fonts libglvnd-devel \
-    nasm ninja-build patchelf perl-FindBin perl-IPC-Cmd perl-lib qt6-qtbase-devel qt6-qtmultimedia-devel qt6-qttools-devel \
-    qt6-qtwayland-devel tar unzip zip zlib-ng-compat-static
+dnf install -y autoconf-archive automake ccache cmake curl google-noto-sans-mono-fonts liberation-sans-fonts \
+    libglvnd-devel libtool nasm ninja-build patchelf perl-FindBin perl-IPC-Cmd perl-lib qt6-qtbase-devel \
+    qt6-qtmultimedia-devel qt6-qttools-devel qt6-qtwayland-devel tar unzip zip zlib-ng-compat-static
