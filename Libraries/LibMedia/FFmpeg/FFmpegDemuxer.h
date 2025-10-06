fcde0f66c8132c6830376b4d46932f273dc8c7d5
Zaggy1024 <Zaggy1024@gmail.com>
2025-10-06 16:59:07 -0500

    LibMedia: Allow FFmpegDemuxer to grab samples from multiple streams
    In order to do so, we create a AVFormatContext for each stream we want
    to demux.

--- Libraries/LibMedia/FFmpeg/FFmpegDemuxer.h
+++ Libraries/LibMedia/FFmpeg/FFmpegDemuxer.h
@@ -7,7 +7,8 @@
 
 #pragma once
 
-#include <AK/Error.h>
+#include <AK/Forward.h>
+#include <AK/HashMap.h>
 #include <AK/NonnullOwnPtr.h>
 #include <LibMedia/Demuxer.h>
 #include <LibMedia/FFmpeg/FFmpegIOContext.h>
@@ -21,9 +22,7 @@ namespace Media::FFmpeg {
 
 class FFmpegDemuxer : public Demuxer {
 public:
-    static ErrorOr<NonnullRefPtr<FFmpegDemuxer>> create(NonnullOwnPtr<SeekableStream> stream);
-
-    FFmpegDemuxer(NonnullOwnPtr<SeekableStream> stream, NonnullOwnPtr<Media::FFmpeg::FFmpegIOContext>);
+    static DecoderErrorOr<NonnullRefPtr<FFmpegDemuxer>> from_data(ReadonlyBytes data);
     virtual ~FFmpegDemuxer() override;
 
     virtual DecoderErrorOr<Vector<Track>> get_tracks_for_type(TrackType type) override;
@@ -41,13 +40,34 @@ public:
     virtual DecoderErrorOr<CodedFrame> get_next_sample_for_track(Track const& track) override;
 
 private:
+    struct TrackContext {
+        TrackContext(NonnullOwnPtr<SeekableStream>&& stream, NonnullOwnPtr<FFmpegIOContext>&& io_context)
+            : stream(move(stream))
+            , io_context(move(io_context))
+        {
+        }
+        ~TrackContext();
+        TrackContext(TrackContext&&) = default;
+
+        NonnullOwnPtr<SeekableStream> stream;
+        NonnullOwnPtr<FFmpegIOContext> io_context;
+        AVFormatContext* format_context { nullptr };
+        AVPacket* packet { nullptr };
+        bool is_seekable { true };
+        bool peeked_packet_already { false };
+    };
+
+    FFmpegDemuxer(ReadonlyBytes, NonnullOwnPtr<SeekableStream>&&, NonnullOwnPtr<Media::FFmpeg::FFmpegIOContext>&&);
+
+    TrackContext& get_track_context(Track const&);
     DecoderErrorOr<Track> get_track_for_stream_index(u32 stream_index);
 
+    ReadonlyBytes m_data;
     NonnullOwnPtr<SeekableStream> m_stream;
-    AVCodecContext* m_codec_context { nullptr };
-    AVFormatContext* m_format_context { nullptr };
-    NonnullOwnPtr<Media::FFmpeg::FFmpegIOContext> m_io_context;
-    AVPacket* m_packet { nullptr };
+    NonnullOwnPtr<FFmpegIOContext> m_io_context;
+    AVFormatContext* m_format_context;
+
+    HashMap<Track, NonnullOwnPtr<TrackContext>> m_track_contexts;
 };
 
 }
