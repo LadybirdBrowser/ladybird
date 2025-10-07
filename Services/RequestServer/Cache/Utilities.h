3516a2344f5503839ceae92efb5856128c7366dd
Timothy Flynn <trflynn89@pm.me>
2025-10-07 19:59:21 -0400

    LibRequests+RequestServer: Begin implementing an HTTP disk cache
    This adds a disk cache for HTTP responses received from the network. For
    now, we take a rather conservative approach to caching. We don't cache a
    response until we're 100% sure it is cacheable (there are heuristics we
    can implement in the future based on the absence of specific headers).
    
    The cache is broken into 2 categories of files:
    
    1. An index file. This is a SQL database containing metadata about each
       cache entry (URL, timestamps, etc.).
    2. Cache files. Each cached response is in its own file. The file is an
       amalgamation of all info needed to reconstruct an HTTP response. This
       includes the status code, headers, body, etc.
    
    A cache entry is created once we receive the headers for a response. The
    index, however, is not updated at this point. We stream the body into
    the cache entry as it is received. Once we've successfully cached the
    entire body, we create an index entry in the database. If any of these
    steps failed along the way, the cache entry is removed and the index is
    left untouched.
    
    Subsequent requests are checked for cache hits from the index. If a hit
    is found, we read just enough of the cache entry to inform WebContent of
    the status code and headers. The body of the response is piped to WC via
    syscalls, such that the transfer happens entirely in the kernel; no need
    to allocate the memory for the body in userspace (WC still allocates a
    buffer to hold the data, of course). If an error occurs while piping the
    body, we currently error out the request. There is a FIXME to switch to
    a network request.
    
    Cache hits are also validated for freshness before they are used. If a
    response has expired, we remove it and its index entry, and proceed with
    a network request.

---
+++ Services/RequestServer/Cache/Utilities.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025, Tim Flynn <trflynn89@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/StringView.h>
+#include <AK/Time.h>
+#include <AK/Types.h>
+#include <LibHTTP/HeaderMap.h>
+#include <LibURL/Forward.h>
+
+namespace RequestServer {
+
+String serialize_url_for_cache_storage(URL::URL const&);
+u64 create_cache_key(StringView url, StringView method);
+
+bool is_cacheable(StringView method, u32 status_code, HTTP::HeaderMap const&);
+bool is_header_exempted_from_storage(StringView name);
+
+AK::Duration calculate_freshness_lifetime(HTTP::HeaderMap const&);
+AK::Duration calculate_age(HTTP::HeaderMap const&, UnixDateTime request_time, UnixDateTime response_time);
+bool is_response_fresh(AK::Duration freshness_lifetime, AK::Duration current_age);
+
+}
