ab3eb9adab7ca0caba266395eed945f0ae6a3c84
Luke Wilde <luke@ladybird.org>
2025-10-20 13:18:38 +0100

    LibWeb/WebGL: Replace manual pointer math with Span
    This deduplicates a lot of sensitive pointer math by using Span, which
    performs this math for us with more safety checks and with type
    information.
    
    This also allows us to use the correctly typed Span for typed arrays,
    which automatically fixes srcOffset to now offset by the number of
    elements instead of bytes. This goes for srcLengthOverride too.
    
    Fixes the Rive animations on Shopify's homepage not appearing.
    Fixes some Unity applications such as ArcViewer having missing
    graphics.

--- Libraries/LibWeb/WebGL/WebGLRenderingContextBase.h
+++ Libraries/LibWeb/WebGL/WebGLRenderingContextBase.h
@@ -6,8 +6,10 @@
 
 #pragma once
 
+#include <LibJS/Runtime/DataView.h>
 #include <LibJS/Runtime/TypedArray.h>
 #include <LibWeb/Forward.h>
+#include <LibWeb/WebIDL/Buffers.h>
 #include <LibWeb/WebIDL/Types.h>
 
 namespace Web::WebGL {
@@ -28,25 +30,76 @@ public:
     virtual OpenGLContext& context() = 0;
     virtual bool ext_texture_filter_anisotropic_extension_enabled() const = 0;
 
-    static Span<float> span_from_float32_list(Float32List& float32_list)
+    template<typename T>
+    static ErrorOr<Span<T>> get_offset_span(Span<T> src_span, WebIDL::UnsignedLongLong src_offset, WebIDL::UnsignedLong src_length_override = 0)
     {
-        if (float32_list.has<Vector<float>>())
-            return float32_list.get<Vector<float>>();
-        return float32_list.get<GC::Root<JS::Float32Array>>()->data();
+        Checked<WebIDL::UnsignedLongLong> length = src_offset;
+        length += src_length_override;
+        if (length.has_overflow() || length.value_unchecked() > src_span.size()) [[unlikely]]
+            return Error::from_errno(EINVAL);
+
+        if (src_length_override == 0)
+            return src_span.slice(src_offset, src_span.size() - src_offset);
+
+        return src_span.slice(src_offset, src_length_override);
     }
 
-    static Span<int> span_from_int32_list(Int32List& int32_list)
+    template<typename T>
+    static ErrorOr<Span<T>> get_offset_span(GC::Ref<WebIDL::BufferableObjectBase> src_data, WebIDL::UnsignedLongLong src_offset, WebIDL::UnsignedLong src_length_override = 0)
     {
-        if (int32_list.has<Vector<int>>())
-            return int32_list.get<Vector<int>>();
-        return int32_list.get<GC::Root<JS::Int32Array>>()->data();
+        auto buffer_size = src_data->byte_length();
+        if (buffer_size % sizeof(T) != 0) [[unlikely]]
+            return Error::from_errno(EINVAL);
+
+        auto raw_object = src_data->raw_object();
+
+        if (auto* array_buffer = as_if<JS::ArrayBuffer>(*raw_object)) {
+            return TRY(get_offset_span(array_buffer->buffer().span(), src_offset, src_length_override)).reinterpret<T>();
+        }
+
+        if (auto* data_view = as_if<JS::DataView>(*raw_object)) {
+            return TRY(get_offset_span(data_view->viewed_array_buffer()->buffer().span(), src_offset, src_length_override)).reinterpret<T>();
+        }
+
+        // NOTE: This has to be done because src_offset is the number of elements to offset by, not the number of bytes.
+#define __JS_ENUMERATE(ClassName, snake_name, PrototypeName, ConstructorName, Type)                         \
+    if (auto* typed_array = as_if<JS::ClassName>(*raw_object)) {                                            \
+        return TRY(get_offset_span(typed_array->data(), src_offset, src_length_override)).reinterpret<T>(); \
+    }
+        JS_ENUMERATE_TYPED_ARRAYS
+#undef __JS_ENUMERATE
+
+        VERIFY_NOT_REACHED();
     }
 
-    static Span<u32> span_from_uint32_list(Uint32List& int32_list)
+    static ErrorOr<Span<float>> span_from_float32_list(Float32List& float32_list, WebIDL::UnsignedLongLong src_offset, WebIDL::UnsignedLong src_length_override = 0)
     {
-        if (int32_list.has<Vector<u32>>())
-            return int32_list.get<Vector<u32>>();
-        return int32_list.get<GC::Root<JS::Uint32Array>>()->data();
+        if (float32_list.has<Vector<float>>()) {
+            auto& vector = float32_list.get<Vector<float>>();
+            return get_offset_span(vector.span(), src_offset, src_length_override);
+        }
+        auto& buffer = float32_list.get<GC::Root<JS::Float32Array>>();
+        return get_offset_span(buffer->data(), src_offset, src_length_override);
+    }
+
+    static ErrorOr<Span<int>> span_from_int32_list(Int32List& int32_list, WebIDL::UnsignedLongLong src_offset, WebIDL::UnsignedLong src_length_override = 0)
+    {
+        if (int32_list.has<Vector<int>>()) {
+            auto& vector = int32_list.get<Vector<int>>();
+            return get_offset_span(vector.span(), src_offset, src_length_override);
+        }
+        auto& buffer = int32_list.get<GC::Root<JS::Int32Array>>();
+        return get_offset_span(buffer->data(), src_offset, src_length_override);
+    }
+
+    static ErrorOr<Span<u32>> span_from_uint32_list(Uint32List& uint32_list, WebIDL::UnsignedLongLong src_offset, WebIDL::UnsignedLong src_length_override = 0)
+    {
+        if (uint32_list.has<Vector<u32>>()) {
+            auto& vector = uint32_list.get<Vector<u32>>();
+            return get_offset_span(vector.span(), src_offset, src_length_override);
+        }
+        auto& buffer = uint32_list.get<GC::Root<JS::Uint32Array>>();
+        return get_offset_span(buffer->data(), src_offset, src_length_override);
     }
 
     struct ConvertedTexture {
