6c9bea55d1124db1a70a0db680f1dc28c6e189cc
Timothy Flynn <trflynn89@pm.me>
2025-10-23 20:44:55 -0400

    RequestServer: Manage request lifetimes as a simple state machine
    We currently manage request lifetime as both an ActiveRequest structure
    and a series of lambda callbacks. In an upcoming patch, we will want to
    "pause" a request to de-duplicate equivalent requests, such that only
    one request goes over the network and saves its response to the disk
    cache.
    
    To make that easier to reason about, this adds a Request class to manage
    the lifetime of a request via a state machine. We will now be able to
    add a "waiting for disk cache" state to stop the request.

--- Services/RequestServer/ConnectionFromClient.h
+++ Services/RequestServer/ConnectionFromClient.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include <AK/Badge.h>
 #include <AK/HashMap.h>
 #include <LibIPC/ConnectionFromClient.h>
 #include <LibWebSocket/WebSocket.h>
@@ -23,6 +24,8 @@ public:
     ~ConnectionFromClient() override;
 
     virtual void die() override;
+
+    void request_complete(Badge<Request>, int request_id);
 
 private:
     explicit ConnectionFromClient(NonnullOwnPtr<IPC::Transport>);
@@ -46,31 +49,21 @@ private:
     virtual void websocket_close(i64 websocket_id, u16, ByteString) override;
     virtual Messages::RequestServer::WebsocketSetCertificateResponse websocket_set_certificate(i64, ByteString, ByteString) override;
 
-    struct ResumeRequestForFailedCacheEntry {
-        size_t start_offset { 0 };
-        int writer_fd { 0 };
-    };
-    void issue_network_request(i32 request_id, ByteString, URL::URL, HTTP::HeaderMap, ByteBuffer, Core::ProxyData, Optional<ResumeRequestForFailedCacheEntry> = {});
-
-    HashMap<i32, RefPtr<WebSocket::WebSocket>> m_websockets;
-
-    struct ActiveRequest;
-    friend struct ActiveRequest;
+    static int on_socket_callback(void*, int sockfd, int what, void* user_data, void*);
+    static int on_timeout_callback(void*, long timeout_ms, void* user_data);
+    void check_active_requests();
 
     static ErrorOr<IPC::File> create_client_socket();
 
-    static int on_socket_callback(void*, int sockfd, int what, void* user_data, void*);
-    static int on_timeout_callback(void*, long timeout_ms, void* user_data);
-    static size_t on_header_received(void* buffer, size_t size, size_t nmemb, void* user_data);
-    static size_t on_data_received(void* buffer, size_t size, size_t nmemb, void* user_data);
-
-    HashMap<i32, NonnullOwnPtr<ActiveRequest>> m_active_requests;
-
-    void check_active_requests();
     void* m_curl_multi { nullptr };
+
+    HashMap<i32, NonnullOwnPtr<Request>> m_active_requests;
+    HashMap<i32, RefPtr<WebSocket::WebSocket>> m_websockets;
+
     RefPtr<Core::Timer> m_timer;
     HashMap<int, NonnullRefPtr<Core::Notifier>> m_read_notifiers;
     HashMap<int, NonnullRefPtr<Core::Notifier>> m_write_notifiers;
+
     NonnullRefPtr<Resolver> m_resolver;
     ByteString m_alt_svc_cache_path;
 };
