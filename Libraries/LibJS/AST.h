44fa9566a8c3b872187cfe032e97b5e47c4847af
Andreas Kling <andreas@ladybird.org>
2025-10-25 14:06:48 +0200

    LibJS: Generate bytecode for the BlockDeclarationInstantiation AO
    This necessitated adding some new instructions for creating mutable and
    immutable bindings.

--- Libraries/LibJS/AST.h
+++ Libraries/LibJS/AST.h
@@ -334,8 +334,6 @@ public:
     ThrowCompletionOr<void> for_each_var_function_declaration_in_reverse_order(ThrowCompletionOrVoidCallback<FunctionDeclaration const&>&& callback) const;
     ThrowCompletionOr<void> for_each_var_scoped_variable_declaration(ThrowCompletionOrVoidCallback<VariableDeclaration const&>&& callback) const;
 
-    void block_declaration_instantiation(VM&, Environment*) const;
-
     ThrowCompletionOr<void> for_each_function_hoistable_with_annexB_extension(ThrowCompletionOrVoidCallback<FunctionDeclaration&>&& callback) const;
 
     auto const& local_variables_names() const { return m_local_variables_names; }
