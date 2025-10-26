8717dca2214ec335bda3e35f6912694f15a7fa35
Glenn Skrzypczak <glenn.skrzypczak@gmail.com>
2025-10-26 18:06:19 +0100

    LibWeb: Support CSS modules
    This adds support for importing CSS stylesheets from CSS files in
    javascript.

--- Libraries/LibWeb/HTML/Scripting/ModuleScript.h
+++ Libraries/LibWeb/HTML/Scripting/ModuleScript.h
@@ -7,33 +7,24 @@
 #pragma once
 
 #include <LibJS/SourceTextModule.h>
+#include <LibJS/SyntheticModule.h>
 #include <LibWeb/Export.h>
 #include <LibWeb/HTML/Scripting/Script.h>
 
 namespace Web::HTML {
 
+// FIXME: Support WebAssembly Module Record
+using ModuleScriptRecord = Variant<Empty, GC::Ref<JS::SourceTextModule>, GC::Ref<JS::SyntheticModule>>;
+
 // https://html.spec.whatwg.org/multipage/webappapis.html#module-script
-class ModuleScript : public Script {
+class WEB_API ModuleScript : public Script {
     GC_CELL(ModuleScript, Script);
 
 public:
     virtual ~ModuleScript() override;
 
-protected:
-    ModuleScript(URL::URL base_url, ByteString filename, JS::Realm&);
-
-private:
-    virtual bool is_module_script() const final { return true; }
-};
-
-class WEB_API JavaScriptModuleScript final : public ModuleScript {
-    GC_CELL(JavaScriptModuleScript, ModuleScript);
-    GC_DECLARE_ALLOCATOR(JavaScriptModuleScript);
-
-public:
-    virtual ~JavaScriptModuleScript() override;
-
-    static WebIDL::ExceptionOr<GC::Ptr<JavaScriptModuleScript>> create(ByteString const& filename, StringView source, JS::Realm&, URL::URL base_url);
+    static WebIDL::ExceptionOr<GC::Ptr<ModuleScript>> create_a_javascript_module_script(ByteString const& filename, StringView source, JS::Realm&, URL::URL base_url);
+    static WebIDL::ExceptionOr<GC::Ptr<ModuleScript>> create_a_css_module_script(ByteString const& filename, StringView source, JS::Realm&);
 
     enum class PreventErrorReporting {
         Yes,
@@ -42,28 +33,24 @@ public:
 
     JS::Promise* run(PreventErrorReporting = PreventErrorReporting::No);
 
-    JS::SourceTextModule const* record() const { return m_record.ptr(); }
-    JS::SourceTextModule* record() { return m_record.ptr(); }
+    ModuleScriptRecord record() const { return m_record; }
 
 protected:
-    JavaScriptModuleScript(URL::URL base_url, ByteString filename, JS::Realm&);
+    ModuleScript(Optional<URL::URL> base_url, ByteString filename, JS::Realm&);
 
 private:
-    virtual bool is_javascript_module_script() const final { return true; }
+    virtual bool is_module_script() const final { return true; }
     virtual void visit_edges(JS::Cell::Visitor&) override;
 
-    GC::Ptr<JS::SourceTextModule> m_record;
+    ModuleScriptRecord m_record;
 
     size_t m_fetch_internal_request_count { 0 };
     size_t m_completed_fetch_internal_request_count { 0 };
 
-    Function<void(JavaScriptModuleScript const*)> m_completed_fetch_internal_callback;
+    Function<void(ModuleScript const*)> m_completed_fetch_internal_callback;
 };
 
 }
 
 template<>
 inline bool JS::Script::HostDefined::fast_is<Web::HTML::ModuleScript>() const { return is_module_script(); }
-
-template<>
-inline bool JS::Script::HostDefined::fast_is<Web::HTML::JavaScriptModuleScript>() const { return is_javascript_module_script(); }
