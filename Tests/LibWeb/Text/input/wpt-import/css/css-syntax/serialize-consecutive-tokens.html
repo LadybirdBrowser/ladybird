LadybirdBrowser/ladybird

commit 61a0898f5fedaa562e3a3cc680b75982894cd03d

LibWeb/CSS: Stop inserting whitespace when serializing component values
Now that we don't remove whitespace when parsing, we don't need to
artificially insert it back in again when serializing. We do now need
to trim leading and trailing whitespace from UnresolvedStyleValues, as
this previously was done as part of the whitespace insertion.

This makes our serialization of UnresolvedStyleValues more correct and
gets us a few WPT passes for each property in the Typed OM tests.

Libraries/LibWeb/CSS/Serialize.cpp
@@ -199,11 +199,9 @@ String serialize_a_css_declaration(StringView property, StringView value, Import
 }
 
 // https://drafts.csswg.org/css-syntax/#serialization
-String serialize_a_series_of_component_values(ReadonlySpan<Parser::ComponentValue> component_values, InsertWhitespace insert_whitespace)
+String serialize_a_series_of_component_values(ReadonlySpan<Parser::ComponentValue> component_values)
 {
     // FIXME: There are special rules here where we should insert a comment between certain tokens. Do that!
-    if (insert_whitespace == InsertWhitespace::Yes)
-        return MUST(Infra::strip_and_collapse_whitespace(MUST(String::join(' ', component_values))));
     return MUST(String::join(""sv, component_values));
 }
 

Libraries/LibWeb/CSS/Serialize.h
@@ -47,11 +47,6 @@ void serialize_a_comma_separated_list(StringBuilder& builder, Vector<T> const& i
 
 String serialize_a_css_declaration(StringView property, StringView value, Important = Important::No);
 
-enum class InsertWhitespace : u8 {
-    No,
-    Yes,
-};
-// FIXME: Remove InsertWhitespace param once style value parsing stops discarding whitespace tokens.
-String serialize_a_series_of_component_values(ReadonlySpan<Parser::ComponentValue>, InsertWhitespace = InsertWhitespace::No);
+String serialize_a_series_of_component_values(ReadonlySpan<Parser::ComponentValue>);
 
 }

Libraries/LibWeb/CSS/StyleValues/UnresolvedStyleValue.cpp
@@ -45,7 +45,7 @@ String UnresolvedStyleValue::to_string(SerializationMode) const
     if (m_original_source_text.has_value())
         return *m_original_source_text;
 
-    return serialize_a_series_of_component_values(m_values, InsertWhitespace::Yes);
+    return MUST(serialize_a_series_of_component_values(m_values).trim_ascii_whitespace());
 }
 
 bool UnresolvedStyleValue::equals(StyleValue const& other) const
@@ -145,8 +145,7 @@ private:
 
     void serialize_unserialized_values()
     {
-        // FIXME: Stop inserting whitespace once we stop removing it during parsing.
-        m_reified_values.append(serialize_a_series_of_component_values(m_unserialized_values, InsertWhitespace::Yes));
+        m_reified_values.append(serialize_a_series_of_component_values(m_unserialized_values));
         m_unserialized_values.clear_with_capacity();
     }
 

commit a43b79d39590636f136974745c4cf315ba29df69

Tests: Import serialize-consecutive-tokens test
This had significant regressions after #6646, so let's import it to
track progress and prevent future regressions.

Tests/LibWeb/Text/input/wpt-import/css/css-syntax/serialize-consecutive-tokens.html
@@ -1,0 +1,137 @@
+<!doctype html>
+<title>Serialization of consecutive tokens.</title>
+<script src="../../resources/testharness.js"></script>
+<script src="../../resources/testharnessreport.js"></script>
+
+<meta name="author" title="Tab Atkins-Bittner">
+<link rel=help href="https://drafts.csswg.org/css-syntax/#serialization">
+<body>
+<!--
+    The serialization chapter provides a table listing all the combinations of consecutive tokens that will,
+    if naively serialized next to each other,
+    produce a different set of tokens when re-parsed.
+    The spec requires that a comment must be inserted between such tokens in the serialization,
+    to ensure that they round-trip correctly.
+-->
+
+<script>
+
+function testTokenPairs(t1, t2) {
+    const b = document.body;
+    test(()=>{
+        b.style.setProperty("--t1", t1);
+        b.style.setProperty("--t2", t2);
+        b.style.setProperty("--result", "var(--t1)var(--t2)");
+        const result = getComputedStyle(b).getPropertyValue("--result");
+        assert_equals(result.slice(0, t1.length), t1, `Result must start with ${t1}`);
+        assert_equals(result.slice(-t2.length), t2, `Result must end with ${t2}`);
+        assert_not_equals(result, t1+t2, `Result must have a comment between ${t1} and ${t2}`);
+    }, `Serialization of consecutive ${t1} and ${t2} tokens.`);
+}
+testTokenPairs("foo", "bar");
+testTokenPairs("foo", "bar()");
+testTokenPairs("foo", "url(bar)");
+testTokenPairs("foo", "-");
+testTokenPairs("foo", "123");
+testTokenPairs("foo", "123%");
+testTokenPairs("foo", "123em");
+testTokenPairs("foo", "-->");
+testTokenPairs("foo", "()");
+
+testTokenPairs("@foo", "bar");
+testTokenPairs("@foo", "bar()");
+testTokenPairs("@foo", "url(bar)");
+testTokenPairs("@foo", "-");
+testTokenPairs("@foo", "123");
+testTokenPairs("@foo", "123%");
+testTokenPairs("@foo", "123em");
+testTokenPairs("@foo", "-->");
+
+testTokenPairs("#foo", "bar");
+testTokenPairs("#foo", "bar()");
+testTokenPairs("#foo", "url(bar)");
+testTokenPairs("#foo", "-");
+testTokenPairs("#foo", "123");
+testTokenPairs("#foo", "123%");
+testTokenPairs("#foo", "123em");
+testTokenPairs("#foo", "-->");
+
+testTokenPairs("123foo", "bar");
+testTokenPairs("123foo", "bar()");
+testTokenPairs("123foo", "url(bar)");
+testTokenPairs("123foo", "-");
+testTokenPairs("123foo", "123");
+testTokenPairs("123foo", "123%");
+testTokenPairs("123foo", "123em");
+testTokenPairs("123foo", "-->");
+
+testTokenPairs("#", "bar");
+testTokenPairs("#", "bar()");
+testTokenPairs("#", "url(bar)");
+testTokenPairs("#", "-");
+testTokenPairs("#", "123");
+testTokenPairs("#", "123%");
+testTokenPairs("#", "123em");
+
+testTokenPairs("-", "bar");
+testTokenPairs("-", "bar()");
+testTokenPairs("-", "url(bar)");
+testTokenPairs("-", "-");
+testTokenPairs("-", "123");
+testTokenPairs("-", "123%");
+testTokenPairs("-", "123em");
+
+testTokenPairs("123", "bar");
+testTokenPairs("123", "bar()");
+testTokenPairs("123", "url(bar)");
+testTokenPairs("123", "123");
+testTokenPairs("123", "123%");
+testTokenPairs("123", "123em");
+testTokenPairs("123", "%");
+
+testTokenPairs("@", "bar");
+testTokenPairs("@", "bar()");
+testTokenPairs("@", "url(bar)");
+testTokenPairs("@", "-");
+
+testTokenPairs(".", "123");
+testTokenPairs(".", "123%");
+testTokenPairs(".", "123em");
+
+testTokenPairs("+", "123");
+testTokenPairs("+", "123%");
+testTokenPairs("+", "123em");
+
+testTokenPairs("/", "*");
+
+// Test that interior comments are preserved, but exterior ones are not.
+function testComments(text, t1, expected) {
+    const b = document.body;
+    test(()=>{
+        b.style.setProperty("--t1", t1);
+        b.style.setProperty("--result", text);
+        const result = getComputedStyle(b).getPropertyValue("--result");
+        assert_equals(result, expected);
+    }, `Comments are handled correctly when computing ${text} using t1:${t1}.`);
+}
+testComments("a/* comment */b", "", "a/* comment */b");
+testComments("a/* comment */var(--t1)", "b", "a/**/b");
+testComments("var(--t1)b", "a/* comment */", "a/**/b");
+
+// Test comments within quotes.
+testComments("var(--t1)b", "'a/* unfinished '", "'a/* unfinished 'b");
+testComments("var(--t1)b", "\"a/* unfinished \"", "\"a/* unfinished \"b");
+testComments("var(--t1)b", "'a \" '/* comment */", "'a \" 'b");
+
+test(()=>{
+    const b = document.body;
+    b.style.setProperty("--t1", "a");
+    b.style.setProperty("--t2", "b");
+    b.style.setProperty("--result", "var(--t1)var(--does-not-exist,)var(--t2)");
+    const result = getComputedStyle(b).getPropertyValue("--result");
+    assert_equals(result[0], "a", `Result must start with a`);
+    assert_equals(result[result.length - 1], "b", `Result must end with b`);
+    assert_not_equals(result, "ab", `Result must have a comment between a and b`);
+}, 'Empty fallback between tokens must not disturb comment insertion');
+
+</script>