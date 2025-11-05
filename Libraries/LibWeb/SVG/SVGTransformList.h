99912054033375cae4814c5a000aeaad3c055abf
Jelle Raaijmakers <jelle@ladybird.org>
2025-11-05 15:20:53 +0100

    LibWeb: Add SVGList<T> and use it for SVGTransformList
    The spec defines a generic list interfaces that we can reuse. Currently
    we only have SVGTransformList, but we will need this to add
    SVGNumberList as well.

--- Libraries/LibWeb/SVG/SVGTransformList.h
+++ Libraries/LibWeb/SVG/SVGTransformList.h
@@ -1,41 +1,35 @@
 /*
  * Copyright (c) 2024, MacDue <macdue@dueutil.tech>
+ * Copyright (c) 2025, Jelle Raaijmakers <jelle@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
-#include <AK/Vector.h>
-#include <LibWeb/Bindings/PlatformObject.h>
+#include <LibWeb/SVG/SVGList.h>
 #include <LibWeb/SVG/SVGTransform.h>
-#include <LibWeb/WebIDL/Types.h>
 
 namespace Web::SVG {
 
 // https://svgwg.org/svg2-draft/single-page.html#coords-InterfaceSVGTransformList
-class SVGTransformList final : public Bindings::PlatformObject {
+class SVGTransformList final
+    : public Bindings::PlatformObject
+    , public SVGList<GC::Ref<SVGTransform>> {
     WEB_PLATFORM_OBJECT(SVGTransformList, Bindings::PlatformObject);
     GC_DECLARE_ALLOCATOR(SVGTransformList);
 
 public:
-    [[nodiscard]] static GC::Ref<SVGTransformList> create(JS::Realm& realm);
-    virtual ~SVGTransformList() override;
-
-    WebIDL::UnsignedLong length();
-    WebIDL::UnsignedLong number_of_items();
-
-    WebIDL::ExceptionOr<GC::Ref<SVGTransform>> get_item(WebIDL::UnsignedLong index);
-
-    GC::Ref<SVGTransform> append_item(GC::Ref<SVGTransform> new_item);
+    [[nodiscard]] static GC::Ref<SVGTransformList> create(JS::Realm& realm, Vector<GC::Ref<SVGTransform>>, ReadOnlyList);
+    [[nodiscard]] static GC::Ref<SVGTransformList> create(JS::Realm& realm, ReadOnlyList);
+    virtual ~SVGTransformList() override = default;
 
 private:
-    SVGTransformList(JS::Realm& realm);
+    SVGTransformList(JS::Realm&, Vector<GC::Ref<SVGTransform>>, ReadOnlyList);
+    SVGTransformList(JS::Realm&, ReadOnlyList);
 
-    virtual void initialize(JS::Realm& realm) override;
-    virtual void visit_edges(Cell::Visitor& visitor) override;
-
-    Vector<GC::Ref<SVGTransform>> m_transforms;
+    virtual void initialize(JS::Realm&) override;
+    virtual void visit_edges(Visitor&) override;
 };
 
 }
