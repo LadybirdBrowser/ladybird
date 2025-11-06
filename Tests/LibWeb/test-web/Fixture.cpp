bdd205dce29b2ae84e264c1892bc81ce6846bd57
ayeteadoe <ayeteadoe@gmail.com>
2025-11-05 20:46:08 -0800

    Tests/LibWeb: Implement HttpEchoServerFixture on Windows
    We redirect the stdout of the python child process to the write handle
    of an anonymous pipe and we redirect the stderr of the python process
    to the file handle of the opened log file that was specified. After the
    child process is spawned, we read from the anonymous pipes read handle
    to get the server port from stdout and we keep the file handle for
    stderr open until the fixture is being teared down.

--- Tests/LibWeb/test-web/Fixture.cpp
+++ Tests/LibWeb/test-web/Fixture.cpp
@@ -13,6 +13,10 @@
 #include <LibCore/StandardPaths.h>
 #include <LibCore/System.h>
 
+#if defined(AK_OS_WINDOWS)
+#    include <AK/Windows.h>
+#endif
+
 namespace TestWeb {
 
 static ByteString s_fixtures_path;
@@ -45,21 +49,10 @@ public:
 private:
     ByteString m_script_path { "http-test-server.py" };
     Optional<Core::Process> m_process;
-};
-
 #if defined(AK_OS_WINDOWS)
-
-ErrorOr<void> HttpEchoServerFixture::setup(WebView::WebContentOptions&)
-{
-    VERIFY(0 && "HttpEchoServerFixture::setup is not implemented");
-}
-
-void HttpEchoServerFixture::teardown_impl()
-{
-    VERIFY(0 && "HttpEchoServerFixture::teardown_impl is not implemented");
-}
-
-#else
+    HANDLE m_stderr_log_handle { nullptr };
+#endif
+};
 
 ErrorOr<void> HttpEchoServerFixture::setup(WebView::WebContentOptions& web_content_options)
 {
@@ -68,23 +61,54 @@ ErrorOr<void> HttpEchoServerFixture::setup(WebView::WebContentOptions& web_conte
 
     // FIXME: Pick a more reasonable log path that is more observable
     auto const log_path = LexicalPath::join(Core::StandardPaths::tempfile_directory(), "http-test-server.log"sv).string();
+    auto const stderr_log_path = ByteString::formatted("{}.stderr", log_path);
 
-    auto stdout_fds = TRY(Core::System::pipe2(0));
-
-    auto const process_options = Core::ProcessSpawnOptions {
+    auto process_options = Core::ProcessSpawnOptions {
         .executable = Application::the().python_executable_path,
         .search_for_executable_in_path = true,
         .arguments = arguments,
-        .file_actions = {
-            Core::FileAction::OpenFile { ByteString::formatted("{}.stderr", log_path), Core::File::OpenMode::Write, STDERR_FILENO },
-            Core::FileAction::DupFd { stdout_fds[1], STDOUT_FILENO } }
     };
+
+    int stdout_read_fd = -1;
+
+#if defined(AK_OS_WINDOWS)
+    HANDLE stdout_read_handle { nullptr };
+    HANDLE stdout_write_handle { nullptr };
+    if (!CreatePipe(&stdout_read_handle, &stdout_write_handle, nullptr, 0))
+        return Error::from_windows_error();
+    if (!SetHandleInformation(stdout_write_handle, HANDLE_FLAG_INHERIT, HANDLE_FLAG_INHERIT))
+        return Error::from_windows_error();
+
+    m_stderr_log_handle = CreateFile(stderr_log_path.characters(), GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+    if (m_stderr_log_handle == INVALID_HANDLE_VALUE)
+        return Error::from_windows_error();
+    if (!SetHandleInformation(m_stderr_log_handle, HANDLE_FLAG_INHERIT, HANDLE_FLAG_INHERIT))
+        return Error::from_windows_error();
+
+    process_options.use_std_handles_startup_info = Core::StartupInfo::UseStdHandles { .stderr_handle = m_stderr_log_handle, .stdout_handle = stdout_write_handle, .stdin_handle = nullptr };
+    process_options.create_new_process_group = true;
+
+    stdout_read_fd = to_fd(stdout_read_handle);
+#else
+    auto stdout_fds = TRY(Core::System::pipe2(0));
+
+    process_options.file_actions = {
+        Core::FileAction::OpenFile { stderr_log_path, Core::File::OpenMode::Write, STDERR_FILENO },
+        Core::FileAction::DupFd { stdout_fds[1], STDOUT_FILENO }
+    };
+
+    stdout_read_fd = stdout_fds[0];
+#endif
 
     m_process = TRY(Core::Process::spawn(process_options));
 
+#if defined(AK_OS_WINDOWS)
+    CloseHandle(stdout_write_handle);
+#else
     TRY(Core::System::close(stdout_fds[1]));
+#endif
 
-    auto const stdout_file = MUST(Core::File::adopt_fd(stdout_fds[0], Core::File::OpenMode::Read));
+    auto const stdout_file = MUST(Core::File::adopt_fd(stdout_read_fd, Core::File::OpenMode::Read));
 
     auto buffer = MUST(ByteBuffer::create_uninitialized(5));
     TRY(stdout_file->read_some(buffer));
@@ -96,6 +120,13 @@ ErrorOr<void> HttpEchoServerFixture::setup(WebView::WebContentOptions& web_conte
     else
         warnln("Failed to read echo server port from buffer: '{}'", raw_output);
 
+#if defined(AK_OS_WINDOWS)
+    // Currently our File/IPC/Event Loop infrastructure on Windows assumes we don't use pipes, only regular file handles
+    // and WinSock2-based socket fds. So currently if we let Core::File try to close the read FD we hit fail an assertion as
+    // System::close() thinks we're a socket. We will just manually close the read FD here as the rest of the codebase is not
+    // planning on using Windows pipes
+    CloseHandle(to_handle(stdout_file->leak_fd()));
+#endif
     return {};
 }
 
@@ -105,6 +136,9 @@ void HttpEchoServerFixture::teardown_impl()
 
     auto script_path = LexicalPath::join(s_fixtures_path, m_script_path);
 
+#if defined(AK_OS_WINDOWS)
+    CloseHandle(m_stderr_log_handle);
+#endif
     if (auto kill_or_error = Core::System::kill(m_process->pid(), SIGINT); kill_or_error.is_error()) {
         if (kill_or_error.error().code() != ESRCH) {
             warnln("Failed to kill HTTP echo server, error: {}", kill_or_error.error());
@@ -115,8 +149,6 @@ void HttpEchoServerFixture::teardown_impl()
 
     m_process = {};
 }
-
-#endif
 
 void Fixture::initialize_fixtures()
 {
