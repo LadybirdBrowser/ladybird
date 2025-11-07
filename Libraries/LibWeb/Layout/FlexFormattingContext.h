57f4b89cabe388a0402c68a27700bab71ddb96b8
Andreas Kling <andreas@ladybird.org>
2025-11-07 13:34:48 +0100

    LibWeb: Mark flex item main size definite if resolved from aspect-ratio
    This matches the behavior of other engines and makes the cards on the
    Apple App Store appear in the narrower layouts.

--- Libraries/LibWeb/Layout/FlexFormattingContext.h
+++ Libraries/LibWeb/Layout/FlexFormattingContext.h
@@ -58,6 +58,7 @@ private:
         LayoutState::UsedValues& used_values;
         Optional<CSS::FlexBasis> used_flex_basis {};
         bool used_flex_basis_is_definite { false };
+        bool main_size_was_resolved_from_aspect_ratio { false };
         CSSPixels flex_base_size { 0 };
         CSSPixels hypothetical_main_size { 0 };
         CSSPixels hypothetical_cross_size { 0 };
