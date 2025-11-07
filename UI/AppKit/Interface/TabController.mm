0cc1ab92f6215dc62e89281a06176a0b84a874b0
Luke Wilde <luke@ladybird.org>
2025-11-07 15:48:07 +0000

    UI/AppKit: Request closure instead of always force closing tabs

--- UI/AppKit/Interface/TabController.mm
+++ UI/AppKit/Interface/TabController.mm
@@ -55,6 +55,8 @@ static NSString* const TOOLBAR_TAB_OVERVIEW_IDENTIFIER = @"ToolbarTabOverviewIde
     OwnPtr<WebView::Autocomplete> m_autocomplete;
 }
 
+@property (nonatomic, assign) BOOL already_requested_close;
+
 @property (nonatomic, strong) Tab* parent;
 
 @property (nonatomic, strong) NSToolbar* toolbar;
@@ -399,6 +401,21 @@ static NSString* const TOOLBAR_TAB_OVERVIEW_IDENTIFIER = @"ToolbarTabOverviewIde
 {
     auto* delegate = (ApplicationDelegate*)[NSApp delegate];
     [delegate setActiveTab:[self tab]];
+}
+
+- (BOOL)windowShouldClose:(NSWindow*)sender
+{
+    // Prevent closing on first request so WebContent can cleanly shutdown (e.g. asking if the user is sure they want
+    // to leave, closing WebSocket connections, etc.)
+    if (!self.already_requested_close) {
+        self.already_requested_close = true;
+        [[[self tab] web_view] requestClose];
+        return false;
+    }
+
+    // If the user has already requested a close, then respect the user's request and just close the tab.
+    // For example, the WebContent process may not be responding.
+    return true;
 }
 
 - (void)windowWillClose:(NSNotification*)notification
