eda46962dc2483b78c9edd56cf7512d6905f89f2
Saksham Mittal <git@gotlou.com>
2025-11-10 16:51:18 +0530

    LibWeb: Split structs into TextDecoderCommon

---
+++ Libraries/LibWeb/Encoding/TextDecoderCommon.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024, Tim Flynn <trflynn89@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibWeb/Encoding/TextDecoderCommon.h>
+
+namespace Web::Encoding {
+
+TextDecoderCommonMixin::TextDecoderCommonMixin() = default;
+TextDecoderCommonMixin::~TextDecoderCommonMixin() = default;
+
+}
