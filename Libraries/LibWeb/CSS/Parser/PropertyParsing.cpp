c26e905b9446bd7f33783337c55e91e2fc8411ac
Callum Law <callumlaw1709@outlook.com>
2025-11-20 22:43:11 +1300

    LibWeb: Parse the `scroll-timeline-name` CSS property

--- Libraries/LibWeb/CSS/Parser/PropertyParsing.cpp
+++ Libraries/LibWeb/CSS/Parser/PropertyParsing.cpp
@@ -745,6 +745,8 @@ Parser::ParseErrorOr<NonnullRefPtr<StyleValue const>> Parser::parse_css_value(Pr
         return parse_all_as(tokens, [this](auto& tokens) { return parse_translate_value(tokens); });
     case PropertyID::Scale:
         return parse_all_as(tokens, [this](auto& tokens) { return parse_scale_value(tokens); });
+    case PropertyID::ScrollTimelineName:
+        return parse_all_as(tokens, [this, property_id](auto& tokens) { return parse_simple_comma_separated_value_list(property_id, tokens); });
     case PropertyID::WhiteSpace:
         return parse_all_as(tokens, [this](auto& tokens) { return parse_white_space_shorthand(tokens); });
     case PropertyID::WhiteSpaceTrim:
