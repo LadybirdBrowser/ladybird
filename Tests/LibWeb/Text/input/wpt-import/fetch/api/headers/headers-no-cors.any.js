LadybirdBrowser/ladybird

commit f675cfe90f5bdf50c670ee3f2ad5d07c08659539

LibWeb: Store HTTP methods and headers as ByteString
The spec declares these as a byte sequence, which we then implemented as
a ByteBuffer. This has become pretty awkward to deal with, as evidenced
by the plethora of `MUST(ByteBuffer::copy(...))` and `.bytes()` calls
everywhere inside Fetch. We would then treat the bytes as a string
anyways by wrapping them in StringView everywhere.

We now store these as a ByteString. This is more comfortable to deal
with, and we no longer need to continually copy underlying storage (as
ByteString is ref-counted).

This work is largely preparatory for an upcoming HTTP header refactor.

Libraries/LibWeb/Fetch/Headers.cpp
@@ -57,20 +57,19 @@ void Headers::visit_edges(JS::Cell::Visitor& visitor)
 WebIDL::ExceptionOr<void> Headers::append(String const& name_string, String const& value_string)
 {
     // The append(name, value) method steps are to append (name, value) to this.
-    auto header = Infrastructure::Header::from_string_pair(name_string, value_string);
+    auto header = Infrastructure::Header::isomorphic_encode(name_string, value_string);
     TRY(append(move(header)));
     return {};
 }
 
 // https://fetch.spec.whatwg.org/#dom-headers-delete
-WebIDL::ExceptionOr<void> Headers::delete_(String const& name_string)
+WebIDL::ExceptionOr<void> Headers::delete_(String const& name)
 {
     // The delete(name) method steps are:
-    auto name = name_string.bytes();
 
     // 1. If validating (name, ``) for headers returns false, then return.
     // NOTE: Passing a dummy header value ought not to have any negative repercussions.
-    auto header = Infrastructure::Header::from_string_pair(name, ""sv);
+    auto header = Infrastructure::Header::isomorphic_encode(name, ""sv);
     if (!TRY(validate(header)))
         return {};
 
@@ -93,10 +92,9 @@ WebIDL::ExceptionOr<void> Headers::delete_(String const& name_string)
 }
 
 // https://fetch.spec.whatwg.org/#dom-headers-get
-WebIDL::ExceptionOr<Optional<String>> Headers::get(String const& name_string)
+WebIDL::ExceptionOr<Optional<String>> Headers::get(String const& name)
 {
     // The get(name) method steps are:
-    auto name = name_string.bytes();
 
     // 1. If name is not a header name, then throw a TypeError.
     if (!Infrastructure::is_header_name(name))
@@ -114,23 +112,22 @@ Vector<String> Headers::get_set_cookie()
     auto values = Vector<String> {};
 
     // 1. If this’s header list does not contain `Set-Cookie`, then return « ».
-    if (!m_header_list->contains("Set-Cookie"sv.bytes()))
+    if (!m_header_list->contains("Set-Cookie"sv))
         return values;
 
     // 2. Return the values of all headers in this’s header list whose name is a byte-case-insensitive match for
     //    `Set-Cookie`, in order.
     for (auto const& header : *m_header_list) {
-        if (StringView { header.name }.equals_ignoring_ascii_case("Set-Cookie"sv))
+        if (header.name.equals_ignoring_ascii_case("Set-Cookie"sv))
             values.append(Infra::isomorphic_decode(header.value));
     }
     return values;
 }
 
 // https://fetch.spec.whatwg.org/#dom-headers-has
-WebIDL::ExceptionOr<bool> Headers::has(String const& name_string)
+WebIDL::ExceptionOr<bool> Headers::has(String const& name)
 {
     // The has(name) method steps are:
-    auto name = name_string.bytes();
 
     // 1. If name is not a header name, then throw a TypeError.
     if (!Infrastructure::is_header_name(name))
@@ -141,16 +138,14 @@ WebIDL::ExceptionOr<bool> Headers::has(String const& name_string)
 }
 
 // https://fetch.spec.whatwg.org/#dom-headers-set
-WebIDL::ExceptionOr<void> Headers::set(String const& name_string, String const& value_string)
+WebIDL::ExceptionOr<void> Headers::set(String const& name, String const& value)
 {
     // The set(name, value) method steps are:
-    auto name = name_string.bytes();
-    auto value = value_string.bytes();
 
     // 1. Normalize value.
     auto normalized_value = Infrastructure::normalize_header_value(value);
 
-    auto header = Infrastructure::Header::from_string_pair(name, normalized_value);
+    auto header = Infrastructure::Header::isomorphic_encode(name, normalized_value);
 
     // 2. If validating (name, value) for headers returns false, then return.
     if (!TRY(validate(header)))
@@ -252,17 +247,15 @@ WebIDL::ExceptionOr<void> Headers::append(Infrastructure::Header header)
 
         // 2. If temporaryValue is null, then set temporaryValue to value.
         if (!temporary_value.has_value()) {
-            temporary_value = MUST(ByteBuffer::copy(value));
+            temporary_value = move(value);
         }
         // 3. Otherwise, set temporaryValue to temporaryValue, followed by 0x2C 0x20, followed by value.
         else {
-            temporary_value->append(0x2c);
-            temporary_value->append(0x20);
-            temporary_value->append(value);
+            temporary_value = ByteString::formatted("{}, {}", *temporary_value, value);
         }
 
         auto temporary_header = Infrastructure::Header {
-            .name = MUST(ByteBuffer::copy(name)),
+            .name = move(name),
             .value = temporary_value.release_value(),
         };
 
@@ -294,7 +287,7 @@ WebIDL::ExceptionOr<void> Headers::fill(HeadersInit const& object)
                     return WebIDL::SimpleException { WebIDL::SimpleExceptionType::TypeError, "Array must contain header key/value pair"sv };
 
                 // 2. Append (header[0], header[1]) to headers.
-                auto header = Infrastructure::Header::from_string_pair(entry[0], entry[1]);
+                auto header = Infrastructure::Header::isomorphic_encode(entry[0], entry[1]);
                 TRY(append(move(header)));
             }
             return {};
@@ -302,7 +295,7 @@ WebIDL::ExceptionOr<void> Headers::fill(HeadersInit const& object)
         // 2. Otherwise, object is a record, then for each key → value of object, append (key, value) to headers.
         [&](OrderedHashMap<String, String> const& object) -> WebIDL::ExceptionOr<void> {
             for (auto const& entry : object) {
-                auto header = Infrastructure::Header::from_string_pair(entry.key, entry.value);
+                auto header = Infrastructure::Header::isomorphic_encode(entry.key, entry.value);
                 TRY(append(move(header)));
             }
             return {};
@@ -321,7 +314,7 @@ void Headers::remove_privileged_no_cors_request_headers()
     // 1. For each headerName of privileged no-CORS request-header names:
     for (auto const& header_name : privileged_no_cors_request_header_names) {
         // 1. Delete headerName from headers’s header list.
-        m_header_list->delete_(header_name.bytes());
+        m_header_list->delete_(header_name);
     }
 }
 

Libraries/LibWeb/ContentSecurityPolicy/Policy.cpp
@@ -21,7 +21,7 @@ namespace Web::ContentSecurityPolicy {
 GC_DEFINE_ALLOCATOR(Policy);
 
 // https://w3c.github.io/webappsec-csp/#abstract-opdef-parse-a-serialized-csp
-GC::Ref<Policy> Policy::parse_a_serialized_csp(GC::Heap& heap, Variant<ByteBuffer, String> serialized, Source source, Disposition disposition)
+GC::Ref<Policy> Policy::parse_a_serialized_csp(GC::Heap& heap, Variant<ByteString, String> serialized, Source source, Disposition disposition)
 {
     // To parse a serialized CSP, given a byte sequence or string serialized, a source source, and a disposition disposition,
     // execute the following steps.
@@ -31,7 +31,7 @@ GC::Ref<Policy> Policy::parse_a_serialized_csp(GC::Heap& heap, Variant<ByteBuffe
     // 1. If serialized is a byte sequence, then set serialized to be the result of isomorphic decoding serialized.
     auto serialized_string = serialized.has<String>()
         ? serialized.get<String>()
-        : Infra::isomorphic_decode(serialized.get<ByteBuffer>());
+        : Infra::isomorphic_decode(serialized.get<ByteString>());
 
     // 2. Let policy be a new policy with an empty directive set, a source of source, and a disposition of disposition.
     auto policy = heap.allocate<Policy>();
@@ -101,9 +101,9 @@ GC::Ref<PolicyList> Policy::parse_a_responses_content_security_policies(GC::Heap
 
     // 2. For each token returned by extracting header list values given Content-Security-Policy and response’s header
     //    list:
-    auto enforce_policy_tokens_or_failure = response->header_list()->extract_header_list_values("Content-Security-Policy"sv.bytes());
+    auto enforce_policy_tokens_or_failure = response->header_list()->extract_header_list_values("Content-Security-Policy"sv);
 
-    if (auto const* enforce_policy_tokens = enforce_policy_tokens_or_failure.get_pointer<Vector<ByteBuffer>>()) {
+    if (auto const* enforce_policy_tokens = enforce_policy_tokens_or_failure.get_pointer<Vector<ByteString>>()) {
         for (auto const& enforce_policy_token : *enforce_policy_tokens) {
             // 1. Let policy be the result of parsing token, with a source of "header", and a disposition of "enforce".
             auto policy = parse_a_serialized_csp(heap, enforce_policy_token, Policy::Source::Header, Policy::Disposition::Enforce);
@@ -116,9 +116,9 @@ GC::Ref<PolicyList> Policy::parse_a_responses_content_security_policies(GC::Heap
 
     // 3. For each token returned by extracting header list values given Content-Security-Policy-Report-Only and
     //    response’s header list:
-    auto report_policy_tokens_or_failure = response->header_list()->extract_header_list_values("Content-Security-Policy-Report-Only"sv.bytes());
+    auto report_policy_tokens_or_failure = response->header_list()->extract_header_list_values("Content-Security-Policy-Report-Only"sv);
 
-    if (auto const* report_policy_tokens = enforce_policy_tokens_or_failure.get_pointer<Vector<ByteBuffer>>()) {
+    if (auto const* report_policy_tokens = enforce_policy_tokens_or_failure.get_pointer<Vector<ByteString>>()) {
         for (auto const& report_policy_token : *report_policy_tokens) {
             // 1. Let policy be the result of parsing token, with a source of "header", and a disposition of "report".
             auto policy = parse_a_serialized_csp(heap, report_policy_token, Policy::Source::Header, Policy::Disposition::Report);

Libraries/LibWeb/ContentSecurityPolicy/Policy.h
@@ -38,7 +38,7 @@ public:
 
     ~Policy() = default;
 
-    [[nodiscard]] static GC::Ref<Policy> parse_a_serialized_csp(GC::Heap&, Variant<ByteBuffer, String> serialized, Source source, Disposition disposition);
+    [[nodiscard]] static GC::Ref<Policy> parse_a_serialized_csp(GC::Heap&, Variant<ByteString, String> serialized, Source source, Disposition disposition);
     [[nodiscard]] static GC::Ref<PolicyList> parse_a_responses_content_security_policies(GC::Heap&, GC::Ref<Fetch::Infrastructure::Response const> response);
     [[nodiscard]] static GC::Ref<Policy> create_from_serialized_policy(GC::Heap&, SerializedPolicy const&);
 

Libraries/LibWeb/ContentSecurityPolicy/Violation.cpp
@@ -29,7 +29,7 @@ GC_DEFINE_ALLOCATOR(Violation);
 Violation::Violation(GC::Ptr<JS::Object> global_object, GC::Ref<Policy const> policy, String directive)
     : m_global_object(global_object)
     , m_policy(policy)
-    , m_effective_directive(directive)
+    , m_effective_directive(move(directive))
 {
 }
 
@@ -38,7 +38,7 @@ GC::Ref<Violation> Violation::create_a_violation_object_for_global_policy_and_di
 {
     // 1. Let violation be a new violation whose global object is global, policy is policy, effective directive is
     //    directive, and resource is null.
-    auto violation = realm.create<Violation>(global_object, policy, directive);
+    auto violation = realm.create<Violation>(global_object, policy, move(directive));
 
     // FIXME: 2. If the user agent is currently executing script, and can extract a source file’s URL, line number,
     //           and column number from the global, set violation’s source file, line number, and column number
@@ -406,7 +406,7 @@ void Violation::report_a_violation(JS::Realm& realm)
 
                         // method
                         //    "POST"
-                        request->set_method(MUST(ByteBuffer::copy("POST"sv.bytes())));
+                        request->set_method("POST"sv);
 
                         // url
                         //    violation’s url
@@ -448,8 +448,7 @@ void Violation::report_a_violation(JS::Realm& realm)
                         //    A header list containing a single header whose name is "Content-Type", and value is
                         //    "application/csp-report"
                         auto header_list = Fetch::Infrastructure::HeaderList::create(vm);
-                        auto content_type_header = Fetch::Infrastructure::Header::from_string_pair("Content-Type"sv, "application/csp-report"sv);
-                        header_list->append(move(content_type_header));
+                        header_list->append({ "Content-Type"sv, "application/csp-report"sv });
                         request->set_header_list(header_list);
 
                         // body

Libraries/LibWeb/DOM/Document.cpp
@@ -385,15 +385,15 @@ WebIDL::ExceptionOr<GC::Ref<Document>> Document::create_and_initialize(Type type
     document->m_window = window;
 
     // NOTE: Non-standard: Pull out the Last-Modified header for use in the lastModified property.
-    if (auto maybe_last_modified = navigation_params.response->header_list()->get("Last-Modified"sv.bytes()); maybe_last_modified.has_value()) {
+    if (auto maybe_last_modified = navigation_params.response->header_list()->get("Last-Modified"sv); maybe_last_modified.has_value()) {
         // rfc9110, 8.8.2: The Last-Modified header field must be in GMT time zone.
         // document->m_last_modified is in local time zone.
         document->m_last_modified = AK::UnixDateTime::parse("%a, %d %b %Y %H:%M:%S GMT"sv, maybe_last_modified.value(), true);
     }
 
     // NOTE: Non-standard: Pull out the Content-Language header to determine the document's language.
-    if (auto maybe_http_content_language = navigation_params.response->header_list()->get("Content-Language"sv.bytes()); maybe_http_content_language.has_value()) {
-        if (auto maybe_content_language = String::from_utf8(maybe_http_content_language.value()); !maybe_content_language.is_error())
+    if (auto maybe_http_content_language = navigation_params.response->header_list()->get("Content-Language"sv); maybe_http_content_language.has_value()) {
+        if (auto maybe_content_language = String::from_byte_string(maybe_http_content_language.value()); !maybe_content_language.is_error())
             document->m_http_content_language = maybe_content_language.release_value();
     }
 
@@ -423,7 +423,7 @@ WebIDL::ExceptionOr<GC::Ref<Document>> Document::create_and_initialize(Type type
     //            navigationParams's response's service worker timing info.
 
     // 15. If navigationParams's response has a `Refresh` header, then:
-    if (auto maybe_refresh = navigation_params.response->header_list()->get("Refresh"sv.bytes()); maybe_refresh.has_value()) {
+    if (auto maybe_refresh = navigation_params.response->header_list()->get("Refresh"sv); maybe_refresh.has_value()) {
         // 1. Let value be the isomorphic decoding of the value of the header.
         auto value = Infra::isomorphic_decode(maybe_refresh.value());
 

Libraries/LibWeb/Fetch/BodyInit.cpp
@@ -72,7 +72,7 @@ WebIDL::ExceptionOr<Infrastructure::BodyWithType> extract_body(JS::Realm& realm,
     Optional<u64> length {};
 
     // 9. Let type be null.
-    Optional<ByteBuffer> type {};
+    Optional<ByteString> type {};
 
     // 10. Switch on object.
     TRY(object.visit(
@@ -83,7 +83,7 @@ WebIDL::ExceptionOr<Infrastructure::BodyWithType> extract_body(JS::Realm& realm,
             length = blob->size();
             // If object’s type attribute is not the empty byte sequence, set type to its value.
             if (!blob->type().is_empty())
-                type = MUST(ByteBuffer::copy(blob->type().bytes()));
+                type = blob->type().to_byte_string();
             return {};
         },
         [&](ReadonlyBytes bytes) -> WebIDL::ExceptionOr<void> {
@@ -103,8 +103,7 @@ WebIDL::ExceptionOr<Infrastructure::BodyWithType> extract_body(JS::Realm& realm,
             source = serialized_form_data.serialized_data;
             // FIXME: Set length to unclear, see html/6424 for improving this.
             // Set type to `multipart/form-data; boundary=`, followed by the multipart/form-data boundary string generated by the multipart/form-data encoding algorithm.
-            auto type_string = MUST(String::formatted("multipart/form-data; boundary={}", serialized_form_data.boundary));
-            type = MUST(ByteBuffer::copy(type_string.bytes()));
+            type = ByteString::formatted("multipart/form-data; boundary={}", serialized_form_data.boundary);
             return {};
         },
         [&](GC::Root<DOMURL::URLSearchParams> const& url_search_params) -> WebIDL::ExceptionOr<void> {
@@ -112,7 +111,7 @@ WebIDL::ExceptionOr<Infrastructure::BodyWithType> extract_body(JS::Realm& realm,
             auto search_params_string = url_search_params->to_string();
             source = MUST(ByteBuffer::copy(search_params_string.bytes()));
             // Set type to `application/x-www-form-urlencoded;charset=UTF-8`.
-            type = MUST(ByteBuffer::copy("application/x-www-form-urlencoded;charset=UTF-8"sv.bytes()));
+            type = "application/x-www-form-urlencoded;charset=UTF-8"sv;
             return {};
         },
         [&](String const& scalar_value_string) -> WebIDL::ExceptionOr<void> {
@@ -120,7 +119,7 @@ WebIDL::ExceptionOr<Infrastructure::BodyWithType> extract_body(JS::Realm& realm,
             // Set source to the UTF-8 encoding of object.
             source = MUST(ByteBuffer::copy(scalar_value_string.bytes()));
             // Set type to `text/plain;charset=UTF-8`.
-            type = MUST(ByteBuffer::copy("text/plain;charset=UTF-8"sv.bytes()));
+            type = "text/plain;charset=UTF-8"sv;
             return {};
         },
         [&](GC::Root<Streams::ReadableStream> const& stream) -> WebIDL::ExceptionOr<void> {
@@ -169,7 +168,7 @@ WebIDL::ExceptionOr<Infrastructure::BodyWithType> extract_body(JS::Realm& realm,
     auto body = Infrastructure::Body::create(vm, *stream, move(source), move(length));
 
     // 14. Return (body, type).
-    return Infrastructure::BodyWithType { .body = move(body), .type = move(type) };
+    return Infrastructure::BodyWithType { .body = body, .type = move(type) };
 }
 
 }

Libraries/LibWeb/Fetch/Fetching/Checks.cpp
@@ -15,7 +15,7 @@ namespace Web::Fetch::Fetching {
 bool cors_check(Infrastructure::Request const& request, Infrastructure::Response const& response)
 {
     // 1. Let origin be the result of getting `Access-Control-Allow-Origin` from response’s header list.
-    auto origin = response.header_list()->get("Access-Control-Allow-Origin"sv.bytes());
+    auto origin = response.header_list()->get("Access-Control-Allow-Origin"sv);
 
     // 2. If origin is null, then return failure.
     // NOTE: Null is not `null`.
@@ -23,7 +23,7 @@ bool cors_check(Infrastructure::Request const& request, Infrastructure::Response
         return false;
 
     // 3. If request’s credentials mode is not "include" and origin is `*`, then return success.
-    if (request.credentials_mode() != Infrastructure::Request::CredentialsMode::Include && origin->span() == "*"sv.bytes())
+    if (request.credentials_mode() != Infrastructure::Request::CredentialsMode::Include && *origin == "*"sv)
         return true;
 
     // 4. If the result of byte-serializing a request origin with request is not origin, then return failure.
@@ -35,10 +35,10 @@ bool cors_check(Infrastructure::Request const& request, Infrastructure::Response
         return true;
 
     // 6. Let credentials be the result of getting `Access-Control-Allow-Credentials` from response’s header list.
-    auto credentials = response.header_list()->get("Access-Control-Allow-Credentials"sv.bytes());
+    auto credentials = response.header_list()->get("Access-Control-Allow-Credentials"sv);
 
     // 7. If credentials is `true`, then return success.
-    if (credentials.has_value() && credentials->span() == "true"sv.bytes())
+    if (credentials == "true"sv)
         return true;
 
     // 8. Return failure.
@@ -53,7 +53,7 @@ bool tao_check(Infrastructure::Request const& request, Infrastructure::Response 
         return false;
 
     // 2. Let values be the result of getting, decoding, and splitting `Timing-Allow-Origin` from response’s header list.
-    auto values = response.header_list()->get_decode_and_split("Timing-Allow-Origin"sv.bytes());
+    auto values = response.header_list()->get_decode_and_split("Timing-Allow-Origin"sv);
 
     // 3. If values contains "*", then return success.
     if (values.has_value() && values->contains_slow("*"sv))

commit 74f47b872392694a1be908b61de9f9c106d61603

LibWeb: Do not move header name/values that are re-used
Fixes a regression from commit:
f675cfe90f5bdf50c670ee3f2ad5d07c08659539

Libraries/LibWeb/Fetch/Headers.cpp
@@ -247,7 +247,7 @@ WebIDL::ExceptionOr<void> Headers::append(Infrastructure::Header header)
 
         // 2. If temporaryValue is null, then set temporaryValue to value.
         if (!temporary_value.has_value()) {
-            temporary_value = move(value);
+            temporary_value = value;
         }
         // 3. Otherwise, set temporaryValue to temporaryValue, followed by 0x2C 0x20, followed by value.
         else {
@@ -255,7 +255,7 @@ WebIDL::ExceptionOr<void> Headers::append(Infrastructure::Header header)
         }
 
         auto temporary_header = Infrastructure::Header {
-            .name = move(name),
+            .name = name,
             .value = temporary_value.release_value(),
         };
 

Tests/LibWeb/Text/input/wpt-import/fetch/api/headers/headers-no-cors.any.html
@@ -1,0 +1,15 @@
+<!doctype html>
+<meta charset=utf-8>
+
+<script>
+self.GLOBAL = {
+  isWindow: function() { return true; },
+  isWorker: function() { return false; },
+  isShadowRealm: function() { return false; },
+};
+</script>
+<script src="../../../resources/testharness.js"></script>
+<script src="../../../resources/testharnessreport.js"></script>
+
+<div id=log></div>
+<script src="../../../fetch/api/headers/headers-no-cors.any.js"></script>

Tests/LibWeb/Text/input/wpt-import/fetch/api/headers/headers-no-cors.any.js
@@ -1,0 +1,59 @@
+// META: global=window,worker
+
+"use strict";
+
+promise_test(() => fetch("../cors/resources/not-cors-safelisted.json").then(res => res.json().then(runTests)), "Loading data…");
+
+const longValue = "s".repeat(127);
+
+[
+  {
+    "headers": ["accept", "accept-language", "content-language"],
+    "values": [longValue, "", longValue]
+  },
+  {
+    "headers": ["accept", "accept-language", "content-language"],
+    "values": ["", longValue]
+  },
+  {
+    "headers": ["content-type"],
+    "values": ["text/plain;" + "s".repeat(116), "text/plain"]
+  }
+].forEach(testItem => {
+  testItem.headers.forEach(header => {
+    test(() => {
+      const noCorsHeaders = new Request("about:blank", { mode: "no-cors" }).headers;
+      testItem.values.forEach((value) => {
+        noCorsHeaders.append(header, value);
+        assert_equals(noCorsHeaders.get(header), testItem.values[0], '1');
+      });
+      noCorsHeaders.set(header, testItem.values.join(", "));
+      assert_equals(noCorsHeaders.get(header), testItem.values[0], '2');
+      noCorsHeaders.delete(header);
+      assert_false(noCorsHeaders.has(header));
+    }, "\"no-cors\" Headers object cannot have " + header + " set to " + testItem.values.join(", "));
+  });
+});
+
+function runTests(testArray) {
+  testArray = testArray.concat([
+    ["dpr", "2"],
+    ["rtt", "1.0"],
+    ["downlink", "-1.0"],
+    ["ect", "6g"],
+    ["save-data", "on"],
+    ["viewport-width", "100"],
+    ["width", "100"],
+    ["unknown", "doesitmatter"]
+  ]);
+  testArray.forEach(testItem => {
+    const [headerName, headerValue] = testItem;
+    test(() => {
+      const noCorsHeaders = new Request("about:blank", { mode: "no-cors" }).headers;
+      noCorsHeaders.append(headerName, headerValue);
+      assert_false(noCorsHeaders.has(headerName));
+      noCorsHeaders.set(headerName, headerValue);
+      assert_false(noCorsHeaders.has(headerName));
+    }, "\"no-cors\" Headers object cannot have " + headerName + "/" + headerValue + " as header");
+  });
+}