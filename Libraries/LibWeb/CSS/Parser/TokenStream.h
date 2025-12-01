a8d0be4382ca69cf252d7c3bfba09e8a5a103eff
Sam Atkins <sam@ladybird.org>
2025-12-01 14:13:45 +0000

    LibWeb/CSS: Expose consumed tokens from TokenStream
    In some situations we want to examine a sequence of tokens that have
    been already consumed from a TokenStream. These methods let us do so:
    - current_index() provides the current internal token index
    - tokens_since() provides a span from the given index to the current one

--- Libraries/LibWeb/CSS/Parser/TokenStream.h
+++ Libraries/LibWeb/CSS/Parser/TokenStream.h
@@ -177,6 +177,15 @@ public:
         return 0;
     }
 
+    size_t current_index() const { return m_index; }
+
+    ReadonlySpan<T> tokens_since(size_t start) const
+    {
+        if (start > m_index)
+            return {};
+        return m_tokens.slice(start, m_index - start);
+    }
+
     void dump_all_tokens()
     {
         dbgln("Dumping all tokens:");
