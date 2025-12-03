f5549291c5c202da43d8c2e9f8d67effebbca9d8
R-Goc <ryszardgoc@gmail.com>
2025-12-03 17:23:30 +0100

    LibCore: Explicitly export symbols in CoreMinimal
    This patch enables explicit symbol export for LibCoreMinimal.

--- Libraries/LibCore/StandardPaths.h
+++ Libraries/LibCore/StandardPaths.h
@@ -9,10 +9,11 @@
 
 #include <AK/Error.h>
 #include <AK/Forward.h>
+#include <LibCore/ExportCoreMinimal.h>
 
 namespace Core {
 
-class StandardPaths {
+class COREMINIMAL_API StandardPaths {
 public:
     static ByteString home_directory();
     static ByteString desktop_directory();
