945c4ca5b3c331b99e4d7d9418914f63857e87ed
Sam Atkins <sam@ladybird.org>
2025-12-04 15:57:54 +0000

    LibWeb/HTML: Implement NavigationTransition.to
    Corresponds to:
    https://github.com/whatwg/html/commit/f19930f98afeb7565cfc1cbc6a1d4a2086535bca

--- Libraries/LibWeb/HTML/NavigationTransition.cpp
+++ Libraries/LibWeb/HTML/NavigationTransition.cpp
@@ -8,6 +8,7 @@
 #include <LibJS/Runtime/Realm.h>
 #include <LibWeb/Bindings/Intrinsics.h>
 #include <LibWeb/Bindings/NavigationTransitionPrototype.h>
+#include <LibWeb/HTML/NavigationDestination.h>
 #include <LibWeb/HTML/NavigationHistoryEntry.h>
 #include <LibWeb/HTML/NavigationTransition.h>
 #include <LibWeb/WebIDL/Promise.h>
@@ -16,15 +17,16 @@ namespace Web::HTML {
 
 GC_DEFINE_ALLOCATOR(NavigationTransition);
 
-GC::Ref<NavigationTransition> NavigationTransition::create(JS::Realm& realm, Bindings::NavigationType navigation_type, GC::Ref<NavigationHistoryEntry> from_entry, GC::Ref<WebIDL::Promise> finished_promise)
+GC::Ref<NavigationTransition> NavigationTransition::create(JS::Realm& realm, Bindings::NavigationType navigation_type, GC::Ref<NavigationHistoryEntry> from_entry, GC::Ref<NavigationDestination> destination, GC::Ref<WebIDL::Promise> finished_promise)
 {
-    return realm.create<NavigationTransition>(realm, navigation_type, from_entry, finished_promise);
+    return realm.create<NavigationTransition>(realm, navigation_type, from_entry, destination, finished_promise);
 }
 
-NavigationTransition::NavigationTransition(JS::Realm& realm, Bindings::NavigationType navigation_type, GC::Ref<NavigationHistoryEntry> from_entry, GC::Ref<WebIDL::Promise> finished_promise)
+NavigationTransition::NavigationTransition(JS::Realm& realm, Bindings::NavigationType navigation_type, GC::Ref<NavigationHistoryEntry> from_entry, GC::Ref<NavigationDestination> destination, GC::Ref<WebIDL::Promise> finished_promise)
     : Bindings::PlatformObject(realm)
     , m_navigation_type(navigation_type)
     , m_from_entry(from_entry)
+    , m_destination(destination)
     , m_finished_promise(finished_promise)
 {
 }
@@ -41,6 +43,7 @@ void NavigationTransition::visit_edges(JS::Cell::Visitor& visitor)
 {
     Base::visit_edges(visitor);
     visitor.visit(m_from_entry);
+    visitor.visit(m_destination);
     visitor.visit(m_finished_promise);
 }
 
