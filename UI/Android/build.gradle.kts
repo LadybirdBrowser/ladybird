8a406a56bd08f2a101ebfd7b58c97a94186a891a
Zaggy1024 <Zaggy1024@gmail.com>
2025-12-10 17:45:26 -0600

    LibMedia: Drop the Oboe audio stream implementation for Android
    The Android build is likely bitrotted, and breaking changes need to be
    made to the PlaybackStream interface. This should be reverted and the
    implementation updated if the Android build is maintained again.

--- UI/Android/build.gradle.kts
+++ UI/Android/build.gradle.kts
@@ -92,5 +92,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.google.oboe:oboe:1.9.3")
 }
