498a070f2b9b5f87007bfdc3a3f792bba3862eff
mikiubo <michele.uboldi@gmail.com>
2025-12-13 08:17:51 +0100

    LibRegex: Special-case trivial lookbehind assertions
    Avoid generating full lookbehind bytecode when the lookbehind body
    consists only of a CheckBegin or CheckEnd opcode. Emit the assertion
    directly instead.
    
    This fixes start-of-line lookbehind matches.

--- Libraries/LibRegex/RegexByteCode.h
+++ Libraries/LibRegex/RegexByteCode.h
@@ -463,6 +463,10 @@ public:
             return;
         }
         case LookAroundType::LookBehind: {
+            if (lookaround_body.size() == 1 && (lookaround_body[0] == (ByteCodeValueType)OpCodeId::CheckBegin || lookaround_body[0] == (ByteCodeValueType)OpCodeId::CheckEnd)) {
+                extend(move(lookaround_body));
+                return;
+            }
             // CheckBoundary already compares the previous and current positions,
             // so applying the generic lookbehind stepback would rewind twice.
             if (lookaround_body.size() == 2 && lookaround_body[0] == (ByteCodeValueType)OpCodeId::CheckBoundary) {
