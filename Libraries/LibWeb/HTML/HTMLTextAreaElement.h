9b9d43e97610307d4e42edecaa3d35db1fc72ac2
Jonathan Gamble <gamblej@gmail.com>
2025-12-23 19:36:14 -0600

    LibWeb: Fix tofu in textarea newlines on MacOS
    Do not insert the codepoint from the platform event. Always insert 10

--- Libraries/LibWeb/HTML/HTMLTextAreaElement.h
+++ Libraries/LibWeb/HTML/HTMLTextAreaElement.h
@@ -137,6 +137,8 @@ public:
 private:
     HTMLTextAreaElement(DOM::Document&, DOM::QualifiedName);
 
+    virtual EventResult handle_return_key(FlyString const& ui_input_type) override;
+
     virtual bool is_html_textarea_element() const final { return true; }
 
     virtual void initialize(JS::Realm&) override;
