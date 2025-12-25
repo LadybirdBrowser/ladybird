LadybirdBrowser/ladybird

commit 63eccc5640799aec5725d5edecb0efc20b751a39

LibJS: Don't make extra copies of every JS function's source code
Instead, let functions have a view into the AST's SourceCode object's
underlying string data. The source string is kept alive by the AST, so
it's fine to have views into it as long as the AST exists.

Reduces memory footprint on my x.com home feed by 65 MiB.

Libraries/LibJS/Runtime/FunctionConstructor.cpp
@@ -200,7 +200,28 @@ ThrowCompletionOr<GC::Ref<ECMAScriptFunctionObject>> FunctionConstructor::create
 
     // 28. Let F be OrdinaryFunctionCreate(proto, sourceText, parameters, body, non-lexical-this, env, privateEnv).
     parsing_insights.might_need_arguments_object = true;
-    auto function = ECMAScriptFunctionObject::create(realm, "anonymous"_utf16_fly_string, *prototype, move(source_text), expr->body(), expr->parameters(), expr->function_length(), expr->local_variables_names(), &environment, private_environment, expr->kind(), expr->is_strict_mode(), parsing_insights);
+
+    auto function_data = vm.heap().allocate<SharedFunctionInstanceData>(
+        vm,
+        kind,
+        "anonymous"_utf16_fly_string,
+        function_length,
+        parameters,
+        expr->body(),
+        Utf16View {},
+        expr->is_strict_mode(),
+        false,
+        parsing_insights,
+        expr->local_variables_names());
+    function_data->m_source_text_owner = Utf16String::from_utf8(source_text);
+    function_data->m_source_text = function_data->m_source_text_owner.utf16_view();
+
+    auto function = ECMAScriptFunctionObject::create_from_function_data(
+        realm,
+        function_data,
+        &environment,
+        private_environment,
+        *prototype);
 
     // FIXME: Remove the name argument from create() and do this instead.
     // 29. Perform SetFunctionName(F, "anonymous").

Libraries/LibJS/AST.cpp
@@ -230,7 +230,7 @@ ThrowCompletionOr<ClassElement::ClassValue> ClassField::class_element_evaluation
             FunctionParsingInsights parsing_insights;
             parsing_insights.uses_this_from_environment = true;
             parsing_insights.uses_this = true;
-            auto function = ECMAScriptFunctionObject::create(realm, "field"_utf16_fly_string, ByteString::empty(), *function_code, FunctionParameters::empty(), 0, {}, vm.lexical_environment(), vm.running_execution_context().private_environment, FunctionKind::Normal, true, parsing_insights, false, property_key_or_private_name);
+            auto function = ECMAScriptFunctionObject::create(realm, "field"_utf16_fly_string, Utf16View {}, *function_code, FunctionParameters::empty(), 0, {}, vm.lexical_environment(), vm.running_execution_context().private_environment, FunctionKind::Normal, true, parsing_insights, false, property_key_or_private_name);
             function->make_method(target);
             initializer = function;
         }
@@ -279,7 +279,7 @@ ThrowCompletionOr<ClassElement::ClassValue> StaticInitializer::class_element_eva
     FunctionParsingInsights parsing_insights;
     parsing_insights.uses_this_from_environment = true;
     parsing_insights.uses_this = true;
-    auto body_function = ECMAScriptFunctionObject::create(realm, {}, ByteString::empty(), *m_function_body, FunctionParameters::empty(), 0, m_function_body->local_variables_names(), lexical_environment, private_environment, FunctionKind::Normal, true, parsing_insights, false);
+    auto body_function = ECMAScriptFunctionObject::create(realm, {}, Utf16View {}, *m_function_body, FunctionParameters::empty(), 0, m_function_body->local_variables_names(), lexical_environment, private_environment, FunctionKind::Normal, true, parsing_insights, false);
 
     // 6. Perform MakeMethod(bodyFunction, homeObject).
     body_function->make_method(home_object);
@@ -801,7 +801,7 @@ void BindingPattern::dump(int indent) const
     }
 }
 
-FunctionNode::FunctionNode(RefPtr<Identifier const> name, ByteString source_text, NonnullRefPtr<Statement const> body, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, FunctionKind kind, bool is_strict_mode, FunctionParsingInsights parsing_insights, bool is_arrow_function, Vector<LocalVariable> local_variables_names)
+FunctionNode::FunctionNode(RefPtr<Identifier const> name, Utf16View source_text, NonnullRefPtr<Statement const> body, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, FunctionKind kind, bool is_strict_mode, FunctionParsingInsights parsing_insights, bool is_arrow_function, Vector<LocalVariable> local_variables_names)
     : m_name(move(name))
     , m_source_text(move(source_text))
     , m_body(move(body))

Libraries/LibJS/AST.h
@@ -776,7 +776,7 @@ class JS_API FunctionNode {
 public:
     Utf16FlyString name() const { return m_name ? m_name->string() : Utf16FlyString {}; }
     RefPtr<Identifier const> name_identifier() const { return m_name; }
-    ByteString const& source_text() const { return m_source_text; }
+    Utf16View source_text() const { return m_source_text; }
     Statement const& body() const { return *m_body; }
     auto const& body_ptr() const { return m_body; }
     auto const& parameters() const { return m_parameters; }
@@ -798,13 +798,13 @@ public:
     virtual ~FunctionNode();
 
 protected:
-    FunctionNode(RefPtr<Identifier const> name, ByteString source_text, NonnullRefPtr<Statement const> body, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, FunctionKind kind, bool is_strict_mode, FunctionParsingInsights parsing_insights, bool is_arrow_function, Vector<LocalVariable> local_variables_names);
+    FunctionNode(RefPtr<Identifier const> name, Utf16View source_text, NonnullRefPtr<Statement const> body, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, FunctionKind kind, bool is_strict_mode, FunctionParsingInsights parsing_insights, bool is_arrow_function, Vector<LocalVariable> local_variables_names);
     void dump(int indent, ByteString const& class_name) const;
 
     RefPtr<Identifier const> m_name { nullptr };
 
 private:
-    ByteString m_source_text;
+    Utf16View m_source_text;
     NonnullRefPtr<Statement const> m_body;
     NonnullRefPtr<FunctionParameters const> m_parameters;
     i32 const m_function_length;
@@ -824,9 +824,9 @@ class FunctionDeclaration final
 public:
     static bool must_have_name() { return true; }
 
-    FunctionDeclaration(SourceRange source_range, RefPtr<Identifier const> name, ByteString source_text, NonnullRefPtr<Statement const> body, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, FunctionKind kind, bool is_strict_mode, FunctionParsingInsights insights, Vector<LocalVariable> local_variables_names)
+    FunctionDeclaration(SourceRange source_range, RefPtr<Identifier const> name, Utf16View source_text, NonnullRefPtr<Statement const> body, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, FunctionKind kind, bool is_strict_mode, FunctionParsingInsights insights, Vector<LocalVariable> local_variables_names)
         : Declaration(move(source_range))
-        , FunctionNode(move(name), move(source_text), move(body), move(parameters), function_length, kind, is_strict_mode, insights, false, move(local_variables_names))
+        , FunctionNode(move(name), source_text, move(body), move(parameters), function_length, kind, is_strict_mode, insights, false, move(local_variables_names))
     {
     }
 
@@ -853,9 +853,9 @@ class FunctionExpression final
 public:
     static bool must_have_name() { return false; }
 
-    FunctionExpression(SourceRange source_range, RefPtr<Identifier const> name, ByteString source_text, NonnullRefPtr<Statement const> body, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, FunctionKind kind, bool is_strict_mode, FunctionParsingInsights insights, Vector<LocalVariable> local_variables_names, bool is_arrow_function = false)
+    FunctionExpression(SourceRange source_range, RefPtr<Identifier const> name, Utf16View source_text, NonnullRefPtr<Statement const> body, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, FunctionKind kind, bool is_strict_mode, FunctionParsingInsights insights, Vector<LocalVariable> local_variables_names, bool is_arrow_function = false)
         : Expression(move(source_range))
-        , FunctionNode(move(name), move(source_text), move(body), move(parameters), function_length, kind, is_strict_mode, insights, is_arrow_function, move(local_variables_names))
+        , FunctionNode(move(name), source_text, move(body), move(parameters), function_length, kind, is_strict_mode, insights, is_arrow_function, move(local_variables_names))
     {
     }
 
@@ -1487,7 +1487,7 @@ public:
 
 class ClassExpression final : public Expression {
 public:
-    ClassExpression(SourceRange source_range, RefPtr<Identifier const> name, ByteString source_text, RefPtr<FunctionExpression const> constructor, RefPtr<Expression const> super_class, Vector<NonnullRefPtr<ClassElement const>> elements)
+    ClassExpression(SourceRange source_range, RefPtr<Identifier const> name, Utf16View source_text, RefPtr<FunctionExpression const> constructor, RefPtr<Expression const> super_class, Vector<NonnullRefPtr<ClassElement const>> elements)
         : Expression(move(source_range))
         , m_name(move(name))
         , m_source_text(move(source_text))
@@ -1499,7 +1499,7 @@ public:
 
     Utf16FlyString name() const { return m_name ? m_name->string() : Utf16FlyString {}; }
 
-    ByteString const& source_text() const { return m_source_text; }
+    Utf16View source_text() const { return m_source_text; }
     RefPtr<FunctionExpression const> constructor() const { return m_constructor; }
 
     virtual void dump(int indent) const override;
@@ -1516,7 +1516,7 @@ private:
     friend ClassDeclaration;
 
     RefPtr<Identifier const> m_name;
-    ByteString m_source_text;
+    Utf16View m_source_text;
     RefPtr<FunctionExpression const> m_constructor;
     RefPtr<Expression const> m_super_class;
     Vector<NonnullRefPtr<ClassElement const>> m_elements;

Libraries/LibJS/Parser.cpp
@@ -1170,7 +1170,7 @@ RefPtr<FunctionExpression const> Parser::try_parse_arrow_function_expression(boo
     auto source_text = m_state.lexer.source().substring_view(function_start_offset, function_end_offset - function_start_offset);
 
     return create_ast_node<FunctionExpression>(
-        { m_source_code, rule_start.position(), position() }, nullptr, MUST(source_text.to_byte_string()),
+        { m_source_code, rule_start.position(), position() }, nullptr, source_text,
         move(body), move(parameters), function_length, function_kind, body->in_strict_mode(),
         parsing_insights, move(local_variables_names), /* is_arrow_function */ true);
 }
@@ -1671,7 +1671,7 @@ NonnullRefPtr<ClassExpression const> Parser::parse_class_expression(bool expect_
             parsing_insights.uses_this_from_environment = true;
             parsing_insights.uses_this = true;
             constructor = create_ast_node<FunctionExpression>(
-                { m_source_code, rule_start.position(), position() }, class_name, "",
+                { m_source_code, rule_start.position(), position() }, class_name, Utf16View {},
                 move(constructor_body), FunctionParameters::create(Vector { FunctionParameter { move(argument_name), nullptr, true } }), 0, FunctionKind::Normal,
                 /* is_strict_mode */ true, parsing_insights, /* local_variables_names */ Vector<LocalVariable> {});
         } else {
@@ -1679,7 +1679,7 @@ NonnullRefPtr<ClassExpression const> Parser::parse_class_expression(bool expect_
             parsing_insights.uses_this_from_environment = true;
             parsing_insights.uses_this = true;
             constructor = create_ast_node<FunctionExpression>(
-                { m_source_code, rule_start.position(), position() }, class_name, "",
+                { m_source_code, rule_start.position(), position() }, class_name, Utf16View {},
                 move(constructor_body), FunctionParameters::empty(), 0, FunctionKind::Normal,
                 /* is_strict_mode */ true, parsing_insights, /* local_variables_names */ Vector<LocalVariable> {});
         }
@@ -1701,7 +1701,7 @@ NonnullRefPtr<ClassExpression const> Parser::parse_class_expression(bool expect_
 
     auto source_text = m_state.lexer.source().substring_view(function_start_offset, function_end_offset - function_start_offset);
 
-    return create_ast_node<ClassExpression>({ m_source_code, rule_start.position(), position() }, move(class_name), MUST(source_text.to_byte_string()), move(constructor), move(super_class), move(elements));
+    return create_ast_node<ClassExpression>({ m_source_code, rule_start.position(), position() }, move(class_name), source_text, move(constructor), move(super_class), move(elements));
 }
 
 Parser::PrimaryExpressionParseResult Parser::parse_primary_expression()
@@ -3039,7 +3039,7 @@ NonnullRefPtr<FunctionNodeType> Parser::parse_function_node(u16 parse_options, O
     }
     return create_ast_node<FunctionNodeType>(
         { m_source_code, rule_start.position(), position() },
-        name, MUST(source_text.to_byte_string()), move(body), parameters.release_nonnull(), function_length,
+        name, source_text, move(body), parameters.release_nonnull(), function_length,
         function_kind, has_strict_directive, parsing_insights,
         move(local_variables_names));
 }

Libraries/LibJS/Runtime/ECMAScriptFunctionObject.cpp
@@ -34,23 +34,53 @@ namespace JS {
 
 GC_DEFINE_ALLOCATOR(ECMAScriptFunctionObject);
 
-GC::Ref<ECMAScriptFunctionObject> ECMAScriptFunctionObject::create(Realm& realm, Utf16FlyString name, ByteString source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind kind, bool is_strict, FunctionParsingInsights parsing_insights, bool is_arrow_function, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name)
+static GC::Ref<Object> prototype_for_function_kind(Realm& realm, FunctionKind kind)
 {
-    Object* prototype = nullptr;
     switch (kind) {
     case FunctionKind::Normal:
-        prototype = realm.intrinsics().function_prototype();
-        break;
+        return realm.intrinsics().function_prototype();
     case FunctionKind::Generator:
-        prototype = realm.intrinsics().generator_function_prototype();
-        break;
+        return realm.intrinsics().generator_function_prototype();
     case FunctionKind::Async:
-        prototype = realm.intrinsics().async_function_prototype();
-        break;
+        return realm.intrinsics().async_function_prototype();
     case FunctionKind::AsyncGenerator:
-        prototype = realm.intrinsics().async_generator_function_prototype();
-        break;
+        return realm.intrinsics().async_generator_function_prototype();
     }
+    VERIFY_NOT_REACHED();
+}
+
+GC::Ref<ECMAScriptFunctionObject> ECMAScriptFunctionObject::create(Realm& realm, Utf16FlyString name, Utf16String source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind kind, bool is_strict, FunctionParsingInsights parsing_insights, bool is_arrow_function, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name)
+{
+    auto prototype = prototype_for_function_kind(realm, kind);
+
+    auto shared_data = realm.heap().allocate<SharedFunctionInstanceData>(
+        realm.vm(),
+        kind,
+        move(name),
+        function_length,
+        *parameters,
+        ecmascript_code,
+        Utf16View {},
+        is_strict,
+        is_arrow_function,
+        parsing_insights,
+        move(local_variables_names));
+
+    shared_data->m_class_field_initializer_name = move(class_field_initializer_name);
+
+    shared_data->m_source_text_owner = move(source_text);
+    shared_data->m_source_text = shared_data->m_source_text_owner.utf16_view();
+
+    return realm.create<ECMAScriptFunctionObject>(
+        move(shared_data),
+        parent_environment,
+        private_environment,
+        *prototype);
+}
+
+GC::Ref<ECMAScriptFunctionObject> ECMAScriptFunctionObject::create(Realm& realm, Utf16FlyString name, Utf16View source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind kind, bool is_strict, FunctionParsingInsights parsing_insights, bool is_arrow_function, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name)
+{
+    auto prototype = prototype_for_function_kind(realm, kind);
 
     auto shared_data = realm.heap().allocate<SharedFunctionInstanceData>(
         realm.vm(),
@@ -74,7 +104,7 @@ GC::Ref<ECMAScriptFunctionObject> ECMAScriptFunctionObject::create(Realm& realm,
         *prototype);
 }
 
-GC::Ref<ECMAScriptFunctionObject> ECMAScriptFunctionObject::create(Realm& realm, Utf16FlyString name, Object& prototype, ByteString source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind kind, bool is_strict, FunctionParsingInsights parsing_insights, bool is_arrow_function, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name)
+GC::Ref<ECMAScriptFunctionObject> ECMAScriptFunctionObject::create(Realm& realm, Utf16FlyString name, Object& prototype, Utf16View source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind kind, bool is_strict, FunctionParsingInsights parsing_insights, bool is_arrow_function, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name)
 {
     auto shared_data = realm.heap().allocate<SharedFunctionInstanceData>(
         realm.vm(),
@@ -91,6 +121,20 @@ GC::Ref<ECMAScriptFunctionObject> ECMAScriptFunctionObject::create(Realm& realm,
     shared_data->m_class_field_initializer_name = move(class_field_initializer_name);
     return realm.create<ECMAScriptFunctionObject>(
         move(shared_data),
+        parent_environment,
+        private_environment,
+        prototype);
+}
+
+GC::Ref<ECMAScriptFunctionObject> ECMAScriptFunctionObject::create_from_function_data(
+    GC::Ref<Realm> realm,
+    GC::Ref<SharedFunctionInstanceData> shared_data,
+    GC::Ptr<Environment> parent_environment,
+    GC::Ptr<PrivateEnvironment> private_environment,
+    Object& prototype)
+{
+    return realm->create<ECMAScriptFunctionObject>(
+        *shared_data,
         parent_environment,
         private_environment,
         prototype);
@@ -137,7 +181,8 @@ GC::Ref<ECMAScriptFunctionObject> ECMAScriptFunctionObject::create_from_function
         function_node.set_shared_data(shared_data);
     }
 
-    return realm->create<ECMAScriptFunctionObject>(
+    return create_from_function_data(
+        realm,
         *shared_data,
         parent_environment,
         private_environment,

Libraries/LibJS/Runtime/ECMAScriptFunctionObject.h
@@ -30,8 +30,16 @@ class JS_API ECMAScriptFunctionObject final : public FunctionObject {
     GC_DECLARE_ALLOCATOR(ECMAScriptFunctionObject);
 
 public:
-    static GC::Ref<ECMAScriptFunctionObject> create(Realm&, Utf16FlyString name, ByteString source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind, bool is_strict, FunctionParsingInsights, bool is_arrow_function = false, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name = {});
-    static GC::Ref<ECMAScriptFunctionObject> create(Realm&, Utf16FlyString name, Object& prototype, ByteString source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind, bool is_strict, FunctionParsingInsights, bool is_arrow_function = false, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name = {});
+    static GC::Ref<ECMAScriptFunctionObject> create(Realm&, Utf16FlyString name, Utf16String source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind, bool is_strict, FunctionParsingInsights, bool is_arrow_function = false, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name = {});
+    static GC::Ref<ECMAScriptFunctionObject> create(Realm&, Utf16FlyString name, Utf16View source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind, bool is_strict, FunctionParsingInsights, bool is_arrow_function = false, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name = {});
+    static GC::Ref<ECMAScriptFunctionObject> create(Realm&, Utf16FlyString name, Object& prototype, Utf16View source_text, Statement const& ecmascript_code, NonnullRefPtr<FunctionParameters const> parameters, i32 function_length, Vector<LocalVariable> local_variables_names, Environment* parent_environment, PrivateEnvironment* private_environment, FunctionKind, bool is_strict, FunctionParsingInsights, bool is_arrow_function = false, Variant<PropertyKey, PrivateName, Empty> class_field_initializer_name = {});
+
+    [[nodiscard]] static GC::Ref<ECMAScriptFunctionObject> create_from_function_data(
+        GC::Ref<Realm>,
+        GC::Ref<SharedFunctionInstanceData>,
+        GC::Ptr<Environment>,
+        GC::Ptr<PrivateEnvironment>,
+        Object& prototype);
 
     [[nodiscard]] static GC::Ref<ECMAScriptFunctionObject> create_from_function_node(
         FunctionNode const&,
@@ -79,8 +87,8 @@ public:
     Object* home_object() const { return m_home_object; }
     void set_home_object(Object* home_object) { m_home_object = home_object; }
 
-    [[nodiscard]] ByteString const& source_text() const { return shared_data().m_source_text; }
-    void set_source_text(ByteString source_text) { const_cast<SharedFunctionInstanceData&>(shared_data()).m_source_text = move(source_text); }
+    [[nodiscard]] Utf16View source_text() const { return shared_data().m_source_text; }
+    void set_source_text(Utf16View source_text) { const_cast<SharedFunctionInstanceData&>(shared_data()).m_source_text = move(source_text); }
 
     Vector<ClassFieldDefinition> const& fields() const { return ensure_class_data().fields; }
     void add_field(ClassFieldDefinition field) { ensure_class_data().fields.append(move(field)); }

Libraries/LibJS/Runtime/SharedFunctionInstanceData.cpp
@@ -18,7 +18,7 @@ SharedFunctionInstanceData::SharedFunctionInstanceData(
     i32 function_length,
     NonnullRefPtr<FunctionParameters const> formal_parameters,
     NonnullRefPtr<Statement const> ecmascript_code,
-    ByteString source_text,
+    Utf16View source_text,
     bool strict,
     bool is_arrow_function,
     FunctionParsingInsights const& parsing_insights,

Libraries/LibJS/Runtime/SharedFunctionInstanceData.h
@@ -38,7 +38,7 @@ public:
         i32 function_length,
         NonnullRefPtr<FunctionParameters const>,
         NonnullRefPtr<Statement const> ecmascript_code,
-        ByteString source_text,
+        Utf16View source_text,
         bool strict,
         bool is_arrow_function,
         FunctionParsingInsights const&,
@@ -50,7 +50,11 @@ public:
     RefPtr<Statement const> m_ecmascript_code;            // [[ECMAScriptCode]]
 
     Utf16FlyString m_name;
-    ByteString m_source_text; // [[SourceText]]
+
+    // NB: m_source_text_owner is used if the source text needs to be owned by the function data.
+    //     Otherwise, m_source_text is a view into the underlying JS::SourceCode we parsed the AST from.
+    Utf16String m_source_text_owner;
+    Utf16View m_source_text; // [[SourceText]]
 
     Vector<LocalVariable> m_local_variables_names;
 

commit e9c983724a96fb958498044d197153e90c525228

LibJS: Propagate function length etc to `new Function` objects
Fixes 12 test262 tests.

Regressed in 63eccc5640799aec5725d5edecb0efc20b751a39.

Libraries/LibJS/Runtime/FunctionConstructor.cpp
@@ -203,10 +203,10 @@ ThrowCompletionOr<GC::Ref<ECMAScriptFunctionObject>> FunctionConstructor::create
 
     auto function_data = vm.heap().allocate<SharedFunctionInstanceData>(
         vm,
-        kind,
+        expr->kind(),
         "anonymous"_utf16_fly_string,
-        function_length,
-        parameters,
+        expr->function_length(),
+        expr->parameters(),
         expr->body(),
         Utf16View {},
         expr->is_strict_mode(),