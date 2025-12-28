79740b04b38688eb0f2e370eaf2feebd98c9f290
Callum Law <callumlaw1709@outlook.com>
2025-12-28 22:13:28 +1300

    LibWeb: Don't generate layer for `background-image` `none` entry
    Reduces the time spent in `background_layers()` from 1.5% to 0.04% when
    loading https://en.wikipedia.org/wiki/2023_in_American_television

--- Libraries/LibWeb/Layout/Node.cpp
+++ Libraries/LibWeb/Layout/Node.cpp
@@ -388,10 +388,9 @@ NodeWithStyle::NodeWithStyle(DOM::Document& document, DOM::Node* node, NonnullOw
 void NodeWithStyle::visit_edges(Visitor& visitor)
 {
     Base::visit_edges(visitor);
-    for (auto& layer : computed_values().background_layers()) {
-        if (layer.background_image && layer.background_image->is_image())
-            layer.background_image->as_image().visit_edges(visitor);
-    }
+    for (auto const& layer : computed_values().background_layers())
+        layer.background_image->visit_edges(visitor);
+
     if (m_list_style_image && m_list_style_image->is_image())
         m_list_style_image->as_image().visit_edges(visitor);
 }
@@ -423,10 +422,8 @@ void NodeWithStyle::apply_style(CSS::ComputedProperties const& computed_style)
 
     auto background_layers = computed_style.background_layers();
 
-    for (auto const& layer : background_layers) {
-        if (layer.background_image)
-            const_cast<CSS::AbstractImageStyleValue&>(*layer.background_image).load_any_resources(document());
-    }
+    for (auto const& layer : background_layers)
+        const_cast<CSS::AbstractImageStyleValue&>(*layer.background_image).load_any_resources(document());
 
     computed_values.set_background_layers(move(background_layers));
 
--- Libraries/LibWeb/Painting/BackgroundPainting.h
+++ Libraries/LibWeb/Painting/BackgroundPainting.h
@@ -13,7 +13,7 @@
 namespace Web::Painting {
 
 struct ResolvedBackgroundLayerData {
-    RefPtr<CSS::AbstractImageStyleValue const> background_image;
+    NonnullRefPtr<CSS::AbstractImageStyleValue const> background_image;
     CSS::BackgroundAttachment attachment;
     CSS::BackgroundBox clip;
     CSSPixels position_x;
