LadybirdBrowser/ladybird

commit 3742138cc3318842c795f3be8e41642702428af4

LibCore+LibThreading: Always wake the event loop when deferred invoking

Libraries/LibCore/EventLoopImplementation.cpp
@@ -26,6 +26,7 @@ EventLoopImplementation::~EventLoopImplementation() = default;
 void EventLoopImplementation::deferred_invoke(Function<void()>&& invokee)
 {
     m_thread_event_queue.deferred_invoke(move(invokee));
+    wake();
 }
 
 static EventLoopManager* s_event_loop_manager = nullptr;

Libraries/LibThreading/BackgroundAction.h
@@ -92,7 +92,6 @@ private:
                         // Our promise's resolution function will never error.
                         (void)promise->resolve(*self);
                     });
-                    origin_event_loop->wake();
                 }
             } else {
                 // We were either unsuccessful or cancelled (in which case there is no error).
@@ -106,7 +105,6 @@ private:
                     origin_event_loop->deferred_invoke([self, error = move(error)]() mutable {
                         self->m_on_error(move(error));
                     });
-                    origin_event_loop->wake();
                 } else if (self->m_on_error) {
                     self->m_on_error(move(error));
                 }

commit 94f1077c0e18cfd7511ca8aed44a73c6a0078a88

LibCore: Only wake the event loop in deferred_invoke from other threads
For some reason, writing to the wake fd from the same thread in
deferred_invoke was causing a deadlock. However, we don't actually need
to wake from the same thread, since the event loop is not waiting and
will therefore process the deferred_invoke on the next iteration.

This issue was introduced in 3742138cc3318842c795f3be8e41642702428af4.
The deadlock could be reproduced consistently by increasing
LOCAL_STORAGE_QUOTA in StorageJar.cpp to a large value like 50 MiB.

Libraries/LibCore/EventLoopImplementation.cpp
@@ -26,7 +26,8 @@ EventLoopImplementation::~EventLoopImplementation() = default;
 void EventLoopImplementation::deferred_invoke(Function<void()>&& invokee)
 {
     m_thread_event_queue.deferred_invoke(move(invokee));
-    wake();
+    if (&m_thread_event_queue != ThreadEventQueue::current_or_null())
+        wake();
 }
 
 static EventLoopManager* s_event_loop_manager = nullptr;