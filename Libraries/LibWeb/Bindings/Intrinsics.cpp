8ea8a7907ffafc9e9d6a8ceb35720ce4aea215d2
Shannon Booth <shannon@serenityos.org>
2026-01-06 16:44:18 +0100

    LibWeb/Bindings: Ensure identity for [LegacyUnforgeable] attributes
    The WebIDL spec requires that attributes marked [LegacyUnforgeable]
    share the same getter and setter function objects across all instances
    within the same Realm.
    
    Previously, we were creating new JS::NativeFunction wrappers during
    every instance initialization, which caused identity checks to fail.
    This change moves these functions into Bindings::Intrinsics so they
    are created once per Realm and reused.

--- Libraries/LibWeb/Bindings/Intrinsics.cpp
+++ Libraries/LibWeb/Bindings/Intrinsics.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2022, Andrew Kaster <akaster@serenityos.org>
+ * Copyright (c) 2026, Shannon Booth <shannon@serenityos.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -22,12 +23,37 @@ void Intrinsics::visit_edges(JS::Cell::Visitor& visitor)
     visitor.visit(m_prototypes);
     visitor.visit(m_constructors);
     visitor.visit(m_realm);
+    visitor.visit(m_unforgeable_functions);
 }
 
 Intrinsics& host_defined_intrinsics(JS::Realm& realm)
 {
     ASSERT(realm.host_defined());
     return static_cast<Bindings::HostDefined&>(*realm.host_defined()).intrinsics;
+}
+
+GC::Ref<JS::NativeFunction> Intrinsics::ensure_web_unforgeable_function(
+    Utf16FlyString const& interface_name,
+    Utf16FlyString const& attribute_name,
+    Function<JS::ThrowCompletionOr<JS::Value>(JS::VM&)> behaviour,
+    UnforgeableKey::Type type)
+{
+    UnforgeableKey key { interface_name, attribute_name, type };
+    if (auto it = m_unforgeable_functions.find(key); it != m_unforgeable_functions.end())
+        return *it->value;
+
+    auto function = JS::NativeFunction::create(*m_realm, move(behaviour), type == UnforgeableKey::Type::Setter ? 1 : 0, attribute_name, m_realm, type == UnforgeableKey::Type::Setter ? "set"sv : "get"sv);
+    m_unforgeable_functions.set(key, *function);
+    return *function;
+}
+
+}
+
+namespace AK {
+
+unsigned Traits<Web::Bindings::UnforgeableKey>::hash(Web::Bindings::UnforgeableKey const& key)
+{
+    return pair_int_hash(pair_int_hash(key.attribute_name.hash(), key.interface_name.hash()), to_underlying(key.type));
 }
 
 }
