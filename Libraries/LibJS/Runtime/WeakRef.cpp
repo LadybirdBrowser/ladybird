4c10f44e3ef19c44a097a0c368bcd3939d56596e
Andreas Kling <andreas@ladybird.org>
2026-01-07 12:51:14 +0100

    LibJS: Remove bogus VERIFY_NOT_REACHED() in WeakRef::remove_dead_cells()
    It's harmless if the WeakRef is asked to remove dead cells when it has
    no dead cells to remove. This can happen if the WeakRef lives longer
    due to someone referencing it. Caught by test-js with different GC
    frequency.

--- Libraries/LibJS/Runtime/WeakRef.cpp
+++ Libraries/LibJS/Runtime/WeakRef.cpp
@@ -38,7 +38,7 @@ WeakRef::WeakRef(Symbol& value, Object& prototype)
 
 void WeakRef::remove_dead_cells(Badge<GC::Heap>)
 {
-    if (m_value.visit([](Cell* cell) -> bool { return cell->state() == Cell::State::Live; }, [](Empty) -> bool { VERIFY_NOT_REACHED(); }))
+    if (m_value.visit([](Cell* cell) -> bool { return cell->state() == Cell::State::Live; }, [](Empty) -> bool { return true; }))
         return;
 
     m_value = Empty {};
