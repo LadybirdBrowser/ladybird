d27c2f912d2b324fcc8ec82c9b5fdfad88e79221
Sam Atkins <sam@ladybird.org>
2026-01-09 10:12:17 +0000

    LibWeb/DOM: Clarify attribute-list comparison
    Corresponds to:
    https://github.com/whatwg/dom/commit/7b84f10e4716bf839ee7cbc3b1210e4ea3abd4c7

--- Libraries/LibWeb/DOM/Node.cpp
+++ Libraries/LibWeb/DOM/Node.cpp
@@ -2215,7 +2215,7 @@ bool Node::is_equal_node(Node const* other_node) const
             || this_element.local_name() != other_element.local_name()
             || this_element.attribute_list_size() != other_element.attribute_list_size())
             return false;
-        // If A is an element, each attribute in its attribute list has an attribute that equals an attribute in B’s attribute list.
+        // If A is an element, each attribute in its attribute list equals an attribute in B’s attribute list.
         bool has_same_attributes = true;
         this_element.for_each_attribute([&](auto const& attribute) {
             if (other_element.get_attribute_ns(attribute.namespace_uri(), attribute.local_name()) != attribute.value())
