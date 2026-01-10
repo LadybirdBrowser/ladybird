a466432574f949fbcfbcda1515af1cb8d3a7f906
Shannon Booth <shannon@serenityos.org>
2026-01-10 15:32:15 +0100

    test-web: Factor out a function to run a batch of tests
    This will make it easier to run test-web tests in different batches
    one after the other.

--- Tests/LibWeb/test-web/main.cpp
+++ Tests/LibWeb/test-web/main.cpp
@@ -454,7 +454,7 @@ static void run_ref_test(TestWebView& view, Test& test, URL::URL const& url, int
     timer->start();
 }
 
-static void run_test(TestWebView& view, Test& test, Application& app)
+static void run_test(TestWebView& view, Test& test, Application const& app)
 {
     s_test_by_view.set(&view, &test);
 
@@ -556,6 +556,118 @@ struct TestStats {
     Vector<TestCompletion> non_passing_tests;
 };
 
+static ErrorOr<void> run_test_batch(
+    ReadonlySpan<NonnullOwnPtr<TestWebView>> views,
+    Vector<Test>& batch_tests,
+    size_t total_test_count,
+    size_t& global_test_index,
+    size_t& total_tests_remaining,
+    TestStats& stats,
+    Application const& app)
+{
+    if (batch_tests.is_empty())
+        return {};
+
+    s_all_tests_complete = Core::Promise<Empty>::construct();
+
+    // Keep clearing and reusing the same line if stdout is a TTY.
+    bool log_on_one_line = app.verbosity < Application::VERBOSITY_LEVEL_LOG_TEST_DURATION && MUST(Core::System::isatty(STDOUT_FILENO));
+
+    auto batch_tests_remaining = batch_tests.size();
+    auto current_test = 0uz;
+
+    auto digits_for_view_id = static_cast<size_t>(log10(views.size()) + 1);
+    auto digits_for_test_id = static_cast<size_t>(log10(total_test_count) + 1);
+
+    for (auto [view_id, view] : enumerate(views)) {
+        set_ui_callbacks_for_tests(*view);
+        view->clear_content_filters();
+
+        auto run_next_test = [&, view_id]() {
+            auto index = current_test++;
+            if (index >= batch_tests.size())
+                return;
+
+            auto& test = batch_tests[index];
+            test.start_time = UnixDateTime::now();
+            test.index = ++global_test_index;
+
+            if (log_on_one_line) {
+                out("\33[2K\r{}/{}: {}", test.index, total_test_count, test.relative_path);
+                (void)fflush(stdout);
+            } else {
+                outln("[{:{}}] {:{}}/{}:  Start {}", view_id, digits_for_view_id, test.index, digits_for_test_id, total_test_count, test.relative_path);
+            }
+
+            Core::deferred_invoke([&]() mutable {
+                if (s_skipped_tests.contains_slow(test.input_path))
+                    view->on_test_complete({ test, TestResult::Skipped });
+                else
+                    run_test(*view, test, app);
+            });
+        };
+
+        view->test_promise().when_resolved([&, run_next_test, view_id](auto result) {
+            view->on_load_finish = {};
+            view->on_test_finish = {};
+            view->on_reference_test_metadata = {};
+            view->on_set_test_timeout = {};
+            view->reset_zoom();
+
+            if (result.test.timeout_timer) {
+                result.test.timeout_timer->stop();
+                result.test.timeout_timer.clear();
+            }
+
+            result.test.end_time = UnixDateTime::now();
+            s_test_by_view.remove(view);
+
+            if (app.verbosity >= Application::VERBOSITY_LEVEL_LOG_TEST_DURATION) {
+                auto duration = result.test.end_time - result.test.start_time;
+                outln("[{:{}}] {:{}}/{}: Finish {}: {}ms", view_id, digits_for_view_id, result.test.index, digits_for_test_id, total_test_count, result.test.relative_path, duration.to_milliseconds());
+            }
+
+            switch (result.result) {
+            case TestResult::Pass:
+                ++stats.pass_count;
+                break;
+            case TestResult::Fail:
+                ++stats.fail_count;
+                break;
+            case TestResult::Timeout:
+                ++stats.timeout_count;
+                break;
+            case TestResult::Crashed:
+                ++stats.crashed_count;
+                break;
+            case TestResult::Skipped:
+                ++stats.skipped_count;
+                break;
+            }
+
+            if (result.result != TestResult::Pass)
+                stats.non_passing_tests.append(move(result));
+
+            --total_tests_remaining;
+            if (--batch_tests_remaining == 0)
+                s_all_tests_complete->resolve({});
+            else
+                run_next_test();
+        });
+
+        Core::deferred_invoke([run_next_test]() {
+            run_next_test();
+        });
+    }
+
+    auto result = s_all_tests_complete->await();
+
+    if (result.is_error())
+        return result.release_error();
+
+    return {};
+}
+
 static ErrorOr<int> run_tests(Core::AnonymousBuffer const& theme, Web::DevicePixelSize window_size)
 {
     auto& app = Application::the();
@@ -604,7 +716,16 @@ static ErrorOr<int> run_tests(Core::AnonymousBuffer const& theme, Web::DevicePix
         return Error::from_string_literal("No tests found matching filter");
     }
 
-    auto concurrency = min(app.test_concurrency, tests.size());
+    TestStats stats;
+    // Keep clearing and reusing the same line if stdout is a TTY.
+    bool log_on_one_line = app.verbosity < Application::VERBOSITY_LEVEL_LOG_TEST_DURATION && TRY(Core::System::isatty(STDOUT_FILENO));
+    outln("Running {} tests...", tests.size());
+
+    size_t global_test_index = 0;
+    size_t total_tests = tests.size();
+    size_t total_tests_remaining = total_tests;
+
+    size_t concurrency = min(app.test_concurrency, tests.size());
     size_t loaded_web_views = 0;
 
     Vector<NonnullOwnPtr<TestWebView>> views;
@@ -615,7 +736,6 @@ static ErrorOr<int> run_tests(Core::AnonymousBuffer const& theme, Web::DevicePix
         view->on_load_finish = [&](auto const&) { ++loaded_web_views; };
         // FIXME: Figure out a better way to ensure that tests use default browser settings.
         view->reset_zoom();
-
         views.unchecked_append(move(view));
     }
 
@@ -625,102 +745,10 @@ static ErrorOr<int> run_tests(Core::AnonymousBuffer const& theme, Web::DevicePix
         return loaded_web_views == concurrency;
     });
 
-    TestStats stats;
-    // Keep clearing and reusing the same line if stdout is a TTY.
-    bool log_on_one_line = app.verbosity < Application::VERBOSITY_LEVEL_LOG_TEST_DURATION && TRY(Core::System::isatty(STDOUT_FILENO));
-    outln("Running {} tests...", tests.size());
-
-    s_all_tests_complete = Core::Promise<Empty>::construct();
-    auto tests_remaining = tests.size();
-    auto current_test = 0uz;
-
-    auto digits_for_view_id = static_cast<size_t>(log10(views.size()) + 1);
-    auto digits_for_test_id = static_cast<size_t>(log10(tests.size()) + 1);
-
-    for (auto [view_id, view] : enumerate(views)) {
-        set_ui_callbacks_for_tests(*view);
-        view->clear_content_filters();
-
-        auto run_next_test = [&, view_id]() {
-            auto index = current_test++;
-            if (index >= tests.size())
-                return;
-
-            auto& test = tests[index];
-            test.start_time = UnixDateTime::now();
-            test.index = index + 1;
-
-            if (log_on_one_line) {
-                out("\33[2K\r{}/{}: {}", test.index, tests.size(), test.relative_path);
-                (void)fflush(stdout);
-            } else {
-                outln("[{:{}}] {:{}}/{}:  Start {}", view_id, digits_for_view_id, test.index, digits_for_test_id, tests.size(), test.relative_path);
-            }
-
-            Core::deferred_invoke([&]() mutable {
-                if (s_skipped_tests.contains_slow(test.input_path))
-                    view->on_test_complete({ test, TestResult::Skipped });
-                else
-                    run_test(*view, test, app);
-            });
-        };
-
-        view->test_promise().when_resolved([&, run_next_test, view_id](auto result) {
-            view->on_load_finish = {};
-            view->on_test_finish = {};
-            view->on_reference_test_metadata = {};
-            view->on_set_test_timeout = {};
-            view->reset_zoom();
-
-            if (result.test.timeout_timer) {
-                result.test.timeout_timer->stop();
-                result.test.timeout_timer.clear();
-            }
-
-            result.test.end_time = UnixDateTime::now();
-            s_test_by_view.remove(view);
-
-            if (app.verbosity >= Application::VERBOSITY_LEVEL_LOG_TEST_DURATION) {
-                auto duration = result.test.end_time - result.test.start_time;
-                outln("[{:{}}] {:{}}/{}: Finish {}: {}ms", view_id, digits_for_view_id, result.test.index, digits_for_test_id, tests.size(), result.test.relative_path, duration.to_milliseconds());
-            }
-
-            switch (result.result) {
-            case TestResult::Pass:
-                ++stats.pass_count;
-                break;
-            case TestResult::Fail:
-                ++stats.fail_count;
-                break;
-            case TestResult::Timeout:
-                ++stats.timeout_count;
-                break;
-            case TestResult::Crashed:
-                ++stats.crashed_count;
-                break;
-            case TestResult::Skipped:
-                ++stats.skipped_count;
-                break;
-            }
-
-            if (result.result != TestResult::Pass)
-                stats.non_passing_tests.append(move(result));
-
-            if (--tests_remaining == 0)
-                s_all_tests_complete->resolve({});
-            else
-                run_next_test();
-        });
-
-        Core::deferred_invoke([run_next_test]() {
-            run_next_test();
-        });
-    }
-
-    auto result_or_rejection = s_all_tests_complete->await();
+    auto result_or_rejection = run_test_batch(views, tests, total_tests, global_test_index, total_tests_remaining, stats, app);
 
     if (result_or_rejection.is_error())
-        outln("Halted; {} tests not executed.", tests_remaining);
+        outln("Halted; {} tests not executed.", total_tests_remaining);
     else if (log_on_one_line)
         outln("\33[2K\rDone!");
 
@@ -761,7 +789,7 @@ static ErrorOr<int> run_tests(Core::AnonymousBuffer const& theme, Web::DevicePix
         }
     }
 
-    return test_stats.fail_count + test_stats.timeout_count + test_stats.crashed_count + tests_remaining;
+    return stats.fail_count + stats.timeout_count + stats.crashed_count + total_tests_remaining;
 }
 
 static void handle_signal(int signal)
