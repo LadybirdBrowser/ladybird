b25f53f0eddd51524e3cc70de914ac5d43397960
Callum Law <callumlaw1709@outlook.com>
2026-01-11 00:17:50 +1300

    LibWeb: Properly parse attributes for CSS-connected `FontFace`
    Previously we would just set the attributes to the serialized
    descriptors, even if they were the empty string.
    
    We now apply defaults when we have empty descriptors and apply parsing
    logic from the various `set_*` methods (only applicable to `font-family`
    so far where we now extract the value from either a string or a
    custom-ident)
    
    Fixes an issue in some css/css-shapes WPT tests where we weren't
    properly matching fonts.

--- Libraries/LibWeb/CSS/FontFace.h
+++ Libraries/LibWeb/CSS/FontFace.h
@@ -40,36 +40,47 @@ public:
 
     String family() const { return m_family; }
     WebIDL::ExceptionOr<void> set_family(String const&);
+    void set_family_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String style() const { return m_style; }
     WebIDL::ExceptionOr<void> set_style(String const&);
+    void set_style_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String weight() const { return m_weight; }
     WebIDL::ExceptionOr<void> set_weight(String const&);
+    void set_weight_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String stretch() const { return m_stretch; }
     WebIDL::ExceptionOr<void> set_stretch(String const&);
+    void set_stretch_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String unicode_range() const { return m_unicode_range; }
     WebIDL::ExceptionOr<void> set_unicode_range(String const&);
+    void set_unicode_range_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String feature_settings() const { return m_feature_settings; }
     WebIDL::ExceptionOr<void> set_feature_settings(String const&);
+    void set_feature_settings_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String variation_settings() const { return m_variation_settings; }
     WebIDL::ExceptionOr<void> set_variation_settings(String const&);
+    void set_variation_settings_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String display() const { return m_display; }
     WebIDL::ExceptionOr<void> set_display(String const&);
+    void set_display_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String ascent_override() const { return m_ascent_override; }
     WebIDL::ExceptionOr<void> set_ascent_override(String const&);
+    void set_ascent_override_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String descent_override() const { return m_descent_override; }
     WebIDL::ExceptionOr<void> set_descent_override(String const&);
+    void set_descent_override_impl(NonnullRefPtr<StyleValue const> const& value);
 
     String line_gap_override() const { return m_line_gap_override; }
     WebIDL::ExceptionOr<void> set_line_gap_override(String const&);
+    void set_line_gap_override_impl(NonnullRefPtr<StyleValue const> const& value);
 
     bool is_css_connected() const { return m_css_font_face_rule != nullptr; }
     void disconnect_from_css_rule();
