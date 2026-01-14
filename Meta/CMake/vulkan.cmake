416f586c11add27f00c7853cff44048ca54fa3e1
Undefine <undefine@undefine.pl>
2026-01-14 15:54:07 +0100

    LibGfx+Meta: Move the USE_VULKAN_IMAGES definition to CMake
    We had the same check in both places so let's just unify it.

--- Meta/CMake/vulkan.cmake
+++ Meta/CMake/vulkan.cmake
@@ -6,5 +6,11 @@ if (NOT APPLE)
     if (VulkanHeaders_FOUND AND Vulkan_FOUND)
         set(HAS_VULKAN ON CACHE BOOL "" FORCE)
         add_cxx_compile_definitions(USE_VULKAN=1)
+
+        # Sharable Vulkan images are currently only implemented on Linux and FreeBSD
+        if ((LINUX AND NOT ANDROID) OR CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
+            set(USE_VULKAN_IMAGES ON CACHE BOOL "" FORCE)
+            add_cxx_compile_definitions(USE_VULKAN_IMAGES=1)
+        endif()
     endif()
 endif()
