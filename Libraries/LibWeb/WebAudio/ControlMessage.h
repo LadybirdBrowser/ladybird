6c34b7a656f7ade82b1c043847717396255914c4
Ben Eidson <b.e.eidson@gmail.com>
2026-01-18 13:42:23 -0500

    LibWeb/WebAudio: Add stable IDs to AudioNodes
    Modeled after UniqueNodeID and uses incremental counter on
    BaseAudioContext to assign IDs.

--- Libraries/LibWeb/WebAudio/ControlMessage.h
+++ Libraries/LibWeb/WebAudio/ControlMessage.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Ben Eidson <b.e.eidson@gmail.com>
+ * Copyright (c) 2025-2026, Ben Eidson <b.e.eidson@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -7,22 +7,21 @@
 #pragma once
 
 #include <AK/Variant.h>
+#include <LibWeb/WebAudio/Types.h>
 
 namespace Web::WebAudio {
 
-using SourceId = u64;
-
 struct StartSource {
-    SourceId id { 0 }; // FIXME: stable per-source id for render-thread routing
+    NodeID node_id { 0 };
     double when { 0.0 };
 };
 
 struct StopSource {
-    SourceId id { 0 }; // FIXME: stable per-source id for render-thread routing
+    NodeID node_id { 0 };
     double when { 0.0 };
 };
 
-// FIXME: add more event types
+// FIXME: add more message types
 
 // https://webaudio.github.io/web-audio-api/#control-message
 using ControlMessage = Variant<StartSource, StopSource>;
--- Tests/LibWeb/TestControlMessageQueue.cpp
+++ Tests/LibWeb/TestControlMessageQueue.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Ben Eidson <b.e.eidson@gmail.com>
+ * Copyright (c) 2025-2026, Ben Eidson <b.e.eidson@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -12,8 +12,8 @@ TEST_CASE(drain_returns_all_and_clears)
 {
     Web::WebAudio::ControlMessageQueue queue;
 
-    queue.enqueue(Web::WebAudio::StartSource { .when = 1.0 });
-    queue.enqueue(Web::WebAudio::StopSource { .when = 2.0 });
+    queue.enqueue(Web::WebAudio::StartSource { .node_id = Web::WebAudio::NodeID { 0 }, .when = 1.0 });
+    queue.enqueue(Web::WebAudio::StopSource { .node_id = Web::WebAudio::NodeID { 1 }, .when = 2.0 });
 
     auto batch = queue.drain();
     EXPECT_EQ(batch.size(), 2u);
@@ -26,19 +26,22 @@ TEST_CASE(drain_preserves_first_in_first_out)
 {
     Web::WebAudio::ControlMessageQueue queue;
 
-    queue.enqueue(Web::WebAudio::StartSource { .when = 1.0 });
-    queue.enqueue(Web::WebAudio::StopSource { .when = 2.0 });
-    queue.enqueue(Web::WebAudio::StartSource { .when = 3.0 });
+    queue.enqueue(Web::WebAudio::StartSource { .node_id = Web::WebAudio::NodeID { 0 }, .when = 1.0 });
+    queue.enqueue(Web::WebAudio::StopSource { .node_id = Web::WebAudio::NodeID { 1 }, .when = 2.0 });
+    queue.enqueue(Web::WebAudio::StartSource { .node_id = Web::WebAudio::NodeID { 2 }, .when = 3.0 });
 
     auto batch = queue.drain();
     EXPECT_EQ(batch.size(), 3u);
 
     EXPECT(batch[0].has<Web::WebAudio::StartSource>());
     EXPECT_EQ(batch[0].get<Web::WebAudio::StartSource>().when, 1.0);
+    EXPECT_EQ(batch[0].get<Web::WebAudio::StartSource>().node_id, 0u);
 
     EXPECT(batch[1].has<Web::WebAudio::StopSource>());
     EXPECT_EQ(batch[1].get<Web::WebAudio::StopSource>().when, 2.0);
+    EXPECT_EQ(batch[1].get<Web::WebAudio::StopSource>().node_id, Web::WebAudio::NodeID { 1 });
 
     EXPECT(batch[2].has<Web::WebAudio::StartSource>());
     EXPECT_EQ(batch[2].get<Web::WebAudio::StartSource>().when, 3.0);
+    EXPECT_EQ(batch[2].get<Web::WebAudio::StartSource>().node_id, Web::WebAudio::NodeID { 2 });
 }
