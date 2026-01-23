LadybirdBrowser/ladybird

commit 0a57e1e8ac465f07af4674cb05850fdc13dc0009

LibWeb: Clarify some scrolling-related code
A lot of our scrolling code is quite old, and doesn't match the spec,
but does use some similar names. This is quite confusing. In particular
`perform_scroll_of_viewport()` is not the same as the spec algorithm.
That algorithm is actually almost implemented in
`scroll_viewport_by_delta()`.

To clarify things, this commit makes a few changes:
- Rename perform_scroll_of_viewport() to
  perform_scroll_of_viewport_scrolling_box(). This is a better match
  for how we use this method, even if it's not actually a match for the
  algorithm. (:yakbait:)
- Move `scroll_viewport_by_delta()`'s code into a new
  `perform_a_scroll_of_the_viewport()` method, and make it take a
  position like it should. `scroll_viewport_by_delta()` now calls it
  with a calculated position.

I've avoided reusing the original `perform_scroll_of_viewport()` name to
avoid accidents.

Libraries/LibWeb/HTML/Navigable.cpp
@@ -2522,8 +2522,11 @@ void Navigable::set_viewport_size(CSSPixelSize size)
     }
 }
 
-void Navigable::perform_scroll_of_viewport(CSSPixelPoint new_position)
+void Navigable::perform_scroll_of_viewport_scrolling_box(CSSPixelPoint new_position)
 {
+    // NB: This method is ad-hoc, but is currently called where "perform a scroll of a scrolling box" would be,
+    //     where the box is the viewport.
+    //     https://drafts.csswg.org/cssom-view/#perform-a-scroll
     if (m_viewport_scroll_offset != new_position) {
         m_viewport_scroll_offset = new_position;
         scroll_offset_did_change();
@@ -2799,8 +2802,13 @@ RefPtr<Gfx::SkiaBackendContext> Navigable::skia_backend_context() const
     return m_skia_backend_context;
 }
 
-// https://drafts.csswg.org/cssom-view/#viewport-perform-a-scroll
 void Navigable::scroll_viewport_by_delta(CSSPixelPoint delta)
+{
+    return perform_a_scroll_of_the_viewport(m_viewport_scroll_offset + delta);
+}
+
+// https://drafts.csswg.org/cssom-view/#viewport-perform-a-scroll
+void Navigable::perform_a_scroll_of_the_viewport(CSSPixelPoint position)
 {
     // 1. Let doc be the viewport’s associated Document.
     auto doc = active_document();
@@ -2817,12 +2825,10 @@ void Navigable::scroll_viewport_by_delta(CSSPixelPoint delta)
     auto max_y = viewport_rect.height().to_double() - vv->height();
 
     // 5. Let dx be the horizontal component of position - the value vv’s pageLeft attribute
-    // NOTE: Function accepts delta so we use that directly.
-    auto dx = delta.x().to_double();
+    auto dx = position.x().to_double() - vv->page_left();
 
     // 6. Let dy be the vertical component of position - the value of vv’s pageTop attribute
-    // NOTE: Function accepts delta so we use that directly.
-    auto dy = delta.y().to_double();
+    auto dy = position.y().to_double() - vv->page_top();
 
     // 7. Let visual x be the value of vv’s offsetLeft attribute.
     auto visual_x = vv->offset_left();
@@ -2852,7 +2858,7 @@ void Navigable::scroll_viewport_by_delta(CSSPixelPoint delta)
     // NOTE: Clamp to the scrolling area.
     new_viewport_scroll_offset.set_x(max(0.0, min(new_viewport_scroll_offset.x(), scrolling_area.width() - viewport_size().width().to_double())));
     new_viewport_scroll_offset.set_y(max(0.0, min(new_viewport_scroll_offset.y(), scrolling_area.height() - viewport_size().height().to_double())));
-    perform_scroll_of_viewport(new_viewport_scroll_offset.to_type<CSSPixels>());
+    perform_scroll_of_viewport_scrolling_box(new_viewport_scroll_offset.to_type<CSSPixels>());
 
     // 15. Perform a scroll of vv’s scrolling box to its current scroll position + (visual dx, visual dy) with element as the associated element, and behavior as the scroll behavior.
     vv->scroll_by({ visual_dx, visual_dy });

Libraries/LibWeb/DOM/Document.cpp
@@ -2966,7 +2966,7 @@ void Document::scroll_to_the_beginning_of_the_document()
 {
     // FIXME: Actually implement this algorithm
     if (auto navigable = this->navigable())
-        navigable->perform_scroll_of_viewport({ 0, 0 });
+        navigable->perform_scroll_of_viewport_scrolling_box({ 0, 0 });
 }
 
 StringView Document::ready_state() const

Libraries/LibWeb/DOM/Element.cpp
@@ -2489,9 +2489,9 @@ static ErrorOr<void> scroll_an_element_into_view(Element& target, Bindings::Scro
 
                 // AD-HOC:
                 // NOTE: Since calculated position is relative to the viewport, we need to add the viewport's position to it
-                //       before passing to perform_scroll_of_viewport() that expects a position relative to the page.
+                //       before passing to perform_a_scroll_of_the_viewport() that expects a position relative to the page.
                 position.set_y(position.y() + document.viewport_rect().y());
-                document.navigable()->perform_scroll_of_viewport(position);
+                document.navigable()->perform_a_scroll_of_the_viewport(position);
             }
         }
 

Libraries/LibWeb/HTML/Navigable.h
@@ -170,7 +170,7 @@ public:
     CSSPixelRect viewport_rect() const { return { m_viewport_scroll_offset, m_viewport_size }; }
     CSSPixelSize viewport_size() const { return m_viewport_size; }
     void set_viewport_size(CSSPixelSize);
-    void perform_scroll_of_viewport(CSSPixelPoint position);
+    void perform_scroll_of_viewport_scrolling_box(CSSPixelPoint position);
 
     // https://html.spec.whatwg.org/multipage/webappapis.html#rendering-opportunity
     [[nodiscard]] bool has_a_rendering_opportunity() const;
@@ -222,6 +222,7 @@ public:
     bool fast_is() const = delete;
 
     void scroll_viewport_by_delta(CSSPixelPoint delta);
+    void perform_a_scroll_of_the_viewport(CSSPixelPoint position);
     void reset_zoom();
 
 protected:

Libraries/LibWeb/HTML/Window.cpp
@@ -1543,7 +1543,7 @@ void Window::scroll(ScrollToOptions const& options)
 
     // 12. Perform a scroll of the viewport to position, document’s root element as the associated element, if there is
     //     one, or null otherwise, and the scroll behavior being the value of the behavior dictionary member of options.
-    navigable->perform_scroll_of_viewport({ x, y });
+    navigable->perform_a_scroll_of_the_viewport({ x, y });
 }
 
 // https://w3c.github.io/csswg-drafts/cssom-view/#dom-window-scroll

Libraries/LibWeb/Painting/PaintableBox.cpp
@@ -1171,7 +1171,7 @@ void PaintableBox::scroll_to_mouse_position(CSSPixelPoint position)
     new_scroll_offset.set_primary_offset_for_orientation(orientation, scroll_position_in_pixels);
 
     if (is_viewport_paintable())
-        document().navigable()->perform_scroll_of_viewport(new_scroll_offset);
+        document().navigable()->perform_scroll_of_viewport_scrolling_box(new_scroll_offset);
     else
         (void)set_scroll_offset(new_scroll_offset);
 }

commit 7eb14e64ce3636edd1c6e7942856d27475f36998

LibWeb: Fix scroll regression when pinch-to-zoom is applied
When scrolling with a visual viewport offset (from pinch-to-zoom),
scroll_viewport_by_delta() was passing m_viewport_scroll_offset + delta
to perform_a_scroll_of_the_viewport(). However, that function calculates
the scroll delta as `position - page_top()`, where page_top() includes
the visual viewport offset. This caused the effective scroll delta to be
reduced by the visual offset amount.

Fix by using the current page position (which includes the visual
offset) as the base for the delta calculation.

Regression from 0a57e1e8ac465f07af4674cb05850fdc13dc0009.

Libraries/LibWeb/HTML/Navigable.cpp
@@ -2803,7 +2803,9 @@ RefPtr<Gfx::SkiaBackendContext> Navigable::skia_backend_context() const
 
 GC::Ref<WebIDL::Promise> Navigable::scroll_viewport_by_delta(CSSPixelPoint delta)
 {
-    return perform_a_scroll_of_the_viewport(m_viewport_scroll_offset + delta);
+    auto vv = active_document()->visual_viewport();
+    CSSPixelPoint page_position { CSSPixels(vv->page_left()), CSSPixels(vv->page_top()) };
+    return perform_a_scroll_of_the_viewport(page_position + delta);
 }
 
 // https://drafts.csswg.org/cssom-view/#viewport-perform-a-scroll

Tests/LibWeb/Text/input/scroll-with-pinch-zoom-applied.html
@@ -1,0 +1,56 @@
+<!DOCTYPE html>
+<style>
+    body {
+        margin: 0;
+        overflow: scroll;
+    }
+
+    .spacer {
+        height: 3000px;
+        background: linear-gradient(to bottom, red, blue);
+    }
+</style>
+<div class="spacer"></div>
+<script src="include.js"></script>
+<script>
+    asyncTest(done => {
+        const vv = window.visualViewport;
+        const scrollDelta = 100;
+
+        println(`Initial: pageTop=${vv.pageTop}, scale=${vv.scale}`);
+
+        // First, zoom in
+        vv.addEventListener("resize", function onResize() {
+            vv.removeEventListener("resize", onResize);
+
+            println(`After zoom: pageTop=${vv.pageTop}, scale=${vv.scale}, offsetTop=${vv.offsetTop}`);
+
+            const pageTopBefore = vv.pageTop;
+
+            // Scroll down
+            internals.wheel(100, 100, 0, scrollDelta);
+
+            // Poll for changes since scroll events may not fire for visual viewport panning
+            requestAnimationFrame(() => {
+                const pageTopAfter = vv.pageTop;
+                const scrolled = pageTopAfter - pageTopBefore;
+
+                println(`pageTop changed by: ${scrolled}`);
+
+                // The scroll amount should be approximately equal to the requested delta
+                // Allow small tolerance for floating point
+                if (Math.abs(scrolled - scrollDelta) < 1) {
+                    println("PASS");
+                } else {
+                    println(`FAIL: Expected scroll of ${scrollDelta}, got ${scrolled}`);
+                }
+                done();
+            });
+        });
+
+        requestAnimationFrame(() => {
+            // Zoom in (scale increases with positive delta)
+            internals.pinch(100, 100, 0.5);
+        });
+    });
+</script>