0adfdc23567dc0759e2c364ab2f281f7681eeaff
R-Goc <ryszardgoc@gmail.com>
2026-01-30 21:46:58 +0100

    LibWeb: Rename CSS::URL to CSS::CSSURL
    This is done in preparation for the unity build, as there is a lot of
    namespace conflicts around URL::URL and CSS::URL.

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -396,7 +396,7 @@ class TransformationStyleValue;
 class TreeCountingFunctionStyleValue;
 class UnicodeRangeStyleValue;
 class UnresolvedStyleValue;
-class URL;
+class CSSURL;
 class URLStyleValue;
 class ViewFunctionStyleValue;
 class VisualViewport;
