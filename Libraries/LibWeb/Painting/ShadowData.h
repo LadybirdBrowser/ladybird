1dd5409630fbc91f55955fe3f2d36de5f5f20534
Jelle Raaijmakers <jelle@ladybird.org>
2026-01-30 16:11:38 +0100

    LibWeb: Add helpers to convert CSS::ShadowData to Painting::ShadowData

--- Libraries/LibWeb/Painting/ShadowData.h
+++ Libraries/LibWeb/Painting/ShadowData.h
@@ -2,6 +2,7 @@
  * Copyright (c) 2018-2020, Andreas Kling <andreas@ladybird.org>
  * Copyright (c) 2021-2022, Sam Atkins <atkinssj@serenityos.org>
  * Copyright (c) 2022, MacDue <macdue@dueutil.tech>
+ * Copyright (c) 2026, Jelle Raaijmakers <jelle@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -18,6 +19,17 @@ enum class ShadowPlacement {
     Inner,
 };
 
+inline ShadowPlacement shadow_placement_from_css(CSS::ShadowPlacement placement)
+{
+    switch (placement) {
+    case CSS::ShadowPlacement::Outer:
+        return ShadowPlacement::Outer;
+    case CSS::ShadowPlacement::Inner:
+        return ShadowPlacement::Inner;
+    }
+    VERIFY_NOT_REACHED();
+}
+
 struct ShadowData {
     Gfx::Color color;
     CSSPixels offset_x;
@@ -25,6 +37,18 @@ struct ShadowData {
     CSSPixels blur_radius;
     CSSPixels spread_distance;
     ShadowPlacement placement;
+
+    static ShadowData from_css(CSS::ShadowData const& shadow, Layout::Node const& layout_node)
+    {
+        return {
+            shadow.color,
+            shadow.offset_x.to_px(layout_node),
+            shadow.offset_y.to_px(layout_node),
+            shadow.blur_radius.to_px(layout_node),
+            shadow.spread_distance.to_px(layout_node),
+            shadow_placement_from_css(shadow.placement),
+        };
+    }
 };
 
 }
