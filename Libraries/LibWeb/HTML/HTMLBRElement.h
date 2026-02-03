ed2018f301a080c4807dae5e59bc1add39c5de51
Psychpsyo <psychpsyo@gmail.com>
2026-02-03 23:30:36 +0100

    LibWeb: Add presentational hints for clear attribute on BR elements

--- Libraries/LibWeb/HTML/HTMLBRElement.h
+++ Libraries/LibWeb/HTML/HTMLBRElement.h
@@ -18,6 +18,8 @@ public:
     virtual ~HTMLBRElement() override;
 
     virtual GC::Ptr<Layout::Node> create_layout_node(GC::Ref<CSS::ComputedProperties>) override;
+    virtual bool is_presentational_hint(FlyString const&) const override;
+    virtual void apply_presentational_hints(GC::Ref<CSS::CascadedProperties>) const override;
     virtual void adjust_computed_style(CSS::ComputedProperties&) override;
 
 private:
