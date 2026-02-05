51330eb5db1e0a4cf20d6ce96baf09cb20faecf6
Sam Atkins <sam@ladybird.org>
2026-02-05 16:06:29 +0000

    LibWeb: Make StyleElementUtils into a base class, StyleElementBase
    This will allow us to cast an Element to a StyleElementBase without
    having to know whether it's an HTML or SVG style element.

--- Libraries/LibWeb/SVG/SVGStyleElement.cpp
+++ Libraries/LibWeb/SVG/SVGStyleElement.cpp
@@ -27,39 +27,25 @@ void SVGStyleElement::initialize(JS::Realm& realm)
 void SVGStyleElement::visit_edges(Cell::Visitor& visitor)
 {
     Base::visit_edges(visitor);
-    m_style_element_utils.visit_edges(visitor);
+    visit_style_element_edges(visitor);
 }
 
 void SVGStyleElement::children_changed(ChildrenChangedMetadata const* metadata)
 {
     Base::children_changed(metadata);
-    m_style_element_utils.update_a_style_block(*this);
+    update_a_style_block();
 }
 
 void SVGStyleElement::inserted()
 {
-    m_style_element_utils.update_a_style_block(*this);
+    update_a_style_block();
     Base::inserted();
 }
 
 void SVGStyleElement::removed_from(Node* old_parent, Node& old_root)
 {
-    m_style_element_utils.update_a_style_block(*this);
+    update_a_style_block();
     Base::removed_from(old_parent, old_root);
-}
-
-// https://www.w3.org/TR/cssom/#dom-linkstyle-sheet
-CSS::CSSStyleSheet* SVGStyleElement::sheet()
-{
-    // The sheet attribute must return the associated CSS style sheet for the node or null if there is no associated CSS style sheet.
-    return m_style_element_utils.sheet();
-}
-
-// https://www.w3.org/TR/cssom/#dom-linkstyle-sheet
-CSS::CSSStyleSheet const* SVGStyleElement::sheet() const
-{
-    // The sheet attribute must return the associated CSS style sheet for the node or null if there is no associated CSS style sheet.
-    return m_style_element_utils.sheet();
 }
 
 }
