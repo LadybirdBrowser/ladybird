755a0a1d1ee2111437a1aa621045c680c947d115
Andreas Kling <andreas@ladybird.org>
2026-02-09 23:10:01 +0100

    Tests/LibJS: Add test for rest-only param with arguments object access
    Test that a function whose only parameter is a rest parameter correctly
    receives its value even when the function also accesses the arguments
    object.

--- Tests/LibJS/Runtime/functions/function-rest-params.js
+++ Tests/LibJS/Runtime/functions/function-rest-params.js
@@ -31,6 +31,15 @@ test("rest parameter after normal parameters with arguments", () => {
     foo("foo", 123, undefined, { foo: "bar" });
 });
 
+test("rest parameter as only parameter with arguments object access", () => {
+    function foo(...args) {
+        expect(args).toEqual([1, 2, 3]);
+        expect(arguments.length).toBe(3);
+        expect(arguments[0]).toBe(1);
+    }
+    foo(1, 2, 3);
+});
+
 test("basic arrow function rest parameters", () => {
     let foo = (...a) => {
         expect(a).toBeInstanceOf(Array);
