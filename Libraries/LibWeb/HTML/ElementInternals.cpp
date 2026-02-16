c82de09faa7381b25cb4cf8927667f37b2629aad
Luke Wilde <luke@ladybird.org>
2026-02-16 18:33:17 +0000

    LibWeb: Implement ElementInternals#form

--- Libraries/LibWeb/HTML/ElementInternals.cpp
+++ Libraries/LibWeb/HTML/ElementInternals.cpp
@@ -80,8 +80,7 @@ WebIDL::ExceptionOr<GC::Ptr<HTMLFormElement>> ElementInternals::form() const
     if (!m_target_element->is_form_associated_custom_element())
         return WebIDL::NotSupportedError::create(realm(), "Element is not a form-associated custom element"_utf16);
 
-    dbgln("FIXME: ElementInternals::form()");
-    return WebIDL::NotFoundError::create(realm(), "FIXME: ElementInternals::form()"_utf16);
+    return m_target_element->form();
 }
 
 // https://html.spec.whatwg.org/multipage/custom-elements.html#dom-elementinternals-setvalidity
