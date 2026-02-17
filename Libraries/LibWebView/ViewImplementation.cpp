fce8e6d362dafd04722f759f98c824dd03e60c6e
jeetsh4h <jeetsh4h@gmail.com>
2026-02-17 14:23:22 +0000

    LibWebView: Treat cancelled web event as handled

--- Libraries/LibWebView/ViewImplementation.cpp
+++ Libraries/LibWebView/ViewImplementation.cpp
@@ -228,7 +228,7 @@ void ViewImplementation::did_finish_handling_input_event(Badge<WebContentClient>
 {
     auto event = m_pending_input_events.dequeue();
 
-    if (event_result == Web::EventResult::Handled)
+    if (event_result == Web::EventResult::Handled || event_result == Web::EventResult::Cancelled)
         return;
 
     // Here we handle events that were not consumed or cancelled by the WebContent. Propagate the event back
