b0b0275e9e9d2effd14eff89475d65c7a115a169
Andreas Kling <andreas@ladybird.org>
2026-02-19 11:18:41 +0100

    LibJS: Add bytecode test for switch statement completion values
    The completion value of a switch case is incorrectly reset to undefined
    when a statement without a result (like a variable declaration) follows
    an expression statement. This will be fixed in the next commit.

---
+++ Tests/LibJS/Bytecode/input/switch-completion-value.js
@@ -0,0 +1,3 @@
+console.log(eval("switch(1) { case 1: 'hello'; let x = 1; }"));
+console.log(eval("switch(1) { case 1: 'first'; 'second'; }"));
+console.log(eval("switch(1) { case 1: 'matched'; break; default: 'default'; }"));
