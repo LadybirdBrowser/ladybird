34b7cb6e5544b3512f7dc906bb8641f9a74326b8
InvalidUsernameException <InvalidUsernameException@users.noreply.github.com>
2026-03-08 11:47:14 +0100

    LibJS: Explicitly handle all operand types when determining clobbering
    The last time a new operand type was added, the effects from that on the
    function changed in this commit were seemingly not properly considered,
    introducing a bug. To avoid such errors in the future, rewrite the code
    to produce a compile-time error if new operand types are added.
    
    No functional changes yet, the actual bugfix will be in a
    followup-commit.

--- Libraries/LibJS/Rust/src/bytecode/generator.rs
+++ Libraries/LibJS/Rust/src/bytecode/generator.rs
@@ -435,12 +435,15 @@ impl Generator {
         &mut self,
         operand: &ScopedOperand,
     ) -> ScopedOperand {
-        if operand.operand().is_local() {
-            let reg = self.allocate_register();
-            self.emit_mov(&reg, operand);
-            reg
-        } else {
-            operand.clone()
+        match operand.operand().operand_type() {
+            OperandType::Register | OperandType::Constant | OperandType::Argument => {
+                operand.clone()
+            }
+            OperandType::Local => {
+                let reg = self.allocate_register();
+                self.emit_mov(&reg, operand);
+                reg
+            }
         }
     }
 
