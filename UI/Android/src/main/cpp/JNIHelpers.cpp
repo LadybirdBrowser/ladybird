7764c0ec371d2e842464e20ecbd6686c3bc4d68e
Eric Apostal <ehapostal04@gmail.com>
2026-03-28 13:03:55 -0400

    UI/Android: Fix `jstring_from_ak_string` incorrectly interpreting ascii

--- UI/Android/src/main/cpp/JNIHelpers.cpp
+++ UI/Android/src/main/cpp/JNIHelpers.cpp
@@ -6,16 +6,25 @@
 
 #include "JNIHelpers.h"
 #include <AK/Utf16String.h>
+#include <AK/Vector.h>
 
 namespace Ladybird {
 
 jstring JavaEnvironment::jstring_from_ak_string(String const& str)
 {
+    auto as_utf16 = AK::Utf16String::from_utf8(str);
+    auto view = as_utf16.utf16_view();
 
-auto as_utf16 = AK::Utf16String::from_utf8(str);
-auto view = as_utf16.utf16_view();
-if (view.has_ascii_storage())
-    return m_env->NewStringUTF(view.ascii_span().data());
-return m_env->NewString(reinterpret_cast<jchar const*>(view.utf16_span().data()), view.length_in_code_units());
+    if (view.has_ascii_storage()) {
+        // ascii_span is UTF-8, so we have to get it to UTF-16 for java
+        auto ascii = view.ascii_span();
+        Vector<jchar> widened;
+        widened.ensure_capacity(ascii.size());
+        for (size_t i = 0; i < ascii.size(); ++i)
+            widened.unchecked_append(static_cast<jchar>(static_cast<unsigned char>(ascii[i])));
+        return m_env->NewString(widened.data(), widened.size());
+    }
+
+    return m_env->NewString(reinterpret_cast<jchar const*>(view.utf16_span().data()), view.length_in_code_units());
 }
 }
