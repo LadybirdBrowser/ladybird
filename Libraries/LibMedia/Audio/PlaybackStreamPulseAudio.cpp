LadybirdBrowser/ladybird

commit 39d865b403941d7d5b76b4a149a618be7b26284f

LibMedia: Provide new PlaybackStreams through promises
This allows us to avoid returning a PlaybackStream in cases where the
async initialization fails.

This is a step towards more graceful fallbacks when audio fails in
AudioMixingSink.

Libraries/LibMedia/Audio/PlaybackStreamPulseAudio.cpp
@@ -1,9 +1,10 @@
 /*
- * Copyright (c) 2023-2025, Gregory Bertilson <gregory@ladybird.org>
+ * Copyright (c) 2023-2026, Gregory Bertilson <gregory@ladybird.org>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibCore/EventLoop.h>
 #include <LibCore/ThreadedPromise.h>
 #include <LibThreading/Thread.h>
 
@@ -11,42 +12,55 @@
 
 namespace Audio {
 
-#define TRY_OR_EXIT_THREAD(expression)                                                                       \
-    ({                                                                                                       \
-        auto&& __temporary_result = (expression);                                                            \
-        if (__temporary_result.is_error()) [[unlikely]] {                                                    \
-            warnln("Failure in PulseAudio control thread: {}", __temporary_result.error().string_literal()); \
-            internal_state->exit();                                                                          \
-            return 1;                                                                                        \
-        }                                                                                                    \
-        __temporary_result.release_value();                                                                  \
+#define TRY_OR_REJECT_AND_EXIT(expression)                                                                              \
+    ({                                                                                                                  \
+        auto&& __temporary_result = (expression);                                                                       \
+        if (__temporary_result.is_error()) [[unlikely]] {                                                               \
+            warnln("Failure in PulseAudio control thread: {}", __temporary_result.error().string_literal());            \
+            auto event_loop = main_thread_event_loop->take();                                                           \
+            event_loop->deferred_invoke([promise = move(promise), error = __temporary_result.release_error()] mutable { \
+                promise->reject(move(error));                                                                           \
+            });                                                                                                         \
+            internal_state->exit();                                                                                     \
+            return 1;                                                                                                   \
+        }                                                                                                               \
+        __temporary_result.release_value();                                                                             \
     })
 
-ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStream::create(OutputState initial_output_state, u32 target_latency_ms, SampleSpecificationCallback&& sample_specification_selected_callback, AudioDataRequestCallback&& data_request_callback)
+NonnullRefPtr<PlaybackStream::CreatePromise> PlaybackStream::create(OutputState initial_output_state, u32 target_latency_ms, AudioDataRequestCallback&& data_request_callback)
 {
-    return PlaybackStreamPulseAudio::create(initial_output_state, target_latency_ms, move(sample_specification_selected_callback), move(data_request_callback));
+    return PlaybackStreamPulseAudio::create(initial_output_state, target_latency_ms, move(data_request_callback));
 }
 
-ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStreamPulseAudio::create(OutputState initial_state, u32 target_latency_ms, SampleSpecificationCallback&& sample_specification_selected_callback, AudioDataRequestCallback&& data_request_callback)
+NonnullRefPtr<PlaybackStream::CreatePromise> PlaybackStreamPulseAudio::create(OutputState initial_state, u32 target_latency_ms, AudioDataRequestCallback&& data_request_callback)
 {
     VERIFY(data_request_callback);
 
+    auto promise = CreatePromise::construct();
+
     // Create an internal state for the control thread to hold on to.
-    auto internal_state = TRY(adopt_nonnull_ref_or_enomem(new (nothrow) InternalState()));
-    auto playback_stream = TRY(adopt_nonnull_ref_or_enomem(new (nothrow) PlaybackStreamPulseAudio(internal_state)));
+    auto internal_state = MUST(adopt_nonnull_ref_or_enomem(new (nothrow) InternalState()));
+    auto playback_stream = MUST(adopt_nonnull_ref_or_enomem(new (nothrow) PlaybackStreamPulseAudio(internal_state)));
 
     // Create the control thread and start it.
-    auto thread = TRY(Threading::Thread::try_create("Audio Control"sv, [=, sample_specification_selected_callback = move(sample_specification_selected_callback), data_request_callback = move(data_request_callback)]() mutable {
-        auto context = TRY_OR_EXIT_THREAD(PulseAudioContext::the());
-        internal_state->set_stream(TRY_OR_EXIT_THREAD(context->create_stream(initial_state, target_latency_ms, [data_request_callback = move(data_request_callback)](PulseAudioStream&, Span<float> buffer) {
+    auto thread = MUST(Threading::Thread::try_create("Audio Control"sv, [=, main_thread_event_loop = Core::EventLoop::current_weak(), data_request_callback = move(data_request_callback)]() mutable {
+        auto context = TRY_OR_REJECT_AND_EXIT(PulseAudioContext::the());
+        internal_state->set_stream(TRY_OR_REJECT_AND_EXIT(context->create_stream(initial_state, target_latency_ms, [data_request_callback = move(data_request_callback)](PulseAudioStream&, Span<float> buffer) {
             return data_request_callback(buffer);
         })));
 
-        sample_specification_selected_callback(internal_state->stream()->sample_specification());
-
         // PulseAudio retains the last volume it sets for an application. We want to consistently
         // start at 100% volume instead.
-        TRY_OR_EXIT_THREAD(internal_state->stream()->set_volume(1.0));
+        TRY_OR_REJECT_AND_EXIT(internal_state->stream()->set_volume(1.0));
+
+        {
+            auto event_loop = main_thread_event_loop->take();
+            if (!event_loop)
+                return 1;
+            event_loop->deferred_invoke([promise = move(promise), playback_stream] {
+                promise->resolve(playback_stream);
+            });
+        }
 
         internal_state->thread_loop();
         return 0;
@@ -54,12 +68,17 @@ ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStreamPulseAudio::create(OutputSt
 
     thread->start();
     thread->detach();
-    return playback_stream;
+    return promise;
 }
 
 PlaybackStreamPulseAudio::PlaybackStreamPulseAudio(NonnullRefPtr<InternalState> state)
     : m_state(move(state))
 {
+}
+
+SampleSpecification PlaybackStreamPulseAudio::sample_specification() const
+{
+    return m_state->stream()->sample_specification();
 }
 
 PlaybackStreamPulseAudio::~PlaybackStreamPulseAudio()

Libraries/LibMedia/Audio/PlaybackStream.cpp
@@ -9,12 +9,14 @@
 namespace Audio {
 
 #if !defined(AK_OS_WINDOWS)
-ErrorOr<NonnullRefPtr<PlaybackStream>> __attribute__((weak)) PlaybackStream::create(OutputState, u32, SampleSpecificationCallback&&, AudioDataRequestCallback&&)
+NonnullRefPtr<PlaybackStream::CreatePromise> __attribute__((weak)) PlaybackStream::create(OutputState, u32, AudioDataRequestCallback&&)
 #else
-ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStream::create(OutputState, u32, SampleSpecificationCallback&&, AudioDataRequestCallback&&)
+NonnullRefPtr<PlaybackStream::CreatePromise> PlaybackStream::create(OutputState, u32, AudioDataRequestCallback&&)
 #endif
 {
-    return Error::from_string_literal("Audio output is not available for this platform");
+    auto promise = CreatePromise::construct();
+    promise->reject(Error::from_string_literal("Audio output is not available for this platform"));
+    return promise;
 }
 
 }

Libraries/LibMedia/Audio/PlaybackStream.h
@@ -10,6 +10,7 @@
 #include <AK/Function.h>
 #include <AK/Time.h>
 #include <LibCore/Forward.h>
+#include <LibCore/Promise.h>
 #include <LibCore/ThreadedPromise.h>
 #include <LibMedia/Audio/SampleSpecification.h>
 #include <LibMedia/Export.h>
@@ -28,20 +29,22 @@ enum class OutputState {
 // Timing information provided by the class should allow audio timestamps to be tracked with the best accuracy possible.
 class MEDIA_API PlaybackStream : public AtomicRefCounted<PlaybackStream> {
 public:
-    using SampleSpecificationCallback = Function<void(SampleSpecification)>;
+    using CreatePromise = Core::Promise<NonnullRefPtr<PlaybackStream>>;
     using AudioDataRequestCallback = Function<ReadonlySpan<float>(Span<float> buffer)>;
 
-    // Creates a new audio Output class.
+    // Begins creating a new audio output and returns a promise that is resolved when it is ready.
     //
     // The initial_output_state parameter determines whether it will begin playback immediately.
     //
-    // The SampleSpecificationCallback will be called when a SampleSpecification has been selected based on the
-    // default output device. It will always be called before the first data request.
+    // The returned promise will be resolved with the PlaybackStream if the audio output was successfully initialized,
+    // or rejected with an error if not.
     //
-    // The AudioDataRequestCallback will be called when the Output needs more audio data to fill its buffers and
+    // The AudioDataRequestCallback will be called when the output needs more audio data to fill its buffers and
     // continue playback. This callback will only be allowed to run on one thread at a time, to prevent any data
     // race on the resource used by the callback.
-    static ErrorOr<NonnullRefPtr<PlaybackStream>> create(OutputState initial_output_state, u32 target_latency_ms, SampleSpecificationCallback&&, AudioDataRequestCallback&&);
+    static NonnullRefPtr<CreatePromise> create(OutputState initial_output_state, u32 target_latency_ms, AudioDataRequestCallback&&);
+
+    virtual SampleSpecification sample_specification() const = 0;
 
     virtual ~PlaybackStream() = default;
 

Libraries/LibMedia/Audio/PlaybackStreamAudioUnit.cpp
@@ -158,7 +158,7 @@ static void check_audio_channel_layout_size(AudioChannelLayout& layout, u32 size
 
 class AudioState : public RefCounted<AudioState> {
 public:
-    static ErrorOr<NonnullRefPtr<AudioState>> create(PlaybackStream::SampleSpecificationCallback sample_specification_callback, PlaybackStream::AudioDataRequestCallback data_request_callback, OutputState initial_output_state)
+    static ErrorOr<NonnullRefPtr<AudioState>> create(PlaybackStream::AudioDataRequestCallback data_request_callback, OutputState initial_output_state)
     {
         auto state = TRY(adopt_nonnull_ref_or_enomem(new (nothrow) AudioState(move(data_request_callback), initial_output_state)));
 
@@ -183,8 +183,6 @@ public:
         check_audio_channel_layout_size(*layout, layout.size());
         auto channel_map = TRY(audio_channel_layout_to_channel_map(*layout));
         state->m_sample_specification = SampleSpecification(static_cast<u32>(description->mSampleRate), channel_map);
-
-        sample_specification_callback(state->m_sample_specification);
 
         AURenderCallbackStruct callbackStruct;
         callbackStruct.inputProc = &AudioState::on_audio_unit_buffer_request;
@@ -216,6 +214,8 @@ public:
         m_task_queue.append(move(task));
         m_task_queue_is_empty = false;
     }
+
+    SampleSpecification const& sample_specification() const { return m_sample_specification; }
 
     AK::Duration last_sample_time() const
     {
@@ -319,15 +319,23 @@ private:
     Atomic<i64> m_last_sample_time { 0 };
 };
 
-ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStream::create(OutputState initial_output_state, u32 target_latency_ms, SampleSpecificationCallback&& sample_specification_callback, AudioDataRequestCallback&& data_request_callback)
+NonnullRefPtr<PlaybackStream::CreatePromise> PlaybackStream::create(OutputState initial_output_state, u32 target_latency_ms, AudioDataRequestCallback&& data_request_callback)
 {
-    return PlaybackStreamAudioUnit::create(initial_output_state, target_latency_ms, move(sample_specification_callback), move(data_request_callback));
+    return PlaybackStreamAudioUnit::create(initial_output_state, target_latency_ms, move(data_request_callback));
 }
 
-ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStreamAudioUnit::create(OutputState initial_output_state, u32, SampleSpecificationCallback&& sample_specification_callback, AudioDataRequestCallback&& data_request_callback)
+NonnullRefPtr<PlaybackStream::CreatePromise> PlaybackStreamAudioUnit::create(OutputState initial_output_state, u32, AudioDataRequestCallback&& data_request_callback)
 {
-    auto state = TRY(AudioState::create(move(sample_specification_callback), move(data_request_callback), initial_output_state));
-    return TRY(adopt_nonnull_ref_or_enomem(new (nothrow) PlaybackStreamAudioUnit(move(state))));
+    auto promise = CreatePromise::construct();
+    auto state_or_error = AudioState::create(move(data_request_callback), initial_output_state);
+    if (state_or_error.is_error()) {
+        promise->reject(state_or_error.release_error());
+        return promise;
+    }
+    auto state = state_or_error.release_value();
+    auto stream = adopt_ref(*new PlaybackStreamAudioUnit(move(state)));
+    promise->resolve(stream);
+    return promise;
 }
 
 PlaybackStreamAudioUnit::PlaybackStreamAudioUnit(NonnullRefPtr<AudioState> impl)
@@ -336,6 +344,11 @@ PlaybackStreamAudioUnit::PlaybackStreamAudioUnit(NonnullRefPtr<AudioState> impl)
 }
 
 PlaybackStreamAudioUnit::~PlaybackStreamAudioUnit() = default;
+
+SampleSpecification PlaybackStreamAudioUnit::sample_specification() const
+{
+    return m_state->sample_specification();
+}
 
 void PlaybackStreamAudioUnit::set_underrun_callback(Function<void()>)
 {

Libraries/LibMedia/Audio/PlaybackStreamAudioUnit.h
@@ -18,7 +18,9 @@ class AudioState;
 
 class PlaybackStreamAudioUnit final : public PlaybackStream {
 public:
-    static ErrorOr<NonnullRefPtr<PlaybackStream>> create(OutputState initial_output_state, u32 target_latency_ms, SampleSpecificationCallback&&, AudioDataRequestCallback&&);
+    static NonnullRefPtr<CreatePromise> create(OutputState initial_output_state, u32 target_latency_ms, AudioDataRequestCallback&&);
+
+    virtual SampleSpecification sample_specification() const override;
 
     virtual void set_underrun_callback(Function<void()>) override;
 

Libraries/LibMedia/Audio/PlaybackStreamPulseAudio.h
@@ -17,7 +17,9 @@ namespace Audio {
 class PlaybackStreamPulseAudio final
     : public PlaybackStream {
 public:
-    static ErrorOr<NonnullRefPtr<PlaybackStream>> create(OutputState, u32 target_latency_ms, SampleSpecificationCallback&&, AudioDataRequestCallback&&);
+    static NonnullRefPtr<CreatePromise> create(OutputState, u32 target_latency_ms, AudioDataRequestCallback&&);
+
+    virtual SampleSpecification sample_specification() const override;
 
     virtual void set_underrun_callback(Function<void()>) override;
 

Libraries/LibMedia/Audio/PlaybackStreamWasapi.cpp
@@ -50,13 +50,6 @@ using namespace Microsoft::WRL;
         }                                                                                  \
     })
 
-#define TRY_HR(expression)                                                        \
-    ({                                                                            \
-        AK_IGNORE_DIAGNOSTIC("-Wshadow", HRESULT&& _temporary_hr = (expression)); \
-        if (FAILED(_temporary_hr)) [[unlikely]]                                   \
-            return Error::from_windows_error(_temporary_hr);                      \
-    })
-
 // GUID for the playback session. That way all render streams have a single volume slider in the OS interface
 constexpr GUID PlaybackSessionGUID = { // 22f2ca89-210a-492c-a0aa-f25b1d2f33a1
     0x22f2ca89,
@@ -158,9 +151,9 @@ PlaybackStreamWASAPI::~PlaybackStreamWASAPI()
     SetEvent(m_state->buffer_event);
 }
 
-ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStream::create(OutputState initial_output_state, u32 target_latency_ms, SampleSpecificationCallback&& specification_callback, AudioDataRequestCallback&& data_callback)
+NonnullRefPtr<PlaybackStream::CreatePromise> PlaybackStream::create(OutputState initial_output_state, u32 target_latency_ms, AudioDataRequestCallback&& data_callback)
 {
-    return PlaybackStreamWASAPI::create(initial_output_state, target_latency_ms, move(specification_callback), move(data_callback));
+    return PlaybackStreamWASAPI::create(initial_output_state, target_latency_ms, move(data_callback));
 }
 
 static void print_audio_format(WAVEFORMATEXTENSIBLE& format)
@@ -228,8 +221,19 @@ static ErrorOr<ChannelMap> convert_bitmask_to_channel_map(u32 channel_bitmask)
     return ChannelMap { channels };
 }
 
-ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStreamWASAPI::create(OutputState initial_output_state, u32, SampleSpecificationCallback&& sample_specification_callback, AudioDataRequestCallback&& data_request_callback)
+NonnullRefPtr<PlaybackStream::CreatePromise> PlaybackStreamWASAPI::create(OutputState initial_output_state, u32, AudioDataRequestCallback&& data_request_callback)
 {
+    auto promise = CreatePromise::construct();
+
+#define TRY_HR(expression)                                                        \
+    ({                                                                            \
+        AK_IGNORE_DIAGNOSTIC("-Wshadow", HRESULT&& _temporary_hr = (expression)); \
+        if (FAILED(_temporary_hr)) [[unlikely]] {                                 \
+            promise->reject(Error::from_windows_error(_temporary_hr));            \
+            return promise;                                                       \
+        }                                                                         \
+    })
+
     HRESULT hr;
     if (!s_com_uninitializer.initialized) {
         TRY_HR(CoInitializeEx(NULL, COINIT_MULTITHREADED));
@@ -257,10 +261,6 @@ ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStreamWASAPI::create(OutputState 
     VERIFY(popcount(device_format->dwChannelMask) == device_format->Format.nChannels);
     auto channels = device_format->Format.nChannels;
 
-    ChannelMap channel_map = MUST(convert_bitmask_to_channel_map(device_format->dwChannelMask));
-
-    sample_specification_callback(SampleSpecification { device_format->Format.nSamplesPerSec, channel_map });
-
     // Set up a 32bit float pcm stream with whatever sample rate and channels we were given.
 
     auto block_align = channels * sizeof(float);
@@ -279,8 +279,10 @@ ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStreamWASAPI::create(OutputState 
 
     WAVEFORMATEXTENSIBLE* closest_match;
     hr = state->audio_client->IsFormatSupported(AUDCLNT_SHAREMODE_SHARED, &state->wave_format.Format, reinterpret_cast<WAVEFORMATEX**>(&closest_match));
-    if (FAILED(hr))
-        return Error::from_windows_error(hr);
+    if (FAILED(hr)) {
+        promise->reject(Error::from_windows_error(hr));
+        return promise;
+    }
     if (hr == S_FALSE) {
         dbgln("Audio format not supported. Current format:\n");
         print_audio_format(state->wave_format);
@@ -300,8 +302,10 @@ ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStreamWASAPI::create(OutputState 
     TRY_HR(state->audio_client->GetService(IID_PPV_ARGS(&state->clock)));
 
     state->buffer_event = CreateEvent(NULL, FALSE, FALSE, NULL);
-    if (!state->buffer_event)
-        return Error::from_windows_error(hr);
+    if (!state->buffer_event) {
+        promise->reject(Error::from_windows_error(hr));
+        return promise;
+    }
 
     TRY_HR(state->audio_client->SetEventHandle(state->buffer_event));
     TRY_HR(state->clock->GetFrequency(&state->audio_client_clock_frequency));
@@ -319,7 +323,17 @@ ErrorOr<NonnullRefPtr<PlaybackStream>> PlaybackStreamWASAPI::create(OutputState 
     audio_thread->start();
     audio_thread->detach();
 
-    return TRY(adopt_nonnull_ref_or_enomem(new (nothrow) PlaybackStreamWASAPI(move(state))));
+    auto stream = adopt_ref(*new PlaybackStreamWASAPI(move(state)));
+    promise->resolve(stream);
+    return promise;
+
+#undef TRY_HR
+}
+
+SampleSpecification PlaybackStreamWASAPI::sample_specification() const
+{
+    auto channel_map = MUST(convert_bitmask_to_channel_map(m_state->wave_format.dwChannelMask));
+    return SampleSpecification { m_state->wave_format.Format.nSamplesPerSec, channel_map };
 }
 
 int PlaybackStreamWASAPI::AudioState::render_thread_loop(PlaybackStreamWASAPI::AudioState& state)

Libraries/LibMedia/Audio/PlaybackStreamWasapi.h
@@ -14,7 +14,9 @@ namespace Audio {
 
 class PlaybackStreamWASAPI final : public PlaybackStream {
 public:
-    static ErrorOr<NonnullRefPtr<PlaybackStream>> create(OutputState initial_output_state, u32 target_latency_ms, SampleSpecificationCallback&&, AudioDataRequestCallback&&);
+    static NonnullRefPtr<CreatePromise> create(OutputState initial_output_state, u32 target_latency_ms, AudioDataRequestCallback&&);
+
+    virtual SampleSpecification sample_specification() const override;
 
     // The overrun callback must be realtime safe. The buffer size might be small.
     virtual void set_underrun_callback(Function<void()>) override;

commit 6f685f78190afdcb74d1e62de744bf5a2e74a09d

LibMedia: Skip PlaybackStream rejection if the main event loop is dead
This was causing a flake on CI due to the race between the teardown of
the event loop and the rejection of the promise from another thread.

Regressed in 39d865b.

Libraries/LibMedia/Audio/PlaybackStreamPulseAudio.cpp
@@ -18,6 +18,8 @@ namespace Audio {
         if (__temporary_result.is_error()) [[unlikely]] {                                                               \
             warnln("Failure in PulseAudio control thread: {}", __temporary_result.error().string_literal());            \
             auto event_loop = main_thread_event_loop->take();                                                           \
+            if (!event_loop)                                                                                            \
+                return 1;                                                                                               \
             event_loop->deferred_invoke([promise = move(promise), error = __temporary_result.release_error()] mutable { \
                 promise->reject(move(error));                                                                           \
             });                                                                                                         \