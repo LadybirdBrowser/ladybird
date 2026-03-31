f3e2c492fe0a3823059d150fd80a7a29b36670b7
Tim Ledbetter <tim.ledbetter@ladybird.org>
2026-03-31 12:48:56 +0100

    LibWeb: Absolutize color in `ShadowStyleValue`
    ShadowStyleValue was not absolutizing its color component, which meant
    that complex color values like `color-mix()` would retain unresolved
    state. This caused a null pointer dereference when `color-mix()` with
    omitted percentages was used as a shadow color. We now absolutize the
    color component, avoiding this crash.

--- Libraries/LibWeb/CSS/StyleValues/ShadowStyleValue.cpp
+++ Libraries/LibWeb/CSS/StyleValues/ShadowStyleValue.cpp
@@ -62,11 +62,12 @@ ValueComparingNonnullRefPtr<StyleValue const> ShadowStyleValue::spread_distance(
 
 ValueComparingNonnullRefPtr<StyleValue const> ShadowStyleValue::absolutized(ComputationContext const& computation_context) const
 {
+    auto absolutized_color = color()->absolutized(computation_context);
     auto absolutized_offset_x = offset_x()->absolutized(computation_context);
     auto absolutized_offset_y = offset_y()->absolutized(computation_context);
     auto absolutized_blur_radius = blur_radius()->absolutized(computation_context);
     auto absolutized_spread_distance = spread_distance()->absolutized(computation_context);
-    return create(m_properties.shadow_type, color(), absolutized_offset_x, absolutized_offset_y, absolutized_blur_radius, absolutized_spread_distance, placement());
+    return create(m_properties.shadow_type, absolutized_color, absolutized_offset_x, absolutized_offset_y, absolutized_blur_radius, absolutized_spread_distance, placement());
 }
 
 }
