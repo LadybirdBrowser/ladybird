f9206d7b8d73e782f3c5910ff2a5a3ccea3df02b
Jonathan Gamble <101470903+schlawg@users.noreply.github.com>
2026-04-11 14:24:59 -0500

    test-web: Move convert_ansi_to_html to CaptureFile.cpp

--- Tests/LibWeb/test-web/Debug.h
+++ Tests/LibWeb/test-web/Debug.h
@@ -6,14 +6,10 @@
 
 #pragma once
 
-#include <AK/StringBuilder.h>
-#include <AK/StringView.h>
 #include <AK/Types.h>
 
 namespace TestWeb {
 
 void maybe_attach_on_fail_fast_timeout(pid_t);
-
-StringBuilder convert_ansi_to_html(StringView);
 
 }
