0ac89f9a087339623d4fc446b266682764ac39ba
Jonathan Gamble <101470903+schlawg@users.noreply.github.com>
2026-04-14 20:26:29 -0500

    LibWebView: Replace ProcessManager mutex with thread affinity assertions
    Add on_process_added callback so test-web can set up captures

--- Libraries/LibWebView/ProcessManager.cpp
+++ Libraries/LibWebView/ProcessManager.cpp
@@ -48,7 +48,8 @@ StringView process_name_from_type(ProcessType type)
 }
 
 ProcessManager::ProcessManager()
-    : on_process_exited([](Process&&) {})
+    : on_process_added([](Process&) {})
+    , on_process_exited([](Process&&) {})
     , m_process_monitor(ProcessMonitor([this](pid_t pid) {
         if (auto process = remove_process(pid); process.has_value())
             on_process_exited(process.release_value());
@@ -66,24 +67,32 @@ ProcessManager::ProcessManager()
 
 Optional<Process&> ProcessManager::find_process(pid_t pid)
 {
+    verify_event_loop();
     return m_processes.get(pid);
 }
 
 void ProcessManager::add_process(WebView::Process&& process)
 {
-    Threading::MutexLocker locker { m_lock };
-
+    verify_event_loop();
     auto pid = process.pid();
+    on_process_added(process);
     auto result = m_processes.set(pid, move(process));
     VERIFY(result == AK::HashSetResult::InsertedNewEntry);
     m_statistics.processes.append(make<Core::Platform::ProcessInfo>(pid));
     m_process_monitor.add_process(pid);
 }
 
+void ProcessManager::for_each_process(Function<void(Process&)> callback)
+{
+    verify_event_loop();
+    for (auto& entry : m_processes)
+        callback(entry.value);
+}
+
 #if defined(AK_OS_MACH)
 void ProcessManager::set_process_mach_port(pid_t pid, Core::MachPort&& port)
 {
-    Threading::MutexLocker locker { m_lock };
+    verify_event_loop();
     for (auto const& info : m_statistics.processes) {
         if (info->pid == pid) {
             info->child_task_port = move(port);
@@ -95,7 +104,7 @@ void ProcessManager::set_process_mach_port(pid_t pid, Core::MachPort&& port)
 
 Optional<Process> ProcessManager::remove_process(pid_t pid)
 {
-    Threading::MutexLocker locker { m_lock };
+    verify_event_loop();
     m_statistics.processes.remove_first_matching([&](auto const& info) {
         return (info->pid == pid);
     });
@@ -104,17 +113,17 @@ Optional<Process> ProcessManager::remove_process(pid_t pid)
 
 void ProcessManager::update_all_process_statistics()
 {
-    Threading::MutexLocker locker { m_lock };
+    verify_event_loop();
     (void)update_process_statistics(m_statistics);
 }
 
 JsonValue ProcessManager::serialize_json()
 {
-    Threading::MutexLocker locker { m_lock };
+    verify_event_loop();
     JsonArray serialized;
 
     m_statistics.for_each_process([&](auto const& process) {
-        auto& process_handle = find_process(process.pid).value();
+        auto& process_handle = m_processes.get(process.pid).value();
 
         auto type = WebView::process_name_from_type(process_handle.type());
         auto const& title = process_handle.title();
@@ -132,6 +141,12 @@ JsonValue ProcessManager::serialize_json()
     });
 
     return serialized;
+}
+
+void ProcessManager::verify_event_loop() const
+{
+    if (Core::EventLoop::is_running())
+        VERIFY(&Core::EventLoop::current() == m_creation_event_loop);
 }
 
 }
--- Libraries/LibWebView/ProcessManager.h
+++ Libraries/LibWebView/ProcessManager.h
@@ -6,10 +6,11 @@
 
 #pragma once
 
+#include <AK/Function.h>
 #include <AK/JsonValue.h>
 #include <AK/Types.h>
+#include <LibCore/EventLoop.h>
 #include <LibCore/Platform/ProcessStatistics.h>
-#include <LibThreading/Mutex.h>
 #include <LibWebView/Forward.h>
 #include <LibWebView/Process.h>
 #include <LibWebView/ProcessMonitor.h>
@@ -27,6 +28,7 @@ public:
     ProcessManager();
 
     void add_process(Process&&);
+    void for_each_process(Function<void(Process&)>);
     Optional<Process> remove_process(pid_t);
     Optional<Process&> find_process(pid_t);
 
@@ -37,13 +39,16 @@ public:
     void update_all_process_statistics();
     JsonValue serialize_json();
 
+    Function<void(Process&)> on_process_added; // test-web
     Function<void(Process&&)> on_process_exited;
 
 private:
+    void verify_event_loop() const;
+
     Core::Platform::ProcessStatistics m_statistics;
     HashMap<pid_t, Process> m_processes;
     ProcessMonitor m_process_monitor;
-    Threading::Mutex m_lock;
+    Core::EventLoop* m_creation_event_loop { &Core::EventLoop::current() };
 };
 
 }
