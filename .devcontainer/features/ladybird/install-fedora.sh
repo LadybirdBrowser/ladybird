d70c6b216de2ece5e97dbc67f9ea82f4ca4fee2d
slydetector <slydetector>
2026-04-28 13:48:04 +0000

    Devcontainer: Fixes for fedora

--- .devcontainer/features/ladybird/install-fedora.sh
+++ .devcontainer/features/ladybird/install-fedora.sh
@@ -6,5 +6,5 @@ dnf install -y git gh
 
 # Ladybird dev dependencies
 dnf install -y autoconf-archive automake bison ccache cmake curl google-noto-sans-mono-fonts liberation-sans-fonts \
-    libglvnd-devel libtool nasm ninja-build patchelf perl-FindBin perl-IPC-Cmd perl-lib qt6-qtbase-devel \
+    libdrm-devel libglvnd-devel libtool nasm ninja-build patchelf perl-FindBin perl-IPC-Cmd perl-lib perl-Time-Piece qt6-qtbase-devel \
     qt6-qttools-devel qt6-qtwayland-devel tar unzip zip zlib-ng-compat-static
