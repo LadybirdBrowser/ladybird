LadybirdBrowser/ladybird

commit e38adbc31b5fdfaf7e1f12b9d255d90f8b690ba1

LibWeb: Enable crash backtraces from WPT runner
When a WebContent process receives a fatal signal, print a backtrace to
stderr before re-raising the signal. The backtrace is captured by
the test runner and written to a .stderr.html file.

Uses SA_RESETHAND so the handler runs only once, then resets to the
default disposition and re-raises the signal for normal crash behavior.

Services/WebContent/main.cpp
@@ -52,6 +52,48 @@
 
 #include <SDL3/SDL_init.h>
 
+#if !defined(AK_OS_WINDOWS)
+#    include <signal.h>
+static void crash_signal_handler(int signo)
+{
+    char const* name = "unknown";
+    switch (signo) {
+    case SIGSEGV:
+        name = "SIGSEGV";
+        break;
+    case SIGBUS:
+        name = "SIGBUS";
+        break;
+    case SIGFPE:
+        name = "SIGFPE";
+        break;
+    case SIGABRT:
+        name = "SIGABRT";
+        break;
+    case SIGILL:
+        name = "SIGILL";
+        break;
+    }
+    warnln("\n\033[31;1mCRASH\033[0m: Received signal {} ({})", name, signo);
+    dump_backtrace(2, 100);
+    signal(signo, SIG_DFL);
+    raise(signo);
+}
+
+static void install_crash_signal_handlers()
+{
+    struct sigaction sa;
+    sa.sa_handler = crash_signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESETHAND;
+    sigaction(SIGSEGV, &sa, nullptr);
+    sigaction(SIGBUS, &sa, nullptr);
+    sigaction(SIGFPE, &sa, nullptr);
+    sigaction(SIGABRT, &sa, nullptr);
+    sigaction(SIGILL, &sa, nullptr);
+}
+#endif
+
 static ErrorOr<void> load_content_filters(StringView config_path);
 
 static ErrorOr<void> initialize_resource_loader(GC::Heap&, int request_server_socket);
@@ -63,6 +105,10 @@ static ErrorOr<void> reinitialize_image_decoder(IPC::File const& image_decoder_s
 ErrorOr<int> ladybird_main(Main::Arguments arguments)
 {
     AK::set_rich_debug_enabled(true);
+
+#if !defined(AK_OS_WINDOWS)
+    install_crash_signal_handlers();
+#endif
 
 #if defined(AK_OS_WINDOWS)
     // NOTE: We need this here otherwise SDL inits COM in the APARTMENTTHREADED model which we don't want as we need to

commit 2d6b0945ef44fbe5fbc36e0437dd90f33664f921

AK+WebContent: Don't print duplicate stack traces after failed assertion
e38adbc31b5fdfaf7e1f12b9d255d90f8b690ba1 introduced a signal handler for
WebContent. When an assertion fails, we print a stack trace, then raise
a trap, which triggers the signal handler to print the same stack trace
again.

Instead, only print a stack trace in the signal handler if it was
triggered by something other than a failing assertion. We keep the
printing in the assertion code because there we know we can skip some
frames we don't care about.

AK/Assertions.cpp
@@ -133,6 +133,7 @@ NEVER_INLINE void ak_trap(void)
     // Skip 3 frames to get to caller. That is dump_backtrace, ak_trap, and ak_verification_failed.
     dump_backtrace(3, 100);
 #endif
+    ak_verification_has_been_triggered = true;
     __builtin_trap();
 }
 

AK/Assertions.h
@@ -6,6 +6,10 @@
 
 #pragma once
 
+#include <csignal>
+
+inline sig_atomic_t volatile ak_verification_has_been_triggered = false;
+
 // All the functions for stack traces are never inline as we want a consistent number of frames
 extern "C" __attribute__((noinline)) void dump_backtrace(unsigned frames_to_skip = 1, unsigned max_depth = 100);
 extern "C" bool ak_colorize_output(void);

Services/WebContent/main.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/Assertions.h>
 #include <AK/LexicalPath.h>
 #include <LibCore/ArgsParser.h>
 #include <LibCore/EventLoop.h>
@@ -60,29 +61,32 @@
 #    include <signal.h>
 static void crash_signal_handler(int signo)
 {
-    char const* name;
-    switch (signo) {
-    case SIGSEGV:
-        name = "SIGSEGV";
-        break;
-    case SIGBUS:
-        name = "SIGBUS";
-        break;
-    case SIGFPE:
-        name = "SIGFPE";
-        break;
-    case SIGABRT:
-        name = "SIGABRT";
-        break;
-    case SIGILL:
-        name = "SIGILL";
-        break;
-    default:
-        name = "unknown";
-        break;
+    // If the signal comes from a verify/assert, we already printed a (filtered) stacktrace
+    if (!ak_verification_has_been_triggered) {
+        char const* name;
+        switch (signo) {
+        case SIGSEGV:
+            name = "SIGSEGV";
+            break;
+        case SIGBUS:
+            name = "SIGBUS";
+            break;
+        case SIGFPE:
+            name = "SIGFPE";
+            break;
+        case SIGABRT:
+            name = "SIGABRT";
+            break;
+        case SIGILL:
+            name = "SIGILL";
+            break;
+        default:
+            name = "unknown";
+            break;
+        }
+        warnln("\n\033[31;1mCRASH\033[0m: Received signal {} ({})", name, signo);
+        dump_backtrace(2, 100);
     }
-    warnln("\n\033[31;1mCRASH\033[0m: Received signal {} ({})", name, signo);
-    dump_backtrace(2, 100);
     exit(128 + signo);
 }
 