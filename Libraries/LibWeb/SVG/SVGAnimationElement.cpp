b2a143cf9e5f401cd9b0585385324c959b017741
Shannon Booth <shannon@serenityos.org>
2026-05-08 14:19:33 +0200

    LibWeb/SVG: Implement event handlers for SVGAnimationElement

--- Libraries/LibWeb/SVG/SVGAnimationElement.cpp
+++ Libraries/LibWeb/SVG/SVGAnimationElement.cpp
@@ -8,6 +8,7 @@
 
 #include <LibWeb/Bindings/Intrinsics.h>
 #include <LibWeb/Bindings/SVGAnimationElement.h>
+#include <LibWeb/HTML/EventNames.h>
 #include <LibWeb/Layout/Node.h>
 
 namespace Web::SVG {
@@ -23,6 +24,42 @@ void SVGAnimationElement::initialize(JS::Realm& realm)
 {
     WEB_SET_PROTOTYPE_FOR_INTERFACE(SVGAnimationElement);
     Base::initialize(realm);
+}
+
+// https://svgwg.org/specs/animations/#__svg__SVGAnimationElement__onbegin
+void SVGAnimationElement::set_onbegin(GC::Ptr<WebIDL::CallbackType> value)
+{
+    set_event_handler_attribute(HTML::EventNames::begin, value);
+}
+
+// https://svgwg.org/specs/animations/#__svg__SVGAnimationElement__onbegin
+GC::Ptr<WebIDL::CallbackType> SVGAnimationElement::onbegin()
+{
+    return event_handler_attribute(HTML::EventNames::begin);
+}
+
+// https://svgwg.org/specs/animations/#__svg__SVGAnimationElement__onend
+void SVGAnimationElement::set_onend(GC::Ptr<WebIDL::CallbackType> value)
+{
+    set_event_handler_attribute(HTML::EventNames::end, value);
+}
+
+// https://svgwg.org/specs/animations/#__svg__SVGAnimationElement__onend
+GC::Ptr<WebIDL::CallbackType> SVGAnimationElement::onend()
+{
+    return event_handler_attribute(HTML::EventNames::error);
+}
+
+// https://svgwg.org/specs/animations/#__svg__SVGAnimationElement__onrepeat
+void SVGAnimationElement::set_onrepeat(GC::Ptr<WebIDL::CallbackType> value)
+{
+    set_event_handler_attribute(HTML::EventNames::end, value);
+}
+
+// https://svgwg.org/specs/animations/#__svg__SVGAnimationElement__onrepeat
+GC::Ptr<WebIDL::CallbackType> SVGAnimationElement::onrepeat()
+{
+    return event_handler_attribute(HTML::EventNames::repeat);
 }
 
 }
