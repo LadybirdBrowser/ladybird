5e796ebbfaf46ebed9a35930e66e08556c3146cf
Sam Atkins <sam@ladybird.org>
2026-05-08 14:56:28 +0100

    LibWeb: Implement ContainerQuery evaluation
    ContainerQuery now stores its feature requirements. When evaluated, it
    uses these and the passed-in container name to identify a container
    element to match against, then evaluates its condition against that
    element.
    
    None of this is yet in use anywhere, but will be soon.

--- Libraries/LibWeb/CSS/ContainerQuery.cpp
+++ Libraries/LibWeb/CSS/ContainerQuery.cpp
@@ -7,6 +7,9 @@
 #include "ContainerQuery.h"
 #include <AK/NonnullRefPtr.h>
 #include <LibWeb/CSS/BooleanExpression.h>
+#include <LibWeb/CSS/ComputedProperties.h>
+#include <LibWeb/DOM/AbstractElement.h>
+#include <LibWeb/DOM/Element.h>
 #include <LibWeb/Dump.h>
 
 namespace Web::CSS {
@@ -20,13 +23,62 @@ ContainerQuery::ContainerQuery(NonnullOwnPtr<BooleanExpression>&& condition)
     : m_condition(move(condition))
     , m_matches(m_condition->evaluate_to_boolean({}))
 {
+    m_condition->collect_container_query_feature_requirements(m_feature_requirements);
 }
 
-ContainerQueryFeatureRequirements ContainerQuery::collect_feature_requirements() const
+static bool container_satisfies_requirements(DOM::Element const& element, ContainerQueryFeatureRequirements const& requirements)
 {
-    ContainerQueryFeatureRequirements requirements;
-    m_condition->collect_container_query_feature_requirements(requirements);
-    return requirements;
+    auto style = element.computed_properties();
+    if (!style)
+        return false;
+
+    auto container_type = style->container_type();
+
+    if (requirements.requires_size_container && !container_type.is_size_container)
+        return false;
+
+    if (requirements.requires_inline_size_container && !(container_type.is_size_container || container_type.is_inline_size_container))
+        return false;
+
+    if (requirements.requires_block_size_container && !container_type.is_size_container)
+        return false;
+
+    if (requirements.requires_scroll_state_container && !container_type.is_scroll_state_container)
+        return false;
+
+    return true;
+}
+
+// https://drafts.csswg.org/css-conditional-5/#container-rule
+MatchResult ContainerQuery::evaluate(DOM::AbstractElement const& element, Optional<FlyString> const& container_name) const
+{
+    // If the <container-query> contains unknown or unsupported container features, no query container will be selected
+    // for that <container-condition>.
+    if (m_feature_requirements.has_unknown_or_unsupported_feature)
+        return MatchResult::Unknown;
+
+    // For each element, the query container to be queried is selected from among the element’s ancestor query
+    // containers that are established as a valid query container for all the container features in the
+    // <container-query>.
+    for (auto const* container = element.element().flat_tree_parent_element(); container; container = container->flat_tree_parent_element()) {
+        // The <container-name> filters the set of query containers considered to just those with a matching query
+        // container name.
+        if (!container_name_matches(*container, container_name))
+            continue;
+
+        if (!container_satisfies_requirements(*container, m_feature_requirements))
+            continue;
+
+        // Once an eligible query container has been selected for an element, each container feature in the
+        // <container-query> is evaluated against that query container.
+        return m_condition->evaluate({
+            .document = &element.document(),
+            .query_container = container,
+        });
+    }
+
+    // If no ancestor is an eligible query container, then the container query is unknown for that element.
+    return MatchResult::Unknown;
 }
 
 String ContainerQuery::to_string() const
@@ -39,6 +91,17 @@ void ContainerQuery::dump(StringBuilder& builder, int indent_levels) const
     dump_indent(builder, indent_levels);
     builder.appendff("Container query: (matches = {})\n", m_matches);
     m_condition->dump(builder, indent_levels + 1);
+}
+
+bool container_name_matches(DOM::Element const& element, Optional<FlyString> const& container_name)
+{
+    if (!container_name.has_value())
+        return true;
+
+    if (auto style = element.computed_properties())
+        return style->container_name().contains_slow(*container_name);
+
+    return false;
 }
 
 }
--- Libraries/LibWeb/CSS/ContainerQuery.h
+++ Libraries/LibWeb/CSS/ContainerQuery.h
@@ -18,7 +18,8 @@ public:
     static NonnullRefPtr<ContainerQuery> create(NonnullOwnPtr<BooleanExpression>&&);
 
     bool matches() const { return m_matches; }
-    ContainerQueryFeatureRequirements collect_feature_requirements() const;
+    ContainerQueryFeatureRequirements const& feature_requirements() const { return m_feature_requirements; }
+    MatchResult evaluate(DOM::AbstractElement const&, Optional<FlyString> const& container_name) const;
     String to_string() const;
 
     void dump(StringBuilder&, int indent_levels = 0) const;
@@ -27,7 +28,10 @@ private:
     explicit ContainerQuery(NonnullOwnPtr<BooleanExpression>&&);
 
     NonnullOwnPtr<BooleanExpression> m_condition;
+    ContainerQueryFeatureRequirements m_feature_requirements;
     bool m_matches { false };
 };
+
+bool container_name_matches(DOM::Element const&, Optional<FlyString> const& container_name);
 
 }
