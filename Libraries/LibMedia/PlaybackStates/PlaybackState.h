cf58764774db1fab877973033e25df813877fe1b
Zaggy1024 <Zaggy1024@gmail.com>
2026-05-07 20:39:18 -0500

    LibMedia: Add a formatter for PlaybackState

--- Libraries/LibMedia/PlaybackStates/PlaybackState.h
+++ Libraries/LibMedia/PlaybackStates/PlaybackState.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include <AK/Format.h>
 #include <AK/Types.h>
 
 namespace Media {
@@ -17,6 +18,37 @@ enum class PlaybackState : u8 {
     Paused,
     Seeking,
     Suspended,
+};
+
+constexpr StringView playback_state_to_string(PlaybackState state)
+{
+    switch (state) {
+    case PlaybackState::Starting:
+        return "Starting"sv;
+    case PlaybackState::Buffering:
+        return "Buffering"sv;
+    case PlaybackState::Playing:
+        return "Playing"sv;
+    case PlaybackState::Paused:
+        return "Paused"sv;
+    case PlaybackState::Seeking:
+        return "Seeking"sv;
+    case PlaybackState::Suspended:
+        return "Suspended"sv;
+    }
+    return "Invalid"sv;
+}
+
+}
+
+namespace AK {
+
+template<>
+struct Formatter<Media::PlaybackState> final : Formatter<StringView> {
+    ErrorOr<void> format(FormatBuilder& builder, Media::PlaybackState state)
+    {
+        return Formatter<StringView>::format(builder, Media::playback_state_to_string(state));
+    }
 };
 
 }
