be5075e25c54eae0571a08c8b9914174a8af3942
Tim Ledbetter <tim.ledbetter@ladybird.org>
2026-05-09 18:51:10 +0100

    LibWeb: Use targeted invalidation when adopting a stylesheet
    Previously, adding or removing a constructed `CSSStyleSheet` via
    `document.adoptedStyleSheets` invalidated style for the entire document
    or shadow root. We now route this through the same style invalidation
    machinery that's used when we add or remove `<style>` elements,
    resulting in fewer full invalidations.

--- Libraries/LibWeb/CSS/Invalidation/AdoptedStyleSheetInvalidator.cpp
+++ Libraries/LibWeb/CSS/Invalidation/AdoptedStyleSheetInvalidator.cpp
@@ -6,26 +6,12 @@
 
 #include <LibWeb/CSS/CSSStyleSheet.h>
 #include <LibWeb/CSS/Invalidation/AdoptedStyleSheetInvalidator.h>
-#include <LibWeb/CSS/StyleScope.h>
+#include <LibWeb/CSS/StyleSheetInvalidation.h>
 #include <LibWeb/DOM/Document.h>
 #include <LibWeb/DOM/Node.h>
-#include <LibWeb/DOM/ShadowRoot.h>
 #include <LibWeb/DOM/StyleInvalidationReason.h>
 
 namespace Web::CSS::Invalidation {
-
-static CSS::StyleScope& style_scope_for_document_or_shadow_root(DOM::Node& document_or_shadow_root)
-{
-    if (auto* shadow_root = as_if<DOM::ShadowRoot>(document_or_shadow_root))
-        return shadow_root->style_scope();
-    return document_or_shadow_root.document().style_scope();
-}
-
-static void invalidate_style_after_adopted_style_sheet_list_change(DOM::Node& document_or_shadow_root)
-{
-    style_scope_for_document_or_shadow_root(document_or_shadow_root).invalidate_rule_cache();
-    document_or_shadow_root.invalidate_style(DOM::StyleInvalidationReason::AdoptedStyleSheetsList);
-}
 
 void invalidate_style_after_adopting_style_sheet(DOM::Node& document_or_shadow_root, CSSStyleSheet& style_sheet)
 {
@@ -37,13 +23,13 @@ void invalidate_style_after_adopting_style_sheet(DOM::Node& document_or_shadow_r
     // chance to populate MediaList::m_matches.
     style_sheet.evaluate_media_queries(document_or_shadow_root.document());
 
-    invalidate_style_after_adopted_style_sheet_list_change(document_or_shadow_root);
+    invalidate_style_for_stylesheet_change(document_or_shadow_root, style_sheet, DOM::StyleInvalidationReason::AdoptedStyleSheetsList);
 }
 
 void invalidate_style_after_removing_adopted_style_sheet(DOM::Node& document_or_shadow_root, CSSStyleSheet& style_sheet)
 {
     style_sheet.remove_owning_document_or_shadow_root(document_or_shadow_root);
-    invalidate_style_after_adopted_style_sheet_list_change(document_or_shadow_root);
+    invalidate_style_for_stylesheet_change(document_or_shadow_root, style_sheet, DOM::StyleInvalidationReason::AdoptedStyleSheetsList);
 }
 
 }
