ed6948441d64f9b5281f79c9daf45dac658c4cab
Callum Law <callumlaw1709@outlook.com>
2026-05-15 19:33:12 +1200

    LibWeb: Rename `get_pseudo_element_node` to `pseudo_element_layout_node`
    This matches the naming convention of the newly introduced
    `pseudo_element_unsafe_layout_node` and the top-level
    `Element::layout_node`

--- Libraries/LibWeb/DOM/Element.h
+++ Libraries/LibWeb/DOM/Element.h
@@ -379,7 +379,7 @@ public:
 
     void set_synthetic_pseudo_element_node(Badge<Layout::TreeBuilder>, CSS::PseudoElement, GC::Ptr<Layout::NodeWithStyle>);
 
-    GC::Ptr<Layout::NodeWithStyle> get_pseudo_element_node(CSS::PseudoElement) const;
+    GC::Ptr<Layout::NodeWithStyle> pseudo_element_layout_node(CSS::PseudoElement) const;
     GC::Ptr<Layout::NodeWithStyle> pseudo_element_unsafe_layout_node(CSS::PseudoElement) const;
 
     bool has_synthetic_pseudo_elements() const;
