85c1663e2369ec15a16faeba3e9e9f7e452c5e8f
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2026-05-23 17:12:14 +0100

    LibWeb: Rename m_rendering_thread_display_list_* to m_compositor_*
    "Rendering thread" is the old name for what is now called the
    compositor. Rename these Navigable members to reflect the current
    terminology:

--- Libraries/LibWeb/HTML/Navigable.h
+++ Libraries/LibWeb/HTML/Navigable.h
@@ -343,9 +343,9 @@ private:
     bool m_needs_to_record_display_list { true };
     bool m_pending_set_browser_zoom_request { false };
     bool m_should_show_line_box_borders { false };
-    Optional<PaintConfig> m_rendering_thread_display_list_paint_config;
+    Optional<PaintConfig> m_compositor_display_list_paint_config;
     Painting::DisplayListResourceStorage m_display_list_resource_storage;
-    Painting::DisplayListResourceSet m_rendering_thread_display_list_resources;
+    Painting::DisplayListResourceSet m_compositor_display_list_resources;
     OwnPtr<Compositor::CompositorContextHandle> m_compositor_context;
     Optional<Painting::CompositorSurfaceId> m_compositor_surface_id;
     RefPtr<Core::Timer> m_async_scroll_hover_update_timer;
