60a95abbf7de6d1710697e5c5eb63e32dee3439a
Luke Wilde <luke@ladybird.org>
2026-05-26 21:42:14 +0100

    LibWeb/Crypto: Serialize CryptoKey [[handle]]
    Fixes WhatsApp crashing when generating the key for the login QR code.

--- Libraries/LibWeb/Crypto/CryptoKey.cpp
+++ Libraries/LibWeb/Crypto/CryptoKey.cpp
@@ -15,6 +15,241 @@
 
 namespace Web::Crypto {
 
+namespace {
+
+enum class HandleTag : u8 {
+    ByteBuffer = 0,
+    JsonWebKey = 1,
+    RSAPublicKey = 2,
+    RSAPrivateKey = 3,
+    ECPublicKey = 4,
+    ECPrivateKey = 5,
+    MLDSAPublicKey = 6,
+    MLDSAPrivateKey = 7,
+    MLKEMPublicKey = 8,
+    MLKEMPrivateKey = 9,
+};
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, ByteBuffer const& buffer)
+{
+    encoder.encode(HandleTag::ByteBuffer);
+    encoder.encode(buffer);
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, RsaOtherPrimesInfo const& info)
+{
+    encoder.encode(info.r);
+    encoder.encode(info.d);
+    encoder.encode(info.t);
+}
+
+RsaOtherPrimesInfo deserialize_rsa_other_primes_info(HTML::TransferDataDecoder& decoder)
+{
+    return RsaOtherPrimesInfo {
+        .r = decoder.decode<Optional<String>>(),
+        .d = decoder.decode<Optional<String>>(),
+        .t = decoder.decode<Optional<String>>(),
+    };
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, JsonWebKey const& jwk)
+{
+    encoder.encode(HandleTag::JsonWebKey);
+    encoder.encode(jwk.kty);
+    encoder.encode(jwk.use);
+    encoder.encode(jwk.key_ops);
+    encoder.encode(jwk.alg);
+    encoder.encode(jwk.ext);
+    encoder.encode(jwk.crv);
+    encoder.encode(jwk.x);
+    encoder.encode(jwk.y);
+    encoder.encode(jwk.d);
+    encoder.encode(jwk.n);
+    encoder.encode(jwk.e);
+    encoder.encode(jwk.p);
+    encoder.encode(jwk.q);
+    encoder.encode(jwk.dp);
+    encoder.encode(jwk.dq);
+    encoder.encode(jwk.qi);
+    encoder.encode(jwk.oth.has_value());
+    if (jwk.oth.has_value()) {
+        encoder.encode(static_cast<u64>(jwk.oth->size()));
+        for (auto const& info : *jwk.oth)
+            serialize_handle(encoder, info);
+    }
+    encoder.encode(jwk.k);
+    encoder.encode(jwk.pub);
+    encoder.encode(jwk.priv);
+}
+
+JsonWebKey deserialize_json_web_key(HTML::TransferDataDecoder& decoder)
+{
+    JsonWebKey jwk;
+    jwk.kty = decoder.decode<Optional<String>>();
+    jwk.use = decoder.decode<Optional<String>>();
+    jwk.key_ops = decoder.decode<Optional<Vector<String>>>();
+    jwk.alg = decoder.decode<Optional<String>>();
+    jwk.ext = decoder.decode<Optional<bool>>();
+    jwk.crv = decoder.decode<Optional<String>>();
+    jwk.x = decoder.decode<Optional<String>>();
+    jwk.y = decoder.decode<Optional<String>>();
+    jwk.d = decoder.decode<Optional<String>>();
+    jwk.n = decoder.decode<Optional<String>>();
+    jwk.e = decoder.decode<Optional<String>>();
+    jwk.p = decoder.decode<Optional<String>>();
+    jwk.q = decoder.decode<Optional<String>>();
+    jwk.dp = decoder.decode<Optional<String>>();
+    jwk.dq = decoder.decode<Optional<String>>();
+    jwk.qi = decoder.decode<Optional<String>>();
+    if (decoder.decode<bool>()) {
+        auto size = decoder.decode<u64>();
+        Vector<RsaOtherPrimesInfo> oth;
+        oth.ensure_capacity(size);
+        for (u64 i = 0; i < size; ++i)
+            oth.unchecked_append(deserialize_rsa_other_primes_info(decoder));
+        jwk.oth = move(oth);
+    }
+    jwk.k = decoder.decode<Optional<String>>();
+    jwk.pub = decoder.decode<Optional<String>>();
+    jwk.priv = decoder.decode<Optional<String>>();
+    return jwk;
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, ::Crypto::PK::RSAPublicKey const& key)
+{
+    encoder.encode(HandleTag::RSAPublicKey);
+    encoder.encode_unsigned_big_integer(key.modulus());
+    encoder.encode_unsigned_big_integer(key.public_exponent());
+}
+
+WebIDL::ExceptionOr<::Crypto::PK::RSAPublicKey> deserialize_rsa_public_key(HTML::TransferDataDecoder& decoder, JS::Realm& realm)
+{
+    auto modulus = TRY(decoder.decode_unsigned_big_integer(realm));
+    auto public_exponent = TRY(decoder.decode_unsigned_big_integer(realm));
+    return ::Crypto::PK::RSAPublicKey { move(modulus), move(public_exponent) };
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, ::Crypto::PK::RSAPrivateKey const& key)
+{
+    encoder.encode(HandleTag::RSAPrivateKey);
+    encoder.encode_unsigned_big_integer(key.modulus());
+    encoder.encode_unsigned_big_integer(key.private_exponent());
+    encoder.encode_unsigned_big_integer(key.public_exponent());
+    encoder.encode_unsigned_big_integer(key.prime1());
+    encoder.encode_unsigned_big_integer(key.prime2());
+    encoder.encode_unsigned_big_integer(key.exponent1());
+    encoder.encode_unsigned_big_integer(key.exponent2());
+    encoder.encode_unsigned_big_integer(key.coefficient());
+}
+
+WebIDL::ExceptionOr<::Crypto::PK::RSAPrivateKey> deserialize_rsa_private_key(HTML::TransferDataDecoder& decoder, JS::Realm& realm)
+{
+    auto modulus = TRY(decoder.decode_unsigned_big_integer(realm));
+    auto private_exponent = TRY(decoder.decode_unsigned_big_integer(realm));
+    auto public_exponent = TRY(decoder.decode_unsigned_big_integer(realm));
+    auto prime1 = TRY(decoder.decode_unsigned_big_integer(realm));
+    auto prime2 = TRY(decoder.decode_unsigned_big_integer(realm));
+    auto exponent1 = TRY(decoder.decode_unsigned_big_integer(realm));
+    auto exponent2 = TRY(decoder.decode_unsigned_big_integer(realm));
+    auto coefficient = TRY(decoder.decode_unsigned_big_integer(realm));
+    return ::Crypto::PK::RSAPrivateKey { move(modulus), move(private_exponent), move(public_exponent), move(prime1), move(prime2), move(exponent1), move(exponent2), move(coefficient) };
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, ::Crypto::PK::ECPublicKey const& key)
+{
+    encoder.encode(HandleTag::ECPublicKey);
+    encoder.encode(MUST(key.x_bytes()));
+    encoder.encode(MUST(key.y_bytes()));
+}
+
+WebIDL::ExceptionOr<::Crypto::PK::ECPublicKey> deserialize_ec_public_key(HTML::TransferDataDecoder& decoder, JS::Realm& realm)
+{
+    auto x_bytes = TRY(decoder.decode_buffer(realm));
+    auto y_bytes = TRY(decoder.decode_buffer(realm));
+    auto scalar_size = x_bytes.size();
+    return ::Crypto::PK::ECPublicKey { ::Crypto::UnsignedBigInteger::import_data(x_bytes), ::Crypto::UnsignedBigInteger::import_data(y_bytes), scalar_size };
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, ::Crypto::PK::ECPrivateKey const& key)
+{
+    encoder.encode(HandleTag::ECPrivateKey);
+    encoder.encode(MUST(key.d_bytes()));
+    encoder.encode(key.parameters());
+    encoder.encode(key.public_key().has_value());
+    if (key.public_key().has_value())
+        serialize_handle(encoder, *key.public_key());
+}
+
+WebIDL::ExceptionOr<::Crypto::PK::ECPrivateKey> deserialize_ec_private_key(HTML::TransferDataDecoder& decoder, JS::Realm& realm)
+{
+    auto d_bytes = TRY(decoder.decode_buffer(realm));
+    auto scalar_size = d_bytes.size();
+    auto d = ::Crypto::UnsignedBigInteger::import_data(d_bytes);
+    auto parameters = decoder.decode<Optional<Vector<int>>>();
+
+    Optional<::Crypto::PK::ECPublicKey> public_key;
+    if (decoder.decode<bool>())
+        public_key = TRY(deserialize_ec_public_key(decoder, realm));
+
+    return ::Crypto::PK::ECPrivateKey { move(d), scalar_size, move(parameters), move(public_key) };
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, ::Crypto::PK::MLDSAPublicKey const& key)
+{
+    encoder.encode(HandleTag::MLDSAPublicKey);
+    encoder.encode(key.public_key());
+}
+
+WebIDL::ExceptionOr<::Crypto::PK::MLDSAPublicKey> deserialize_mldsa_public_key(HTML::TransferDataDecoder& decoder, JS::Realm& realm)
+{
+    return ::Crypto::PK::MLDSAPublicKey { TRY(decoder.decode_buffer(realm)) };
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, ::Crypto::PK::MLDSAPrivateKey const& key)
+{
+    encoder.encode(HandleTag::MLDSAPrivateKey);
+    encoder.encode(key.seed());
+    encoder.encode(key.public_key());
+    encoder.encode(key.private_key());
+}
+
+WebIDL::ExceptionOr<::Crypto::PK::MLDSAPrivateKey> deserialize_mldsa_private_key(HTML::TransferDataDecoder& decoder, JS::Realm& realm)
+{
+    auto seed = TRY(decoder.decode_buffer(realm));
+    auto public_key = TRY(decoder.decode_buffer(realm));
+    auto private_key = TRY(decoder.decode_buffer(realm));
+    return ::Crypto::PK::MLDSAPrivateKey { move(seed), move(public_key), move(private_key) };
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, ::Crypto::PK::MLKEMPublicKey const& key)
+{
+    encoder.encode(HandleTag::MLKEMPublicKey);
+    encoder.encode(key.public_key());
+}
+
+WebIDL::ExceptionOr<::Crypto::PK::MLKEMPublicKey> deserialize_mlkem_public_key(HTML::TransferDataDecoder& decoder, JS::Realm& realm)
+{
+    return ::Crypto::PK::MLKEMPublicKey { TRY(decoder.decode_buffer(realm)) };
+}
+
+void serialize_handle(HTML::TransferDataEncoder& encoder, ::Crypto::PK::MLKEMPrivateKey const& key)
+{
+    encoder.encode(HandleTag::MLKEMPrivateKey);
+    encoder.encode(key.seed());
+    encoder.encode(key.public_key());
+    encoder.encode(key.private_key());
+}
+
+WebIDL::ExceptionOr<::Crypto::PK::MLKEMPrivateKey> deserialize_mlkem_private_key(HTML::TransferDataDecoder& decoder, JS::Realm& realm)
+{
+    auto seed = TRY(decoder.decode_buffer(realm));
+    auto public_key = TRY(decoder.decode_buffer(realm));
+    auto private_key = TRY(decoder.decode_buffer(realm));
+    return ::Crypto::PK::MLKEMPrivateKey { move(seed), move(public_key), move(private_key) };
+}
+
+}
+
 GC_DEFINE_ALLOCATOR(CryptoKey);
 GC_DEFINE_ALLOCATOR(CryptoKeyPair);
 
@@ -154,7 +389,8 @@ WebIDL::ExceptionOr<void> CryptoKey::serialization_steps(HTML::TransferDataEncod
     // 4. Set serialized.[[Usages]] to the sub-serialization of the [[usages]] internal slot of value.
     serialized.encode(m_usages);
 
-    // FIXME: 5. Set serialized.[[Handle]] to the [[handle]] internal slot of value.
+    // 5. Set serialized.[[Handle]] to the [[handle]] internal slot of value.
+    m_key_data.visit([&](auto const& handle) { serialize_handle(serialized, handle); });
 
     return {};
 }
@@ -178,7 +414,40 @@ WebIDL::ExceptionOr<void> CryptoKey::deserialization_steps(HTML::TransferDataDec
     auto usages = serialized.decode<Vector<Bindings::KeyUsage>>();
     set_usages(move(usages));
 
-    // FIXME: 5. Initialize the [[handle]] internal slot of value to serialized.[[Handle]].
+    // 5. Initialize the [[handle]] internal slot of value to serialized.[[Handle]].
+    auto tag = serialized.decode<HandleTag>();
+    switch (tag) {
+    case HandleTag::ByteBuffer:
+        m_key_data = TRY(serialized.decode_buffer(realm));
+        break;
+    case HandleTag::JsonWebKey:
+        m_key_data = deserialize_json_web_key(serialized);
+        break;
+    case HandleTag::RSAPublicKey:
+        m_key_data = TRY(deserialize_rsa_public_key(serialized, realm));
+        break;
+    case HandleTag::RSAPrivateKey:
+        m_key_data = TRY(deserialize_rsa_private_key(serialized, realm));
+        break;
+    case HandleTag::ECPublicKey:
+        m_key_data = TRY(deserialize_ec_public_key(serialized, realm));
+        break;
+    case HandleTag::ECPrivateKey:
+        m_key_data = TRY(deserialize_ec_private_key(serialized, realm));
+        break;
+    case HandleTag::MLDSAPublicKey:
+        m_key_data = TRY(deserialize_mldsa_public_key(serialized, realm));
+        break;
+    case HandleTag::MLDSAPrivateKey:
+        m_key_data = TRY(deserialize_mldsa_private_key(serialized, realm));
+        break;
+    case HandleTag::MLKEMPublicKey:
+        m_key_data = TRY(deserialize_mlkem_public_key(serialized, realm));
+        break;
+    case HandleTag::MLKEMPrivateKey:
+        m_key_data = TRY(deserialize_mlkem_private_key(serialized, realm));
+        break;
+    }
 
     return {};
 }
