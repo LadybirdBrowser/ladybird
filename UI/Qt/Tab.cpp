LadybirdBrowser/ladybird

commit 8f14e85af4b2f389d2f7143029a593b0276e869a

UI/Qt: Store the toolbar and bookmarks bar in a common container
The bookmarks bar sticks out a bit too much in its own container with
its own background color. This adds a common container for both bars
so that we can apply the background color gradient to that common
container. This naturally handles showing/hiding the bookmarks bar
gracefully.

UI/Qt/Tab.cpp
@@ -101,10 +101,19 @@ Tab::Tab(BrowserWindow* window, RefPtr<WebView::WebContentClient> parent_client,
     m_view = new WebContentView(this, parent_client, page_index, AK::move(view_initial_state));
     m_find_in_page = new FindInPageWidget(this, m_view);
     m_find_in_page->setVisible(false);
+
+    m_toolbar_container = new QWidget(this);
+    m_toolbar_container->setObjectName("LadybirdToolbarContainer");
+    m_toolbar_container->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
+
     m_toolbar = new QWidget(this);
     m_toolbar->setObjectName("LadybirdNavigationToolbar");
     m_toolbar->setFixedHeight(47);
     m_toolbar->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
+
+    auto* toolbar_container_layout = new QVBoxLayout(m_toolbar_container);
+    toolbar_container_layout->setSpacing(0);
+    toolbar_container_layout->setContentsMargins(0, 0, 0, 0);
 
     auto* toolbar_layout = new QHBoxLayout(m_toolbar);
     toolbar_layout->setSpacing(6);
@@ -132,8 +141,9 @@ Tab::Tab(BrowserWindow* window, RefPtr<WebView::WebContentClient> parent_client,
     focus_location_editor_action->setShortcuts({ QKeySequence("Ctrl+L"), QKeySequence("Alt+D") });
     addAction(focus_location_editor_action);
 
-    tab_layout->addWidget(m_toolbar);
-    tab_layout->addWidget(m_bookmarks_bar);
+    toolbar_container_layout->addWidget(m_toolbar);
+    toolbar_container_layout->addWidget(m_bookmarks_bar);
+    tab_layout->addWidget(m_toolbar_container);
     tab_layout->addWidget(m_view);
     tab_layout->addWidget(m_find_in_page);
 
@@ -427,13 +437,13 @@ Tab::Tab(BrowserWindow* window, RefPtr<WebView::WebContentClient> parent_client,
     };
 
     view().on_fullscreen_window = [this]() {
-        m_toolbar->hide();
+        m_toolbar_container->hide();
         m_window->fullscreen_mode().enter(this);
     };
 
     view().on_exit_fullscreen_window = [this]() {
         m_window->fullscreen_mode().exit(FullscreenMode::ExitInitiatedBy::WebContent);
-        m_toolbar->show();
+        m_toolbar_container->show();
     };
 
     view().on_audio_play_state_changed = [this](auto play_state) {
@@ -659,9 +669,8 @@ void Tab::update_chrome_style()
 {
     if (m_is_updating_chrome_style)
         return;
-
     m_is_updating_chrome_style = true;
-    m_toolbar->setStyleSheet(ChromeStyle::navigation_toolbar_style_sheet(palette()));
+    m_toolbar_container->setStyleSheet(ChromeStyle::toolbar_container_style_sheet(palette()));
     auto hover_surface = ChromeStyle::style_sheet_color(ChromeStyle::chrome_surface(palette()));
     auto hover_border = ChromeStyle::style_sheet_color(ChromeStyle::chrome_border(palette()));
     auto hover_text = ChromeStyle::style_sheet_color(ChromeStyle::chrome_text(palette()));

UI/Qt/ChromeStyle.cpp
@@ -239,7 +239,7 @@ QString style_sheet_color(QColor const& color)
     return qformatted("rgb({}, {}, {})", color.red(), color.green(), color.blue());
 }
 
-QString navigation_toolbar_style_sheet(QPalette const& palette)
+QString toolbar_container_style_sheet(QPalette const& palette)
 {
     auto background = style_sheet_color(chrome_active_tab_surface_top(palette));
     auto background_bottom = style_sheet_color(chrome_active_tab_surface_bottom(palette));
@@ -251,7 +251,7 @@ QString navigation_toolbar_style_sheet(QPalette const& palette)
     auto disabled_text = style_sheet_color(chrome_muted_text(palette));
 
     return qformatted(R"(
-QWidget#LadybirdNavigationToolbar {{
+QWidget#LadybirdToolbarContainer {{
     background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 {0}, stop:1 {1});
     border: 0;
     border-bottom: 1px solid {5};
@@ -355,25 +355,21 @@ QToolButton#LadybirdLocationAction:hover {{
 
 QString bookmarks_bar_style_sheet(QPalette const& palette)
 {
-    auto background = style_sheet_color(chrome_background(palette));
     auto hover = style_sheet_color(chrome_control_surface_hover(palette));
     auto pressed = style_sheet_color(chrome_control_surface_pressed(palette));
-    auto border = style_sheet_color(chrome_border(palette));
     auto control_border = style_sheet_color(chrome_control_border(palette));
     auto text = style_sheet_color(chrome_button_text(palette));
 
     return qformatted(R"(
 QToolBar#LadybirdBookmarksBar {{
-    color: {4};
-    background: {0};
+    color: {2};
     border: 0;
-    border-bottom: 1px solid {3};
     padding: 4px 8px;
     spacing: 3px;
 }}
 
 QToolBar#LadybirdBookmarksBar QToolButton {{
-    color: {4};
+    color: {2};
     background: transparent;
     border: 1px solid transparent;
     border-radius: 7px;
@@ -381,17 +377,17 @@ QToolBar#LadybirdBookmarksBar QToolButton {{
 }}
 
 QToolBar#LadybirdBookmarksBar QToolButton:hover {{
-    background: {1};
-    border-color: {5};
+    background: {0};
+    border-color: {3};
 }}
 
 QToolBar#LadybirdBookmarksBar QToolButton:pressed,
 QToolBar#LadybirdBookmarksBar QToolButton:checked {{
-    background: {2};
-    border-color: {5};
+    background: {1};
+    border-color: {3};
 }}
 )",
-        background, hover, pressed, border, text, control_border);
+        hover, pressed, text, control_border);
 }
 
 QString find_in_page_style_sheet(QPalette const& palette)

UI/Qt/ChromeStyle.h
@@ -28,7 +28,7 @@ QColor chrome_accent(QPalette const&);
 QColor chrome_muted_text(QPalette const&);
 
 QString style_sheet_color(QColor const&);
-QString navigation_toolbar_style_sheet(QPalette const&);
+QString toolbar_container_style_sheet(QPalette const&);
 QString location_edit_style_sheet(QPalette const&);
 QString bookmarks_bar_style_sheet(QPalette const&);
 QString find_in_page_style_sheet(QPalette const&);

UI/Qt/Tab.h
@@ -107,6 +107,7 @@ private:
     void update_tab_icon();
     int tab_index();
 
+    QWidget* m_toolbar_container { nullptr };
     QWidget* m_toolbar { nullptr };
     BookmarksBar* m_bookmarks_bar { nullptr };
     QToolButton* m_hamburger_button { nullptr };

commit c8472ebec3b54d214c10b67639a3edceb090f573

UI/Qt: Allow shrinking the browser window smaller than the bookmarks bar
This regressed in commit 8f14e85af4b2f389d2f7143029a593b0276e869a.

UI/Qt/Tab.cpp
@@ -104,7 +104,7 @@ Tab::Tab(BrowserWindow* window, RefPtr<WebView::WebContentClient> parent_client,
 
     m_toolbar_container = new QWidget(this);
     m_toolbar_container->setObjectName("LadybirdToolbarContainer");
-    m_toolbar_container->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
+    m_toolbar_container->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
 
     m_toolbar = new QWidget(this);
     m_toolbar->setObjectName("LadybirdNavigationToolbar");