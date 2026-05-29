af410da20ca9886363f4bd35ad772ae6a8084873
Timothy Flynn <trflynn89@pm.me>
2026-05-29 07:42:11 -0400

    UI/Qt: Paint the drag preview for all tabs
    We attempted to paint a preview of the dragged tab, but this only worked
    for the first tab. For other tabs, we were passing a negative offset to
    QWidget::render, causing the preview to render transparently.

--- UI/Qt/TabBar.cpp
+++ UI/Qt/TabBar.cpp
@@ -400,7 +400,7 @@ QPixmap TabBar::render_tab_drag_pixmap(int index) const
     pixmap.fill(Qt::transparent);
 
     QPainter painter(&pixmap);
-    const_cast<TabBar&>(*this).render(&painter, QPoint(-tab_rect.x(), -tab_rect.y()), QRegion(tab_rect), QWidget::DrawChildren);
+    const_cast<TabBar&>(*this).render(&painter, QPoint(), QRegion(tab_rect), QWidget::DrawChildren);
     return pixmap;
 }
 
