8d434234f5718a134f7326bea9917fd500f3b25e
R-Goc <ryszardgoc@gmail.com>
2026-05-30 19:23:01 +0200

    Tests/AK: Merge sorting tests together

--- Tests/AK/TestSorting.cpp
+++ Tests/AK/TestSorting.cpp
@@ -1,16 +1,20 @@
 /*
  * Copyright (c) 2020, the SerenityOS developers.
+ * Copyright (c) 2022, Marc Luqué <marc.luque@outlook.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include <LibTest/TestCase.h>
 
+#include <AK/InsertionSort.h>
 #include <AK/Noncopyable.h>
 #include <AK/QuickSort.h>
+#include <AK/Random.h>
 #include <AK/StdLibExtras.h>
+#include <AK/Vector.h>
 
-TEST_CASE(sorts_without_copy)
+TEST_CASE(quick_sort_sorts_without_copy)
 {
     struct NoCopy {
         AK_MAKE_NONCOPYABLE(NoCopy);
@@ -46,7 +50,7 @@ TEST_CASE(sorts_without_copy)
 // This test case may fail to construct a worst-case input if the pivot choice
 // of the underlying quick_sort no longer matches the one used here.
 // So it provides no strong guarantees about the properties of quick_sort.
-TEST_CASE(maximum_stack_depth)
+TEST_CASE(quick_sort_maximum_stack_depth)
 {
     int const size = 256;
     int* data = new int[size];
@@ -94,4 +98,73 @@ TEST_CASE(maximum_stack_depth)
         EXPECT(data[i] == i);
 
     delete[] data;
+}
+
+static constexpr int const n = 10;
+static constexpr int const iterations = 10;
+static constexpr int const seed = 1337;
+
+TEST_CASE(insertion_sort_sorts_ascending)
+{
+    srand(seed);
+
+    for (int i = 0; i < iterations; ++i) {
+        Vector<int, n> ints;
+        for (int j = 0; j < n; ++j)
+            ints.append(get_random<int>());
+        Vector<int, n> ints_copy = ints;
+
+        insertion_sort(ints);
+        insertion_sort(ints_copy);
+
+        for (int j = 0; j < n - 1; ++j) {
+            EXPECT(ints[j] <= ints[j + 1]);
+            EXPECT_EQ(ints[j], ints_copy[j]);
+            EXPECT_EQ(ints[j + 1], ints_copy[j + 1]);
+        }
+    }
+}
+
+TEST_CASE(insertion_sort_sorts_descending)
+{
+    srand(seed);
+
+    for (int i = 0; i < iterations; ++i) {
+        Vector<int, n> ints;
+        for (int j = 0; j < n; ++j)
+            ints.append(get_random<int>());
+        Vector<int, n> ints_copy = ints;
+
+        insertion_sort(ints, [](auto& a, auto& b) { return a > b; });
+        insertion_sort(ints_copy, [](auto& a, auto& b) { return a > b; });
+
+        for (int j = 0; j < n - 1; ++j) {
+            EXPECT(ints[j] >= ints[j + 1]);
+            EXPECT_EQ(ints[j], ints_copy[j]);
+            EXPECT_EQ(ints[j + 1], ints_copy[j + 1]);
+        }
+    }
+}
+
+TEST_CASE(insertion_sort_sorts_subrange_without_affecting_outside_elements)
+{
+    Vector<int> ints = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+    Vector<int> const original_ints = ints;
+
+    ssize_t const start = 3;
+    ssize_t const end = 6;
+
+    insertion_sort(ints, start, end, [](auto& a, auto& b) { return a < b; });
+
+    for (ssize_t i = start; i < end; ++i) {
+        EXPECT(ints[i] <= ints[i + 1]);
+    }
+
+    for (ssize_t i = 0; i < start; ++i) {
+        EXPECT_EQ(ints[i], original_ints[i]);
+    }
+
+    for (ssize_t i = end + 1; i < static_cast<ssize_t>(ints.size()); ++i) {
+        EXPECT_EQ(ints[i], original_ints[i]);
+    }
 }
