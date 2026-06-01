ce5c76f3825ef4bda091fb64e7b5020798fb8724
Timothy Flynn <trflynn89@pm.me>
2026-06-01 07:12:02 -0400

    UI/Qt: Remove unnecessary update to toolbar buttons for vertical tabs
    Commits a7e68ef and 24e767a shuffled things around in that they ended up
    performing manual updates to the toolbar buttons in order to update the
    collapse/expand vertical tabs button. However, this isn't needed - that
    button's state is totally controlled by LibWebView already (through the
    WebView::Action infrastructure).

--- UI/Qt/Tab.h
+++ UI/Qt/Tab.h
@@ -81,10 +81,8 @@ public:
     QToolButton* hamburger_button() const { return m_hamburger_button; }
 
     void set_vertical_tabs_enabled(bool);
-    void set_vertical_tabs_expanded(bool);
     void set_window(BrowserWindow&);
     void set_toolbar_window_controls_visible(bool);
-    void set_toolbar_window_drag_enabled(bool);
     void update_window_control_icons();
     void update_hover_label();
 
@@ -121,7 +119,6 @@ private:
     WindowControlButton* m_maximize_window_button { nullptr };
     WindowControlButton* m_close_window_button { nullptr };
     BookmarksBar* m_bookmarks_bar { nullptr };
-    QToolButton* m_toggle_vertical_tabs_expanded_button { nullptr };
     QToolButton* m_hamburger_button { nullptr };
     LocationEdit* m_location_edit { nullptr };
     WebContentView* m_view { nullptr };
@@ -142,10 +139,10 @@ private:
     QMenu* m_media_context_menu { nullptr };
     QMenu* m_select_dropdown { nullptr };
 
+    QAction* m_toggle_vertical_tabs_expanded_action { nullptr };
     QAction* m_navigate_back_action { nullptr };
     QAction* m_navigate_forward_action { nullptr };
     QAction* m_reload_action { nullptr };
-    QAction* m_toggle_vertical_tabs_expanded_action { nullptr };
 
     QPointer<QDialog> m_dialog;
 
