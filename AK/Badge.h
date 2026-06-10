071b4f70628cab2be31f1c36768eab6b4b64259a
Sam Atkins <sam@ladybird.org>
2026-06-10 12:04:27 +0100

    AK: Allow derived types to create base badges
    Derived classes can now mint their own badge and pass it to APIs that
    accept a badge for an authorized base class.

--- AK/Badge.h
+++ AK/Badge.h
@@ -8,8 +8,17 @@
 
 #include <AK/Noncopyable.h>
 #include <AK/Platform.h>
+#include <AK/StdLibExtraDetails.h>
 
 namespace AK {
+
+namespace Detail {
+
+template<typename AuthorizedType, typename BadgeType>
+inline constexpr bool IsBadgeType = IsSame<AuthorizedType, BadgeType>
+    || IsBaseOf<AuthorizedType, BadgeType>;
+
+}
 
 template<typename...>
 class Badge;
@@ -21,6 +30,14 @@ class Badge<T> {
 
 public:
     using Type = T;
+
+    // Friendship is not inherited, so derived classes mint Badge<Derived> and
+    // convert it to Badge<Base>.
+    template<typename U>
+    requires(IsBaseOf<T, U> && !IsSame<T, U>)
+    constexpr Badge(Badge<U> const&)
+    {
+    }
 
 private:
     friend T;
@@ -34,6 +51,13 @@ class Badge<T, U> {
     AK_MAKE_NONCOPYABLE(Badge);
     AK_MAKE_NONMOVABLE(Badge);
 
+public:
+    template<typename V>
+    requires(Detail::IsBadgeType<T, V> || Detail::IsBadgeType<U, V>)
+    constexpr Badge(Badge<V> const&)
+    {
+    }
+
 private:
     friend T;
     friend U;
@@ -44,6 +68,14 @@ template<typename T, typename U, typename V>
 class Badge<T, U, V> {
     AK_MAKE_NONCOPYABLE(Badge);
     AK_MAKE_NONMOVABLE(Badge);
+
+public:
+    template<typename W>
+    requires(Detail::IsBadgeType<T, W> || Detail::IsBadgeType<U, W>
+        || Detail::IsBadgeType<V, W>)
+    constexpr Badge(Badge<W> const&)
+    {
+    }
 
 private:
     friend T;
@@ -57,6 +89,14 @@ class Badge<T, U, V, W> {
     AK_MAKE_NONCOPYABLE(Badge);
     AK_MAKE_NONMOVABLE(Badge);
 
+public:
+    template<typename X>
+    requires(Detail::IsBadgeType<T, X> || Detail::IsBadgeType<U, X>
+        || Detail::IsBadgeType<V, X> || Detail::IsBadgeType<W, X>)
+    constexpr Badge(Badge<X> const&)
+    {
+    }
+
 private:
     friend T;
     friend U;
@@ -69,6 +109,15 @@ template<typename T, typename U, typename V, typename W, typename X>
 class Badge<T, U, V, W, X> {
     AK_MAKE_NONCOPYABLE(Badge);
     AK_MAKE_NONMOVABLE(Badge);
+
+public:
+    template<typename Y>
+    requires(Detail::IsBadgeType<T, Y> || Detail::IsBadgeType<U, Y>
+        || Detail::IsBadgeType<V, Y> || Detail::IsBadgeType<W, Y>
+        || Detail::IsBadgeType<X, Y>)
+    constexpr Badge(Badge<Y> const&)
+    {
+    }
 
 private:
     friend T;
