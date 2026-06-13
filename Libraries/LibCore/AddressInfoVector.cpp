0fca7d1a6e7571ec85fe9af3d12c3b7501e20b3e
Andreas Kling <andreas@ladybird.org>
2026-06-13 19:46:15 +0200

    LibCore: Own addrinfo results without OwnPtr
    Store the getaddrinfo result pointer directly in AddressInfoVector and
    free it with freeaddrinfo from the destructor. This keeps the special
    cleanup logic local to LibCore instead of relying on OwnPtr custom
    deleter support.

--- Libraries/LibCore/AddressInfoVector.cpp
+++ Libraries/LibCore/AddressInfoVector.cpp
@@ -18,16 +18,26 @@ namespace Core::System {
 
 AddressInfoVector::AddressInfoVector(Vector<struct addrinfo> addresses, struct addrinfo* ptr)
     : m_addresses(move(addresses))
-    , m_ptr(adopt_own_if_nonnull(ptr))
+    , m_ptr(ptr)
 {
 }
 
-AddressInfoVector::~AddressInfoVector() = default;
-
-void AddressInfoVector::AddrInfoDeleter::operator()(struct addrinfo* ptr)
+AddressInfoVector::AddressInfoVector(AddressInfoVector&& other)
+    : m_addresses(move(other.m_addresses))
+    , m_ptr(exchange(other.m_ptr, nullptr))
 {
-    if (ptr)
-        ::freeaddrinfo(ptr);
+}
+
+AddressInfoVector::~AddressInfoVector()
+{
+    if (m_ptr)
+        ::freeaddrinfo(m_ptr);
+}
+
+void AddressInfoVector::swap(AddressInfoVector& other)
+{
+    AK::swap(m_addresses, other.m_addresses);
+    AK::swap(m_ptr, other.m_ptr);
 }
 
 }
