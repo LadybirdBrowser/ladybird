LadybirdBrowser/ladybird

commit 76f17f77033c997880404686c2b7507a5e289cc7

LibGC: Add support for coordinated multi-heap collection
Allow having separate GC heaps and implement coordinated marking between
them; this is useful for keeping wasm and js GC heaps separated with a
clear boundary.

Libraries/LibGC/CellAllocator.cpp
@@ -20,6 +20,16 @@ CellAllocator::CellAllocator(size_t cell_size, Optional<StringView> class_name, 
 {
 }
 
+CellAllocator& CellAllocatorDescriptorBase::for_heap(Heap& heap)
+{
+    if (m_last_heap == &heap) [[likely]]
+        return *m_last_allocator;
+    auto& allocator = heap.cell_allocator_for({}, *this);
+    m_last_heap = &heap;
+    m_last_allocator = &allocator;
+    return allocator;
+}
+
 Cell* CellAllocator::allocate_cell(Heap& heap)
 {
     if (!m_list_node.is_in_list())

Libraries/LibGC/CellAllocator.h
@@ -22,6 +22,45 @@
 
 namespace GC {
 
+class GC_API CellAllocatorDescriptorBase {
+    AK_MAKE_NONCOPYABLE(CellAllocatorDescriptorBase);
+    AK_MAKE_NONMOVABLE(CellAllocatorDescriptorBase);
+
+public:
+    Optional<StringView> class_name() const { return m_class_name; }
+    size_t cell_size() const { return m_cell_size; }
+    bool overrides_must_survive_garbage_collection() const { return m_overrides_must_survive_garbage_collection; }
+    bool overrides_finalize() const { return m_overrides_finalize; }
+
+    CellAllocator& for_heap(Heap&);
+
+    void forget_heap(Badge<Heap>, Heap& heap)
+    {
+        if (m_last_heap == &heap) {
+            m_last_heap = nullptr;
+            m_last_allocator = nullptr;
+        }
+    }
+
+protected:
+    CellAllocatorDescriptorBase(size_t cell_size, StringView class_name, bool overrides_must_survive_garbage_collection, bool overrides_finalize)
+        : m_class_name(class_name)
+        , m_cell_size(cell_size)
+        , m_overrides_must_survive_garbage_collection(overrides_must_survive_garbage_collection)
+        , m_overrides_finalize(overrides_finalize)
+    {
+    }
+
+private:
+    Optional<StringView> m_class_name;
+    size_t m_cell_size { 0 };
+    bool m_overrides_must_survive_garbage_collection { false };
+    bool m_overrides_finalize { false };
+
+    Heap* m_last_heap { nullptr };
+    CellAllocator* m_last_allocator { nullptr };
+};
+
 class GC_API CellAllocator {
 public:
     CellAllocator(size_t cell_size, Optional<StringView> = {}, bool overrides_must_survive_garbage_collection = false, bool overrides_finalize = false);
@@ -81,16 +120,14 @@ private:
 };
 
 template<typename T>
-class GC_API TypeIsolatingCellAllocator {
+class GC_API TypeIsolatingCellAllocator final : public CellAllocatorDescriptorBase {
 public:
     using CellType = T;
 
     TypeIsolatingCellAllocator(StringView class_name, bool overrides_must_survive_garbage_collection, bool overrides_finalize)
-        : allocator(sizeof(T), class_name, overrides_must_survive_garbage_collection, overrides_finalize)
+        : CellAllocatorDescriptorBase(sizeof(T), class_name, overrides_must_survive_garbage_collection, overrides_finalize)
     {
     }
-
-    NeverDestroyed<CellAllocator> allocator;
 };
 
 }

Libraries/LibGC/CMakeLists.txt
@@ -4,12 +4,15 @@ set(SOURCES
     CellAllocator.cpp
     ConservativeHashMap.cpp
     ConservativeHashTable.cpp
+    ConservativeRangeProvider.cpp
     ConservativeVector.cpp
+    CrossHeapMember.cpp
     Root.cpp
     RootHashMap.cpp
     RootHashTable.cpp
     RootVector.cpp
     Heap.cpp
+    HeapGroup.cpp
     HeapBlock.cpp
     Timer.cpp
     WeakBlock.cpp

Libraries/LibGC/ConservativeRangeProvider.cpp
@@ -1,0 +1,24 @@
+/*
+ * Copyright (c) 2026, Ali Mohammad Pur <ali@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibGC/ConservativeRangeProvider.h>
+#include <LibGC/Heap.h>
+
+namespace GC {
+
+ConservativeRangeProvider::ConservativeRangeProvider(Heap& heap)
+    : m_heap(&heap)
+{
+    m_heap->did_create_conservative_range_provider({}, *this);
+}
+
+ConservativeRangeProvider::~ConservativeRangeProvider()
+{
+    if (m_heap)
+        m_heap->did_destroy_conservative_range_provider({}, *this);
+}
+
+}

Libraries/LibGC/ConservativeRangeProvider.h
@@ -1,0 +1,38 @@
+/*
+ * Copyright (c) 2026, Ali Mohammad Pur <ali@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Function.h>
+#include <AK/IntrusiveList.h>
+#include <AK/Span.h>
+#include <LibGC/Forward.h>
+
+namespace GC {
+
+class GC_API ConservativeRangeProvider {
+    AK_MAKE_NONCOPYABLE(ConservativeRangeProvider);
+    AK_MAKE_NONMOVABLE(ConservativeRangeProvider);
+
+public:
+    virtual ~ConservativeRangeProvider();
+
+    // Called while gathering roots; every word of every reported range is treated as a possible cell pointer.
+    virtual void for_each_conservative_range(AK::Function<void(ReadonlySpan<FlatPtr>)> const&) const = 0;
+
+    void detach_from_heap(Badge<Heap>) { m_heap = nullptr; }
+
+protected:
+    explicit ConservativeRangeProvider(Heap&);
+
+    Heap* m_heap { nullptr };
+    IntrusiveListNode<ConservativeRangeProvider> m_list_node;
+
+public:
+    using List = IntrusiveList<&ConservativeRangeProvider::m_list_node>;
+};
+
+}

Libraries/LibGC/CrossHeapMember.cpp
@@ -1,0 +1,28 @@
+/*
+ * Copyright (c) 2026, Ali Mohammad Pur <ali@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibGC/CrossHeapMember.h>
+#include <LibGC/Heap.h>
+
+namespace GC {
+
+void CrossHeapMemberBase::reset(Cell* cell)
+{
+    auto* new_heap = cell ? &HeapBlockBase::from_cell(cell)->heap() : nullptr;
+    if (m_registered_heap == new_heap) {
+        m_cell = cell;
+        return;
+    }
+
+    if (m_registered_heap)
+        m_registered_heap->did_destroy_cross_heap_member({}, *this);
+    m_cell = cell;
+    m_registered_heap = new_heap;
+    if (m_registered_heap)
+        m_registered_heap->did_create_cross_heap_member({}, *this);
+}
+
+}

Libraries/LibGC/CrossHeapMember.h
@@ -1,0 +1,77 @@
+/*
+ * Copyright (c) 2026, Ali Mohammad Pur <ali@ladybird.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <LibGC/Cell.h>
+#include <LibGC/Forward.h>
+#include <LibGC/Internals.h>
+
+namespace GC {
+
+// A strong reference from a cell on one heap to a cell on another heap in the same HeapGroup.
+class GC_API CrossHeapMemberBase {
+    AK_MAKE_NONCOPYABLE(CrossHeapMemberBase);
+    AK_MAKE_NONMOVABLE(CrossHeapMemberBase);
+
+public:
+    Cell* cell_base() const { return static_cast<Cell*>(m_cell); }
+
+    void detach_from_heap(Badge<Heap>)
+    {
+        m_registered_heap = nullptr;
+        m_cell = nullptr;
+    }
+
+protected:
+    explicit CrossHeapMemberBase(Cell* cell)
+    {
+        reset(cell);
+    }
+
+    ~CrossHeapMemberBase()
+    {
+        reset(nullptr);
+    }
+
+    void reset(Cell* cell);
+
+    void* m_cell { nullptr }; // Not a member of "this" heap.
+    Heap* m_registered_heap { nullptr };
+};
+
+template<typename T>
+class CrossHeapMember final : public CrossHeapMemberBase {
+public:
+    CrossHeapMember()
+        : CrossHeapMemberBase(nullptr)
+    {
+    }
+
+    explicit CrossHeapMember(T* cell)
+        : CrossHeapMemberBase(cell)
+    {
+    }
+
+    CrossHeapMember& operator=(T* cell)
+    {
+        reset(cell);
+        return *this;
+    }
+
+    T* ptr() const { return static_cast<T*>(m_cell); }
+    T* operator->() const { return ptr(); }
+    explicit operator bool() const { return m_cell != nullptr; }
+
+    // Called by the holder heap.
+    void visit(Cell::Visitor& visitor) const
+    {
+        if (m_cell)
+            visitor.visit(cell_base());
+    }
+};
+
+}

Libraries/LibGC/Forward.h
@@ -16,6 +16,8 @@ class CellAllocator;
 class DeferGC;
 class RootImpl;
 class Heap;
+class HeapGroup;
+class CrossHeapMemberBase;
 class HeapBlock;
 class NanBoxedValue;
 class Timer;

commit c156682d1d77ea7c04e135ac94a68d3e949b262b

LibGC: Decommit used physical memory when Heaps get destroyed
This makes sure we don't leak physical memory on the heap-death of the
universe, an issue 76f17f77033c997880404686c2b7507a5e289cc7 introduced
by changing the lifetime of heaps.

Libraries/LibGC/CellAllocator.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <AK/Badge.h>
+#include <AK/NeverDestroyed.h>
 #include <LibGC/BlockAllocator.h>
 #include <LibGC/CellAllocator.h>
 #include <LibGC/Heap.h>
@@ -12,12 +13,33 @@
 
 namespace GC {
 
+BlockAllocator& CellAllocator::shared_block_allocator()
+{
+    static AK::NeverDestroyed<BlockAllocator> allocator;
+    return *allocator;
+}
+
 CellAllocator::CellAllocator(size_t cell_size, Optional<StringView> class_name, bool overrides_must_survive_garbage_collection, bool overrides_finalize)
     : m_class_name(class_name)
     , m_cell_size(cell_size)
+    , m_block_allocator(shared_block_allocator())
     , m_overrides_must_survive_garbage_collection(overrides_must_survive_garbage_collection)
     , m_overrides_finalize(overrides_finalize)
 {
+}
+
+CellAllocator::~CellAllocator()
+{
+    m_blocks_pending_sweep.clear();
+
+    auto reclaim_all = [this](BlockList& blocks) {
+        while (auto* block = blocks.take_first()) {
+            block->~HeapBlock();
+            m_block_allocator.deallocate_block(block, DeferDecommit::Yes);
+        }
+    };
+    reclaim_all(m_full_blocks);
+    reclaim_all(m_usable_blocks);
 }
 
 CellAllocator& CellAllocatorDescriptorBase::for_heap(Heap& heap)

Libraries/LibGC/CellAllocator.h
@@ -64,7 +64,9 @@ private:
 class GC_API CellAllocator {
 public:
     CellAllocator(size_t cell_size, Optional<StringView> = {}, bool overrides_must_survive_garbage_collection = false, bool overrides_finalize = false);
-    ~CellAllocator() = default;
+    ~CellAllocator();
+
+    static BlockAllocator& shared_block_allocator();
 
     Optional<StringView> class_name() const { return m_class_name; }
     size_t cell_size() const { return m_cell_size; }
@@ -106,7 +108,7 @@ private:
     Optional<StringView> m_class_name;
     size_t const m_cell_size;
 
-    BlockAllocator m_block_allocator;
+    BlockAllocator& m_block_allocator;
 
     using BlockList = IntrusiveList<&HeapBlock::m_list_node>;
     using SweepBlockList = IntrusiveList<&HeapBlock::m_sweep_list_node>;