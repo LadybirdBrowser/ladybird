b445c2ae877f652cf8bb5beec55479b127f2f9e6
Sam Atkins <sam@ladybird.org>
2026-06-26 12:22:20 +0100

    LibWeb/SVG: Split SVG tag name definitions into strings and C++ names
    Preparation for the `<switch>` element, which is a C++ keyword.

--- Libraries/LibWeb/SVG/TagNames.h
+++ Libraries/LibWeb/SVG/TagNames.h
@@ -10,58 +10,58 @@
 
 namespace Web::SVG::TagNames {
 
-#define ENUMERATE_SVG_TAGS                   \
-    __ENUMERATE_SVG_TAG(a)                   \
-    __ENUMERATE_SVG_TAG(circle)              \
-    __ENUMERATE_SVG_TAG(clipPath)            \
-    __ENUMERATE_SVG_TAG(defs)                \
-    __ENUMERATE_SVG_TAG(desc)                \
-    __ENUMERATE_SVG_TAG(ellipse)             \
-    __ENUMERATE_SVG_TAG(feBlend)             \
-    __ENUMERATE_SVG_TAG(feColorMatrix)       \
-    __ENUMERATE_SVG_TAG(feComponentTransfer) \
-    __ENUMERATE_SVG_TAG(feComposite)         \
-    __ENUMERATE_SVG_TAG(feDisplacementMap)   \
-    __ENUMERATE_SVG_TAG(feDropShadow)        \
-    __ENUMERATE_SVG_TAG(feFlood)             \
-    __ENUMERATE_SVG_TAG(feFuncA)             \
-    __ENUMERATE_SVG_TAG(feFuncB)             \
-    __ENUMERATE_SVG_TAG(feFuncG)             \
-    __ENUMERATE_SVG_TAG(feFuncR)             \
-    __ENUMERATE_SVG_TAG(feGaussianBlur)      \
-    __ENUMERATE_SVG_TAG(feImage)             \
-    __ENUMERATE_SVG_TAG(feMerge)             \
-    __ENUMERATE_SVG_TAG(feMergeNode)         \
-    __ENUMERATE_SVG_TAG(feMorphology)        \
-    __ENUMERATE_SVG_TAG(feOffset)            \
-    __ENUMERATE_SVG_TAG(feTurbulence)        \
-    __ENUMERATE_SVG_TAG(filter)              \
-    __ENUMERATE_SVG_TAG(foreignObject)       \
-    __ENUMERATE_SVG_TAG(g)                   \
-    __ENUMERATE_SVG_TAG(image)               \
-    __ENUMERATE_SVG_TAG(line)                \
-    __ENUMERATE_SVG_TAG(linearGradient)      \
-    __ENUMERATE_SVG_TAG(mask)                \
-    __ENUMERATE_SVG_TAG(metadata)            \
-    __ENUMERATE_SVG_TAG(path)                \
-    __ENUMERATE_SVG_TAG(pattern)             \
-    __ENUMERATE_SVG_TAG(polygon)             \
-    __ENUMERATE_SVG_TAG(polyline)            \
-    __ENUMERATE_SVG_TAG(radialGradient)      \
-    __ENUMERATE_SVG_TAG(rect)                \
-    __ENUMERATE_SVG_TAG(script)              \
-    __ENUMERATE_SVG_TAG(stop)                \
-    __ENUMERATE_SVG_TAG(style)               \
-    __ENUMERATE_SVG_TAG(svg)                 \
-    __ENUMERATE_SVG_TAG(symbol)              \
-    __ENUMERATE_SVG_TAG(text)                \
-    __ENUMERATE_SVG_TAG(textPath)            \
-    __ENUMERATE_SVG_TAG(title)               \
-    __ENUMERATE_SVG_TAG(tspan)               \
-    __ENUMERATE_SVG_TAG(use)                 \
-    __ENUMERATE_SVG_TAG(view)
+#define ENUMERATE_SVG_TAGS                                          \
+    __ENUMERATE_SVG_TAG(a, "a")                                     \
+    __ENUMERATE_SVG_TAG(circle, "circle")                           \
+    __ENUMERATE_SVG_TAG(clipPath, "clipPath")                       \
+    __ENUMERATE_SVG_TAG(defs, "defs")                               \
+    __ENUMERATE_SVG_TAG(desc, "desc")                               \
+    __ENUMERATE_SVG_TAG(ellipse, "ellipse")                         \
+    __ENUMERATE_SVG_TAG(feBlend, "feBlend")                         \
+    __ENUMERATE_SVG_TAG(feColorMatrix, "feColorMatrix")             \
+    __ENUMERATE_SVG_TAG(feComponentTransfer, "feComponentTransfer") \
+    __ENUMERATE_SVG_TAG(feComposite, "feComposite")                 \
+    __ENUMERATE_SVG_TAG(feDisplacementMap, "feDisplacementMap")     \
+    __ENUMERATE_SVG_TAG(feDropShadow, "feDropShadow")               \
+    __ENUMERATE_SVG_TAG(feFlood, "feFlood")                         \
+    __ENUMERATE_SVG_TAG(feFuncA, "feFuncA")                         \
+    __ENUMERATE_SVG_TAG(feFuncB, "feFuncB")                         \
+    __ENUMERATE_SVG_TAG(feFuncG, "feFuncG")                         \
+    __ENUMERATE_SVG_TAG(feFuncR, "feFuncR")                         \
+    __ENUMERATE_SVG_TAG(feGaussianBlur, "feGaussianBlur")           \
+    __ENUMERATE_SVG_TAG(feImage, "feImage")                         \
+    __ENUMERATE_SVG_TAG(feMerge, "feMerge")                         \
+    __ENUMERATE_SVG_TAG(feMergeNode, "feMergeNode")                 \
+    __ENUMERATE_SVG_TAG(feMorphology, "feMorphology")               \
+    __ENUMERATE_SVG_TAG(feOffset, "feOffset")                       \
+    __ENUMERATE_SVG_TAG(feTurbulence, "feTurbulence")               \
+    __ENUMERATE_SVG_TAG(filter, "filter")                           \
+    __ENUMERATE_SVG_TAG(foreignObject, "foreignObject")             \
+    __ENUMERATE_SVG_TAG(g, "g")                                     \
+    __ENUMERATE_SVG_TAG(image, "image")                             \
+    __ENUMERATE_SVG_TAG(line, "line")                               \
+    __ENUMERATE_SVG_TAG(linearGradient, "linearGradient")           \
+    __ENUMERATE_SVG_TAG(mask, "mask")                               \
+    __ENUMERATE_SVG_TAG(metadata, "metadata")                       \
+    __ENUMERATE_SVG_TAG(path, "path")                               \
+    __ENUMERATE_SVG_TAG(pattern, "pattern")                         \
+    __ENUMERATE_SVG_TAG(polygon, "polygon")                         \
+    __ENUMERATE_SVG_TAG(polyline, "polyline")                       \
+    __ENUMERATE_SVG_TAG(radialGradient, "radialGradient")           \
+    __ENUMERATE_SVG_TAG(rect, "rect")                               \
+    __ENUMERATE_SVG_TAG(script, "script")                           \
+    __ENUMERATE_SVG_TAG(stop, "stop")                               \
+    __ENUMERATE_SVG_TAG(style, "style")                             \
+    __ENUMERATE_SVG_TAG(svg, "svg")                                 \
+    __ENUMERATE_SVG_TAG(symbol, "symbol")                           \
+    __ENUMERATE_SVG_TAG(text, "text")                               \
+    __ENUMERATE_SVG_TAG(textPath, "textPath")                       \
+    __ENUMERATE_SVG_TAG(title, "title")                             \
+    __ENUMERATE_SVG_TAG(tspan, "tspan")                             \
+    __ENUMERATE_SVG_TAG(use, "use")                                 \
+    __ENUMERATE_SVG_TAG(view, "view")
 
-#define __ENUMERATE_SVG_TAG(name) extern FlyString const& name;
+#define __ENUMERATE_SVG_TAG(name, tag) extern FlyString const& name;
 ENUMERATE_SVG_TAGS
 #undef __ENUMERATE_SVG_TAG
 
