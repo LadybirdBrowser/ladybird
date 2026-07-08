f26b22a8dd421d20221729d4c5d1111745f6ce5d
Shannon Booth <shannon.ml.booth@gmail.com>
2026-07-08 15:16:52 +0200

    LibWeb: Add ReplicatedNavigableState
    Introduce a shared representation of the per-navigable state that the UI
    process will own for each canonical navigable. This will also be the
    state RemoteNavigable will need as its local mirror of a document hosted
    in another process.

---
+++ Libraries/LibWeb/HTML/ReplicatedNavigableState.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026-present, the Ladybird developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <LibIPC/Decoder.h>
+#include <LibIPC/Encoder.h>
+#include <LibWeb/HTML/ReplicatedNavigableState.h>
+
+namespace IPC {
+
+template<>
+ErrorOr<void> encode(Encoder& encoder, Web::HTML::ReplicatedNavigableState const& state)
+{
+    TRY(encoder.encode(state.target_name));
+    TRY(encoder.encode(state.active_document_origin));
+    TRY(encoder.encode(state.active_document_is_fully_active));
+    return {};
+}
+
+template<>
+ErrorOr<Web::HTML::ReplicatedNavigableState> decode(Decoder& decoder)
+{
+    return Web::HTML::ReplicatedNavigableState {
+        .target_name = TRY(decoder.decode<String>()),
+        .active_document_origin = TRY(decoder.decode<URL::Origin>()),
+        .active_document_is_fully_active = TRY(decoder.decode<bool>()),
+    };
+}
+
+}
---
+++ Libraries/LibWeb/HTML/ReplicatedNavigableState.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2026-present, the Ladybird developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/String.h>
+#include <LibIPC/Forward.h>
+#include <LibURL/Origin.h>
+#include <LibWeb/Export.h>
+
+namespace Web::HTML {
+
+struct ReplicatedNavigableState {
+    String target_name;
+    URL::Origin active_document_origin;
+    bool active_document_is_fully_active { false };
+};
+
+}
+
+namespace IPC {
+
+template<>
+WEB_API ErrorOr<void> encode(Encoder&, Web::HTML::ReplicatedNavigableState const&);
+template<>
+WEB_API ErrorOr<Web::HTML::ReplicatedNavigableState> decode(Decoder&);
+
+}
