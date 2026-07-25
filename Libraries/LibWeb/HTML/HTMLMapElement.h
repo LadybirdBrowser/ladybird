f28d2d015cd47cc0cb1713c8a06c41d3dc334002
Tim Ledbetter <tim.ledbetter@ladybird.org>
2026-07-25 04:01:36 +0100

    LibWeb: Allow area elements to receive focus
    Area elements are never rendered, so the general rule that only
    rendered elements are focusable areas made them unfocusable. The
    shapes of area elements in an image map are focusable areas per the
    specification. An area element with a hyperlink or a valid tabindex
    attribute is now focusable whenever an image associated with its image
    map is being rendered and is not inert.

--- Libraries/LibWeb/HTML/HTMLMapElement.h
+++ Libraries/LibWeb/HTML/HTMLMapElement.h
@@ -8,6 +8,7 @@
 
 #include <LibWeb/DOM/HTMLCollection.h>
 #include <LibWeb/HTML/HTMLElement.h>
+#include <LibWeb/HTML/HTMLImageElement.h>
 #include <LibWeb/PixelUnits.h>
 
 namespace Web::HTML {
@@ -23,8 +24,34 @@ public:
 
     GC::Ptr<HTMLAreaElement> area_for_point(CSSPixelPoint, CSSPixelSize image_size);
 
+    template<typename Callback>
+    void for_each_associated_image(Callback callback) const
+    {
+        const_cast<DOM::Node&>(root()).for_each_in_inclusive_subtree_of_type<HTMLImageElement>([&](HTMLImageElement& image_element) {
+            if (image_element.associated_map_element().ptr() != this)
+                return TraversalDecision::Continue;
+            return callback(image_element);
+        });
+    }
+
+    GC::Ptr<HTMLImageElement> first_image_with_focusable_shapes() const;
+
 private:
     HTMLMapElement(DOM::Document&, DOM::QualifiedName);
+
+    template<typename Predicate>
+    GC::Ptr<HTMLImageElement> first_associated_image_matching(Predicate predicate) const
+    {
+        GC::Ptr<HTMLImageElement> first_image;
+        for_each_associated_image([&](HTMLImageElement& image_element) {
+            if (!predicate(image_element))
+                return TraversalDecision::Continue;
+            first_image = image_element;
+            return TraversalDecision::Break;
+        });
+        return first_image;
+    }
+
     virtual void visit_edges(Cell::Visitor&) override;
 
     GC::Ptr<DOM::HTMLCollection> m_areas;
