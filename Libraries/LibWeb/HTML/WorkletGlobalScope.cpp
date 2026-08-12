ecd76f4b5d52cafdcf618b4ea03acf8fe5c66f03
Ali Mohammad Pur <ali.mpfard@gmail.com>
2026-08-12 13:44:48 +0200

    LibWeb: Add the Worklet interface and worklet environment settings

--- Libraries/LibWeb/HTML/WorkletGlobalScope.cpp
+++ Libraries/LibWeb/HTML/WorkletGlobalScope.cpp
@@ -4,7 +4,20 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <LibWeb/Bindings/Wrappable.h>
+#include <LibWeb/HTML/Scripting/Environments.h>
 #include <LibWeb/HTML/WorkletGlobalScope.h>
+
+namespace Web::Bindings {
+
+JS::Realm& main_world_realm(HTML::WorkletGlobalScope const& worklet_global_scope)
+{
+    auto wrapper = worklet_global_scope.cached_main_world_wrapper();
+    VERIFY(wrapper);
+    return wrapper->realm();
+}
+
+}
 
 namespace Web::HTML {
 
@@ -15,5 +28,28 @@ WorkletGlobalScope::WorkletGlobalScope()
 }
 
 WorkletGlobalScope::~WorkletGlobalScope() = default;
+
+JS::Realm& WorkletGlobalScope::realm() const
+{
+    return Bindings::main_world_realm(*this);
+}
+
+EnvironmentSettingsObject& WorkletGlobalScope::settings_object() const
+{
+    VERIFY(m_settings_object);
+    return *m_settings_object;
+}
+
+void WorkletGlobalScope::set_settings_object(Badge<WorkletEnvironmentSettingsObject>, GC::Ref<EnvironmentSettingsObject> settings_object)
+{
+    m_settings_object = settings_object;
+}
+
+void WorkletGlobalScope::visit_edges(Cell::Visitor& visitor)
+{
+    Base::visit_edges(visitor);
+    UniversalGlobalScopeMixin::visit_edges(visitor);
+    visitor.visit(m_settings_object);
+}
 
 }
--- Libraries/LibWeb/HTML/WorkletGlobalScope.h
+++ Libraries/LibWeb/HTML/WorkletGlobalScope.h
@@ -6,20 +6,60 @@
 
 #pragma once
 
-#include <LibWeb/Bindings/Wrappable.h>
+#include <LibURL/URL.h>
+#include <LibWeb/DOM/EventTarget.h>
+#include <LibWeb/Export.h>
+#include <LibWeb/Forward.h>
+#include <LibWeb/HTML/UniversalGlobalScope.h>
+
+namespace Web::Bindings {
+
+WEB_API JS::Realm& main_world_realm(HTML::WorkletGlobalScope const&);
+
+}
 
 namespace Web::HTML {
 
 // https://html.spec.whatwg.org/multipage/worklets.html#workletglobalscope
-class WorkletGlobalScope : public Bindings::GCAllocatedWrappable {
-    WEB_WRAPPABLE(WorkletGlobalScope, Bindings::GCAllocatedWrappable);
+// NOTE: The IDL interface does not inherit from EventTarget; deriving from DOM::EventTarget here is
+//       internal plumbing so worklet scopes fit the machinery that types "a global" as an event
+//       target (MessagePort creation, relevant-global helpers). No EventTarget API is exposed.
+class WEB_API WorkletGlobalScope
+    : public DOM::EventTarget
+    , public UniversalGlobalScopeMixin {
+    WEB_WRAPPABLE(WorkletGlobalScope, DOM::EventTarget);
     GC_DECLARE_ALLOCATOR(WorkletGlobalScope);
 
 public:
     virtual ~WorkletGlobalScope() override;
 
+    virtual DOM::EventTarget& this_impl() override { return *this; }
+    virtual DOM::EventTarget const& this_impl() const override { return *this; }
+
+    JS::Realm& realm() const;
+
+    EnvironmentSettingsObject& settings_object() const;
+    void set_settings_object(Badge<WorkletEnvironmentSettingsObject>, GC::Ref<EnvironmentSettingsObject>);
+
+    URL::URL const& url() const { return m_url; }
+    void set_url(URL::URL url) { m_url = move(url); }
+
+    // Called once the realm's [[HostDefined]] is installed; subclass overrides are generated from IDL
+    // and install the global's exposed interfaces and mixin members.
+    virtual void initialize_web_interfaces_impl() { }
+
 protected:
     WorkletGlobalScope();
+
+    virtual void visit_edges(Cell::Visitor&) override;
+
+private:
+    friend JS::Realm& Bindings::main_world_realm(WorkletGlobalScope const&);
+
+    virtual bool is_universal_global_scope_mixin() const final { return true; }
+
+    GC::Ptr<EnvironmentSettingsObject> m_settings_object;
+    URL::URL m_url;
 };
 
 }
