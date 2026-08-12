14051ac934cfc59162268ddcaee2fff66f7b0e16
Ali Mohammad Pur <ali.mpfard@gmail.com>
2026-08-12 13:50:06 +0200

    LibWeb: Add AudioWorkletNode's control side

--- Libraries/LibWeb/Forward.h
+++ Libraries/LibWeb/Forward.h
@@ -1335,6 +1335,7 @@ namespace Web::WebAudio {
 class AudioBuffer;
 class AudioWorklet;
 class AudioWorkletGlobalScope;
+class AudioWorkletNode;
 class AudioWorkletProcessor;
 class AudioBufferSourceNode;
 class AudioContext;
