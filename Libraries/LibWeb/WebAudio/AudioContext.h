7b73170d05f33cd599af7bf5dd4fe180ac2ee4b5
Ali Mohammad Pur <ali.mpfard@gmail.com>
2026-08-12 13:41:34 +0200

    LibWeb: Implement MediaStreamAudioDestinationNode

--- Libraries/LibWeb/WebAudio/AudioContext.h
+++ Libraries/LibWeb/WebAudio/AudioContext.h
@@ -9,6 +9,7 @@
 #include <LibWeb/Bindings/AudioContext.h>
 #include <LibWeb/WebAudio/BaseAudioContext.h>
 #include <LibWeb/WebAudio/MediaElementAudioSourceNode.h>
+#include <LibWeb/WebAudio/MediaStreamAudioDestinationNode.h>
 #include <LibWeb/WebAudio/MediaStreamAudioSourceNode.h>
 #include <LibWeb/WebAudio/Rendering/RealtimeAudioRenderer.h>
 
@@ -41,6 +42,7 @@ public:
 
     WebIDL::ExceptionOr<GC::Ref<MediaElementAudioSourceNode>> create_media_element_source(GC::Ptr<HTML::HTMLMediaElement>);
     WebIDL::ExceptionOr<GC::Ref<MediaStreamAudioSourceNode>> create_media_stream_source(GC::Ptr<MediaCapture::MediaStream>);
+    WebIDL::ExceptionOr<GC::Ref<MediaStreamAudioDestinationNode>> create_media_stream_destination();
 
 private:
     explicit AudioContext(GC::Ref<DOM::EventTarget> relevant_global_object)
