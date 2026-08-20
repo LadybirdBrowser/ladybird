e96c05e004ff6b27956d229741b030c7e8dc48fa
sideshowbarker <mike@w3.org>
2026-08-20 15:58:12 +0900

    LibGfx: Remove now-unused Path::text() and place_text_along()
    After SVG text path-building was reworked to build from shaped glyph
    runs, Path::text() and place_text_along() no longer have any callers.

--- Libraries/LibGfx/PathSkia.h
+++ Libraries/LibGfx/PathSkia.h
@@ -28,8 +28,6 @@ public:
     virtual void arc_to(FloatPoint point, float radius, bool large_arc, bool sweep) override;
     virtual void quadratic_bezier_curve_to(FloatPoint through, FloatPoint point) override;
     virtual void cubic_bezier_curve_to(FloatPoint c1, FloatPoint c2, FloatPoint p2) override;
-    virtual void text(Utf8View const&, Font const&) override;
-    virtual void text(Utf16View const&, Font const&) override;
     virtual void glyph_run(GlyphRun const&) override;
     virtual void offset(Gfx::FloatPoint const&) override;
 
@@ -48,8 +46,6 @@ public:
 
     virtual NonnullOwnPtr<PathImpl> clone() const override;
     virtual NonnullOwnPtr<PathImpl> copy_transformed(Gfx::AffineTransform const&) const override;
-    virtual NonnullOwnPtr<PathImpl> place_text_along(Utf8View const& text, Font const&, float offset = 0) const override;
-    virtual NonnullOwnPtr<PathImpl> place_text_along(Utf16View const& text, Font const&, float offset = 0) const override;
     virtual NonnullOwnPtr<PathImpl> place_glyph_runs_along(ReadonlySpan<NonnullRefPtr<GlyphRun>>, float offset = 0) const override;
 
     virtual String to_svg_string() const override;
