ce046921207127b24f885e97542f2c2985996022
Andreas Kling <andreas@ladybird.org>
2026-08-24 03:48:10 +0200

    LibWeb: Highlight CSS from lean Rust tokens
    Consume token categories and positions directly from Rust instead of
    materializing C++ tokens. Use the same path for embedded CSS in HTML
    source highlighting.

--- Libraries/LibWeb/CSS/SyntaxHighlighter/SyntaxHighlighter.cpp
+++ Libraries/LibWeb/CSS/SyntaxHighlighter/SyntaxHighlighter.cpp
@@ -7,6 +7,7 @@
 #include <AK/Debug.h>
 #include <LibWeb/CSS/Parser/RustTokenizer.h>
 #include <LibWeb/CSS/SyntaxHighlighter/SyntaxHighlighter.h>
+#include <LibWeb/RustFFI.h>
 
 namespace Web::CSS {
 
@@ -17,7 +18,7 @@ void SyntaxHighlighter::rehighlight(Palette const& palette)
 
     Vector<Syntax::TextDocumentSpan> spans;
 
-    auto highlight = [&](auto start_line, auto start_column, auto end_line, auto end_column, Gfx::TextAttributes attributes, CSS::Parser::Token::Type type) {
+    auto highlight = [&](auto start_line, auto start_column, auto end_line, auto end_column, Gfx::TextAttributes attributes, CSS::Parser::FFI::CssTokenType type) {
         if (start_line > end_line || (start_line == end_line && start_column >= end_column)) {
             dbgln_if(SYNTAX_HIGHLIGHTING_DEBUG, "(CSS::SyntaxHighlighter) discarding ({}-{}) to ({}-{}) because it has zero or negative length", start_line, start_column, end_line, end_column);
             return;
@@ -33,84 +34,96 @@ void SyntaxHighlighter::rehighlight(Palette const& palette)
             false);
     };
 
-    auto tokens = CSS::Parser::RustTokenizer::tokenize(text, "utf-8"sv);
+    auto filtered_input = CSS::Parser::RustTokenizer::normalize_input(text, "utf-8"sv);
+    auto filtered_input_view = filtered_input.utf16_view();
+    Vector<CSS::Parser::FFI::CssSyntaxToken> tokens;
+    tokens.ensure_capacity((filtered_input_view.length_in_code_units() / 2) + 1);
+    CSS::Parser::FFI::rust_css_tokenize_for_syntax_highlighting(
+        filtered_input_view.has_ascii_storage() ? reinterpret_cast<u8 const*>(filtered_input_view.ascii_span().data()) : nullptr,
+        filtered_input_view.has_ascii_storage() ? nullptr : reinterpret_cast<u16 const*>(filtered_input_view.utf16_span().data()),
+        filtered_input_view.length_in_code_units(),
+        &tokens,
+        [](void* raw_tokens, CSS::Parser::FFI::CssSyntaxToken const* token) {
+            static_cast<Vector<CSS::Parser::FFI::CssSyntaxToken>*>(raw_tokens)->append(*token);
+        });
+
     for (auto const& token : tokens) {
-        if (token.is(Parser::Token::Type::EndOfFile))
+        if (token.token_type == Parser::FFI::CssTokenType::EndOfFile)
             break;
 
-        switch (token.type()) {
-        case Parser::Token::Type::Ident:
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column, { palette.syntax_identifier(), {} }, token.type());
+        switch (token.token_type) {
+        case Parser::FFI::CssTokenType::Ident:
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column, { palette.syntax_identifier(), {} }, token.token_type);
             break;
 
-        case Parser::Token::Type::String:
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column, { palette.syntax_string(), {} }, token.type());
+        case Parser::FFI::CssTokenType::String:
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column, { palette.syntax_string(), {} }, token.token_type);
             break;
 
-        case Parser::Token::Type::Whitespace:
+        case Parser::FFI::CssTokenType::Whitespace:
             // CSS doesn't produce comment tokens, they're just included as part of whitespace.
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column, { palette.syntax_comment(), {} }, token.type());
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column, { palette.syntax_comment(), {} }, token.token_type);
             break;
 
-        case Parser::Token::Type::AtKeyword:
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column, { palette.syntax_keyword(), {} }, token.type());
+        case Parser::FFI::CssTokenType::AtKeyword:
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column, { palette.syntax_keyword(), {} }, token.token_type);
             break;
 
-        case Parser::Token::Type::Function:
+        case Parser::FFI::CssTokenType::Function:
             // Function tokens include the opening '(', so we split that into two tokens for highlighting purposes.
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column - 1, { palette.syntax_keyword(), {} }, token.type());
-            highlight(token.end_position().line, token.end_position().column - 1, token.end_position().line, token.end_position().column, { palette.syntax_punctuation(), {} }, Parser::Token::Type::OpenParen);
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column - 1, { palette.syntax_keyword(), {} }, token.token_type);
+            highlight(token.end_line, token.end_column - 1, token.end_line, token.end_column, { palette.syntax_punctuation(), {} }, Parser::FFI::CssTokenType::OpenParen);
             break;
 
-        case Parser::Token::Type::Url:
+        case Parser::FFI::CssTokenType::Url:
             // An Url token is a `url()` function with its parameter string unquoted.
             // url
-            highlight(token.start_position().line, token.start_position().column, token.start_position().line, token.start_position().column + 3, { palette.syntax_keyword(), {} }, token.type());
+            highlight(token.start_line, token.start_column, token.start_line, token.start_column + 3, { palette.syntax_keyword(), {} }, token.token_type);
             // (
-            highlight(token.start_position().line, token.start_position().column + 3, token.start_position().line, token.start_position().column + 4, { palette.syntax_punctuation(), {} }, Parser::Token::Type::OpenParen);
+            highlight(token.start_line, token.start_column + 3, token.start_line, token.start_column + 4, { palette.syntax_punctuation(), {} }, Parser::FFI::CssTokenType::OpenParen);
             // <string>
-            highlight(token.start_position().line, token.start_position().column + 4, token.end_position().line, token.end_position().column - 1, { palette.syntax_string(), {} }, Parser::Token::Type::String);
+            highlight(token.start_line, token.start_column + 4, token.end_line, token.end_column - 1, { palette.syntax_string(), {} }, Parser::FFI::CssTokenType::String);
             // )
-            highlight(token.end_position().line, token.end_position().column - 1, token.end_position().line, token.end_position().column, { palette.syntax_punctuation(), {} }, Parser::Token::Type::CloseParen);
+            highlight(token.end_line, token.end_column - 1, token.end_line, token.end_column, { palette.syntax_punctuation(), {} }, Parser::FFI::CssTokenType::CloseParen);
             break;
 
-        case Parser::Token::Type::Number:
-        case Parser::Token::Type::Dimension:
-        case Parser::Token::Type::Percentage:
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column, { palette.syntax_number(), {} }, token.type());
+        case Parser::FFI::CssTokenType::Number:
+        case Parser::FFI::CssTokenType::Dimension:
+        case Parser::FFI::CssTokenType::Percentage:
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column, { palette.syntax_number(), {} }, token.token_type);
             break;
 
-        case Parser::Token::Type::Delim:
-        case Parser::Token::Type::Colon:
-        case Parser::Token::Type::Comma:
-        case Parser::Token::Type::Semicolon:
-        case Parser::Token::Type::OpenCurly:
-        case Parser::Token::Type::OpenParen:
-        case Parser::Token::Type::OpenSquare:
-        case Parser::Token::Type::CloseCurly:
-        case Parser::Token::Type::CloseParen:
-        case Parser::Token::Type::CloseSquare:
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column, { palette.syntax_punctuation(), {} }, token.type());
+        case Parser::FFI::CssTokenType::Delim:
+        case Parser::FFI::CssTokenType::Colon:
+        case Parser::FFI::CssTokenType::Comma:
+        case Parser::FFI::CssTokenType::Semicolon:
+        case Parser::FFI::CssTokenType::OpenCurly:
+        case Parser::FFI::CssTokenType::OpenParen:
+        case Parser::FFI::CssTokenType::OpenSquare:
+        case Parser::FFI::CssTokenType::CloseCurly:
+        case Parser::FFI::CssTokenType::CloseParen:
+        case Parser::FFI::CssTokenType::CloseSquare:
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column, { palette.syntax_punctuation(), {} }, token.token_type);
             break;
 
-        case Parser::Token::Type::CDO:
-        case Parser::Token::Type::CDC:
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column, { palette.syntax_comment(), {} }, token.type());
+        case Parser::FFI::CssTokenType::CDO:
+        case Parser::FFI::CssTokenType::CDC:
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column, { palette.syntax_comment(), {} }, token.token_type);
             break;
 
-        case Parser::Token::Type::Hash:
+        case Parser::FFI::CssTokenType::Hash:
             // FIXME: Hash tokens can be ID selectors or colors, we don't know which without parsing properly.
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column, { palette.syntax_number(), {} }, token.type());
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column, { palette.syntax_number(), {} }, token.token_type);
             break;
 
-        case Parser::Token::Type::Invalid:
-        case Parser::Token::Type::BadUrl:
-        case Parser::Token::Type::BadString:
+        case Parser::FFI::CssTokenType::Invalid:
+        case Parser::FFI::CssTokenType::BadUrl:
+        case Parser::FFI::CssTokenType::BadString:
             // FIXME: Error highlighting color in palette?
-            highlight(token.start_position().line, token.start_position().column, token.end_position().line, token.end_position().column, { Color(Color::NamedColor::Red), {}, true }, token.type());
+            highlight(token.start_line, token.start_column, token.end_line, token.end_column, { Color(Color::NamedColor::Red), {}, true }, token.token_type);
             break;
 
-        case Parser::Token::Type::EndOfFile:
+        case Parser::FFI::CssTokenType::EndOfFile:
         default:
             break;
         }
