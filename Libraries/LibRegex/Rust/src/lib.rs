2f325acf6c0fda70462c2bfcdce88622eb71d83b
Ali Mohammad Pur <ali.mpfard@gmail.com>
2026-09-06 13:50:22 +0200

    LibRegex: Port the relevant optimisations from the old engine back
    This is overall an ~80% performance improvement on the RegExp benchmark
    set.

--- Libraries/LibRegex/Rust/src/lib.rs
+++ Libraries/LibRegex/Rust/src/lib.rs
@@ -12,6 +12,7 @@ pub mod ast;
 pub mod bytecode;
 pub mod compiler;
 pub mod ffi;
+mod optimizer;
 pub mod parser;
 pub mod regex;
 pub mod vm;
--- Libraries/LibUnicode/Rust/src/lib.rs
+++ Libraries/LibUnicode/Rust/src/lib.rs
@@ -10,5 +10,7 @@ mod rust_allocator;
 
 pub mod calendar;
 pub mod character_types;
+#[cfg(any(test, feature = "ffi-stubs"))]
+mod ffi_stubs;
 #[cfg(feature = "idna")]
 pub mod idna;
