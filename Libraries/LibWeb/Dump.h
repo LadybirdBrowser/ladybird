7b8195fb912dde3919d0bf073481af143f39044d
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2026-09-08 20:29:03 +0200

    LibWeb: Drop the unused show_computed_properties layout dump option
    No caller ever passed true. The flag threaded through every recursive
    call and pulled the CSS property serializer into Dump.cpp for a listing
    nobody could reach. Remove the parameter and the listing code.

--- Libraries/LibWeb/Dump.h
+++ Libraries/LibWeb/Dump.h
@@ -17,8 +17,8 @@ namespace Web {
 WEB_API void dump_tree(HTML::LocalNavigable&);
 void dump_tree(StringBuilder&, DOM::Node const&);
 WEB_API void dump_tree(DOM::Node const&);
-WEB_API void dump_tree(StringBuilder&, Layout::Node const&, bool show_computed_properties = false, bool colorize = false);
-WEB_API void dump_tree(Layout::Node const&, bool show_computed_properties = false);
+WEB_API void dump_tree(StringBuilder&, Layout::Node const&, bool colorize = false);
+WEB_API void dump_tree(Layout::Node const&);
 void dump_sheet(StringBuilder&, CSS::StyleSheet const&, int indent_levels = 0);
 WEB_API void dump_sheet(CSS::StyleSheet const&);
 void dump_rule(StringBuilder&, CSS::CSSRule const&, int indent_levels = 0);
