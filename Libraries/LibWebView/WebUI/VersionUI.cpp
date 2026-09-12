23149527d7977f4a5c8a0a190efe1c29e8c26b21
Timothy Flynn <trflynn89@pm.me>
2026-09-12 17:06:57 -0400

    LibWebView: Show the commit hash in about:version
    We have the commit hash for crash reports now, so let's show it in the
    version info page.

--- Libraries/LibWebView/WebUI/VersionUI.cpp
+++ Libraries/LibWebView/WebUI/VersionUI.cpp
@@ -5,10 +5,12 @@
  */
 
 #include <AK/JsonObject.h>
+#include <AK/NeverDestroyed.h>
 #include <AK/String.h>
 #include <LibCore/System.h>
 #include <LibWeb/Loader/UserAgent.h>
 #include <LibWebView/Application.h>
+#include <LibWebView/BuildInformation.h>
 #include <LibWebView/WebUI/VersionUI.h>
 
 namespace WebView {
@@ -22,20 +24,22 @@ void VersionUI::register_interfaces()
 
 void VersionUI::load_version_info()
 {
-    static auto& browser_name = *new String(String::from_utf8_without_validation({ BROWSER_NAME, __builtin_strlen(BROWSER_NAME) }));
-    static auto& browser_version = *new String(String::from_utf8_without_validation({ BROWSER_VERSION, __builtin_strlen(BROWSER_VERSION) }));
-    static auto& arch = *new String(String::from_utf8_without_validation({ CPU_STRING, __builtin_strlen(CPU_STRING) }));
-    static auto& platform_name = *new String(String::from_utf8_without_validation({ OS_STRING, __builtin_strlen(OS_STRING) }));
-    static auto& command_line = *new String(MUST(String::join(' ', Application::the().command_line_arguments().strings)));
-    static auto& executable_path = *new String(MUST(String::from_byte_string(MUST(Core::System::current_executable_path()))));
+    static NeverDestroyed<String> browser_name = String::from_utf8_without_validation({ BROWSER_NAME, __builtin_strlen(BROWSER_NAME) });
+    static NeverDestroyed<String> browser_version = String::from_utf8_without_validation({ BROWSER_VERSION, __builtin_strlen(BROWSER_VERSION) });
+    static NeverDestroyed<String> browser_commit = String::from_utf8_without_validation(LADYBIRD_COMMIT.bytes());
+    static NeverDestroyed<String> arch = String::from_utf8_without_validation({ CPU_STRING, __builtin_strlen(CPU_STRING) });
+    static NeverDestroyed<String> platform_name = String::from_utf8_without_validation({ OS_STRING, __builtin_strlen(OS_STRING) });
+    static NeverDestroyed<String> command_line = MUST(String::join(' ', Application::the().command_line_arguments().strings));
+    static NeverDestroyed<String> executable_path = MUST(String::from_byte_string(MUST(Core::System::current_executable_path())));
 
     JsonObject version_info;
-    version_info.set("browserName"_string, browser_name);
-    version_info.set("browserVersion"_string, browser_version);
-    version_info.set("arch"_string, arch);
-    version_info.set("platformName"_string, platform_name);
-    version_info.set("commandLine"_string, command_line);
-    version_info.set("executablePath"_string, executable_path);
+    version_info.set("browserName"_string, *browser_name);
+    version_info.set("browserVersion"_string, *browser_version);
+    version_info.set("browserCommit"_string, *browser_commit);
+    version_info.set("arch"_string, *arch);
+    version_info.set("platformName"_string, *platform_name);
+    version_info.set("commandLine"_string, *command_line);
+    version_info.set("executablePath"_string, *executable_path);
 
     async_send_message("renderVersionInfo"sv, version_info);
 }
