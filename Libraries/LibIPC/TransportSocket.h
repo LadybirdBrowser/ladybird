99866430e57085fe4d06d210d09c969b4b188025
Zaggy1024 <Zaggy1024@gmail.com>
2026-09-15 04:33:35 -0500

    LibIPC: Drop the TransportSocket message attachment retaining queue
    The bug that makes this necessary is only present in the macOS kernel,
    but we're not even using this there anymore.
    
    In addition, the send queue and the attachment descriptor queue were
    not kept in the same order when messages were sent from multiple
    threads simultaneously. This meant that the acknowledgement of receipt
    could cause us to drop an attachment that was still yet to be sent.
    
    Since it's not needed on the platforms using this code, just remove it
    so that we don't have this race. A compiler error is emitted by its
    header if it is used on the platforms affected instead.
    
    This is the suspected cause of a video-object-fit failure, as the
    failures were always accompanied by logs of EBADF errors.

--- Libraries/LibIPC/TransportSocket.h
+++ Libraries/LibIPC/TransportSocket.h
@@ -10,6 +10,7 @@
 #include <AK/Atomic.h>
 #include <AK/ConditionVariable.h>
 #include <AK/Mutex.h>
+#include <AK/Platform.h>
 #include <AK/Queue.h>
 #include <AK/SinglyLinkedList.h>
 #include <AK/SinglyLinkedListSizePolicy.h>
@@ -21,12 +22,18 @@
 #include <LibIPC/TransportHandle.h>
 #include <LibThreading/Forward.h>
 
+// A descriptor is closed once written, as the kernel then holds its own reference to the file for the message in
+// flight. However, the macOS kernel may collect it while it is still in the remote message queue
+// (https://openradar.me/9477351), so we must use TransportMachPort there.
+#if defined(AK_OS_MACOS) || defined(AK_OS_IOS)
+#    error "TransportSocket must not be used on Darwin"
+#endif
+
 namespace IPC {
 
 struct SocketMessageHeader {
     enum class Type : u8 {
         Payload = 0,
-        FileDescriptorAcknowledgement = 1,
     };
     Type type { Type::Payload };
     u32 payload_size { 0 };
@@ -35,7 +42,7 @@ struct SocketMessageHeader {
 
 class SendQueue : public AtomicRefCounted<SendQueue> {
 public:
-    void enqueue_message(SocketMessageHeader, MessageDataType payload, Vector<int>&& fds);
+    void enqueue_message(SocketMessageHeader, MessageDataType payload, Vector<NonnullRefPtr<AutoCloseFileDescriptor>>&& fds);
     struct BytesAndFds {
         Vector<u8> bytes;
         Vector<int> fds;
@@ -50,7 +57,7 @@ private:
     struct QueuedMessage {
         SocketMessageHeader header;
         MessageDataType payload;
-        size_t unsent_fd_count { 0 };
+        Vector<NonnullRefPtr<AutoCloseFileDescriptor>> unsent_fds;
         size_t start_offset { 0 };
 
         size_t size() const { return sizeof(SocketMessageHeader) + payload.size(); }
@@ -58,7 +65,6 @@ private:
 
     SinglyLinkedList<QueuedMessage, AK::DefaultSizeCalculationPolicy> m_queued_messages;
     size_t m_queued_byte_count { 0 };
-    Vector<int> m_fds;
     Mutex m_mutex;
     ConditionVariable m_drained_cv { m_mutex };
     bool m_drain_waiters_released { false };
@@ -143,12 +149,6 @@ private:
 
     NonnullOwnPtr<Core::LocalSocket> m_socket;
     Atomic<bool> m_socket_is_open { true };
-
-    // After file descriptor is sent, it is moved to the wait queue until an acknowledgement is received from the peer.
-    // This is necessary to handle a specific behavior of the macOS kernel, which may prematurely garbage-collect the file
-    // descriptor contained in the message before the peer receives it. https://openradar.me/9477351
-    Queue<NonnullRefPtr<AutoCloseFileDescriptor>> m_fds_retained_until_received_by_peer;
-    Mutex m_fds_retained_until_received_by_peer_mutex;
 
     RefPtr<Threading::Thread> m_io_thread;
     RefPtr<SendQueue> m_send_queue;
